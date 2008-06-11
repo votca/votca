dd8ab86c842697034776bd8bce17d81b61270c4d
Victor Ruehle <ruehle@mpip-mainz.mpg.de>
2008-06-11 08:42:10 +0000

    added template for new programs

---
+++ src/template.cc
@@ -0,0 +1,89 @@
+// 
+// File:   template.cc
+// Author: ruehle
+//
+// Created on June 8, 2008, 10:41 PM
+//
+
+#include <math.h>
+#include <boost/tokenizer.hpp>
+#include <iostream>
+#include <fstream>
+#include <boost/program_options.hpp>
+#include <cgengine.h>
+#include <libversion.h>
+
+using namespace std;
+
+class CGAnalyzer
+    : public CGObserver
+{
+public:
+    void BeginCG(Topology *top, Topology *top_atom) {
+    };
+    void EndCG() {
+    };
+    
+    void EvalConfiguration(Configuration *conf, Configuration *conf_atom = 0) {
+    }
+    
+protected:
+};
+
+
+int main(int argc, char** argv)
+{    
+    // we have one observer
+    CGAnalyzer no;        
+    // The CGEngine does the work
+    CGEngine cg_engine;
+    
+    // add our observer that it gets called to analyze frames
+    cg_engine.AddObserver((CGObserver*)&no);
+
+
+    // initialize the readers/writers,
+    // this will be combined in an initialize function later
+    TrajectoryWriter::RegisterPlugins();
+    TrajectoryReader::RegisterPlugins();
+    TopologyReader::RegisterPlugins();
+
+    
+    // lets read in some program options
+    namespace po = boost::program_options;
+        
+    
+    // Declare the supported options.
+    po::options_description desc("Allowed options");    
+    
+    // let cg_engine add some program options
+    cg_engine.AddProgramOptions(desc);
+    
+    // now read in the command line
+    po::variables_map vm;
+    po::store(po::parse_command_line(argc, argv, desc), vm);
+    po::notify(vm);
+
+    // does the user want help?
+    if (vm.count("help")) {
+        cout << "csg_nemat, lib version " << LIB_VERSION_STR << "\n\n";                
+        cout << desc << endl;
+        return 0;
+    }
+    // or asks for the program version?
+    if (vm.count("version")) {
+        cout << "csg_nemat, lib version " << LIB_VERSION_STR  << "\n";                        
+        return 0;
+    }
+    
+    // try to run the cg process, go through the frames, etc...
+    try {
+        cg_engine.Run(desc, vm);
+    }
+    // did an error occour?
+    catch(string error) {
+        cerr << "An error occoured!" << endl << error << endl;
+    }
+    return 0;
+}
+
