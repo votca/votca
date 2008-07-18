b2ad88a2918f78c07e416962cb63a6d4d371158f
Victor Ruehle <ruehle@mpip-mainz.mpg.de>
2008-07-18 12:15:08 +0000

    projects, ...

---
+++ include/beadlist.h
@@ -0,0 +1,27 @@
+/* 
+ * File:   beadlist.h
+ * Author: ruehle
+ *
+ * Created on July 17, 2008, 5:14 PM
+ */
+
+#ifndef _BEADLIST_H
+#define	_BEADLIST_H
+
+#include <string>
+
+using namespace std;
+
+class BeadList
+{
+public:
+    BeadList() {}
+    ~BeadList() {}
+    
+    int CreateList(string select);
+    
+private:
+};
+
+#endif	/* _BEADLIST_H */
+
