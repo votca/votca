9c45737a5cc4e209b2e41f8a1a71e848ed4af219
Alexander Lukyanov <lukyanov@mpip-mainz.mpg.de>
2008-07-28 14:47:19 +0000

    adjusted output path in project

--- netbeans/libtools/nbproject/Makefile-Debug.mk
+++ netbeans/libtools/nbproject/Makefile-Debug.mk
@@ -15,7 +15,7 @@ RANLIB=ranlib
 CC=gcc
 CCC=g++
 CXX=g++
-FC=
+FC=g95
 
 # Include project Makefile
 include Makefile_nb
@@ -25,16 +25,16 @@ OBJECTDIR=build/Debug/GNU-Linux-x86
 
 # Object Files
 OBJECTFILES= \
-	${OBJECTDIR}/_ext/home/ruehle/src/tools/src/libtools/tokenizer.o \
-	${OBJECTDIR}/_ext/home/ruehle/src/tools/src/libtools/parcer.o \
-	${OBJECTDIR}/_ext/home/ruehle/src/tools/src/libtools/random.o \
-	${OBJECTDIR}/_ext/home/ruehle/src/tools/src/libtools/matrix.o \
-	${OBJECTDIR}/_ext/home/ruehle/src/tools/src/libtools/crosscorrelate.o \
-	${OBJECTDIR}/_ext/home/ruehle/src/tools/src/libtools/correlate.o \
-	${OBJECTDIR}/_ext/home/ruehle/src/tools/src/libtools/rangeparser.o \
-	${OBJECTDIR}/_ext/home/ruehle/src/tools/src/libtools/histogram.o \
-	${OBJECTDIR}/_ext/home/ruehle/src/tools/src/libtools/table.o \
-	${OBJECTDIR}/_ext/home/ruehle/src/tools/src/libtools/datacollection.o
+	${OBJECTDIR}/_ext/people/thnfs/homes/lukyanov/src/tools/src/libtools/tokenizer.o \
+	${OBJECTDIR}/_ext/people/thnfs/homes/lukyanov/src/tools/src/libtools/parcer.o \
+	${OBJECTDIR}/_ext/people/thnfs/homes/lukyanov/src/tools/src/libtools/random.o \
+	${OBJECTDIR}/_ext/people/thnfs/homes/lukyanov/src/tools/src/libtools/matrix.o \
+	${OBJECTDIR}/_ext/people/thnfs/homes/lukyanov/src/tools/src/libtools/crosscorrelate.o \
+	${OBJECTDIR}/_ext/people/thnfs/homes/lukyanov/src/tools/src/libtools/correlate.o \
+	${OBJECTDIR}/_ext/people/thnfs/homes/lukyanov/src/tools/src/libtools/rangeparser.o \
+	${OBJECTDIR}/_ext/people/thnfs/homes/lukyanov/src/tools/src/libtools/histogram.o \
+	${OBJECTDIR}/_ext/people/thnfs/homes/lukyanov/src/tools/src/libtools/table.o \
+	${OBJECTDIR}/_ext/people/thnfs/homes/lukyanov/src/tools/src/libtools/datacollection.o
 
 # C Compiler Flags
 CFLAGS=
@@ -50,53 +50,53 @@ FFLAGS=
 LDLIBSOPTIONS=
 
 # Build Targets
-.build-conf: ${BUILD_SUBPROJECTS} ../src/libtools/libtools.a
+.build-conf: ${BUILD_SUBPROJECTS} ../../src/libtools/libtools.a
 
-../src/libtools/libtools.a: ${OBJECTFILES}
-	${MKDIR} -p ../src/libtools
-	${RM} ../src/libtools/libtools.a
-	${AR} rv ../src/libtools/libtools.a ${OBJECTFILES} 
-	$(RANLIB) ../src/libtools/libtools.a
+../../src/libtools/libtools.a: ${OBJECTFILES}
+	${MKDIR} -p ../../src/libtools
+	${RM} ../../src/libtools/libtools.a
+	${AR} rv ../../src/libtools/libtools.a ${OBJECTFILES} 
+	$(RANLIB) ../../src/libtools/libtools.a
 
-${OBJECTDIR}/_ext/home/ruehle/src/tools/src/libtools/tokenizer.o: ../../src/libtools/tokenizer.cc 
-	${MKDIR} -p ${OBJECTDIR}/_ext/home/ruehle/src/tools/src/libtools
-	$(COMPILE.cc) -g -I../../include -o ${OBJECTDIR}/_ext/home/ruehle/src/tools/src/libtools/tokenizer.o ../../src/libtools/tokenizer.cc
+${OBJECTDIR}/_ext/people/thnfs/homes/lukyanov/src/tools/src/libtools/tokenizer.o: ../../src/libtools/tokenizer.cc 
+	${MKDIR} -p ${OBJECTDIR}/_ext/people/thnfs/homes/lukyanov/src/tools/src/libtools
+	$(COMPILE.cc) -g -I../../include -o ${OBJECTDIR}/_ext/people/thnfs/homes/lukyanov/src/tools/src/libtools/tokenizer.o ../../src/libtools/tokenizer.cc
 
-${OBJECTDIR}/_ext/home/ruehle/src/tools/src/libtools/parcer.o: ../../src/libtools/parcer.cc 
-	${MKDIR} -p ${OBJECTDIR}/_ext/home/ruehle/src/tools/src/libtools
-	$(COMPILE.cc) -g -I../../include -o ${OBJECTDIR}/_ext/home/ruehle/src/tools/src/libtools/parcer.o ../../src/libtools/parcer.cc
+${OBJECTDIR}/_ext/people/thnfs/homes/lukyanov/src/tools/src/libtools/parcer.o: ../../src/libtools/parcer.cc 
+	${MKDIR} -p ${OBJECTDIR}/_ext/people/thnfs/homes/lukyanov/src/tools/src/libtools
+	$(COMPILE.cc) -g -I../../include -o ${OBJECTDIR}/_ext/people/thnfs/homes/lukyanov/src/tools/src/libtools/parcer.o ../../src/libtools/parcer.cc
 
-${OBJECTDIR}/_ext/home/ruehle/src/tools/src/libtools/random.o: ../../src/libtools/random.cc 
-	${MKDIR} -p ${OBJECTDIR}/_ext/home/ruehle/src/tools/src/libtools
-	$(COMPILE.cc) -g -I../../include -o ${OBJECTDIR}/_ext/home/ruehle/src/tools/src/libtools/random.o ../../src/libtools/random.cc
+${OBJECTDIR}/_ext/people/thnfs/homes/lukyanov/src/tools/src/libtools/random.o: ../../src/libtools/random.cc 
+	${MKDIR} -p ${OBJECTDIR}/_ext/people/thnfs/homes/lukyanov/src/tools/src/libtools
+	$(COMPILE.cc) -g -I../../include -o ${OBJECTDIR}/_ext/people/thnfs/homes/lukyanov/src/tools/src/libtools/random.o ../../src/libtools/random.cc
 
-${OBJECTDIR}/_ext/home/ruehle/src/tools/src/libtools/matrix.o: ../../src/libtools/matrix.cc 
-	${MKDIR} -p ${OBJECTDIR}/_ext/home/ruehle/src/tools/src/libtools
-	$(COMPILE.cc) -g -I../../include -o ${OBJECTDIR}/_ext/home/ruehle/src/tools/src/libtools/matrix.o ../../src/libtools/matrix.cc
+${OBJECTDIR}/_ext/people/thnfs/homes/lukyanov/src/tools/src/libtools/matrix.o: ../../src/libtools/matrix.cc 
+	${MKDIR} -p ${OBJECTDIR}/_ext/people/thnfs/homes/lukyanov/src/tools/src/libtools
+	$(COMPILE.cc) -g -I../../include -o ${OBJECTDIR}/_ext/people/thnfs/homes/lukyanov/src/tools/src/libtools/matrix.o ../../src/libtools/matrix.cc
 
-${OBJECTDIR}/_ext/home/ruehle/src/tools/src/libtools/crosscorrelate.o: ../../src/libtools/crosscorrelate.cc 
-	${MKDIR} -p ${OBJECTDIR}/_ext/home/ruehle/src/tools/src/libtools
-	$(COMPILE.cc) -g -I../../include -o ${OBJECTDIR}/_ext/home/ruehle/src/tools/src/libtools/crosscorrelate.o ../../src/libtools/crosscorrelate.cc
+${OBJECTDIR}/_ext/people/thnfs/homes/lukyanov/src/tools/src/libtools/crosscorrelate.o: ../../src/libtools/crosscorrelate.cc 
+	${MKDIR} -p ${OBJECTDIR}/_ext/people/thnfs/homes/lukyanov/src/tools/src/libtools
+	$(COMPILE.cc) -g -I../../include -o ${OBJECTDIR}/_ext/people/thnfs/homes/lukyanov/src/tools/src/libtools/crosscorrelate.o ../../src/libtools/crosscorrelate.cc
 
-${OBJECTDIR}/_ext/home/ruehle/src/tools/src/libtools/correlate.o: ../../src/libtools/correlate.cc 
-	${MKDIR} -p ${OBJECTDIR}/_ext/home/ruehle/src/tools/src/libtools
-	$(COMPILE.cc) -g -I../../include -o ${OBJECTDIR}/_ext/home/ruehle/src/tools/src/libtools/correlate.o ../../src/libtools/correlate.cc
+${OBJECTDIR}/_ext/people/thnfs/homes/lukyanov/src/tools/src/libtools/correlate.o: ../../src/libtools/correlate.cc 
+	${MKDIR} -p ${OBJECTDIR}/_ext/people/thnfs/homes/lukyanov/src/tools/src/libtools
+	$(COMPILE.cc) -g -I../../include -o ${OBJECTDIR}/_ext/people/thnfs/homes/lukyanov/src/tools/src/libtools/correlate.o ../../src/libtools/correlate.cc
 
-${OBJECTDIR}/_ext/home/ruehle/src/tools/src/libtools/rangeparser.o: ../../src/libtools/rangeparser.cc 
-	${MKDIR} -p ${OBJECTDIR}/_ext/home/ruehle/src/tools/src/libtools
-	$(COMPILE.cc) -g -I../../include -o ${OBJECTDIR}/_ext/home/ruehle/src/tools/src/libtools/rangeparser.o ../../src/libtools/rangeparser.cc
+${OBJECTDIR}/_ext/people/thnfs/homes/lukyanov/src/tools/src/libtools/rangeparser.o: ../../src/libtools/rangeparser.cc 
+	${MKDIR} -p ${OBJECTDIR}/_ext/people/thnfs/homes/lukyanov/src/tools/src/libtools
+	$(COMPILE.cc) -g -I../../include -o ${OBJECTDIR}/_ext/people/thnfs/homes/lukyanov/src/tools/src/libtools/rangeparser.o ../../src/libtools/rangeparser.cc
 
-${OBJECTDIR}/_ext/home/ruehle/src/tools/src/libtools/histogram.o: ../../src/libtools/histogram.cc 
-	${MKDIR} -p ${OBJECTDIR}/_ext/home/ruehle/src/tools/src/libtools
-	$(COMPILE.cc) -g -I../../include -o ${OBJECTDIR}/_ext/home/ruehle/src/tools/src/libtools/histogram.o ../../src/libtools/histogram.cc
+${OBJECTDIR}/_ext/people/thnfs/homes/lukyanov/src/tools/src/libtools/histogram.o: ../../src/libtools/histogram.cc 
+	${MKDIR} -p ${OBJECTDIR}/_ext/people/thnfs/homes/lukyanov/src/tools/src/libtools
+	$(COMPILE.cc) -g -I../../include -o ${OBJECTDIR}/_ext/people/thnfs/homes/lukyanov/src/tools/src/libtools/histogram.o ../../src/libtools/histogram.cc
 
-${OBJECTDIR}/_ext/home/ruehle/src/tools/src/libtools/table.o: ../../src/libtools/table.cc 
-	${MKDIR} -p ${OBJECTDIR}/_ext/home/ruehle/src/tools/src/libtools
-	$(COMPILE.cc) -g -I../../include -o ${OBJECTDIR}/_ext/home/ruehle/src/tools/src/libtools/table.o ../../src/libtools/table.cc
+${OBJECTDIR}/_ext/people/thnfs/homes/lukyanov/src/tools/src/libtools/table.o: ../../src/libtools/table.cc 
+	${MKDIR} -p ${OBJECTDIR}/_ext/people/thnfs/homes/lukyanov/src/tools/src/libtools
+	$(COMPILE.cc) -g -I../../include -o ${OBJECTDIR}/_ext/people/thnfs/homes/lukyanov/src/tools/src/libtools/table.o ../../src/libtools/table.cc
 
-${OBJECTDIR}/_ext/home/ruehle/src/tools/src/libtools/datacollection.o: ../../src/libtools/datacollection.cc 
-	${MKDIR} -p ${OBJECTDIR}/_ext/home/ruehle/src/tools/src/libtools
-	$(COMPILE.cc) -g -I../../include -o ${OBJECTDIR}/_ext/home/ruehle/src/tools/src/libtools/datacollection.o ../../src/libtools/datacollection.cc
+${OBJECTDIR}/_ext/people/thnfs/homes/lukyanov/src/tools/src/libtools/datacollection.o: ../../src/libtools/datacollection.cc 
+	${MKDIR} -p ${OBJECTDIR}/_ext/people/thnfs/homes/lukyanov/src/tools/src/libtools
+	$(COMPILE.cc) -g -I../../include -o ${OBJECTDIR}/_ext/people/thnfs/homes/lukyanov/src/tools/src/libtools/datacollection.o ../../src/libtools/datacollection.cc
 
 # Subprojects
 .build-subprojects:
@@ -104,7 +104,7 @@ ${OBJECTDIR}/_ext/home/ruehle/src/tools/src/libtools/datacollection.o: ../../src
 # Clean Targets
 .clean-conf:
 	${RM} -r build/Debug
-	${RM} ../src/libtools/libtools.a
+	${RM} ../../src/libtools/libtools.a
 
 # Subprojects
 .clean-subprojects:
