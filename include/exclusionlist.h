d5c34bc3a02a7ba190b22dbf1065c6a13dc60ae7
Victor Ruehle <ruehle@mpip-mainz.mpg.de>
2008-11-24 15:08:46 +0000

    added a bit of functionality

--- include/exclusionlist.h
+++ include/exclusionlist.h
@@ -10,8 +10,11 @@
 
 #include <iostream>
 #include <list>
+#include <map>
 
 using namespace std;
+
+/// \todo fill _excl_by_bead
 
 class ExclusionList
 {
@@ -23,15 +26,32 @@ public:
     void ExcludeAll(int N);    
     void Remove(list<int> l);
 
-private:
     struct exclusion_t {
         int _atom;
         list<int> _exclude;
-    };
+    };    
+    
+    //void CreateExclusions(Topology top);
+    exclusion_t *GetExclusions(int bead);
+    
+    typedef list< exclusion_t * >::iterator iterator;
+    
+    iterator begin() { return _exclusions.begin(); }
+    iterator end() { return _exclusions.end(); }
+
+private:
     list< exclusion_t * > _exclusions;
+    map<int, exclusion_t *> _excl_by_bead;
     
     friend std::ostream &operator<<(std::ostream &out, ExclusionList& exl);
 };
+
+inline ExclusionList::exclusion_t * ExclusionList::GetExclusions(int bead)
+{
+   map<int, exclusion_t *>::iterator iter  = _excl_by_bead.find(bead);
+   if(iter == _excl_by_bead.end()) return NULL;
+   return (*iter).second;
+}
 
 std::ostream &operator<<(std::ostream &out,ExclusionList& ex);
 
