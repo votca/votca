06644bc55cf932794b421acb5bf46aab55cdd35a
Alexander Lukyanov <lukyanov@mpip-mainz.mpg.de>
2008-12-10 09:19:50 +0000

    exclude atoms, which have bonded interactions

--- src/libcsg/exclusionlist.cc
+++ src/libcsg/exclusionlist.cc
@@ -52,6 +52,65 @@ void ExclusionList::Remove(list<int> l)
     _exclusions.remove(NULL);
 }
 
+void ExclusionList::ExcludeList( list<int> l ) {
+    l.sort();
+    list<int>::iterator i, j, k;
+    list<exclusion_t*>::iterator ex;
+    
+    for ( i = l.begin(); i != l.end(); ++i ) {
+        for (ex = _exclusions.begin(); ex != _exclusions.end(); ++ex)
+            if ( (*ex)->_atom == (*i) ) break;
+        if (ex==_exclusions.end()) { // so far there are no exclusions for i
+            exclusion_t *e = new exclusion_t;
+            e->_atom = (*i);
+            
+            j = i;
+            for (++j; j != l.end(); ++j)
+                e->_exclude.push_back( (*j) );
+            _exclusions.push_back(e);
+              
+        }
+        else {
+            // there are some exclusions for i already. Add new exclusions if they are 
+            // not there yet!
+            j = i;
+            for (++j; j != l.end(); ++j) {
+                
+                for ( k = (*ex)->_exclude.begin(); k != (*ex)->_exclude.end(); ++k ) 
+                    if ( (*j) == (*k) ) break;
+                if ( k == (*ex)->_exclude.end() ) (*ex)->_exclude.push_back( (*j) );
+            }
+            
+        }
+    }
+    
+}
+    
+void ExclusionList::CreateExclusions(Topology *top) {
+    
+    InteractionContainer &ic = top->getBondedInteractions();
+    InteractionContainer::iterator ia;
+    list<int> l;
+    
+    for (ia = ic.begin(); ia != ic.end(); ++ia) {
+        int beads_in_int = (*ia)->BeadCount();
+        l.clear();
+        
+        for (int ibead = 0; ibead < beads_in_int; ibead ++) {
+            int ii = (*ia)->getBeadId(ibead);
+            l.push_back(ii);
+        }
+        ExcludeList(l);  
+    }
+    // Create map
+    list< exclusion_t * >::iterator it;
+    for ( it = _exclusions.begin(); it != _exclusions.end(); ++it ) {
+        int iatom = (*it)->_atom;
+        _excl_by_bead[ iatom ] = (*it);
+    }
+    
+}
+
 std::ostream &operator<<(std::ostream &out, ExclusionList& exl)
 {
     list<ExclusionList::exclusion_t*>::iterator ex;
