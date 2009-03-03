074da98725d6b1574fdbb2fcc1e1fe5a66717950
Alexander Lukyanov <lukyanov@mpip-mainz.mpg.de>
2009-03-03 10:53:08 +0000

    CubicSpline::Fit smoothing conditions are treated exactly

--- src/libtools/cubicspline.cc
+++ src/libtools/cubicspline.cc
@@ -104,16 +104,121 @@ void CubicSpline::Fit(ub::vector<double> &x, ub::vector<double> &y)
     // and b[i]=0 for i>=N (for smoothing condition)
     // A[i,j] contains the data fitting + the spline smoothing conditions
     
-    ub::matrix<double> A(N + ngrid, 2*ngrid);
-    A = ub::zero_matrix<double>(N + ngrid, 2*ngrid);
-    ub::vector<double> b(N + ngrid);
-    b  = ub::zero_vector<double>(N+ngrid);
+    ub::matrix<double> A(N, 2*ngrid);
+    A = ub::zero_matrix<double>(N, 2*ngrid);
+    ub::vector<double> b(N);
+    b  = ub::zero_vector<double>(N);
+    
+    ub::matrix<double> B_constr(ngrid, 2*ngrid);  // Matrix with smoothing conditions
+    B_constr = ub::zero_matrix<double>(ngrid, 2*ngrid);
+    
+    ub::matrix<double> Q(2*ngrid, 2*ngrid);       // Q matrix: QR decomposition of trans(B)
+    Q = ub::zero_matrix<double>(2*ngrid, 2*ngrid);
+    
+    ub::matrix<double> A2(N, ngrid);             // Matrix A2 (see manual)
+    A2 = ub::zero_matrix<double>(N, ngrid);
+    
+    // Construct smoothing matrix
+    AddBCToFitMatrix(B_constr, 0);
+    
+    // Transpose B_constr:
+    B_constr = trans(B_constr);
+    
+//================ START QR =============================//
+    ub::matrix<double> Q_k(2*ngrid, 2*ngrid);
+    Q_k = ub::zero_matrix<double>(2*ngrid, 2*ngrid);
+       
+    ub::identity_matrix<double> I (2*ngrid); 
 
-    // construct the matrix to fit the points
+    ub::vector<double> v(2*ngrid); 
+    v = ub::zero_vector<double>(2*ngrid);
+    
+    double* pointer_m = & B_constr(0,0);    
+    
+    gsl_matrix_view B_t 
+      = gsl_matrix_view_array (pointer_m, 2*ngrid, ngrid);
+     
+    gsl_vector *tau_qr = gsl_vector_alloc (ngrid); 
+    
+    gsl_linalg_QR_decomp (&B_t.matrix, tau_qr);   
+
+    Q = I;
+              
+    for (int k = ngrid; k > 0 ; k--) {
+           
+        for (int icout = 0; icout < k - 1; icout++) {
+             v(icout) = 0;
+        }
+        v(k - 1) = 1.0;
+
+        for (int icout = k; icout < 2*ngrid; icout++) {
+             v(icout) = gsl_matrix_get(&B_t.matrix, icout, k - 1 );
+        }
+           
+        Q_k = I - gsl_vector_get(tau_qr, k - 1 ) * outer_prod ( v, v );
+        Q = prec_prod(Q, Q_k);
+           
+    }
+    
+    Q = trans(Q);  
+    gsl_vector_free (tau_qr);
+    
+//=============== END QR ====================================//
+    
+    // construct the matrix to fit the points and the vector b
     AddToFitMatrix(A, x, 0);
-   
-    ub::vector_range< ub::vector<double> > b1(b, ub::range(0,N));
-    b1 = -y;
+    b = -y; // why is it -y?
+    
+    // Calculate A * Q and store the result in A
+    A = prec_prod(A, Q); 
+    
+    // A = [A1 A2], so A2 is just a block of A
+    for (int iraw = 0; iraw < N; iraw++) {
+        for (int icol = ngrid; icol < 2*ngrid; icol++) {
+            A2(iraw, icol - ngrid) = A(iraw, icol);
+        }
+    }    
+    
+//============== START SOLVE QR-DECOMPOSITION================//
+    pointer_m = & A2(0,0);
+    double* pointer_b = & b(0);
+    
+    gsl_matrix_view m
+         = gsl_matrix_view_array (pointer_m, N, ngrid);
+    
+    gsl_vector_view gsl_b
+         = gsl_vector_view_array (pointer_b, N);
+    
+    gsl_vector *z = gsl_vector_alloc (ngrid);
+  gsl_vector *tau_solve = gsl_vector_alloc (ngrid);  // already done!         
+    gsl_vector *residual = gsl_vector_alloc (N);    
+    
+    gsl_linalg_QR_decomp (&m.matrix, tau_solve);
+    gsl_linalg_QR_lssolve (&m.matrix, tau_solve, &gsl_b.vector, z, residual);
+    
+    // Next two cycles assemble vector from y (which is zero-vector) and z 
+    // (which we just got by gsl_linalg_QR_lssolve)
+    
+    for (int i = 0; i < ngrid; i++ ) {
+           _f[i] = 0.0;
+    }  
+    
+    for (int i = ngrid; i < 2 * ngrid; i++ ) {
+           _f[i] = gsl_vector_get(z, i - ngrid);
+    }      
+    
+    // To get the final answer this vector should be multiplied by matrix Q
+    _f = prec_prod( Q, _f );    
+    
+    gsl_vector_free (z);
+    gsl_vector_free (tau_solve);
+    gsl_vector_free (residual);
+      
+//============== END SOLVE QR ===============================//
+    
+    for (int i=0; i < N; i++){
+    cout << x(i) << '\t' << Calculate(x(i)) << endl;    
+    }
     
     // construct the smoothing condition
     // first derivatives have to be continuous:
@@ -126,7 +231,7 @@ void CubicSpline::Fit(ub::vector<double> &x, ub::vector<double> &y)
     // QR decomposition if the system doesn't have the full rank.
     // MATLAB can handle this problem easily.
         
-    double* pointer_m = &A(0,0);
+/*    double* pointer_m = &A(0,0);
     double* pointer_b = & b(0);        
 
     gsl_matrix_view m
@@ -150,7 +255,7 @@ void CubicSpline::Fit(ub::vector<double> &x, ub::vector<double> &y)
     gsl_vector_free (gx);
     gsl_vector_free (tau);
     gsl_vector_free (residual);
-    
+  */  
     return;
     
   /*  using namespace boost::numeric::ublas;
