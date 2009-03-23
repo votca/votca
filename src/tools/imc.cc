bbf1c35e42632c923cd88ab8f37883c99f2bc5b7
Victor Ruehle <ruehle@mpip-mainz.mpg.de>
2009-03-23 15:59:26 +0000

    minor changes

--- src/tools/imc.cc
+++ src/tools/imc.cc
@@ -23,13 +23,16 @@ void Imc::BeginCG(Topology *top, Topology *top_atom) {
         AddInteraction(*iter);
 };
 
-interaction *Imc::AddInteraction(Property *p)
+Imc::interaction_t *Imc::AddInteraction(Property *p)
 {
     string name = p->get("name").value();
+    string group = p->get("group").value();
+    
     interaction_t *i = new interaction_t;
 
     _interactions[name] = i;
     // \todo check weather exists       
+    getGroup(group)->_interactions.push_back(i);
     
     i->_step = p->get("step").as<double>();
     i->_min = p->get("min").as<double>();
@@ -40,7 +43,7 @@ interaction *Imc::AddInteraction(Property *p)
     i->_current.Initialize(i->_min, i->_max, n);
     i->_average.Initialize(i->_min, i->_max, n);
     
-    return *i;
+    return i;
 }
 
 void Imc::EndCG()
@@ -95,4 +98,76 @@ void Imc::DoNonbonded(Topology *top)
         }
         i._average.data().y() += i._current.data().y();
     }    
+}
+
+Imc::group_t *Imc::getGroup(const string &name)
+{
+    map<string, group_t *>::iterator iter;
+    iter = _groups.find(name);
+    if(iter == _groups.end())
+        return _groups[name] = new group_t;
+    return (*iter).second;
+}
+
+void Imc::InitializeGroups()
+{
+    map<string, group_t *>::iterator group_iter;
+
+    // iterator over all groups
+    for (group_iter = _groups.begin(); group_iter != _groups.end(); ++group_iter) {
+        group_t *grp = (*group_iter).second;
+        grp->_correlations.clear();
+        int n = grp->_interactions.size();
+        // resize vector for correlation matrices
+        grp->_correlations.resize(0.5 * n * (n + 1));
+        
+        vector< ub::matrix<double> >::iterator corr;
+        corr = grp->_correlations.begin();
+        
+        // iterate over all possible compinations for correlations
+        for (list<interaction_t*>::iterator i1 = grp->_interactions.begin();
+                i1 != grp->_interactions.end(); ++i1) {
+            for (list<interaction_t*>::iterator i2 = i1;
+                    i2 != grp->_interactions.end(); ++i2, ++corr) {
+                // allocate memory for correlation matrix
+                corr->resize((*i1)->_current.getNBins(), (*i2)->_current.getNBins());
+            }
+        }
+    }
+}
+
+void Imc::UpdateCorrelations()
+{
+        map<string, group_t *>::iterator group_iter;
+
+    // iterator over all groups
+    for (group_iter = _groups.begin(); group_iter != _groups.end(); ++group_iter) {
+        group_t *grp = (*group_iter).second;
+        grp->_correlations.clear();
+        int n = grp->_interactions.size();
+        
+        vector< ub::matrix<double> >::iterator corr;
+        corr = grp->_correlations.begin();
+        
+        // iterate over all possible compinations for correlations
+        for (list<interaction_t*>::iterator i1 = grp->_interactions.begin();
+                i1 != grp->_interactions.end(); ++i1) {
+            for (list<interaction_t*>::iterator i2 = i1;
+                    i2 != grp->_interactions.end(); ++i2, ++corr) {
+                Correlate(*corr, (*i1)->_current.data().y(),
+                    (*i1)->_current.data().y());
+            }
+        }
+    }
+}
+
+void Imc::Correlate(ub::matrix<double> &corr, ub::vector<double> &v1,
+        ub::vector<double> &v2)
+{
+    ub::matrix<double>::size_type x;
+    ub::matrix<double>::size_type y;
+    
+    for(x=0; x<v1.size(); ++x)
+        for(y=0; y<v2.size(); ++y)
+            corr(x)(y)=v1[x]*v2[y];
 }
--- src/tools/imc.h
+++ src/tools/imc.h
@@ -41,16 +41,27 @@ protected:
         double _min, _max, _step;
     };
     
-    struct groups_t {
+    struct group_t {
         list<interaction_t*> _interactions;
-        ub::matrix<double> _correlations;
+        vector< ub::matrix<double> > _correlations;
     };
     
     map<string, interaction_t *> _interactions;
+    map<string, group_t *> _groups;
+            
     int _nframes;
     
+    interaction_t *AddInteraction(Property *p);
+    
     void DoNonbonded(Topology *top);
-};
+    group_t *getGroup(const string &name);
+    
+    void InitializeGroups();
+    void UpdateCorrelations();
+
+    void Correlate(ub::matrix<double> &corr, ub::vector<double> &v1,
+        ub::vector<double> &v2);
+ };
 
 
 #endif	/* _IMC_H */
