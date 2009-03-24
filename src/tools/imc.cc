a1dede0c59a72d6d1b6730f0878f0dea19726940
Victor Ruehle <ruehle@mpip-mainz.mpg.de>
2009-03-24 14:41:10 +0000

    worked on imc

--- src/tools/imc.cc
+++ src/tools/imc.cc
@@ -16,7 +16,6 @@ void Imc::BeginCG(Topology *top, Topology *top_atom) {
     cout << "# of non-bonded interactions: " << _nonbonded.size() << endl;
     
    _nframes = 0;
-   
    // initialize non-bonded structures
    for (list<Property*>::iterator iter = _nonbonded.begin();
             iter != _nonbonded.end(); ++iter)
@@ -26,13 +25,12 @@ void Imc::BeginCG(Topology *top, Topology *top_atom) {
 Imc::interaction_t *Imc::AddInteraction(Property *p)
 {
     string name = p->get("name").value();
-    string group = p->get("group").value();
+    string group = p->get("imc.group").value();
     
     interaction_t *i = new interaction_t;
-
     _interactions[name] = i;
-    // \todo check weather exists       
     getGroup(group)->_interactions.push_back(i);
+
     
     i->_step = p->get("step").as<double>();
     i->_min = p->get("min").as<double>();
@@ -47,11 +45,13 @@ Imc::interaction_t *Imc::AddInteraction(Property *p)
 }
 
 void Imc::EndCG()
-{        
+{
+    CalcMatrix();
+    
     map<string, interaction_t *>::iterator iter;
     for(iter=_interactions.begin(); iter!=_interactions.end(); ++iter) {
-        cout << (iter->first) << endl;
-        cout << iter->second->_average;        
+        iter->second->_average.data().Save((iter->first) + ".imc");
+        cout << "written " << (iter->first) + ".imc";
         delete iter->second;
     }
     _interactions.clear();
@@ -103,9 +103,13 @@ void Imc::DoNonbonded(Topology *top)
 Imc::group_t *Imc::getGroup(const string &name)
 {
     map<string, group_t *>::iterator iter;
+    cout << "looking for " + name + "\n";
     iter = _groups.find(name);
-    if(iter == _groups.end())
+    if(iter == _groups.end()) {
+        cout << name + "not found\n";
         return _groups[name] = new group_t;
+    }
+    cout << name + " found\n";
     return (*iter).second;
 }
 
@@ -138,7 +142,7 @@ void Imc::InitializeGroups()
 
 void Imc::UpdateCorrelations()
 {
-        map<string, group_t *>::iterator group_iter;
+    map<string, group_t *>::iterator group_iter;
 
     // iterator over all groups
     for (group_iter = _groups.begin(); group_iter != _groups.end(); ++group_iter) {
@@ -154,20 +158,41 @@ void Imc::UpdateCorrelations()
                 i1 != grp->_interactions.end(); ++i1) {
             for (list<interaction_t*>::iterator i2 = i1;
                     i2 != grp->_interactions.end(); ++i2, ++corr) {
-                Correlate(*corr, (*i1)->_current.data().y(),
-                    (*i1)->_current.data().y());
+                ub::vector<double> &a = (*i1)->_current.data().y();
+                ub::vector<double> &b = (*i1)->_current.data().y();
+                ub::matrix<double> &M = *corr;
+                M+=ub::outer_prod(a, b);
             }
         }
     }
 }
 
-void Imc::Correlate(ub::matrix<double> &corr, ub::vector<double> &v1,
-        ub::vector<double> &v2)
+void Imc::CalcMatrix()
 {
-    ub::matrix<double>::size_type x;
-    ub::matrix<double>::size_type y;
-    
-    for(x=0; x<v1.size(); ++x)
-        for(y=0; y<v2.size(); ++y)
-            corr(x)(y)=v1[x]*v2[y];
+    map<string, group_t *>::iterator group_iter;
+
+    // iterator over all groups
+    for (group_iter = _groups.begin(); group_iter != _groups.end(); ++group_iter) {
+        group_t *grp = (*group_iter).second;
+        grp->_correlations.clear();
+        int n = grp->_interactions.size();
+        
+        vector< ub::matrix<double> >::iterator corr;
+        corr = grp->_correlations.begin();
+        
+        // iterate over all possible compinations for correlations
+        for (list<interaction_t*>::iterator i1 = grp->_interactions.begin();
+                i1 != grp->_interactions.end(); ++i1) {
+            for (list<interaction_t*>::iterator i2 = i1;
+                    i2 != grp->_interactions.end(); ++i2, ++corr) {
+                ub::vector<double> &a = (*i1)->_average.data().y();
+                ub::vector<double> &b = (*i1)->_average.data().y();
+                ub::matrix<double> &M = *corr;
+                    
+                //M(i,j) = M(i,j) - a(i)*b(j);
+                M = M*(1./(double)_nframes);
+                M = M - ub::outer_prod(a, b);
+            }
+        }
+    }
 }
--- src/tools/imc.h
+++ src/tools/imc.h
@@ -17,50 +17,75 @@
 namespace ub = boost::numeric::ublas;
 using namespace std;
 
+/**
+ * \brief class to calculate distribution functions and cross correlations for inverse monte carlo
+ * 
+ * This class calculates distribution functions as well as cross-correlations for specific groups of
+ * interactions based on a given trajectory.
+ * 
+ */
 class Imc
     : public CGObserver
 {
 public:
 
+    /// load cg definitions file
     void LoadOptions(const string &file);
     
+    /// begin coarse graining a trajectory
     void BeginCG(Topology *top, Topology *top_atom);
     
+    /// end coarse graining a trajectory
     void EndCG();
     
+    /// evaluate current conformation
     void EvalConfiguration(Topology *top, Topology *top_atom = 0);    
     
 protected:
+    /// the options parsed from cg definition file
     Property _options;
+    
+    /// list of bonded interactions
     list<Property *> _bonded;
+    /// list of non-bonded interactions
     list<Property *> _nonbonded;
     
+    /// struct to store collected information for interactions
     struct interaction_t {
         HistogramNew _average;
         HistogramNew _current;
         double _min, _max, _step;
     };
     
+    /// struct to store collected information for groups (e.g. crosscorrelations)
     struct group_t {
         list<interaction_t*> _interactions;
         vector< ub::matrix<double> > _correlations;
     };
     
+    /// map ineteractionm-name to interaction
     map<string, interaction_t *> _interactions;
+    /// map group-name to group
     map<string, group_t *> _groups;
             
     int _nframes;
     
+    /// create a new interaction entry based on given options
     interaction_t *AddInteraction(Property *p);
     
+    /// process non-bonded interactions for given frame
     void DoNonbonded(Topology *top);
+    
+    /// get group by name, creates one if it doesn't exist
     group_t *getGroup(const string &name);
     
+    /// initializes the group structs after interactions were added
     void InitializeGroups();
+    /// update the correlations after interations were processed
     void UpdateCorrelations();
 
-    void Correlate(ub::matrix<double> &corr, ub::vector<double> &v1,
-        ub::vector<double> &v2);
+    /// update the correlations after interations were processed
+    void CalcMatrix();
  };
 
 
