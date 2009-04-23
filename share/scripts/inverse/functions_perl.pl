b0222a858b4f29ca70e2053aa507bee571f684e6
Christoph Junghans <junghans@mpip-mainz.mpg.de>
2009-04-23 10:20:23 +0000

    fixed a bug in perl functions

--- share/scripts/inverse/functions_perl.pl
+++ share/scripts/inverse/functions_perl.pl
@@ -4,11 +4,11 @@ use strict;
 
 sub get_sim_property{
   ($_[0]) || die "get_sim_property: Missig arrgument for get_sim_property";
-  open(CSG,"csg_property --file $ENV{'CSGXMLFILE'} --path cg.inverse.$_[0] --short --print |") || 
+  open(CSG,"csg_property --file $ENV{'CSGXMLFILE'} --path cg.inverse.$_[0] --short --print . |") || 
     die "get_sim_property: Could not open pipe\n";
   defined(my $value=<CSG>) || die "get_sim_property: Could not get value $_[0]\n";
   close(CSG) || die "get_sim_property: error from csg_property\n";
-  return $_;
+  return $value;
 }
 
 sub csg_get{
