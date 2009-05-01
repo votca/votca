9c70aafded94094c17b737b784764583f9b34d38
Christoph Junghans <junghans@mpip-mainz.mpg.de>
2009-05-01 19:12:53 +0000

    pressure_cor.pl cleaned out

--- share/scripts/inverse/pressure_cor.pl
+++ share/scripts/inverse/pressure_cor.pl
@@ -1,13 +1,18 @@
 #! /usr/bin/perl -w
 
 use strict;
-
-# added this hope its righ (victor)
 ( my $progname = $0 ) =~ s#^.*/##;
+if ("$ARGV[0]" eq "--help"){
+  print <<EOF;
+Usage: $progname p_target p_cur outfile
+This script calls the pressure corrections dU=A*(1-r/r_c) 
+EOF
+  exit 0;
+}
+
 (my $function_file=`$ENV{SOURCE_WRAPPER} functions perl`) || die "$progname: $ENV{SOURCE_WRAPPER} function perl failed\n";
 chomp($function_file);
 (do "$function_file") || die "$progname: source $function_file failed\n";
-####
 
 die "3 parameters are nessary\n" if ($#ARGV<2);
 
@@ -18,21 +23,7 @@ my $delta_r=csg_get("step");
 my $p_target=$ARGV[0];
 my $p_now=$ARGV[1];
 
-# my $file="rdf.xvg";
-# open(FILE1,$file) or die "$file not found\n";
-# my $integral=0;
-# while (<FILE1>){
-#    next if /^#/;
-#    my @parts=split(' ');
-#    my $rdf=$parts[1];
-#    my $r=$parts[0];
-#    if (($rdf>=0) && ($r<=$r_cut)){
-#       $integral+=$rdf*$r*$r*$r*$delta_r;
-#    }
-# }
-# close(FILE1) or die "Error at closing $file\n";
-
-
+#Determine the sign
 my $pref;
 if ($p_now>$p_target){
    $pref=-0.1*$kBT;
@@ -40,15 +31,22 @@ if ($p_now>$p_target){
    $pref=0.1*$kBT;
 }
 
+#Determine pressure factor
 my $p_factor=($p_now-$p_target)/3000;
 $p_factor=-$p_factor if $p_factor<0;
+
+#Only use pressure factor if not too big
+#max is 0.1kbT
 $pref*=$p_factor if $p_factor<1;
 
-my $file="$ARGV[2]";
-open(FILE,"> $file") or die "$file not found\n";
-for(my $count=0;$count<=$max/$delta_r;$count++){
-   my $tmp_r=$count*$delta_r;
-   my $pot=$pref*(1-$tmp_r/$max);
-   print FILE "$tmp_r $pot\n";
+my @r;
+my @pot;
+my @flag;
+my $outfile="$ARGV[2]";
+for(my $i=0;$i<=$max/$delta_r;$i++){
+  $r[$i]=$i*$delta_r;
+  $pot[$i]=$pref*(1-$r[$i]/$max);
+  $flag[$i]="i";
 }
-close(FILE);
+saveto_table($outfile,\@r,\@pot,\@flag) || die "$progname: error at save table\n";
+
