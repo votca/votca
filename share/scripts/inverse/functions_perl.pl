83264fa142e4b559de0e9cc7b2e2d6d9bdd184a3
Christoph Junghans <junghans@mpip-mainz.mpg.de>
2009-05-06 20:02:15 +0000

    functions_perl.pl: replaced s/\n$// by chomp

--- share/scripts/inverse/functions_perl.pl
+++ share/scripts/inverse/functions_perl.pl
@@ -8,7 +8,7 @@ sub get_sim_property($){
     die "get_sim_property: Could not open pipe\n";
   defined(my $value=<CSG>) || die "get_sim_property: Could not get value $_[0]\n";
   close(CSG) || die "get_sim_property: error from csg_property\n";
-  $value =~ s/\n$//;
+  chomp($value);
   return $value;
 }
 
@@ -17,7 +17,7 @@ sub csg_get($){
   open(CSG,"$ENV{'csg_get'} $_[0] |") || die "csg_get: Could not open pipe\n";
   defined(my $value=<CSG>) || die "csg_get: Could not get value $_[0]\n";
   close(CSG) || die "csg_get: error from csg_property\n";
-  $value =~ s/\n$//;
+  chomp($value);
   return $value;
 }
 
