0ee12b29b8d6a1f590326cf6536baf08d619f5a4
Victor Ruehle <ruehle@mpip-mainz.mpg.de>
2009-05-12 23:01:38 +0000

    share/scripts/inverse/Makefile.am is automatically created, all scripts are added adjusted  CSGSHARE in CSGRC MD5SUM is calculated in make

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,24 @@
+# die function
+die () {
+  echo "$*" 
+  exit 1
+}
+
+# creating Makefile.am for share/scripts/inverse, this is automated here because we always forgot to update
+dir=$PWD
+cd share/scripts/inverse || die "error: cd shared/scripts/inverse failed"
+rm -f Makefile.am
+sed -ne '1,/%scripts/p' Makefile.am.in | sed -e '$d' > Makefile.am \
+  || die "error creating share/scripts/inverse/Makefile.am"
+ls *.sh *.pl csg_table *.m *.octave | sed -e 's/$/ \\/' -e '$s/ \\//' >> Makefile.am \
+  || die "error creating share/scripts/inverse/Makefile.am"
+sed -ne '/%scripts/,$p' Makefile.am.in | sed -e '1d' >> Makefile.am \
+  || die "error creating share/scripts/inverse/Makefile.am"
+cd $dir
+
+# now do usual stuff
 aclocal -I config
 autoheader
 automake --add-missing --copy
 autoconf 
+
--- share/scripts/inverse/recalc_md5.sh
+++
@@ -1,3 +0,0 @@
-md5sum *.pl *.sh linsolve.m linsolve.octave csg_table > MD5SUM
-sed -ie '/[:space:]*recalc_md5.sh$/d' MD5SUM
-
