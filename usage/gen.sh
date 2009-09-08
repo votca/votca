6cef526c8f6b7a35522b342ce66be2043e64e804
Victor Ruehle <ruehle@mpip-mainz.mpg.de>
2009-09-08 09:54:32 +0000

    started documenting inverse options

--- usage/gen.sh
+++ usage/gen.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
-file="mapping_tags.tex"
-xml="mapping.xml"
-echo "" > $file
+xml=$1
 
-echo "\\begin{itemize}" >> $file
+echo "\\begin{itemize}" 
 
 for name in $(csg_property --file $xml --path tags.item --print name --short | sort); do
-  echo "\\item \\textbf{$name} \\\\" >> $file
-  echo "$(csg_property --file $xml --path tags.item --filter \"name=$name\" --print desc --short | awk '{if(NF>0 && !first) print $0; else first=true;}' )" >> $file
+  echo "\\item \\textbf{$name} \\\\" 
+  echo "$(csg_property --file $xml --path tags.item --filter \"name=$name\" --print desc --short | awk '{if(NF>0 && !first) print $0; else first=true;}' )"
 done
 
-echo "\\end{itemize}" >> $file
+echo "\\end{itemize}"
 
