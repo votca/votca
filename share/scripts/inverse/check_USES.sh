0b1e74d13bacf380b05e520e5daea3a0b6cc4fb4
Christoph Junghans <junghans@mpip-mainz.mpg.de>
2009-09-16 09:18:24 +0000

    If you are lazy just run 'check_USES.sh --' ;-)

--- share/scripts/inverse/check_USES.sh
+++ share/scripts/inverse/check_USES.sh
@@ -3,13 +3,20 @@
 #note the space at the beginning and the end !!!
 not_to_check=" errorbars.sh ${0##*/} "
 
-[[ -z "$1" ]] && echo help with ${0##*/} --help && exit 
+if [ -z "$1" ]; then
+  echo MIssing argument >&2 
+  echo help with ${0##*/} --help >&2
+  exit 1
+fi
+
 if [ "$1" = "--help" ]; then
   echo Usage: ${0##*/} WORD FILE1 FILE2 ...
   echo Checks if a program is in USES block of a file
   echo "Example ${0##*/} awk *.sh"
   echo WORD = -- does some magic
   echo "WORD is set to \$(for i in *.sh *.pl; do ./\$i --help 2>&1 | sed -n 's/USES: \+\(.*\) *$/\1/p' | sed 's/ /\n/g'; done | sort | uniq)"
+  echo "missing FILE means '*.sh *.pl'"
+  echo "So if you are LAZY just run '${0##*/} --'"
   echo It will always ignore: $not_to_check
   exit 0
 fi
@@ -21,7 +28,14 @@ else
 fi
 shift
 
-echo what: $whates
+if [ -z "$1" ]; then
+  set -- *.pl *.sh
+fi
+
+echo files to check: $@
+echo
+echo what to check: $whates
+echo
 for i in $@; do
   [[ -z "${not_to_check##* $i *}" ]] && continue
   echo Checking $i
