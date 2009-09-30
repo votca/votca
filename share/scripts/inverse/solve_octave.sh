c4330f39cebe792c9dcd34de0187b7ab352456f3
Christoph Junghans <junghans@mpip-mainz.mpg.de>
2009-09-30 14:24:31 +0000

    solve_octave.sh: more save version

--- share/scripts/inverse/solve_octave.sh
+++ share/scripts/inverse/solve_octave.sh
@@ -13,11 +13,12 @@ check_deps "$0"
 [[ -n "$2" ]] || die "${0##*/}: Missing arguments"
 
 # initialize & run the matlab file
-sed -e "s/\$name_out/$2/"  -e "s/\$name/$1/" $CSGINVERSE/linsolve.octave > solve_$1.octave
+sed -e "s/\$name_out/$2/"  -e "s/\$name/$1/" $CSGINVERSE/linsolve.octave > solve_$1.octave || die "${0##*/}: sed failed"
 run_or_exit octave solve_$1.octave
 #rm -f solve_$1.octave
 
+[[ -f "$2" ]] || die "Octave failed"
 # temporary compatibility issue
-sed -ie 's/NaN/0.0/' $2
-sed -ie 's/Inf/0.0/' $2
+run_or_exit sed -ie 's/NaN/0.0/' $2
+run_or_exit sed -ie 's/Inf/0.0/' $2
 
