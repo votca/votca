cebe1f2c39a7dc1688b2137ac494aceadcaddac2
Victor Ruehle <ruehle@mpip-mainz.mpg.de>
2009-10-07 14:21:23 +0000

    renamed usage to reference

---
+++ reference/gen.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+xml=$1
+
+echo "\\begin{itemize}" 
+
+for name in $(csg_property --file $xml --path tags.item --print name --short ); do
+  echo "\\item \\textbf{$name} \\\\" 
+  echo "$(csg_property --file $xml --path tags.item --filter \"name=$name\" --print desc --short | awk '{if(NF>0 && !first) print $0; else first=true;}' )"
+done
+
+echo "\\end{itemize}"
+
---
+++ reference/src/xmltotxt.sh
@@ -0,0 +1,30 @@
+#!/bin/bash 
+
+if [ "$1" = "--help" ]; then
+  echo Usage: ${0##*/} infile outfile
+  echo Will convert xml to txt2tags file
+  exit 0
+fi
+
+if [ -z "$2" ]; then
+  echo "Missing argument" >&2
+  exit 1
+fi
+
+if [ -z "$(type -p csg_property)" ]; then
+  echo "csg_property not found" >&2
+  exit 1
+fi
+
+#header lines
+echo $1 > $2
+date -r cgoptions.xml  +%F >> $2
+echo >> $2
+echo '%!includeconf: config.t2t' >> $2
+
+for name in $(csg_property --file $1 --path tags.item --print name --short ); do
+  echo ": $name anchor($name)" >> $2
+  echo "$(csg_property --file $1 --path tags.item --filter "name=$name" --print desc --short | sed -e 's/^[[:space:]]*//' -e '/^$/d' )" >> $2
+done
+
+
