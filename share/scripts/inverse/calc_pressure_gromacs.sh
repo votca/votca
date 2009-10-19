1940f23f88dcf252679feffb995edae6995c5a7b
Christoph Junghans <junghans@mpip-mainz.mpg.de>
2009-10-19 10:29:21 +0000

    calc_pressure_gromacs.sh: fixed equi_time

--- share/scripts/inverse/calc_pressure_gromacs.sh
+++ share/scripts/inverse/calc_pressure_gromacs.sh
@@ -13,11 +13,12 @@ check_deps "$0"
 
 nsteps=$(get_from_mdp nsteps)
 dt=$(get_from_mdp dt)
-equi_time="$(csg_get_property cg.inverse.gromacs.equi_time)"
-equi_time=${equi_time%\%}
-equi=$(awk "BEGIN{print $equi_time/100*$nsteps*$dt}")
+equi_time="$(csg_get_property cg.inverse.gromacs.equi_time 0)"
+first_frame="$(csg_get_property cg.inverse.gromacs.first_frame 0)"
+
+begin="$(awk -v dt=$dt -v frames=$first_frame -v eqtime=$equi_time 'BEGIN{print (eqtime > dt*frames ? eqtime : dt*frames) }')"
 
 log "Running g_energy"
-run_or_exit "echo Pressure | g_energy -b $equi"
+run_or_exit "echo Pressure | g_energy -b ${begin}"
 p_now=$(csg_taillog -30 | awk '/^Pressure/{print $3}' ) || die "${0##*/}: awk failed"
 echo ${p_now}
