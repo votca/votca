7b7afece8ba35d731822b296c84d236067beb4f2
Victor Ruehle <ruehle@mpip-mainz.mpg.de>
2009-12-01 12:30:01 +0000

    improved warning for unknown molecule type

--- src/libcsg/cgengine.cc
+++ src/libcsg/cgengine.cc
@@ -29,9 +29,13 @@ TopologyMap *CGEngine::CreateCGTopology(Topology &in, Topology &out)
         Molecule *mol = *iter;
         CGMoleculeDef *def = getMoleculeDef(mol->getName());
         if(!def) {
-            cout << "unknown molecule " << mol->getName() << " with id "
+            cout << "--------------------------------------\n"
+                 << "WARNING: unknown molecule \"" << mol->getName() << "\" with id "
                  << mol->getId() << " in topology" << endl
-                 << "molecule will not be mapped to CG representation" << endl;
+                 << "molecule will not be mapped to CG representation\n"
+                 << "Check weather a mapping file for all molecule exists, was specified in --cg "
+                 << "separated by ; and the ident tag in xml-file matches the molecule name\n"
+                 << "--------------------------------------\n";
             continue;
         }
         Molecule *mcg = def->CreateMolecule(out);
