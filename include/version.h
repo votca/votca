acba588dd2459b32c39a9d8049846e69f72bb43c
Christoph Junghans <junghans@mpip-mainz.mpg.de>
2009-12-02 10:55:16 +0000

    super version system 1st version

---
+++ include/version.h
@@ -0,0 +1,11 @@
+#ifndef _tools_version_H
+#define	_tools_version_H
+
+#include <string>
+
+namespace votca { namespace tools {
+    const std::string & VersionStr();
+}}
+
+#endif	/* _tools_version_H */
+
