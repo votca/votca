d5893f0c7e83c871b96e10988043ae66949f9c8a
Christoph Junghans <junghans@mpip-mainz.mpg.de>
2009-12-03 17:28:16 +0000

    reference programs section reworked

---
+++ reference/programs/proghelp2t2t.sh
@@ -0,0 +1,33 @@
+#! /bin/bash
+
+die() {
+  echo -e "$*" >&2
+  exit 1
+}
+
+[ -z "$1" ] && die "${0##*/}: Missing argument"
+prog="$1"
+
+[ -z "${CSGSHARE}" ] && die "${0##*/}: CSGSHARE not defined"
+ 
+[ -z "$(type $prog)" ] && die "${0##*/}: $prog not found"
+
+helpmsg="$($prog --help)" || die "${0##*/}: $prog --help failed"
+
+echo "$prog"
+echo ${0##*/}
+date
+echo
+echo '%!includeconf: config.t2t'
+echo 
+
+echo -e "++$prog++"
+echo -e "$helpmsg" | sed \
+   -e '1,3d' \
+   -e 's/^[[:space:]]*//' \
+   -e '/^-/s/ \{2\}/``  /' \
+   -e '/^-/s/^/- ``/' \
+   -e 's/^\(Usage:[[:space:]]*\)\(.*\)$/\1``\2``/' \
+   -e 's/^\(Examples\|USES\|NEEDS|Usage\):/\n&/' \
+   -e 's/^\* \(.*\)$/- ``\1``/' \
+
