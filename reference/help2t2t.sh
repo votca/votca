802bd4a6d4a53fe8b889e023dc040557f5ecd4f5
Christoph Junghans <junghans@mpip-mainz.mpg.de>
2009-12-04 22:50:02 +0000

    reference/script section reworked

--- reference/help2t2t.sh
+++ reference/help2t2t.sh
@@ -9,26 +9,44 @@ die() {
 prog="$1"
 shift
 
+if [ "$1" = "--direct" ]; then
+  [ -z "$2" ] && die "${0##*/}: Missing argument after --direct"
+  progname="$2"
+else
+  progname="$prog"
+fi
+
 [ -z "${CSGSHARE}" ] && die "${0##*/}: CSGSHARE not defined"
  
 [ -z "$(type $prog)" ] && die "${0##*/}: $prog not found"
 
-helpmsg="$($prog "$@" --help)" || die "${0##*/}: $prog $@ --help failed"
+helpmsg="$($prog "$@" --help)" || die "${0##*/}: $progname --help failed"
 
-echo "$prog $@"
+echo "$progname"
 echo ${0##*/}
 date
 echo
 echo '%!includeconf: config.t2t'
 echo 
+echo -e "++$progname++"
 
-echo -e "++$prog++"
+#Here comes the magic:
+#-header is trash
+#-space at begining and end are removed
+#-optionlines (-[^ ]) -> - ``--option`` text
+#-usageline -> usage: ``code``
+#-extra empty line before new section to close itemize
+#-examplelines (^*) -> - ``line``
+#-
 echo -e "$helpmsg" | sed \
    -e '1,3d' \
    -e 's/^[[:space:]]*//' \
-   -e '/^-/s/ \{2\}/``  /' \
-   -e '/^-/s/^/- ``/' \
+   -e 's/[[:space:]]*$//' \
+   -e '/^-[^ ]/s/ \{2\}/`` /' \
+   -e '/^-[^ ].*``/s/^/- ``/' \
    -e 's/^\(Usage:[[:space:]]*\)\(.*\)$/\1``\2``/' \
-   -e 's/^\(Examples\|USES\|NEEDS|Usage\):/\n&/' \
-   -e 's/^\* \(.*\)$/- ``\1``/' \
+   -e 's/^\(Examples\|USES\|NEEDS\|Usage\|PROVIDES\|OPTIONAL\):/\n&/' \
+   -e 's/^\(Allowed\|Trajectory\|Specific\) options:/\n&/' \
+   -e '/^\* /s/\( \{2\}\|$\)/`` /' \
+   -e '/^\*.*``/s/^\*[[:space:]]*/- ``/' \
 
--- reference/xml/xmltotxt.sh
+++ reference/xml/xmltotxt.sh
@@ -1,45 +1,30 @@
 #!/bin/bash 
 
-unset -f die
 die () {
-  [[ -z "$CSGLOG" ]] || log --no-warn "$*"
-  echo -e "$*" 1>&2
-  log --no-warn "killing all processes...."
-  #send kill signal to all process within the process groups
-  kill 0
+  echo -e "$*" >&2
   exit 1
 }
 
 if [ "$1" = "--help" ]; then
-  echo Usage: ${0##*/} infile outfile
+  echo Usage: ${0##*/} infile 
   echo Will convert xml to txt2tags file
   exit 0
 fi
 
-if [ -z "$2" ]; then
-  echo "Missing argument" >&2
-  exit 1
-fi
-
-if [ -z "$(type -p csg_property)" ]; then
-  echo "csg_property not found" >&2
-  exit 1
-fi
-
-if [ ! -f "$1" ]; then
-  echo "Inputfile '$1' not found" >&2
-  exit 1
-fi
+[ -z "$1" ] && die "${0##*/}: Missing argument"
+[ -z "$(type -p csg_property)" ] && die "${0##*/}: csg_property not found"
+[ -f "$1" ] || die "Inputfile '$1' not found"
 
 #header lines
-echo $1 > $2
-date -r $1  +%F >> $2
-echo >> $2
-echo '%!includeconf: config.t2t' >> $2
+echo $1 
+echo ${0##*/}
+date -r $1  +%F 
+echo '%!includeconf: config.t2t'
+echo
 
 for name in $(csg_property --file $1 --path tags.item --print name --short || die "parsing xml failed" ); do
-  echo ": $name anchor(${name//\$})" >> $2
-  echo "$(csg_property --file $1 --path tags.item --filter "name=$name" --print desc --short | sed -e 's/^[[:space:]]*//' -e '/^$/d' || die \"parsing xml failed\" )" >> $2
+  echo ": $name anchor(${name//\$})"
+  echo "$(csg_property --file $1 --path tags.item --filter "name=$name" --print desc --short | sed -e 's/^[[:space:]]*//' -e '/^$/d' || die \"parsing xml failed\" )"
 done
 
 
