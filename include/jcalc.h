d4d6cc9a171a5164154df636c731308af44970b3
James Kirkpatrick <james.a.kirkpatrick@gmail.com>
2009-12-07 17:23:51 +0000

    added Jcalc cfunction and renamed qm_molecule

---
+++ include/jcalc.h
@@ -0,0 +1,21 @@
+/* 
+ * File:   jcalc.h
+ * Author: james
+ *
+ * Created on 07 December 2009, 17:19
+ */
+
+#ifndef _JCALC_H
+#define	_JCALC_H
+#include "crgunittype.h"
+
+class JCalc{
+
+public:
+
+private:
+    vector <CrgUnitType *>;
+};
+
+#endif	/* _JCALC_H */
+
