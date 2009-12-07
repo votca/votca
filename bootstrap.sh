45fa5af25a46ac1bb522a29b42e55567208da492
Christoph Junghans <junghans@mpip-mainz.mpg.de>
2009-12-07 17:42:10 +0100

    scripts better versioning

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,21 +1,7 @@
-#!/bin/bash
-# die function
-die () {
-  echo "$*" 
-  exit 1
-}
+#!/bin/bash -e
 
 # creating Makefile.am for share/scripts/inverse, this is automated here because we always forgot to update
-dir=$PWD
-cd share/scripts/inverse || die "error: cd shared/scripts/inverse failed"
-rm -f Makefile.am
-sed -ne '1,/%scripts/p' Makefile.am.in | sed -e '$d' > Makefile.am \
-  || die "error creating share/scripts/inverse/Makefile.am"
-ls *.sh *.pl csg_table *.m *.octave *.pm | sed -e 's/$/ \\/' -e '$s/ \\//' >> Makefile.am \
-  || die "error creating share/scripts/inverse/Makefile.am"
-sed -ne '/%scripts/,$p' Makefile.am.in | sed -e '1d' >> Makefile.am \
-  || die "error creating share/scripts/inverse/Makefile.am"
-cd $dir
+make -C share/scripts/inverse -f Makefile.am.in Makefile.am
 
 # now do usual stuff
 aclocal -I config
