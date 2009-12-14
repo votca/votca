4bedccf71102c5cd05cb2d8f1d10544ee5ac097e
Victor Ruehle <ruehle@mpip-mainz.mpg.de>
2009-12-14 20:28:47 +0100

    added missing netbeans file

---
+++ netbeans/libmd2qm/nbproject/Makefile-variables.mk
@@ -0,0 +1,24 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=GNU-Linux-x86
+CND_ARTIFACT_DIR_Debug=dist/Debug/GNU-Linux-x86
+CND_ARTIFACT_NAME_Debug=liblibmd2qm.a
+CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-Linux-x86/liblibmd2qm.a
+CND_PACKAGE_DIR_Debug=dist/Debug/GNU-Linux-x86/package
+CND_PACKAGE_NAME_Debug=libmd2qm.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/GNU-Linux-x86/package/libmd2qm.tar
+# Release configuration
+CND_PLATFORM_Release=GNU-Linux-x86
+CND_ARTIFACT_DIR_Release=dist/Release/GNU-Linux-x86
+CND_ARTIFACT_NAME_Release=liblibmd2qm.a
+CND_ARTIFACT_PATH_Release=dist/Release/GNU-Linux-x86/liblibmd2qm.a
+CND_PACKAGE_DIR_Release=dist/Release/GNU-Linux-x86/package
+CND_PACKAGE_NAME_Release=libmd2qm.tar
+CND_PACKAGE_PATH_Release=dist/Release/GNU-Linux-x86/package/libmd2qm.tar
