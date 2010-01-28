c90e26b12950a671e821628d844fe800fed6f216
Sebastian Fritsch <fritsch@mpip-mainz.mpg.de>
2010-01-28 15:08:37 +0100

    initialization should work now

--- calc_thermforce.sh
+++ calc_thermforce.sh
@@ -12,14 +12,14 @@ fi
 
 check_deps "$0"
 
-nsteps=$(get_from_mdp nsteps)
-dt=$(get_from_mdp dt)
-equi_time="$(csg_get_property cg.inverse.gromacs.equi_time 0)"
-first_frame="$(csg_get_property cg.inverse.gromacs.first_frame 0)"
+#nsteps=$(get_from_mdp nsteps)
+#dt=$(get_from_mdp dt)
+#equi_time="$(csg_get_property cg.inverse.gromacs.equi_time 0)"
+#first_frame="$(csg_get_property cg.inverse.gromacs.first_frame 0)"
 name=$(csg_get_interaction_property name)
 
-#xstart="$(csg_get_property cg.tf.xstart)"
-#xstop="$(csg_get_property cg.tf.xstop)"
+
+splinedelta="$(csg_get_property cg.tf.splinedelta)"
 step="$(csg_get_interaction_property step)"
 prefactor="$(csg_get_property cg.tf.prefactor)"
 splinestep="$(csg_get_property cg.tf.splinesmoothstep)"
@@ -38,20 +38,20 @@ xstop=$(echo "scale=8; $adressc+$adressw+$adressh" | bc)
 #rho_0="$(cat dens_$name.xvg | awk -f $CSGSCRIPTDIR/calc_non_hybrid_dens.awk -v adressc=$adressc adressw=$adressw adressh=$adressh)"
 #log "Density in non hybrid zone $rho_0"
 
-log "Symmetrizing density profile"
+
 infile="dens_$name.xvg"
 outfile="dens_$name\_symm.xvg"
 run_or_exit do_external density symmetrize --infile $infile --outfile $outfile --adressc $adressc
 
-log "Smoothing density profile"
+
 infile="dens_$name\_symm.xvg"
 outfile="dens_$name\_smooth.xvg"
 
-
-run_or_exit csg_resample --in $infile --out $outfile --grid $xstart:$step:$xstop --derivative $forcefile --spfit $xstart:$splinestep:$xstop
-
 forcefile="thermforce_$name.xvg"
 forcefile_smooth="thermforce_smooth_$name.xvg"
+spxstart=$(echo "scale=8; $adressc+$adressw-$splinedelta" | bc)
+spxstop=$(echo "scale=8; $adressc+$adressw+$adressh+$splinedelta" | bc)
+run_or_exit csg_resample --in $infile --out $outfile --grid $spxstart:$step:$spxstop --derivative $forcefile --spfit $spxstart:$splinestep:$spxstop
 
 run_or_exit do_external table smooth_borders --infile $forcefile --outfile $forcefile_smooth --xstart $xstart --xstop $xstop  
 
--- initialize_tf.sh
+++ initialize_tf.sh
@@ -23,26 +23,9 @@ name=$(csg_get_interaction_property name)
    run_or_exit csg_resample --in ../${name}.pot.in --out ${name}.pot.new --grid ${min}:${step}:${max}
  elif [ -f ../dens_${name}.xvg ]; then
    log "Calculating initial therm force from input density file dens_${name}.xvg"
-    xstart="$(csg_get_property cg.tf.xstart)"
-    xstop="$(csg_get_property cg.tf.xstop)"
-    step="$(csg_get_interaction_property step)"
-    prefactor="$(csg_get_property cg.tf.prefactor)"
-    splinestep="$(csg_get_property cg.tf.splinesmoothstep)"
-    
-    adressw="$(csg_get_property cg.tf.adressw)"
-    adressh="$(csg_get_property cg.tf.adressh)"
-    adressc="$(csg_get_property cg.tf.adressc)"
-    
-    infile="dens_$name.xvg"
-    outfile="dens_$name\_smooth.xvg"
-    forcefile="thermforce_$name.xvg"
-    forcefile_smooth="thermforce_smooth_$name.xvg"
-
-    run_or_exit csg_resample --in ../$infile --out $outfile --grid $xstart:$step:$xstop --derivative $forcefile --spfit $xstart:$splinestep:$xstop
-    
-    run_or_exit do_external table smooth_borders --infile $forcefile --outfile $forcefile_smooth --xstart $xstart --xstop $xstop
-    
-    run_or_exit do_external table integrate $forcefile_smooth ${name}.pot.new $prefactor
+   cp ../dens_$name.xvg .
+   run_or_exit do_external calc thermforce
+   cp ${name}.dpot.new ${name}.pot.new 
  else
 	die "initial therm_force_file ${name}.pot.in not found or initial density file dens_${name}.xvg not found" 
    # RDF_to_POT.pl just does log g(r) + extrapolation
