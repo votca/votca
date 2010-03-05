7f262b0d72e18bad3395a0a3afa0789fd5679adf
Christoph Junghans <junghans@mpip-mainz.mpg.de>
2010-03-05 23:30:44 +0100

    added some missing boost headers

--- src/libboost/find_boost.sh
+++ src/libboost/find_boost.sh
@@ -8,7 +8,8 @@ die() {
 [ -z "$1" ] && die "Give the path to votca src as argument"
 [ -d "$1" ] || die "Arg '$1' is not a dir"
 
-find "$1" -name "*.cc" -o -name "*.h" \
+find "$1" -not -name "*libboost*" -and \( -name "*.cc" -o -name "*.h" \) \
           -exec grep boost {} \; | \
-grep include | \
-sed 's/^.*<\([^>]*\)>.*$/\1/'
+grep -Ee "include" | \
+sed 's/^.*<\([^>]*\)>.*$/\1/' | \
+sort -u
