572c2e58f43e414eb1ae9808d6bde81877a8ada6
Victor Ruehle <ruehle@mpip-mainz.mpg.de>
2010-03-15 13:57:42 +0100

    updated netbeans project for csg_nemat

--- netbeans/csg_nemat/nbproject/Makefile-Release.mk
+++ netbeans/csg_nemat/nbproject/Makefile-Release.mk
@@ -1,5 +1,5 @@
 #
-# Gererated Makefile - do not edit!
+# Generated Makefile - do not edit!
 #
 # Edit the Makefile in the project folder instead (../Makefile). Each target
 # has a -pre and a -post target defined where you can add customized code.
@@ -15,17 +15,23 @@ RANLIB=ranlib
 CC=gcc
 CCC=g++
 CXX=g++
-FC=gfortran
+FC=g95
+AS=
+
+# Macros
+CND_PLATFORM=GNU-Linux-x86
+CND_CONF=Release
+CND_DISTDIR=dist
 
 # Include project Makefile
 include Makefile
 
 # Object Directory
-OBJECTDIR=build/Release/GNU-Linux-x86
+OBJECTDIR=build/${CND_CONF}/${CND_PLATFORM}
 
 # Object Files
 OBJECTFILES= \
-	${OBJECTDIR}/_ext/people/thnfs/homes/ruehle/src/csg_noconf/netbeans/csg_nemat/../../src/tools/csg_nemat.o
+	${OBJECTDIR}/_ext/_DOTDOT/_DOTDOT/src/tools/csg_nemat.o
 
 # C Compiler Flags
 CFLAGS=
@@ -37,21 +43,26 @@ CXXFLAGS=
 # Fortran Compiler Flags
 FFLAGS=
 
+# Assembler Flags
+ASFLAGS=
+
 # Link Libraries and Options
 LDLIBSOPTIONS=-L../../../../gmx/install/lib ../../../libcsg/trunk/netbeans/../dist/libcsg.a -lgmx -lboost_program_options -lfftw3 -lxml2 -lm
 
 # Build Targets
-.build-conf: ${BUILD_SUBPROJECTS} dist/Release/GNU-Linux-x86/csg_nemat
+.build-conf: ${BUILD_SUBPROJECTS}
+	${MAKE}  -f nbproject/Makefile-Release.mk dist/Release/GNU-Linux-x86/csg_nemat
 
-dist/Release/GNU-Linux-x86/csg_nemat: ${BUILD_SUBPROJECTS}
+dist/Release/GNU-Linux-x86/csg_nemat: ../../../libcsg/trunk/netbeans/../dist/libcsg.a
 
 dist/Release/GNU-Linux-x86/csg_nemat: ${OBJECTFILES}
 	${MKDIR} -p dist/Release/GNU-Linux-x86
-	${LINK.cc} -o dist/Release/GNU-Linux-x86/csg_nemat ${OBJECTFILES} ${LDLIBSOPTIONS} 
+	${LINK.cc} -o ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/csg_nemat ${OBJECTFILES} ${LDLIBSOPTIONS} 
 
-${OBJECTDIR}/_ext/people/thnfs/homes/ruehle/src/csg_noconf/netbeans/csg_nemat/../../src/tools/csg_nemat.o: ../../src/tools/csg_nemat.cc 
-	${MKDIR} -p ${OBJECTDIR}/_ext/people/thnfs/homes/ruehle/src/csg_noconf/netbeans/csg_nemat/../../src/tools
-	$(COMPILE.cc) -O2 -I../../../libcsg/trunk -I/people/thnfs/homes/ruehle/gmx/install/include/gromacs -o ${OBJECTDIR}/_ext/people/thnfs/homes/ruehle/src/csg_noconf/netbeans/csg_nemat/../../src/tools/csg_nemat.o ../../src/tools/csg_nemat.cc
+${OBJECTDIR}/_ext/_DOTDOT/_DOTDOT/src/tools/csg_nemat.o: nbproject/Makefile-${CND_CONF}.mk ../../src/tools/csg_nemat.cc 
+	${MKDIR} -p ${OBJECTDIR}/_ext/_DOTDOT/_DOTDOT/src/tools
+	${RM} $@.d
+	$(COMPILE.cc) -O2 -I../../../libcsg/trunk -I/people/thnfs/homes/ruehle/gmx/install/include/gromacs -MMD -MP -MF $@.d -o ${OBJECTDIR}/_ext/_DOTDOT/_DOTDOT/src/tools/csg_nemat.o ../../src/tools/csg_nemat.cc
 
 # Subprojects
 .build-subprojects:
@@ -65,3 +76,8 @@ ${OBJECTDIR}/_ext/people/thnfs/homes/ruehle/src/csg_noconf/netbeans/csg_nemat/..
 # Subprojects
 .clean-subprojects:
 	cd ../../../libcsg/trunk/netbeans && ${MAKE}  -f Makefile CONF=Debug clean
+
+# Enable dependency checking
+.dep.inc: .depcheck-impl
+
+include .dep.inc
