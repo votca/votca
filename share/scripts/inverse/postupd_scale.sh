3765a954980adb739cfb998a833de5a2d8e6976e
Victor Ruehle <ruehle@mpip-mainz.mpg.de>
2010-03-20 22:42:23 +0100

    added postupd_scale

---
+++ share/scripts/inverse/postupd_scale.sh
@@ -0,0 +1,40 @@
+#! /bin/bash
+#
+# Copyright 2009 The VOTCA Development Team (http://www.votca.org)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+if [ "$1" = "--help" ]; then
+cat <<EOF
+${0##*/}, version %version%
+This script implemtents smoothing of the potential update (.dpot)
+
+Usage: ${0##*/}
+
+USES: die csg_get_interaction_property mktemp do_external cp log run_or_exit check_deps
+
+NEEDS: name inverse.post_update_options.smooth.iterations
+EOF
+   exit 0
+fi
+
+check_deps "$0"
+
+name=$(csg_get_interaction_property name)
+scale=$(csg_get_interaction_property inverse.post_update_options.scale 1.0)
+
+log "scaling potential update"
+
+do_external table linearop ${name}.dpot.cur ${name}.dpot.new $scale 0
+
