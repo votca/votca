25628b119aa6208eafad2f2325a917830602f3db
Mara Jochum <jochum@mpip-mainz.mpg.de>
2010-03-29 15:41:46 +0200

    added my_prepare_gromacs.sh

---
+++ my_prepare_gromacs.sh
@@ -0,0 +1,39 @@
+#! /bin/bash
+# 
+# Copyright 2009 The VOTCA Development Team (http://www.votca.org)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+if [ "$1" = "--help" ]; then
+cat <<EOF
+${0##*/}, version %version%
+This script implemtents the function initialize
+for the Inverse Boltzmann Method
+
+Usage: ${0##*/} last_sim_dir
+
+USES: die cp run_or_exit grompp check_deps get_last_step_dir
+
+NEEDS:
+EOF
+  exit 0
+fi
+
+check_deps "$0"
+
+main_dir=$(get_main_dir);
+
+cp ${main_dir}/conf.gro ./conf.gro || die "${0##*/} cp ${last_step}/conf.gro ./conf.gro failed" 
+
+run_or_exit grompp -n index.ndx 
