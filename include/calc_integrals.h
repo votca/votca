2649b19dff673956facf574ec1ec3d1ee95b7366
Thorsten Vehoff <vehoff@mpip-mainz.mpg.de>
2010-04-08 10:52:21 +0200

    calc_integrals II

---
+++ include/calc_integrals.h
@@ -0,0 +1,31 @@
+/* 
+ * File:   calc_integrals.h
+ * Author: vehoff
+ *
+ * Created on April 8, 2010, 10:33 AM
+ */
+
+#ifndef _CALC_INTEGRALS_H
+#define	_CALC_INTEGRALS_H
+
+#include "qmpair.h"
+
+
+class CalcIntegrals : public PairCalculator
+{
+public:
+    CalcIntegrals();
+    ~CalcIntegrals();
+
+    void EvaluatePair(QMTopology *top, QMPair *pair) {};
+};
+
+void CalcIntegrals::EvaluatePair(QMTopology *top, QMPair* pair){
+    CrgUnit *crg1 = pair->Crg1();
+    CrgUnit *crg2 = pair->Crg2();
+    vector <double> Js = top.GetJCalc().CalcJ(*crg1, *crg2);
+    pair->setJs(Js);
+}
+
+#endif	/* _CALC_INTEGRALS_H */
+
