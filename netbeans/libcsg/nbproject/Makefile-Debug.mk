7a20e643279fa42777fbacd7a4a528be8be5cfb6
Victor Ruehle <ruehle@mpip-mainz.mpg.de>
2010-07-06 14:17:21 +0200

    csgapplication

--- netbeans/libcsg/nbproject/Makefile-Debug.mk
+++ netbeans/libcsg/nbproject/Makefile-Debug.mk
@@ -15,8 +15,8 @@ RANLIB=ranlib
 CC=gcc
 CCC=g++
 CXX=g++
-FC=
-AS=as
+FC=g95
+AS=
 
 # Macros
 CND_PLATFORM=GNU-Linux-x86
@@ -57,6 +57,7 @@ OBJECTFILES= \
 	${OBJECTDIR}/_ext/_DOTDOT/_DOTDOT/src/libcsg/map.o \
 	${OBJECTDIR}/_ext/_DOTDOT/_DOTDOT/src/libcsg/nblistgrid.o \
 	${OBJECTDIR}/_ext/_DOTDOT/_DOTDOT/src/libcsg/version_nb.o \
+	${OBJECTDIR}/_ext/_DOTDOT/_DOTDOT/src/libcsg/csgapplication.o \
 	${OBJECTDIR}/_ext/_DOTDOT/_DOTDOT/src/libcsg/modules/io/gmxtrajectoryreader.o
 
 # C Compiler Flags
@@ -214,6 +215,11 @@ ${OBJECTDIR}/_ext/_DOTDOT/_DOTDOT/src/libcsg/version_nb.o: nbproject/Makefile-${
 	${MKDIR} -p ${OBJECTDIR}/_ext/_DOTDOT/_DOTDOT/src/libcsg
 	${RM} $@.d
 	$(COMPILE.cc) -g -I../../include -I../../../include -I/usr/include/libxml2 -I../../../../../ruehle/gmx/include/gromacs -MMD -MP -MF $@.d -o ${OBJECTDIR}/_ext/_DOTDOT/_DOTDOT/src/libcsg/version_nb.o ../../src/libcsg/version_nb.cc
+
+${OBJECTDIR}/_ext/_DOTDOT/_DOTDOT/src/libcsg/csgapplication.o: nbproject/Makefile-${CND_CONF}.mk ../../src/libcsg/csgapplication.cc 
+	${MKDIR} -p ${OBJECTDIR}/_ext/_DOTDOT/_DOTDOT/src/libcsg
+	${RM} $@.d
+	$(COMPILE.cc) -g -I../../include -I../../../include -I/usr/include/libxml2 -I../../../../../ruehle/gmx/include/gromacs -MMD -MP -MF $@.d -o ${OBJECTDIR}/_ext/_DOTDOT/_DOTDOT/src/libcsg/csgapplication.o ../../src/libcsg/csgapplication.cc
 
 ${OBJECTDIR}/_ext/_DOTDOT/_DOTDOT/src/libcsg/modules/io/gmxtrajectoryreader.o: nbproject/Makefile-${CND_CONF}.mk ../../src/libcsg/modules/io/gmxtrajectoryreader.cc 
 	${MKDIR} -p ${OBJECTDIR}/_ext/_DOTDOT/_DOTDOT/src/libcsg/modules/io
