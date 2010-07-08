32ec97cdab7a6539e09c76a12959615ddf809e69
Victor Ruehle <ruehle@mpip-mainz.mpg.de>
2010-07-08 10:45:29 +0200

    updated csg_stat netbeans project

--- netbeans/csg_stat/nbproject/Makefile-Release.mk
+++ netbeans/csg_stat/nbproject/Makefile-Release.mk
@@ -1,5 +1,5 @@
 #
-# Gererated Makefile - do not edit!
+# Generated Makefile - do not edit!
 #
 # Edit the Makefile in the project folder instead (../Makefile). Each target
 # has a -pre and a -post target defined where you can add customized code.
@@ -15,18 +15,24 @@ RANLIB=ranlib
 CC=gcc
 CCC=g++
 CXX=g++
-FC=gfortran
+FC=g95
+AS=
+
+# Macros
+CND_PLATFORM=GNU-Linux-x86
+CND_CONF=Release
+CND_DISTDIR=dist
 
 # Include project Makefile
 include Makefile
 
 # Object Directory
-OBJECTDIR=build/Release/GNU-Linux-x86
+OBJECTDIR=build/${CND_CONF}/${CND_PLATFORM}
 
 # Object Files
 OBJECTFILES= \
-	${OBJECTDIR}/_ext/people/thnfs/homes/ruehle/src/csg/src/tools/imc.o \
-	${OBJECTDIR}/_ext/people/thnfs/homes/ruehle/src/csg/src/tools/csg_stat.o
+	${OBJECTDIR}/_ext/_DOTDOT/_DOTDOT/src/tools/imc.o \
+	${OBJECTDIR}/_ext/_DOTDOT/_DOTDOT/src/tools/csg_stat.o
 
 # C Compiler Flags
 CFLAGS=
@@ -38,25 +44,33 @@ CXXFLAGS=
 # Fortran Compiler Flags
 FFLAGS=
 
+# Assembler Flags
+ASFLAGS=
+
 # Link Libraries and Options
 LDLIBSOPTIONS=../libcsg/../../src/libcsg/libcsg.a ../../../tools/netbeans/libtools/../../src/libtools/libtools.a -lgmx -lboost_program_options -lxml2 -lm
 
 # Build Targets
-.build-conf: ${BUILD_SUBPROJECTS} ../../src/tools/csg_stat
+.build-conf: ${BUILD_SUBPROJECTS}
+	${MAKE}  -f nbproject/Makefile-Release.mk ../../src/tools/csg_stat
 
-../../src/tools/csg_stat: ${BUILD_SUBPROJECTS}
+../../src/tools/csg_stat: ../libcsg/../../src/libcsg/libcsg.a
+
+../../src/tools/csg_stat: ../../../tools/netbeans/libtools/../../src/libtools/libtools.a
 
 ../../src/tools/csg_stat: ${OBJECTFILES}
 	${MKDIR} -p ../../src/tools
 	${LINK.cc} -o ../../src/tools/csg_stat ${OBJECTFILES} ${LDLIBSOPTIONS} 
 
-${OBJECTDIR}/_ext/people/thnfs/homes/ruehle/src/csg/src/tools/imc.o: ../../src/tools/imc.cc 
-	${MKDIR} -p ${OBJECTDIR}/_ext/people/thnfs/homes/ruehle/src/csg/src/tools
-	$(COMPILE.cc) -g -O -I../../include -I../../../include -o ${OBJECTDIR}/_ext/people/thnfs/homes/ruehle/src/csg/src/tools/imc.o ../../src/tools/imc.cc
+${OBJECTDIR}/_ext/_DOTDOT/_DOTDOT/src/tools/imc.o: nbproject/Makefile-${CND_CONF}.mk ../../src/tools/imc.cc 
+	${MKDIR} -p ${OBJECTDIR}/_ext/_DOTDOT/_DOTDOT/src/tools
+	${RM} $@.d
+	$(COMPILE.cc) -g -O -I../../include -I../../../include -MMD -MP -MF $@.d -o ${OBJECTDIR}/_ext/_DOTDOT/_DOTDOT/src/tools/imc.o ../../src/tools/imc.cc
 
-${OBJECTDIR}/_ext/people/thnfs/homes/ruehle/src/csg/src/tools/csg_stat.o: ../../src/tools/csg_stat.cc 
-	${MKDIR} -p ${OBJECTDIR}/_ext/people/thnfs/homes/ruehle/src/csg/src/tools
-	$(COMPILE.cc) -g -O -I../../include -I../../../include -o ${OBJECTDIR}/_ext/people/thnfs/homes/ruehle/src/csg/src/tools/csg_stat.o ../../src/tools/csg_stat.cc
+${OBJECTDIR}/_ext/_DOTDOT/_DOTDOT/src/tools/csg_stat.o: nbproject/Makefile-${CND_CONF}.mk ../../src/tools/csg_stat.cc 
+	${MKDIR} -p ${OBJECTDIR}/_ext/_DOTDOT/_DOTDOT/src/tools
+	${RM} $@.d
+	$(COMPILE.cc) -g -O -I../../include -I../../../include -MMD -MP -MF $@.d -o ${OBJECTDIR}/_ext/_DOTDOT/_DOTDOT/src/tools/csg_stat.o ../../src/tools/csg_stat.cc
 
 # Subprojects
 .build-subprojects:
@@ -72,3 +86,8 @@ ${OBJECTDIR}/_ext/people/thnfs/homes/ruehle/src/csg/src/tools/csg_stat.o: ../../
 .clean-subprojects:
 	cd ../libcsg && ${MAKE}  -f Makefile_nb CONF=Release clean
 	cd ../../../tools/netbeans/libtools && ${MAKE}  -f Makefile_nb CONF=Release clean
+
+# Enable dependency checking
+.dep.inc: .depcheck-impl
+
+include .dep.inc
