e325ecb58d8ece267f74f08a2d5f076990ffc03b
Christoph Junghans <christoph.junghans@googlemail.com>
2010-09-23 16:42:18 +0200

    updated way to generate hg version

--- src/libcsg/version.cc
+++ src/libcsg/version.cc
@@ -39,11 +39,9 @@
 
 namespace votca { namespace csg {
 
-#ifdef HGVERSION
-  static const std::string version_str = VERSION " " HGVERSION " (compiled " __DATE__ ", " __TIME__ ")";
-#else
-  static const std::string version_str = VERSION "(compiled " __DATE__ ", " __TIME__ ")";
-#endif
+//defines hgversion
+#include "hgversion.h"
+static const std::string version_str = std::string(VERSION) + " " + hgversion + " (compiled " __DATE__ ", " __TIME__ ")";
 
 const std::string &CsgVersionStr()
 {
