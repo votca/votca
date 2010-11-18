a010571db738b1a5515730c3c172d0836efca62c
Konstantin Koschke <konstantin.koschke@gmail.com>
2010-11-18 12:14:38 +0100

    convenient class EasyLock added; not clear if needed at some point

---
+++ include/easylock.h
@@ -0,0 +1,29 @@
+/*
+ * easylock.h
+ *
+ *  Created on: Oct 26, 2010
+ *      Author: koschke
+ */
+
+#ifndef EASYLOCK_H_
+#define EASYLOCK_H_
+
+#include "mutex.h"
+
+class EasyLock {
+public:
+  EasyLock(Mutex *mutex);
+  //destructor unlocks mutex
+  ~EasyLock();
+
+  //return mutex used for lock
+  //Mutex * mutex() const;
+
+  //void Unlock();
+  //void Relock();
+
+private:
+  Mutex *myMutex;
+};
+
+#endif /* EASYLOCK_H_ */
---
+++ src/libtools/easylock.cc
@@ -0,0 +1,19 @@
+/*
+ * easylock.cc
+ *
+ *  Created on: Oct 26, 2010
+ *      Author: koschke
+ */
+
+#include "easylock.h"
+
+EasyLock::EasyLock(Mutex *mutex) {
+  // TODO Auto-generated constructor stub
+  myMutex = mutex;
+  myMutex->Lock();
+}
+
+EasyLock::~EasyLock() {
+  // TODO Auto-generated destructor stub
+  myMutex->Unlock();
+}
