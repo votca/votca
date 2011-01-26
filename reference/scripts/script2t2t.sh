0aa25c5d98b2ebb3f2c547887008b59ccf8f6485
Christoph Junghans <christoph.junghans@googlemail.com>
2011-01-26 19:54:39 +0100

    cleanup Makefiles

--- reference/scripts/script2t2t.sh
+++ reference/scripts/script2t2t.sh
@@ -5,12 +5,20 @@ die() {
   exit 1
 }
 
+assert() {
+  local x pipestatus=${PIPESTATUS[*]}
+  for x in $pipestatus ; do
+      [[ $x -eq 0 ]] || die "$@"
+  done
+}
+
 [ -z "$1" ] && die "${0##*/}: Missing argument"
 script="$1"
 shift
 
 [ -z "$(type csg_call)" ] && die "${0##*/}: csg_call not found"
-tags=$(csg_call -l | awk -v name="$script" '($3==name){print $1,$2;exit}') || die "could not get tags"
+tags=$(csg_call -l | awk -v name="$script" '($3==name){print $1,$2;exit}') 
+assert "could not get tags"
 helpmsg="$(csg_call $tags --help)" || die "${0##*/}: csg_call $tags --help failed"
 
 echo "$script"
@@ -47,18 +55,18 @@ echo -e "$helpmsg" | sed \
    -e 's/^\(Examples\|USES\|NEEDS\|Usage\|PROVIDES\|OPTIONAL\):/\n&/' \
    -e 's/^\(Allowed\|Trajectory\|Specific\) options:/\n&/' \
 
-echo
-echo
-echo
+assert "sed 1 failed"
 #do not parse xml options of functions files
-[ -z "${script##function_*}" ] && exit 0
+[ -z "${script##functions_*}" ] && exit 0
 
 content="$(csg_call --cat $tags)" || die "${0##*/}: csg_call --cat $tags failed"
-helpmsg="$(echo -e "$content" | sed -n -e '/^USES/d' -e '/csg_get_\(interaction_\)\?property/p')" || die "${0##*/}: sed failed"
+helpmsg="$(echo -e "$content" | sed -n -e '/^USES/d' -e '/csg_get_\(interaction_\)\?property/p')" 
+assert "${0##*/}: sed 2 failed"
 
 #no properties found
 [ -n "$helpmsg" ] || exit 0
 
+echo
 echo "Used xml options:"
 #set -x
 
@@ -70,9 +78,9 @@ echo "Used xml options:"
 echo -e "$helpmsg" | \
 if [ -z "${script%%*.sh}" ]; then
   perl -n \
-    -e 'if (/\(csg_get_(interaction_)?property\s+--allow-empty\s+(\S+?)\s*\)/) { print "$2 (default: empty)\n"; }
-        elsif (/\(csg_get_(interaction_)?property\s+(\S+?)\s+(\S+?)\s*\)/) { print "$2 (default: $3)\n";}
-        elsif (/\(csg_get_(interaction_)?property\s+(\S+?)\s*\)/) { print "$2\n";}
+    -e 'if (/csg_get_(interaction_)?property\s+--allow-empty\s+(\S+?)\s*\)/) { print "$2 (default: empty)\n"; }
+        elsif (/csg_get_(interaction_)?property\s+(\S+?)\s+(\S+?)\s*\)/) { print "$2 (default: $3)\n";}
+        elsif (/csg_get_(interaction_)?property\s+(\S+?)\s*\)/) { print "$2\n";}
  	elsif (/csg_get_(interaction_)?property/) {die "Oho, I do NOT understand the line $_\n";}'
 elif [ -z "${script%%*.pl}" ]; then
   perl -n \
@@ -84,3 +92,5 @@ else
 fi | \
 sed -e 's/"//g' -e "s/'//g" | \
 perl -pe 's/^(\S+)/- link(PREFIX$1)($1)/;' -e 's/PREFIX([^c][^g])/cg.interaction.$1/;' -e 's/PREFIX//;'
+
+assert "${0##*/}: sed 3 failed"
