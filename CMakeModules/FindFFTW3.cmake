2658ec947c027ccd70a6540b704afe6d225e4106
Christoph Junghans <christoph.junghans@googlemail.com>
2011-02-27 20:29:47 +0100

    cmake: working

---
+++ CMakeModules/FindFFTW3.cmake
@@ -0,0 +1,37 @@
+# - Find gsl
+# Find the native FFTW3 headers and libraries.
+#
+#  FFTW3_INCLUDE_DIRS - where to find expat.h, etc.
+#  FFTW3_LIBRARIES    - List of libraries when using expat.
+#  FFTW3_FOUND        - True if expat found.
+
+#=============================================================================
+# Copyright 2011 VOTCA Development Team
+#
+# Distributed under the OSI-approved BSD License (the "License");
+# see accompanying file Copyright.txt for details.
+#
+# This software is distributed WITHOUT ANY WARRANTY; without even the
+# implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the License for more information.
+#=============================================================================
+# (To distributed this file outside of CMake, substitute the full
+#  License text for the above reference.)
+
+find_package(PkgConfig)
+
+pkg_check_modules(PC_FFTW3 fftw3)
+find_path(FFTW3_INCLUDE_DIR fftw3.h HINTS ${PC_FFTW3_INCLUDE_DIRS})
+
+find_library(FFTW3_LIBRARY NAMES fftw3 HINTS ${PC_FFTW3_LIBRARY_DIRS} )
+
+set(FFTW3_LIBRARIES ${FFTW3_LIBRARY} )
+set(FFTW3_INCLUDE_DIRS ${FFTW3_INCLUDE_DIR} )
+
+include(FindPackageHandleStandardArgs)
+# handle the QUIETLY and REQUIRED arguments and set FFTW3_FOUND to TRUE
+# if all listed variables are TRUE
+
+find_package_handle_standard_args(FFTW3 DEFAULT_MSG FFTW3_LIBRARY FFTW3_INCLUDE_DIR )
+
+mark_as_advanced(FFTW3_INCLUDE_DIR FFTW3_LIBRARY )
---
+++ CMakeModules/FindGSL.cmake
@@ -0,0 +1,37 @@
+# - Find gsl
+# Find the native GSL headers and libraries.
+#
+#  GSL_INCLUDE_DIRS - where to find expat.h, etc.
+#  GSL_LIBRARIES    - List of libraries when using expat.
+#  GSL_FOUND        - True if expat found.
+
+#=============================================================================
+# Copyright 2011 VOTCA Development Team
+#
+# Distributed under the OSI-approved BSD License (the "License");
+# see accompanying file Copyright.txt for details.
+#
+# This software is distributed WITHOUT ANY WARRANTY; without even the
+# implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the License for more information.
+#=============================================================================
+# (To distributed this file outside of CMake, substitute the full
+#  License text for the above reference.)
+
+find_package(PkgConfig)
+
+pkg_check_modules(PC_GSL gsl)
+find_path(GSL_INCLUDE_DIR gsl/gsl_linalg.h HINTS ${PC_GSL_INCLUDE_DIRS})
+
+find_library(GSL_LIBRARY NAMES gsl HINTS ${PC_GSL_LIBRARY_DIRS} )
+
+set(GSL_LIBRARIES ${GSL_LIBRARY} )
+set(GSL_INCLUDE_DIRS ${GSL_INCLUDE_DIR} )
+
+include(FindPackageHandleStandardArgs)
+# handle the QUIETLY and REQUIRED arguments and set GSL_FOUND to TRUE
+# if all listed variables are TRUE
+
+find_package_handle_standard_args(GSL DEFAULT_MSG GSL_LIBRARY GSL_INCLUDE_DIR )
+
+mark_as_advanced(GSL_INCLUDE_DIR GSL_LIBRARY )
