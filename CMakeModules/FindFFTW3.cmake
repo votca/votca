f459fa8ca0009105dc958a23e76a6bbfad76cb4e
Christoph Junghans <junghans@mpip-mainz.mpg.de>
2011-03-01 15:41:14 +0100

    cmake: update Find Modules

--- CMakeModules/FindFFTW3.cmake
+++ CMakeModules/FindFFTW3.cmake
@@ -4,19 +4,21 @@
 #  FFTW3_INCLUDE_DIRS - where to find fftw3.h, etc.
 #  FFTW3_LIBRARIES    - List of libraries when using fftw3.
 #  FFTW3_FOUND        - True if fftw3 found.
-
-#=============================================================================
-# Copyright 2011 VOTCA Development Team
 #
-# Distributed under the OSI-approved BSD License (the "License");
-# see accompanying file Copyright.txt for details.
+# Copyright 2009-2011 The VOTCA Development Team (http://www.votca.org)
 #
-# This software is distributed WITHOUT ANY WARRANTY; without even the
-# implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-# See the License for more information.
-#=============================================================================
-# (To distributed this file outside of CMake, substitute the full
-#  License text for the above reference.)
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
 find_package(PkgConfig)
 
--- CMakeModules/FindGSL.cmake
+++ CMakeModules/FindGSL.cmake
@@ -4,35 +4,40 @@
 #  GSL_INCLUDE_DIRS - where to find gsl/gsl_linalg.h, etc.
 #  GSL_LIBRARIES    - List of libraries when using gsl.
 #  GSL_FOUND        - True if gsl found.
-
-#=============================================================================
-# Copyright 2011 VOTCA Development Team
 #
-# Distributed under the OSI-approved BSD License (the "License");
-# see accompanying file Copyright.txt for details.
+# Copyright 2009-2011 The VOTCA Development Team (http://www.votca.org)
 #
-# This software is distributed WITHOUT ANY WARRANTY; without even the
-# implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-# See the License for more information.
-#=============================================================================
-# (To distributed this file outside of CMake, substitute the full
-#  License text for the above reference.)
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
 find_package(PkgConfig)
 
 pkg_check_modules(PC_GSL gsl)
 find_path(GSL_INCLUDE_DIR gsl/gsl_linalg.h HINTS ${PC_GSL_INCLUDE_DIRS})
 
-find_library(GSL_LIBRARY NAMES gsl HINTS ${PC_GSL_LIBRARY_DIRS} )
-find_library(GSL_CBLAS_LIBRARY NAMES gslcblas HINTS ${PC_GSL_LIBRARY_DIRS} )
+if (NOT GSL_LIBRARY)
+  foreach (LIB ${PC_GSL_LIBRARIES})
+    find_library(GSL_${LIB} NAMES ${LIB} HINTS ${PC_GSL_LIBRARY_DIRS} )
+    list(APPEND GSL_LIBRARY ${GSL_${LIB}})
+  endforeach(LIB)
+endif (NOT GSL_LIBRARY)
 
 include(FindPackageHandleStandardArgs)
 # handle the QUIETLY and REQUIRED arguments and set GSL_FOUND to TRUE
 # if all listed variables are TRUE
 find_package_handle_standard_args(GSL DEFAULT_MSG GSL_LIBRARY GSL_INCLUDE_DIR )
-find_package_handle_standard_args(GSL DEFAULT_MSG GSL_CBLAS_LIBRARY GSL_INCLUDE_DIR )
 
-set(GSL_LIBRARIES "${GSL_LIBRARY};${GSL_CBLAS_LIBRARY}" )
+set(GSL_LIBRARIES ${GSL_LIBRARY} )
 set(GSL_INCLUDE_DIRS ${GSL_INCLUDE_DIR} )
 
-mark_as_advanced(GSL_INCLUDE_DIR GSL_LIBRARY GSL_CBLAS_LIBRARY)
+mark_as_advanced(GSL_INCLUDE_DIR GSL_LIBRARY)
