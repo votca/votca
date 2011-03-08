3540b825027cdbc7d218211baa5c87c44c74b24b
Christoph Junghans <junghans@mpip-mainz.mpg.de>
2011-03-08 15:59:49 +0100

    cmake: added linktest to FindVOTCA_TOOLS.cmake

--- CMakeModules/FindVOTCA_TOOLS.cmake
+++ CMakeModules/FindVOTCA_TOOLS.cmake
@@ -24,9 +24,10 @@ find_package(PkgConfig)
 
 pkg_check_modules(PC_VOTCA_TOOLS libvotca_tools)
 find_path(VOTCA_TOOLS_INCLUDE_DIR votca/tools/version.h HINTS ${PC_VOTCA_TOOLS_INCLUDE_DIRS})
-find_path(VOTCA_BOOST_INCLUDE_DIR boost/algorithm/string/trim.hpp HINTS ${PC_VOTCA_TOOLS_INCLUDE_DIRS})
+find_path(VOTCA_BOOST_INCLUDE_DIR boost/algorithm/string/trim.hpp HINTS ${PC_VOTCA_TOOLS_INCLUDE_DIRS} ${VOTCA_TOOLS_INCLUDE_DIR}/votca )
 find_library(VOTCA_TOOLS_LIBRARY NAMES votca_tools HINTS ${PC_VOTCA_TOOLS_LIBRARY_DIRS} )
 
+#add deps
 if (NOT BUILD_SHARED_LIBS)
   list(REMOVE_ITEM PC_VOTCA_TOOLS_LIBRARIES votca_tools)
   foreach (LIB ${PC_VOTCA_TOOLS_LIBRARIES})
@@ -43,5 +44,13 @@ include(FindPackageHandleStandardArgs)
 # handle the QUIETLY and REQUIRED arguments and set VOTCA_TOOLS_FOUND to TRUE
 # if all listed variables are TRUE
 find_package_handle_standard_args(VOTCA_TOOLS DEFAULT_MSG VOTCA_TOOLS_LIBRARY VOTCA_TOOLS_INCLUDE_DIR VOTCA_BOOST_INCLUDE_DIR)
+
+if (VOTCA_TOOLS_FOUND)
+  include(CheckVotcaToolsExists)
+  check_votca_tools_exists("${VOTCA_TOOLS_LIBRARY}" ToolsVersionStr "" FOUND_VOTCA_TOOLS_VERSION)
+  if(NOT FOUND_VOTCA_TOOLS_VERSION)
+    message(FATAL_ERROR "Could not find votca::tools::ToolsVersionStr in ${VOTCA_TOOLS_LIBRARY}, take look at the error message in ${CMAKE_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/CMakeError.log to find out what was going wrong. If you don't have pkg-config installed you will most likely have to set VOTCA_TOOLS_LIBRARY by hand and include all it's deps in there (i.e. -DVOTCA_TOOLS_LIBRARY='/path/to/libvotca_tools.so;/path/to/libgsl.so;/path/to/libm.so') !")
+  endif(NOT FOUND_VOTCA_TOOLS_VERSION)
+endif (VOTCA_TOOLS_FOUND)
 
 mark_as_advanced(VOTCA_TOOLS_INCLUDE_DIR VOTCA_TOOLS_LIBRARY )
