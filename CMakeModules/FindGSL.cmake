aa2af9263fafb87fcaef2e04749d5fdfada80d57
Christoph Junghans <christoph.junghans@googlemail.com>
2011-04-09 17:36:09 +0200

    cmake: FindGSL.cmake, fixed a bug on OpenSuse

--- CMakeModules/FindGSL.cmake
+++ CMakeModules/FindGSL.cmake
@@ -27,33 +27,33 @@ find_path(GSL_INCLUDE_DIR gsl/gsl_linalg.h HINTS ${PC_GSL_INCLUDE_DIRS})
 find_library(GSL_LIBRARY NAMES gsl HINTS ${PC_GSL_LIBRARY_DIRS} )
 
 #added gsl depends
-#BUG in OpenSuse (missing libgslcblas.so link libgsl.so)
+#BUG in OpenSuse (missing libgslcblas.so link in libgsl.so)
 #if (NOT BUILD_SHARED_LIBS and PC_GSL_LIBRARIES)
-if (PC_GSL_LIBRARIES AND NOT GSL_LIBRARY)
+if (PC_GSL_LIBRARIES AND NOT GSL_DEP_LIBRARY)
   list(REMOVE_ITEM PC_GSL_LIBRARIES gsl)
   foreach (LIB ${PC_GSL_LIBRARIES})
     find_library(GSL_${LIB} NAMES ${LIB} HINTS ${PC_GSL_LIBRARY_DIRS} )
-    list(APPEND GSL_LIBRARY ${GSL_${LIB}})
+    list(APPEND GSL_DEP_LIBRARY ${GSL_${LIB}})
     unset(GSL_${LIB} CACHE)
   endforeach(LIB)
-  set(GSL_LIBRARY "${GSL_LIBRARY}" CACHE FILEPATH "lib GSL and its deps" FORCE)
-endif (PC_GSL_LIBRARIES AND NOT GSL_LIBRARY)
+  set(GSL_DEP_LIBRARY "${GSL_DEP_LIBRARY}" CACHE FILEPATH "GSL dep libs" FORCE)
+endif (PC_GSL_LIBRARIES AND NOT GSL_DEP_LIBRARY)
 #endif (NOT BUILD_SHARED_LIBS and PC_GSL_LIBRARIES)
 
 include(FindPackageHandleStandardArgs)
 # handle the QUIETLY and REQUIRED arguments and set GSL_FOUND to TRUE
 # if all listed variables are TRUE
-find_package_handle_standard_args(GSL DEFAULT_MSG GSL_LIBRARY GSL_INCLUDE_DIR )
+find_package_handle_standard_args(GSL DEFAULT_MSG GSL_LIBRARY GSL_INCLUDE_DIR GSL_DEP_LIBRARY )
+
+set(GSL_LIBRARIES "${GSL_LIBRARY};${GSL_DEP_LIBRARY}")
+set(GSL_INCLUDE_DIRS ${GSL_INCLUDE_DIR} )
 
 if (GSL_FOUND)
   include(CheckLibraryExists)
-  check_library_exists("${GSL_LIBRARY}" gsl_linalg_QR_decomp "" FOUND_QR_DECOMP)
+  check_library_exists("${GSL_LIBRARIES}" gsl_linalg_QR_decomp "" FOUND_QR_DECOMP)
   if(NOT FOUND_QR_DECOMP)
-    message(FATAL_ERROR "Could not find gsl_linalg_QR_decompx in ${GSL_LIBRARY}, take a look at the error message in ${CMAKE_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/CMakeError.log to find out what was going wrong. If you don't have pkg-config installed you will most likely have to set GSL_LIBRARY by hand and include libgslcblas and libm in there (i.e. -DGSL_LIBRARY='/path/to/libgsl.so;/path/to/libgslcblas.so;/path/to/libm.so') !")
+    message(FATAL_ERROR "Could not find gsl_linalg_QR_decompx in ${GSL_LIBRARIES}, take a look at the error message in ${CMAKE_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/CMakeError.log to find out what was going wrong. If you don't have pkg-config installed you will most likely have to set GSL_LIBRARY by hand and include libgslcblas and libm in there (i.e. -DGSL_LIBRARY='/path/to/libgsl.so;/path/to/libgslcblas.so;/path/to/libm.so') !")
   endif(NOT FOUND_QR_DECOMP)
 endif (GSL_FOUND)
-
-set(GSL_LIBRARIES ${GSL_LIBRARY} )
-set(GSL_INCLUDE_DIRS ${GSL_INCLUDE_DIR} )
 
 mark_as_advanced(GSL_INCLUDE_DIR GSL_LIBRARY)
