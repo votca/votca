c5479b11cada37c13308534943e8d18dc024f247
Victor Ruehle <ruehle@mpip-mainz.mpg.de>
2011-05-02 15:01:31 +0200

    added calculator to recalculate nblist

--- src/libmd2qm/calculatorfactory.cc
+++ src/libmd2qm/calculatorfactory.cc
@@ -17,6 +17,7 @@
 #include "calculators/jortnerrates.h"
 #include "calculators/marcusrateslambdaouter.h"
 #include "calculators/hdf5writer.h"
+#include "calculators/nbgen.h"
 
 void CalculatorFactory::RegisterAll(void)
 {
@@ -37,4 +38,5 @@ void CalculatorFactory::RegisterAll(void)
         Calculators().Register<JortnerRates>("jortnerrates");
         Calculators().Register<MarcusRatesLambdaOuter>("marcusrateslambdaouter");
         Calculators().Register<HDF5Writer>("hdf5writer");
+        Calculators().Register<NBGen>("nbgen");
 }
---
+++ src/libmd2qm/calculators/nbgen.h
@@ -0,0 +1,36 @@
+#ifndef __VOTCA_MD2QM_NBGEN_H
+#define	__VOTCA_MD2QM_NBGEN_H
+
+#include <qmcalculator.h>
+
+class NBGen : public QMCalculator
+{
+public:
+    NBGen() {}
+    ~NBGen() {}
+
+    void Initialize(QMTopology *top, Property *options);
+    bool EvaluateFrame(QMTopology *top);
+    
+protected:
+    double _cutoff;
+};
+
+inline void NBGen::Initialize(QMTopology *top, Property *options){
+    _cutoff = options->get("options.nbgen.cutoff").as<double>();
+}
+
+inline bool NBGen::EvaluateFrame(QMTopology *top)
+{
+    top->nblist().Cleanup();
+    top->nblist().setCutoff(_cutoff);
+    BeadList list;
+    list.Generate(*top, "*");
+    top->nblist().Generate(list);
+
+
+}
+
+
+#endif	/* NBGEN_H */
+
