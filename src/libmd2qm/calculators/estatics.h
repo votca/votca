8ce2758348cadc9e9a74457e0003bea800c899cc
Falk May <mayfalk@mpip-mainz.mpg.de>
2011-06-16 15:15:54 +0200

    more documentation

--- src/libmd2qm/calculators/estatics.h
+++ src/libmd2qm/calculators/estatics.h
@@ -7,12 +7,13 @@
 /**
     \brief Calculates electrostatic contribution to site-energies
 
-Callname: "estat"
+Callname : "estat"
 
 Electrostatic contributions to site-energies of \f$E_i\f$ are computed from Coulomb's law for all sites \f$i\f$
-    \f[E_{i}  = \frac{1}{4 \pi \epsilon_0} \sum_{a_i} \sum_{\substack{b_k   \\ k\neq i }}
+    \f[E_{i}  = \frac{1}{4 \pi \epsilon_0} \sum_{a_i} \sum_{b_k k\neq i }
 \frac{ \left( q^c_{a_i} - q^n_{a_i} \right) q^n_{b_k}}{ \epsilon_{s} r_{a_i b_k}}\f]
-where \f$r_{a_i b_k}=|\vec{r}_{a_i} - \vec{r}_{b_k}|\f$ is the distance between atoms \f$a_i\f$ and \f$b_k\$f,  \f$\epsilon_\text{s}\f$ is the static relative dielectric constant. Depending on the option chosen in the main file the dielectric is either constant or distance dependent.
+where \f$r_{a_i b_k}=|\vec{r}_{a_i} - \vec{r}_{b_k}|\f$
+is the distance between atoms \f$a_i\f$ (which are in site \f$i\f$) and \f$b_k\f$ (which are in site \f$j\f$). The minimum image convention is used and the partial charges are taken for neutral(\f$q^n\f$) or charged sites(\f$q^n\f$). Finally \f$\epsilon_s\f$ is the static relative dielectric constant. Depending on the option chosen in the main file \f$\epsilon_s\f$ is either constant or distance dependent (then you need to specify a screening length \f$s\f$).
 
 */
 class CalcEstatics : public QMCalculator
