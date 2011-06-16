a7a884b5be33039c9778f9c280fbcbe49b4277ce
Falk May <mayfalk@mpip-mainz.mpg.de>
2011-06-16 14:29:41 +0200

    doxygen headers

--- src/libmd2qm/calculators/estatics.h
+++ src/libmd2qm/calculators/estatics.h
@@ -1,17 +1,20 @@
-/* 
- * File:   Estatic_Calculator.h
- * Author: mayfalk
- *
- * Created on May 21, 2010, 11:23 AM
- */
-
 #ifndef _CALC_ESTATICS_H
 #define	_CALC_ESTATICS_H
 
 #include "qmpair.h"
 #include "qmcalculator.h"
 
+/**
+    \brief Calculates electrostatic contribution to site-energies
 
+Callname: "estat"
+
+Electrostatic contributions to site-energies of \f$E_i\f$ are computed from Coulomb's law for all sites \f$i\f$
+    \f[E_{i}  = \frac{1}{4 \pi \epsilon_0} \sum_{a_i} \sum_{\substack{b_k   \\ k\neq i }}
+\frac{ \left( q^c_{a_i} - q^n_{a_i} \right) q^n_{b_k}}{ \epsilon_{s} r_{a_i b_k}}\f]
+where \f$r_{a_i b_k}=|\vec{r}_{a_i} - \vec{r}_{b_k}|\f$ is the distance between atoms \f$a_i\f$ and \f$b_k\$f,  \f$\epsilon_\text{s}\f$ is the static relative dielectric constant. Depending on the option chosen in the main file the dielectric is either constant or distance dependent.
+
+*/
 class CalcEstatics : public QMCalculator
 {
 public:
