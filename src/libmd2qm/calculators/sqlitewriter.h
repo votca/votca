6a5662f7fdbb4bd07b55df8b66e75d87f2663622
Falk May <mayfalk@mpip-mainz.mpg.de>
2011-06-16 16:39:55 +0200

    news documentation

--- src/libmd2qm/calculators/sqlitewriter.h
+++ src/libmd2qm/calculators/sqlitewriter.h
@@ -4,6 +4,24 @@
 #include "qmcalculator.h"
 #include "statesaversqlite.h"
 
+/** \brief Writes out data to sqlite3 format
+
+Callname: sqlitewriter
+
+Writes out the following pair properties to sqlite3 format:
+ - Distance between sites i and j in the pair in nm.
+ - Distance vector coordinates x,y,z in nm.
+ - Transfer integral in eV (or the effective value if more frontier orbitals are involved)
+ - Intramolecular reorganization energy in eV.
+ - Outer sphere reorganization energy in eV.
+
+Writes out the following site properties to sqlite3 format:
+ - Site energy in eV
+ - Site occupation probability
+ - Coordinates x,y,z of the center of mass in nm
+*/
+
+
 class SQLiteWriter : public QMCalculator {
 public:
     SQLiteWriter() {};
