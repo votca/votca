57463a23e45e28df2ae30cde815e0d5bf7b57eb6
Christoph Junghans <junghans@mpip-mainz.mpg.de>
2011-07-01 11:56:48 +0200

    updated run.sh for atomistic meth

--- methanol/atomistic/run.sh
+++ methanol/atomistic/run.sh
@@ -2,18 +2,15 @@
 
 grompp -v
 
-simnane=nvt
-mdrun -append -cpi state.cpt &> log_mdrun 
+mdrun -v
 
 echo Running g_energy
 equi=2000
 echo equi = $equi
-echo -e "Pressure" | g_energy -b $equi &> log_g_energy
-[[ $? -ne 0 ]] && echo Error at running g_energy && exit 1
+echo -e "Pressure" | g_energy -b $equi
 
-echo Running g_rdf
-echo -e "a O\nq" | make_ndx -f conf.gro &> log_make_ndx
-echo -e "MeO\nMeO" | g_rdf -f topol.trr -b $equi -bin 0.01 -rdf mol_com -n index.ndx -o rdf_methanol.xvg &> log_g_rdf_CG_CG
+echo Calculating distributions
+csg_stat --top topol.tpr --trj traj.trr --cg methanol.xml --options settings.xml --nt 3
 
 echo "Mapping confout.gro to get configuration for coarse-grained run"
 csg_map --top topol.tpr --trj confout.gro --cg methanol.xml --out conf_cg.gro 
