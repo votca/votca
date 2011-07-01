251c46a8ab6a5e24daa020f3f6ffac1c60917d79
Christoph Junghans <junghans@mpip-mainz.mpg.de>
2011-07-01 11:47:52 +0200

    updated run.sh of atomistic spce to use csg_stat

--- spce/atomistic/run.sh
+++ spce/atomistic/run.sh
@@ -2,18 +2,15 @@
 
 grompp -v
 
-simnane=nvt
-mdrun -append -cpi state.cpt &> log_mdrun 
+mdrun -v
 
 echo Running g_energy
 equi=2000
 echo equi = $equi
-echo -e "Pressure" | g_energy -b $equi &> log_g_energy
-[[ $? -ne 0 ]] && echo Error at running g_energy && exit 1
+echo -e "Pressure" | g_energy -b $equi
 
-echo Running g_rdf
-echo -e "a OW\nq" | make_ndx -f conf.gro &> log_make_ndx
-echo -e "SOL\nSOL" | g_rdf -f topol.trr -b $equi -bin 0.01 -rdf mol_com -n index.ndx -o rdf_CG_CG_aim.xvg &> log_g_rdf_CG_CG
+echo Calculating distributions
+csg_stat --top topol.tpr --trj traj.trr --cg water.xml --options fmatch.xml --nt 3
 
 echo "Mapping confout.gro to get configuration for coarse-grained run"
 csg_map --top topol.tpr --trj confout.gro --cg water.xml --out conf_cg.gro 
