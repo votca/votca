78669cb924edbeced6a297ea65134fc20cbf3cbd
Falk May <mayfalk@mpip-mainz.mpg.de>
2011-07-08 11:50:00 +0200

    cleaned up errormesseages in ecoulomb

--- src/libmd2qm/calculators/ecoulomb.cc
+++ src/libmd2qm/calculators/ecoulomb.cc
@@ -8,29 +8,35 @@
 
 void CalcEstatics::Initialize(QMTopology *top, Property *options) {
     _options = options;
-    if (options->exists("options.ecoulomb.epsilon")) {
-        _epsilon_dielectric = options->get("options.ecoulomb.epsilon").as<double>();
-    } else {
-        _epsilon_dielectric = 3.0;
-        cout << "Warning: dielectric constant in not provided, using default" << endl;
-    }
-
-    if (options->exists("options.ecoulomb.screening")) {
-        _s_eps = options->get("options.ecoulomb.screening").as<double>();
-    } else {
-        _s_eps = 3.0;
-        cout << "Warning: s_eps is not provided, using default" << endl;
-    }
-
-    if (options->exists("options.ecoulomb.method")) {
+      if (options->exists("options.ecoulomb.method")) {
         if (options->get("options.ecoulomb.method").as<string > () == "distdep") {
             _estatic_method = &CalcEstatics::dist_dep_eps;
+            if (options->exists("options.ecoulomb.epsilon")) {
+                _epsilon_dielectric = options->get("options.ecoulomb.epsilon").as<double>();
+            } else {
+                _epsilon_dielectric = 3.0;
+                cout << "Warning: dielectric constant in not provided, using default 3.0" << endl;
+            }
+            if (options->exists("options.ecoulomb.screening")) {
+                _s_eps = options->get("options.ecoulomb.screening").as<double>();
+            } else {
+                _s_eps = 3.0;
+                cout << "Warning: s_eps is not provided, using default 3.0" << endl;
+            }
             cout << "Doing distance-dependent-eps estatic with eps " << _epsilon_dielectric << " and s_eps " << _s_eps << endl;
         }
         else if (options->get("options.ecoulomb.method").as<string > () == "simple") {
             _estatic_method = &CalcEstatics::constant_epsilon;
+             if (options->exists("options.ecoulomb.epsilon")) {
+        _epsilon_dielectric = options->get("options.ecoulomb.epsilon").as<double>();
+    } else {
+        _epsilon_dielectric = 3.0;
+        cout << "Warning: dielectric constant in not provided, using default 3.0" << endl;
+    }
             cout << "Doing simple estatic with constant eps " << _epsilon_dielectric << endl;
-        } else throw std::runtime_error("Error in CalcEstatics::Initialize : no such estatic method, should be simple or distdep");
+        }
+
+        else throw std::runtime_error("Error in CalcEstatics::Initialize : no such estatic method, should be simple or distdep");
     } else throw std::runtime_error("Error in CalcEstatics:Initialize : no estatic_method specified");
 }
 
