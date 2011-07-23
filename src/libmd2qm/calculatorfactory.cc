4c24db5c5848ce90edfe7368a701ab61c02821b5
Denis Andrienko <andrienk@mpip-mainz.mpg.de>
2011-07-23 09:23:20 +0200

    cleaned up calculatorfactory

--- src/libmd2qm/calculatorfactory.cc
+++ src/libmd2qm/calculatorfactory.cc
@@ -19,19 +19,20 @@
 #include "calculators/neighborlist.h"
 #include "calculators/etinker.h"
 
+#include "calculators/rates.h"
 
-#include "calculators/marcusrates.h"
 //#include "calculators/readxml.h"
+//#include "calculators/marcusrates.h"
+//#include "calculators/jortnerrates.h"
+//#include "calculators/marcusrateslambdaouter.h"
+
 #include "calculators/writexml.h"
 #include "calculators/polymerrates.h"
 #include "calculators/pairdump.h"
 #include "calculators/CurrentDensity.h"
-#include "calculators/jortnerrates.h"
-#include "calculators/marcusrateslambdaouter.h"
 #include "calculators/sqlitewriter.h"
 #include "calculators/lambdaout.h"
 #include "calculators/dump_atoms_bj.h"
-#include "calculators/rates.h"
 
 #ifdef WITH_VOTCA_KMCOLD        
     #include "calculators/contkmc.h"
@@ -39,33 +40,33 @@
 
 void CalculatorFactory::RegisterAll(void)
 {
-	Calculators().Register<CalcIntegrals>("izindo");
-        Calculators().Register<CalcHistIntegrals>("ihistogram");
-//        Calculators().Register<ReadXML>("readxml");
+	Calculators().Register<CalcIntegrals>("izindo"); // ZINDO-based transfer integrals
+        Calculators().Register<CalcHistIntegrals>("ihistogram"); // histogram of transfer integrals
 
-	Calculators().Register<CalcEstatics>("ecoulomb");
-        Calculators().Register<GenerateNrgs>("egaussian");
-        Calculators().Register<EnergyCorr>("ecorrelation");
-        Calculators().Register<CalcHistEnergeticDisorder>("ehistogram");
-        Calculators().Register<Eshuffle>("eshuffle");
-        Calculators().Register<Etinker>("etinker");
+	Calculators().Register<CalcEstatics>("ecoulomb"); // Coulomb part of site energies
+        Calculators().Register<GenerateNrgs>("egaussian"); // gaussian distribution of site energies
+        Calculators().Register<EnergyCorr>("ecorrelation"); // site energy correlation function
+        Calculators().Register<CalcHistEnergeticDisorder>("ehistogram"); // site energy histogram
+        Calculators().Register<Eshuffle>("eshuffle"); // removes spatial energy correlations
+        Calculators().Register<Etinker>("etinker"); // input for the TINKER package (site energies)
 	
-        Calculators().Register<Neighborlist>("neighborlist");
-        Calculators().Register<Oboltzmann>("oboltzmann");
-	Calculators().Register<Vaverage>("vaverage"); // average charge velocities
+        Calculators().Register<Neighborlist>("neighborlist"); // fragment-based neighbor list
+        Calculators().Register<Oboltzmann>("oboltzmann"); // Boltzmann distribution of site energies
+	Calculators().Register<Vaverage>("vaverage"); // average charge velocities (requires site occupations)
 
 	Calculators().Register<CalcLambdaOut>("lambdaout");
-        Calculators().Register<MarcusRates>("marcusrates");
+//        Calculators().Register<MarcusRates>("marcusrates");
         Calculators().Register<PolymerRates>("polymerrates");
         Calculators().Register<PairDump>("pairdump");
         Calculators().Register<CurrentDensity>("currden");
-        Calculators().Register<JortnerRates>("jortnerrates");
-        Calculators().Register<MarcusRatesLambdaOuter>("marcusrateslambdaouter");
+//        Calculators().Register<JortnerRates>("jortnerrates");
+//        Calculators().Register<MarcusRatesLambdaOuter>("marcusrateslambdaouter");
         Calculators().Register<SQLiteWriter>("sqlitewriter");
         Calculators().Register<DumpAtomsBJ>("dumpatomsbj");
         Calculators().Register<DumpTrajectory>("tdump");
         Calculators().Register<Rates>("rates");
         Calculators().Register<WriteXML>("writexml");
+//        Calculators().Register<ReadXML>("readxml");
 	#ifdef WITH_VOTCA_KMCOLD        
         Calculators().Register<ContKmc>("kmc");
         #endif
