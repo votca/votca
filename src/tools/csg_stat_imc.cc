92eb0901aeee5e34b8f56bde2a139bc30529a189
Christoph Junghans <junghans@mpip-mainz.mpg.de>
2011-08-24 18:45:05 +0200

    csg_stat: added error if bonded interaction was not found in topology

--- src/tools/csg_stat_imc.cc
+++ src/tools/csg_stat_imc.cc
@@ -270,6 +270,8 @@ void Imc::Worker::DoBonded(Topology *top)
         // now fill with new data
         std::list<Interaction *> list = top->InteractionsInGroup(name);
 
+        if (list.begin() == list.end() )
+            throw std::runtime_error("Bonded interaction '" + name + "' defined in options xml-file, but not in topology - check definition in the mapping file again");
         std::list<Interaction *>::iterator ic_iter;
         for(ic_iter=list.begin(); ic_iter!=list.end(); ++ic_iter) {
             Interaction *ic = *ic_iter;
