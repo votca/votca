c7ec94cf18141267e6c844f0222ad0073a12bc51
Victor Ruehle <ruehle@mpip-mainz.mpg.de>
2011-09-23 17:29:45 +0200

    Apache License

--- include/votca/moo/crgunittype.h
+++ include/votca/moo/crgunittype.h
@@ -1,21 +1,22 @@
-// 
-// File:   crgunittype.h
-// Author: james
-//
-// Created on 27 June 2007, 09:21
-//
+/*
+ * Copyright 2009-2011 The VOTCA Development Team (http://www.votca.org)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
 
 #ifndef _CRGUNITTYPE_H
 #define	_CRGUNITTYPE_H
-/**
-    \brief information about a CrgUnitTpe
- 
-    The CrgUnitType class describes a known charge unit type. It stores the ID,
-    reorganization energie, internal coordinates and orbitals.
-
-    \todo mod orbitals to be "stripped down" for memory?
-
-*/
 
 //#define DEBUG
 
@@ -28,6 +29,15 @@ namespace votca { namespace moo {
 
 class CrgUnit;
 
+/**
+    \brief information about a CrgUnitTpe
+
+    The CrgUnitType class describes a known charge unit type. It stores the ID,
+    reorganization energie, internal coordinates and orbitals.
+
+    \todo mod orbitals to be "stripped down" for memory?
+
+*/
 class CrgUnitType{
 public:
     
