c5567a33908f2f17c82fa9986fa0c412ba53a6ef
Victor Ruehle <ruehle@mpip-mainz.mpg.de>
2011-09-23 17:38:06 +0200

    Apache license

--- src/libctp/calculators/izindo.h
+++ src/libctp/calculators/izindo.h
@@ -1,20 +1,36 @@
+/*
+ * Copyright 2009-2011 The VOTCA Development Team (http://www.votca.org)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
 #ifndef _CALC_INTEGRALS_H
 #define	_CALC_INTEGRALS_H
 
 #include <votca/ctp/qmpair.h>
 #include <votca/ctp/paircalculator.h>
 
+namespace votca { namespace ctp {
+
 /**
 	\brief Semi-empirical electronic coupling elements for all neighbor list pairs
 
 Semi-emprirical (ZINDO) electronic coupling elements for all conjuageted segments from the neighbout list (J. Int. J. Quantum Chem. 2008, 108, 51-56.). Requires molecular orbitals in GAUSSIAN format.
 
-Callname: izindo    
+Callname: izindo
 
 */
-
-namespace votca { namespace ctp {
-
 class Izindo : public PairCalculator
 {
 public:
--- src/tools/projobserver.h
+++ src/tools/projobserver.h
@@ -1,12 +1,22 @@
-/* 
- * File:   tofobserver.h
- * Author: ruehle
+/*
+ * Copyright 2009-2011 The VOTCA Development Team (http://www.votca.org)
  *
- * Created on December 9, 2009, 9:39 AM
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
  */
 
 #ifndef _EASYJOBSERVER_H
-#define	_EASYJTOFOBSERVER_H
+#define	_EASYJOBSERVER_H
 
 #include <votca/csg/cgobserver.h>
 #include <votca/moo/jcalc.h>
