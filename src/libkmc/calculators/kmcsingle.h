d794bbf79f3ec387631a85d350578e8f509b4827
Alexander Malafeev <malafeev.a@gmail.com>
2011-11-29 15:20:53 +0100

    global variables deleted from kmcsingle

--- src/libkmc/calculators/kmcsingle.h
+++ src/libkmc/calculators/kmcsingle.h
@@ -26,42 +26,12 @@
 #include <votca/tools/database.h>
 #include <votca/tools/statement.h>
 #include <votca/tools/tokenizer.h>
+#include "node.h"
+
 using namespace std;
 using namespace votca::kmc;
 
-struct link_t;
 
-class node_t : public VSSMGroup<link_t> {
-  public:
-	node_t(int id)
-	  : _id(id), _occ(0) {}
-	double _occ;
-	int _id;
-
-	void onExecute() {
-		_occ+=WaitingTime();
-		VSSMGroup<link_t>::onExecute();
-	}
-};
-
-node_t *current;
-vec r(0,0,0);
-
-struct link_t {
-	link_t(node_t *dest, double rate, vec r)
-	: _dest(dest), _rate(rate), _r(r) {}
-	double Rate() {
-		return _rate;
-	}
-
-	void onExecute() {
-		r+=_r;
-		current = _dest;
-	}
-	double _rate;
-	node_t *_dest;
-	vec _r;
-};
 
 class KMCSingle : public KMCCalculator 
 {
