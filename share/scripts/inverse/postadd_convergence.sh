f0541725fe42e36bc6204e9e3d8ca59658627d84
Sikandar Mashayak <symashayak@gmail.com>
2012-03-11 15:26:52 -0500

    convergence check for csg_reupdate

--- share/scripts/inverse/postadd_convergence.sh
+++ share/scripts/inverse/postadd_convergence.sh
@@ -42,9 +42,17 @@ step=$(csg_get_interaction_property step)
 
 
 
-#these two are arrays
+#these are arrays
 weights=( $(csg_get_interaction_property inverse.post_add_options.convergence.weight) )
 what_to_do_list=( $(csg_get_interaction_property inverse.post_add_options.convergence.what) )
+
+#base stands for the extension(s) of file(s) of data against which convergence is checked
+base=( $(csg_get_interaction_property inverse.post_add_options.convergence.base "none") )
+
+#default value for base is 'tgt' and for RE it is 'cur'
+if [[ ${base} = "none" ]]; then
+   base=tgt
+fi
 
 [[ ${#weights[@]} -ne ${#what_to_do_list[@]} ]] && die "${0##*/}: number of weights does not match number of 'what' to calc convergence from"
 
@@ -53,23 +61,37 @@ sum=0
 for ((i=0;i<${#what_to_do_list[@]};i++)); do
   dist=${what_to_do_list[$i]}
   weight=${weights[$i]}
-  tmp1="$(critical mktemp ${name}.${dist}.new.resample.XXX)"
 
-  if [[ ! -f "${name}.${dist}.tgt" ]]; then
+  if [[ ${base} = "tgt" ]]; then
+
+   tmp1="$(critical mktemp ${name}.${dist}.new.resample.XXX)"
+
+  else 
+
+   tmp1=${name}.${dist}.new
+
+  fi
+
+  if [[ ! -f "${name}.${dist}.${base}" ]]; then
     #if we need $name.dist.tgt we know how to create it
     if [[ ${dist} = "dist" ]]; then
-      do_external resample target "$(csg_get_interaction_property inverse.target)" "${name}.dist.tgt"
+      do_external resample target "$(csg_get_interaction_property inverse.target)" "${name}.dist.${base}"
     else
-      die "${0##*/}: file '${name}.${dist}.tgt' was not found. Add the script to create this file to the postadd routine of interaction $name or put it in the maindir and add it to cg.inverse.filelist."
+      die "${0##*/}: file '${name}.${dist}.${base}' was not found. Add the script to create this file to the postadd routine of interaction $name or put it in the maindir and add it to cg.inverse.filelist."
     fi
   fi
 
   [[ -f ${name}.${dist}.new ]] || die "${0##*/}: file '${name}.${dist}.new' was not found, add a postadd routine of interaction $name to calculate it."
   #resample this, as density dist maybe has the wrong grid
+if [[ ${base} = "tgt" ]]; then
+
   critical csg_resample --in ${name}.${dist}.new --out $tmp1 --grid "$min:$step:$max"
 
-  diff=$(do_external table combine --sum --no-flags --op d "$tmp1" "${name}.${dist}.tgt")
-  is_num "$diff" || die "${0##*/}: strange - result of do_external table difference '$tmp1' and '${name}.${dist}.tgt' was not a number" 
+fi
+
+
+  diff=$(do_external table combine --sum --no-flags --op d "$tmp1" "${name}.${dist}.${base}")
+  is_num "$diff" || die "${0##*/}: strange - result of do_external table difference '$tmp1' and '${name}.${dist}.${base}' was not a number" 
   wdiff=$(csg_calc "$weight" "*" "${diff}")
   echo "Convergence of $dist for ${name} was ${diff} and has weight $weight, so difference is $wdiff"
   sum=$(csg_calc $sum + $wdiff)
