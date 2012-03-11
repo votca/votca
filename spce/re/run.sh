7d9ada3abbee4cd80b52adeda52ff87e5594f833
Sikandar Mashayak <symashayak@gmail.com>
2012-03-11 15:24:45 -0500

    made consistent with modified csg_reupdate

--- spce/re/run.sh
+++ spce/re/run.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-source /home/mashaya1/Research/My_Votca/bin/VOTCARC.bash 
-echo 'running csg_inverse "re.xml"'
-csg_inverse --options re.xml
-
+echo 'running csg_inverse "settings.xml"'
+csg_inverse --options settings.xml
