adc67779b3c80335dc056401a124603381c3b794
Christoph Junghans <junghans@votca.org>
2012-04-11 14:22:53 -0600

    urea-water/atomistic: use csg_stat

--- urea-water/run.sh
+++ urea-water/run.sh
@@ -9,15 +9,7 @@ mdrun -v
 echo "Finished simulation"
 
 echo "Calculating target RDFs"
-
-echo "Calculating target RDF between urea-urea"
-echo "1 1"|g_rdf -f traj.xtc -o UR-UR.dist.tgt -b 10000 -rdf mol_com -n
-
-echo "Calculating target RDF between urea-water"
-echo "1 2"|g_rdf -f traj.xtc -o UR-SOL.dist.tgt -b 10000 -rdf mol_com -n
-
-echo "Calculating target RDF between water-water"
-echo "2 2"|g_rdf -f traj.xtc -o SOL-SOL.dist.tgt -b 10000 -rdf mol_com -n -dt 10
+csg_stat --nt 5 --top topol.tpr --cg "urea.xml;water.xml" --options non-bonded.xml --trj traj.xtc --begin 10000
 
 echo "Mapping confout.gro to get configuration for coarse-grained run"
 csg_map --top topol.tpr --trj confout.gro --cg "urea.xml;water.xml" --out conf_cg.gro
