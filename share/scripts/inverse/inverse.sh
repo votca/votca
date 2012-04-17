a3a707fafd34a1a130066957bd05db29e1fdca63
Sikandar Mashayak <symashayak@gmail.com>
2012-04-17 14:54:20 -0500

    adding script for post_done

--- share/scripts/inverse/inverse.sh
+++ share/scripts/inverse/inverse.sh
@@ -305,6 +305,9 @@ for ((i=$begin;i<$iterations+1;i++)); do
   cd $(get_main_dir) || die "cd $(get_main_dir) failed"
 done
 
+msg "Post done"
+do_external post_done $method
+
 touch "done"
 echo "All done at $(date)"
 exit 0
