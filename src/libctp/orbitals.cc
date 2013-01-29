f9a4f7c77305912307db87d041a0b962a39984a2
Denis Andrienko <denis.andrienko@mpip-mainz.mpg.de>
2013-01-29 22:30:29 +0100

    clean-up of the gaussian reading-in function

--- src/libctp/orbitals.cc
+++ src/libctp/orbitals.cc
@@ -22,6 +22,7 @@
 #include <iostream>
 #include <iomanip>
 #include <fstream>
+#include <map>
 #include <boost/algorithm/string.hpp>
 #include <boost/lexical_cast.hpp>
 
@@ -34,40 +35,33 @@ namespace votca { namespace ctp {
  */
 bool Orbitals::ReadOrbitalsGaussian( const char * filename )
 {
-    
+ 
+ map <int, vector<double> > _coefficients;
+ string line;
+ 
  ifstream _input_file ( filename ); 
  if ( _input_file.fail() ){
      cerr << "File "<< filename << " with molecular orbitals is not found " << endl;
      return 1;
  };
 
- string line;
-
-
- int i,j;
- int NBasis = 43;
- 
  std::vector <string> file;
- std::vector <string> nrg;
-
- // number of coefficients per line is  given by the first line of the file (e.g. 5D15.8)
- int n_lin_in_wt = ( NBasis-1 ) / 5 + 2;
   
- int k = 0;
- i = 0;
- 
 // number of coefficients per line is  given by the first line of the file (e.g. 5D15.8)
  getline ( _input_file, line );
  std::vector<string> strs;
  boost::algorithm::split(strs, line, boost::is_any_of("(D)"));
- cout << strs.at(1) << endl;
+ //cout << strs.at(1) << endl;
  int nrecords_in_line = boost::lexical_cast<int>(strs.at(1));
  string format = strs.at(2);
  
- cout    << "Formatted orbital file: " 
-         <<  nrecords_in_line << " records per line, in D" << format << " format." << endl;;
+ cout    << "Orbital file: " 
+         <<  nrecords_in_line << " records per line, in D" 
+         << format << " format." << endl;;
 
- unsigned _level = 0;
+ unsigned _levels = 0;
+ unsigned _level;
+ unsigned _basis_size = 0;
  
  while (_input_file){
 	getline (_input_file, line);
@@ -75,52 +69,48 @@ bool Orbitals::ReadOrbitalsGaussian( const char * filename )
         //cout << line << endl ;
         
         // if line has an equality sign, must be energy
-        std::string::size_type energy_pos = line.find('=');
+        std::string::size_type energy_pos = line.find("=");
         
         if ( energy_pos != std::string::npos ) {
-            cout << "energy line found at " << energy_pos << endl;            
-            std::string energy_string = line.substr( energy_pos + 1 );
-            cout << energy_pos << " " << energy_string << endl;
-            /*energy_string.replace(energy_string.find("D"), sizeof("D")-1, "e");
-            std::string::iterator _newend = std::remove(energy_string.begin(), energy_string.end(), ' ');
-            energy_string.erase( _newend++, energy_string.end() );
-            //double energy =  boost::lexical_cast<double>( energy_string );
-            //cout << _level++ << " " << energy << endl; */
+            
+            vector<string> results;
+            boost::algorithm::split(results, line, boost::is_any_of("\t ="), boost::algorithm::token_compress_on);
+            //cout << results[1] << ":" << results[2] << ":" << results[3] << ":" << results[4] << endl;
+            
+            _level = boost::lexical_cast<int>( results[1] );
+            boost::replace_first( results[4], "D", "e"  );
+            double _energy = boost::lexical_cast<double>( results[4]);  
+            _levels++;
             
         } else {
                 while (line.size() > 1 )
                 {
-                        /* string _coefficient;
+                         string _coefficient;
                         _coefficient.assign(line,0,15);
-                        cout << _coefficient << " " << endl;
-                        
-                        std::string::iterator _newend = std::remove(_coefficient.begin(), _coefficient.end(), ' ');
-                        _coefficient.erase(_newend++,_coefficient.end());
-                         cout << _coefficient << " ";
-                        _coefficient.replace(_coefficient.find("D"), sizeof("D")-1, "e");
-                        cout << _coefficient << " ";
-                        //double coefficient = boost::lexical_cast<double>( _coefficient );
-                        //cout << scientific << setprecision (7)<< coefficient << endl;
-                        line.erase(0,15); */
+                        boost::trim(_coefficient);
+                        boost::replace_first( _coefficient, "D", "e");
+                        double coefficient = boost::lexical_cast<double>( _coefficient );
+                        _coefficients[_level].push_back(coefficient);
+                        line.erase(0,15);
                 }
         }
    }
  
+   // some sanity checks 
+ cout << "Read in " << _levels << " levels. ";
  
-   if (file.size() != NBasis*NBasis ){
-       throw runtime_error(string("I expect so many basis on this molecule ") + boost::lexical_cast<string>(NBasis*NBasis) +
-               string(" but i read so many: " ) + boost::lexical_cast<string>(file.size()));
-   }
-   for(i=0,j=0,k=0;i<file.size();i++,k++)
-   {
-
-        //file[i].replace(file[i].size()-4,1,1,'e');
-        //sscanf(file[i].c_str(), "%lf" , &psi[j][k]);
-        //if (i%NBasis==NBasis-1){k=-1;j++;}
-   }
-   k=0;
-   file.clear();
-   return 0;
+ std::map< int, vector<double> >::iterator iter = _coefficients.begin();
+ _basis_size = iter->second.size();
+ 
+ for (iter = _coefficients.begin()++; iter != _coefficients.end(); iter++) {
+     if ( iter->second.size() != _basis_size ) {
+         cerr << "Error reading "<< filename << ". Basis set size change from level to level.";
+         
+     }
+ }
+ cout << "Basis set size: " << _basis_size << "." << endl;
+ 
+ return 0;
 }
 
 
