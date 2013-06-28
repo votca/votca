efdc5e079d2aa724267566d2900cf04812d6c87f
Carl Poelking <carl.poelking@mpip-mainz.mpg.de>
2013-06-28 18:52:46 +0200

    Add files ...

---
+++ src/libctp/calculators/jobwriter.h
@@ -0,0 +1,43 @@
+#ifndef VOTCA_CTP_JOBWRITER_H
+#define VOTCA_CTP_JOBWRITER_H
+
+#include<votca/ctp/topology.h>
+#include<votca/ctp/qmcalculator.h>
+
+
+namespace votca { namespace ctp {
+    
+  
+class JobWriter : public QMCalculator
+{
+
+public:
+
+    typedef void (JobWriter::*WriteFunct)(Topology*);
+    
+    string Identify() { return "Job Writer"; }
+    void Initialize(Topology *top, Property *options);
+    bool EvaluateFrame(Topology *top);    
+    
+    // NEED TO REGISTER ALL WRITE MEMBERS IN ::Initialize
+    void xqmultipole_ct(Topology *top);
+    void xqmultipole_chrg(Topology *top);
+    void xqmultipole_kmc(Topology *top);
+    
+
+private:
+
+    Property *_options;
+    vector<string> _keys;
+    map<string,WriteFunct> _key_funct;
+};
+
+
+
+
+    
+    
+    
+}}
+
+#endif
