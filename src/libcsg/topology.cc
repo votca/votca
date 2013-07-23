3a8f60481d209df362883e0cfb6a6f9ffc74f780
Christoph Junghans <junghans@votca.org>
2013-07-22 22:57:55 -0600

    xmltopologyreader: use resid, resname from pdb

--- src/libcsg/topology.cc
+++ src/libcsg/topology.cc
@@ -66,20 +66,23 @@ void Topology::Cleanup()
     _bc = new OpenBox();
 }
 
-/// \todo implement checking!!
+/// \todo implement checking, only used in xml topology reader
 void Topology::CreateMoleculesByRange(string name, int first, int nbeads, int nmolecules)
 {
     Molecule *mol = CreateMolecule(name);
     int beadcount=0;
+    int res_offset=0;
    
-    BeadContainer::iterator bead;    
+    BeadContainer::iterator bead;
     for(bead=_beads.begin(); bead!=_beads.end(); ++bead) {
         while(--first > 0) continue;
-        string bname = //lexical_cast<string>(mol->getId()) + ":" 
-                      //lexical_cast<string>(bead->Residue()) + ":" 
-                      //+
-                      (*bead)->getName();
-        mol->AddBead((*bead), bname);
+	//This is not 100% correct, but let's assume for now that the resnr do increase
+	if ( beadcount == 0 ) {
+	    res_offset = (*bead)->getResnr();
+	}
+        stringstream bname;
+	bname << (*bead)->getResnr() - res_offset + 1 << ":" << getResidue((*bead)->getResnr())->getName() << ":" << (*bead)->getName();
+        mol->AddBead((*bead), bname.str());
         if(++beadcount == nbeads) {
             if(--nmolecules <= 0) break;
             mol = CreateMolecule(name);            
