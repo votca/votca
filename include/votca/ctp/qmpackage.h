c924177bcd75f4605cd78f778b737a625c016bd4
Denis Andrienko <denis.andrienko@mpip-mainz.mpg.de>
2013-08-02 00:00:55 +0200

    help bug fixes

---
+++ include/votca/ctp/qmpackage.h
@@ -0,0 +1,75 @@
+#ifndef __QMPACKAGE__H
+#define	__QMPACKAGE__H
+
+#include <votca/ctp/segment.h>
+#include <votca/ctp/orbitals.h>
+#include <votca/ctp/apolarsite.h>
+#include <votca/ctp/logger.h>
+
+namespace votca { namespace ctp {
+
+using namespace std;    
+// ========================================================================== //
+// QMPackage base class for wrappers of TURBOMOLE, GAUSSIAN, etc          //
+// ========================================================================== //
+    
+class QMPackage
+{
+
+public:
+
+   QMPackage(){};
+   virtual ~QMPackage(){};
+
+   virtual string getPackageName() = 0;
+   
+   virtual bool WriteInputFile( vector< Segment* > segments, Orbitals* orbitals = NULL) = 0;
+   
+   virtual bool Run() = 0;
+
+   virtual bool ParseLogFile( Orbitals* _orbitals ) = 0;
+
+   virtual bool ParseOrbitalsFile( Orbitals* _orbitals ) = 0;
+   
+   virtual void CleanUp() = 0;
+
+   void setRunDir( string run_dir ) { _run_dir = run_dir; }
+   
+   void setInputFileName( string input_file_name ) { _input_file_name = input_file_name; }
+
+   void setLogFileName( string log_file_name ) { _log_file_name = log_file_name; }
+
+   void setOrbitalsFileName( string orb_file ) { _orb_file_name = orb_file; }
+   
+   void setLog( Logger* pLog ) { _pLog = pLog; }
+      
+   bool GuessRequested( ) { return _write_guess; }
+   
+private:
+
+    string                              _executable;
+    
+    string                              _input_file_name;
+    string                              _log_file_name;
+    string                              _xyz_file_name;
+    string                              _orb_file_name;
+    
+    string                              _run_dir;
+        
+    list< string >                      _cleanup_list;
+    
+    bool                                _get_orbitals;
+    bool                                _get_overlap;
+    bool                                _get_charges;
+    bool                                _get_self_energy;
+    
+    bool                                _write_guess;
+    bool                                _write_charges;
+    
+    Logger*                             _pLog;
+    
+};
+
+}}
+
+#endif
