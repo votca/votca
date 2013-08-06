9a7628a41f5a5aa842ac7c2c1c1fd0145696b8c3
Denis Andrienko <denis.andrienko@mpip-mainz.mpg.de>
2013-08-06 09:00:33 +0200

    moved PrintDescription to QMApplication

--- src/tools/ctp_run.cc
+++ src/tools/ctp_run.cc
@@ -3,12 +3,10 @@
 #include <iostream>
 #include <votca/ctp/qmapplication.h>
 #include <votca/ctp/calculatorfactory.h>
-#include <boost/format.hpp>
 
 
 using namespace std;
 using namespace votca::ctp;
-using boost::format;
 
 
 class CtpRun : public QMApplication
@@ -25,8 +23,7 @@ public:
     
 private:
     
-    enum HelpOutputType { _helpShort, _helpLong };
-    void    PrintDescription(string name, HelpOutputType _help_output_type);
+    //void    PrintDescription(string name, HelpOutputType _help_output_type);
 
 };
 
@@ -51,11 +48,10 @@ bool CtpRun::EvaluateOptions() {
             for(Calculatorfactory::assoc_map::const_iterator iter=
                     Calculators().getObjects().begin();
                     iter != Calculators().getObjects().end(); ++iter) {
-                PrintDescription( (iter->first), _helpShort );
+                PrintDescription( std::cout, (iter->first), _helpShort );
             }
             StopExecution();
             return true;
-       //Application::StopExecution();
     }
  
     
@@ -70,7 +66,7 @@ bool CtpRun::EvaluateOptions() {
                         iter != Calculators().getObjects().end(); ++iter) {
 
                     if ( (*n).compare( (iter->first).c_str() ) == 0 ) {
-                        PrintDescription( (iter->first), _helpLong );
+                        PrintDescription( std::cout, (iter->first), _helpLong );
                         printerror = false;
                         break;
                     }
@@ -79,8 +75,6 @@ bool CtpRun::EvaluateOptions() {
             }
             StopExecution();
             return true;     
-        //cout << "Sorry... Note implemented." << endl;
-        //Application::StopExecution();
     }
 
     QMApplication::EvaluateOptions();
@@ -91,37 +85,8 @@ bool CtpRun::EvaluateOptions() {
     for (it = calcs.begin(); it != calcs.end(); it++) {
         QMApplication::AddCalculator(Calculators().Create((*it).c_str()));
     }
-    return 1;
+    return true;
 }
-
-void CtpRun::PrintDescription(string name, HelpOutputType _help_output_type) {
-    // loading the documentation xml file from VOTCASHARE
-    char *votca_share = getenv("VOTCASHARE");
-    if (votca_share == NULL) throw std::runtime_error("VOTCASHARE not set, cannot open help files.");
-    string xmlFile = string(getenv("VOTCASHARE")) + string("/ctp/xml/") + name + string(".xml");
-    string _format("%|3t|%1% %|20t|%2% \n");
-    try {
-        Property options;
-        load_property_from_xml(options, xmlFile);
-
-        string _help;
-        switch (_help_output_type) {
-
-            case _helpShort:
-                _help = options.get("options." + name).getAttribute<string>("help");
-                cout << format(_format) % name % _help;
-                break;
-
-            case _helpLong:
-                cout << HLP << setlevel(2) << options;
-        }
-
-    } catch (std::exception &error) {
-        cout << format(_format) % name % "Undocumented";
-    }
-}
-
-
 
 int main(int argc, char** argv) {
     
