1fc25caa6dab8e8e861c10a659c9dbe9f23b8b22
Denis Andrienko <denis.andrienko@mpip-mainz.mpg.de>
2013-10-11 17:57:43 +0200

    quick bug fixes

--- manual/reference/calculators/extract_calculators.sh
+++ manual/reference/calculators/extract_calculators.sh
@@ -5,20 +5,21 @@ VOTCASHARE="$(csg_call --show-share)"
 texfile="$PWD/calculators.tex"
 rm -f $texfile; touch $texfile
 
-for package in ctp kmc; do
-
-	calculators="$(${package}_run --list | sed -ne 's/^\s\+\([a-z]*\)\s*\(.*\)/\1/p')"
+for package in ctp_tools ctp_run ctp_parallel ctp_dump kmc_run; do
+	calculators="$(${package} --list | sed -ne 's/^\s\+\([a-z,0-9]*\)\s*\(.*\)/\1/p')"
+	echo $calculators
 
 	# loop over all calculators
 	for calculator in ${calculators}; do
-
-		xmlfile=${VOTCASHARE}/${package}/xml/$calculator.xml
-
+		library="$(echo ${package} | sed -e 's/\_[^\_]*$//g')"
+		xmlfile=${VOTCASHARE}/${library}/xml/$calculator.xml
+		echo $calculator
+		echo $xmlfile
 		if [ ! -f "$xmlfile" ]; then 
 			continue
 		fi
-
-# 		votca_property --file $xmlfile --format TEX >> $texfile
+		echo "votca_property --file $xmlfile --format TEX --level 2"
+		votca_property --file $xmlfile --format TEX --level 2 >> $texfile
 
 	done
 done
