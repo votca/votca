6588fe9ffa78fb8335e3b8efb5b16331cbe79bdd
Denis Andrienko <denis.andrienko@mpip-mainz.mpg.de>
2013-10-17 17:06:24 +0200

    coupling tool advanced; added qmpackages xml share

--- src/libctp/tools/coupling.h
+++ src/libctp/tools/coupling.h
@@ -21,6 +21,7 @@
 #define _VOTCA_CTP_COUPLINGH_H
 
 #include <votca/ctp/logger.h>
+#include <votca/ctp/qmpackagefactory.h>
 
 namespace votca { namespace ctp {
     using namespace std;
@@ -47,6 +48,9 @@ private:
     int         _trimA, _trimB;
     string      _qmpackageXML;
     double      _degeneracy;
+
+    string              _package;
+    Property            _package_options; 
     
     Logger      _log;
 
@@ -59,7 +63,6 @@ void Coupling::Initialize(Property* options)
     UpdateWithDefaults( options );
     std::string key = "options." + Identify();    
 
-    
     _degeneracy = options->get(key + ".degeneracy").as<double> ();
     _qmpackageXML = options->get(key + ".package").as<string> ();
             
@@ -76,15 +79,41 @@ void Coupling::Initialize(Property* options)
  
     _trimA  = options->get(key + ".moleculeA.trim").as<int> ();
     _trimB  = options->get(key + ".moleculeB.trim").as<int> ();
+   
+     //key = "package";
+    //_package = _package_options.get(key+".name").as<string> ();
+
+    _package = "gaussian";
+    
+    // get the path to the shared folders with xml files
+    char *votca_share = getenv("VOTCASHARE");    
+    if(votca_share == NULL) throw std::runtime_error("VOTCASHARE not set, cannot open help files.");
+    string xmlFile = string(getenv("VOTCASHARE")) + string("/ctp/qmpackages/") + _package + string("/idft.pair.xml ");
+    //load_property_from_xml( _package_options, xmlFile );    
+    
 }
-
-
 
 bool Coupling::Evaluate() {
 
     LOG( logINFO, _log ) << "Loading orbitals " << _orbA << flush;
     LOG( logINFO, _log ) << "Loading orbitals " << _orbB << flush;
     LOG( logINFO, _log ) << "Loading orbitals " << _orbAB << flush;
+   
+    // get the corresponding object from the QMPackageFactory
+    QMPackage *_qmpackage =  QMPackages().Create( _package );
+   _qmpackage->setLog( &_log );       
+   //_qmpackage->Initialize( &_package_options );
+   
+    Orbitals _orbitalsA, _orbitalsB, _orbitalsAB;
+    
+    int _parse_logA_status = _qmpackage->ParseLogFile( &_orbitalsA );
+    int _parse_logB_status = _qmpackage->ParseLogFile( &_orbitalsB );
+    int _parse_logAB_status = _qmpackage->ParseLogFile( &_orbitalsAB );
+
+    int _parse_orbitalsA_status = _qmpackage->ParseOrbitalsFile( &_orbitalsA );
+    int _parse_orbitalsB_status = _qmpackage->ParseOrbitalsFile( &_orbitalsB );
+    int _parse_orbitalsAB_status = _qmpackage->ParseOrbitalsFile( &_orbitalsAB );
+
     
     std::cout << _log;
     
