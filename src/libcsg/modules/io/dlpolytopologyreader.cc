470fbf2711b494fb99f58abb5d0062adf358e6c8
Christoph Junghans <junghans@votca.org>
2013-11-07 17:09:58 -0700

    dlpolytopologyreader: read box from CONFIG file (fixes issue 141)

--- src/libcsg/modules/io/dlpolytopologyreader.cc
+++ src/libcsg/modules/io/dlpolytopologyreader.cc
@@ -1,5 +1,5 @@
-/* 
- * Copyright 2009-2011 The VOTCA Development Team (http://www.votca.org)
+/*
+ * Copyright 2009-2013 The VOTCA Development Team (http://www.votca.org)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,11 +15,8 @@
  *
  */
 
-#ifndef HAVE_NO_CONFIG
-#include <votca_config.h>
-#endif
-
 #include <iostream>
+#include <fstream>
 
 #include "dlpolytopologyreader.h"
 #include "dlpoly/dlp_io_layer.h"
@@ -36,15 +33,15 @@ bool DLPOLYTopologyReader::ReadTopology(string file, Topology &top)
 
     int idnode,matms,natms,nmols,nmolt;
     int istateF;
-    
+
     int inode=matms=natms=nmols=nmolt=0;
-    
+
     // TODO: istateF must be an enum!
-    istateF=1;    
-    
+    istateF=1;
+
     // TODO: we need to fix the file naming!
     field_scan_(&istateF,&matms,&natms,&nmolt);
-    
+
     MolecBase = new MolecSpecsT[nmolt];
     FieldSite = new FieldSiteT[natms];
 
@@ -57,10 +54,9 @@ bool DLPOLYTopologyReader::ReadTopology(string file, Topology &top)
     // AB: NOT TO RE-/DE-ALLOCATE F-arrays in the next F-call, reset istateF = 0
     istateF = 0;
 
-
     // TODO: fix residue naming / assignment
     Residue *res = top.CreateResidue("no");
-    
+
     // read the atoms
     int is=0;
     for(int im=0; im<nmolt; im++){
@@ -68,8 +64,8 @@ bool DLPOLYTopologyReader::ReadTopology(string file, Topology &top)
             Molecule *mi = top.CreateMolecule(MolecBase[im].name);
             for(int ims=0; ims<MolecBase[im].nsites; ims++) {
 
-                BeadType *type = top.GetOrCreateBeadType(FieldSite[is].name); // what is 
-                Bead *bead = top.CreateBead(1, FieldSite[is].type, type, res->getId(), FieldSite[is].m, FieldSite[is].q);  
+                BeadType *type = top.GetOrCreateBeadType(FieldSite[is].name); // what is
+                Bead *bead = top.CreateBead(1, FieldSite[is].type, type, res->getId(), FieldSite[is].m, FieldSite[is].q);
 
                 stringstream nm;
                 nm << bead->getResnr() + 1 << ":" <<  top.getResidue(bead->getResnr())->getName() << ":" << bead->getName();
@@ -77,10 +73,34 @@ bool DLPOLYTopologyReader::ReadTopology(string file, Topology &top)
             }
         }
     }
-    
+
     delete [] MolecBase;
     delete [] FieldSite;
-   
+
+    std::ifstream fl;
+    fl.open("CONFIG");
+    if(fl.is_open()) {
+      string line;
+      getline(fl, line); //title
+      getline(fl, line); // 2nd header line
+      vec box_vectors[3];
+      for (int i=0;i<3;i++){ // read 3 box lines
+        getline(fl, line);
+        if(fl.eof())
+          throw std::runtime_error("unexpected end of file in dlpoly file CONFIG, when reading box vector"  +
+              boost::lexical_cast<string>(i));
+        Tokenizer tok(line, " ");
+        vector<double> fields;
+        tok.ConvertToVector<double>(fields);
+        box_vectors[i]=vec(fields[0],fields[1],fields[2]);
+      }
+      matrix box(box_vectors[0],box_vectors[1],box_vectors[2]);
+      top.setBox(box);
+    }
+    else {
+      cout << "NOTE: Could open dlploy file CONFIG, so no boundary conditions, where set in the topology" << endl;
+    }
+
     return true;
 }
 
--- src/libcsg/modules/io/dlpolytopologyreader.h
+++ src/libcsg/modules/io/dlpolytopologyreader.h
@@ -1,5 +1,5 @@
-/* 
- * Copyright 2009-2011 The VOTCA Development Team (http://www.votca.org)
+/*
+ * Copyright 2009-2013 The VOTCA Development Team (http://www.votca.org)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -26,7 +26,7 @@ namespace votca { namespace csg {
 using namespace votca::tools;
 
 using namespace std;
-    
+
 /**
     \brief reader for gromacs topology files
 
