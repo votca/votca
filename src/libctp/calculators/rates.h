6642bf37a4b20e87ab25d73569268eb663accf9d
Bjoern Baumeier <baumeier@mpip-mainz.mpg.de>
2014-08-22 13:03:26 +0200

    self-implemented complex (l)gamma(z)

--- src/libctp/calculators/rates.h
+++ src/libctp/calculators/rates.h
@@ -77,6 +77,35 @@ private:
 complex <double> cgamma (complex <double> argument)
 {   // complex result of Gamma(z) with complex z
     complex<double> result;
+    
+     // complec Gamma function of complex argument from Lanczos approximation
+    static const int g=7;
+    static const double pi =
+        3.1415926535897932384626433832795028841972 ;
+    static const double p[g+2] = {0.99999999999980993, 676.5203681218851,
+      -1259.1392167224028, 771.32342877765313, -176.61502916214059,
+      12.507343278686905, -0.13857109526572012, 9.9843695780195716e-6,
+      1.5056327351493116e-7};
+
+    if ( real(argument)<0.5 ) {
+       result = pi / (sin(pi*argument)*cgamma(1.0-argument));
+    } else {
+       argument -= 1.0;
+       complex<double> x = p[0];
+       for (int i=1; i<g+2; i++) {
+          x += p[i]/(argument+complex<double>(i,0));
+       }
+       complex<double> t = argument + (g + 0.5);
+       result = sqrt(2*pi) * pow(t,argument+0.5) * exp(-t) * x;
+    }
+
+     
+     
+     
+     
+     
+    
+    
     /* gsl_sf_result result_logradius;
     gsl_sf_result result_phi;
     gsl_sf_lngamma_complex_e(real(argument),imag(argument), &result_logradius, &result_phi);
@@ -84,10 +113,89 @@ complex <double> cgamma (complex <double> argument)
     radius = exp(radius);
     double phi = result_phi.val;
     complex<double> result  = polar(radius,phi);*/
-    cout << "Complex Gamma functions not supported by MKL " << endl;
-    exit(1);
+    // cout << "Complex Gamma functions not supported by MKL " << endl;
+    // exit(1);
     return result;
 }
+
+
+
+complex<double> ccgamma(complex<double> z,int OPT)
+{
+    complex<double> g,z0,z1;
+    double x0,q1,q2,x,y,th,th1,th2,g0,gr,gi,gr1,gi1;
+    double na,t,x1,y1,sr,si;
+    int i,j,k;
+
+    static double a[] = {
+        8.333333333333333e-02,
+       -2.777777777777778e-03,
+        7.936507936507937e-04,
+       -5.952380952380952e-04,
+        8.417508417508418e-04,
+       -1.917526917526918e-03,
+        6.410256410256410e-03,
+       -2.955065359477124e-02,
+        1.796443723688307e-01,
+       -1.39243221690590};
+
+    x = real(z);
+    y = imag(z);
+    if (x > 171) return complex<double>(1e308,0);
+    if ((y == 0.0) && (x == (int)x) && (x <= 0.0))
+        return complex<double>(1e308,0);
+    else if (x < 0.0) {
+        x1 = x;
+        y1 = y;
+        x = -x;
+        y = -y;
+    }
+    x0 = x;
+    if (x <= 7.0) {
+        na = (int)(7.0-x);
+        x0 = x+na;
+    }
+    q1 = sqrt(x0*x0+y*y);
+    th = atan(y/x0);
+    gr = (x0-0.5)*log(q1)-th*y-x0+0.5*log(2.0*M_PI);
+    gi = th*(x0-0.5)+y*log(q1)-y;
+    for (k=0;k<10;k++){
+        t = pow(q1,-1.0-2.0*k);
+        gr += (a[k]*t*cos((2.0*k+1.0)*th));
+        gi -= (a[k]*t*sin((2.0*k+1.0)*th));
+    }
+    if (x <= 7.0) {
+        gr1 = 0.0;
+        gi1 = 0.0;
+        for (j=0;j<na;j++) {
+            gr1 += (0.5*log((x+j)*(x+j)+y*y));
+            gi1 += atan(y/(x+j));
+        }
+        gr -= gr1;
+        gi -= gi1;
+    }
+    if (x1 <= 0.0) {
+        q1 = sqrt(x*x+y*y);
+        th1 = atan(y/x);
+        sr = -sin(M_PI*x)*cosh(M_PI*y);
+        si = -cos(M_PI*x)*sinh(M_PI*y);
+        q2 = sqrt(sr*sr+si*si);
+        th2 = atan(si/sr);
+        if (sr < 0.0) th2 += M_PI;
+        gr = log(M_PI/(q1*q2))-gr;
+        gi = -th1-th2-gi;
+        x = x1;
+        y = y1;
+    }
+    if (OPT == 0) {
+        g0 = exp(gr);
+        gr = g0*cos(gi);
+        gi = g0*sin(gi);
+    }
+    g = complex<double>(gr,gi);
+    return g;
+}
+
 
 
 void Rates::Initialize(Property *options) {
