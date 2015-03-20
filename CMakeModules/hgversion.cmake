83adc410989fe9500f38eecf8849c0eff77241a0
Christoph Junghans <junghans@votca.org>
2015-03-20 15:15:33 -0600

    cmake: allow version from git, too

--- CMakeModules/hgversion.cmake
+++ CMakeModules/hgversion.cmake
@@ -1,12 +1,26 @@
-set(CMAKE_MODULE_PATH ${TOP_SOURCE_DIR}/CMakeModules)
-find_package(Mercurial)
-if (MERCURIAL_FOUND AND IS_DIRECTORY ${TOP_SOURCE_DIR}/.hg)
+if (GIT_EXECUTABLE)
+  #later use git describe here
+  execute_process( COMMAND ${GIT_EXECUTABLE} rev-parse --short HEAD
+    WORKING_DIRECTORY ${TOP_SOURCE_DIR}
+    OUTPUT_VARIABLE THIS_GIT_ID OUTPUT_STRIP_TRAILING_WHITESPACE)
+  execute_process( COMMAND ${GIT_EXECUTABLE} diff-index --name-only HEAD
+    WORKING_DIRECTORY ${TOP_SOURCE_DIR}
+    OUTPUT_VARIABLE _HAS_CHANGES OUTPUT_STRIP_TRAILING_WHITESPACE ERROR_QUIET)
+  if (NOT "${_HAS_CHANGES}" STREQUAL "")
+    set(THIS_GIT_ID "${THIS_GIT_ID} (dirty)")
+  endif()
+  message("Current git revision is ${THIS_GIT_ID}")
+  set(THIS_HG_ID "gitid: ${THIS_GIT_ID}")
+elseif (MERCURIAL_EXECUTABLE)
+  set(CMAKE_MODULE_PATH ${TOP_SOURCE_DIR}/CMakeModules)
+  find_package(Mercurial) #for MERCURIAL_HG_INFO
   MERCURIAL_HG_INFO(${TOP_SOURCE_DIR} THIS)
-  MESSAGE("Current revision is ${THIS_HG_ID}")
+  MESSAGE("Current merurial revision is ${THIS_HG_ID}")
   set (THIS_HG_ID "hgid: ${THIS_HG_ID}")
-else (MERCURIAL_FOUND AND IS_DIRECTORY ${TOP_SOURCE_DIR}/.hg)
+else()
   set (THIS_HG_ID)
-endif (MERCURIAL_FOUND AND IS_DIRECTORY ${TOP_SOURCE_DIR}/.hg)
+endif()
+
 set (HG_HEADER "hgversion.h")
 set (NEW_HG_HEADER "new_hgversion.h")
 file(WRITE ${NEW_HG_HEADER} "static const std::string hgversion = \"${THIS_HG_ID}\";\n")
