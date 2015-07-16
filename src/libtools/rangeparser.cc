a696b774a9af20fef570d2c46e7e89c310d0b2fa
Christoph Junghans <junghans@votca.org>
2015-07-15 18:04:33 -0600

    re-added disable RangeParser constructor

--- src/libtools/rangeparser.cc
+++ src/libtools/rangeparser.cc
@@ -24,10 +24,10 @@
 
 namespace votca { namespace tools {
 
-/*RangeParser::RangeParser()
-    : _has_begin(false) , _has_end(false)
+RangeParser::RangeParser()
+    //: _has_begin(false) , _has_end(false)
 {
-}*/
+}
 
 void RangeParser::Parse(string str)
 {
