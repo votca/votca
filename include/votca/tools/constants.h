ee0af2984db03c7f9c164517c057428e2baef0d5
12AngryMen <jenswehner@gmail.com>
2016-02-11 14:27:07 +0100

    added float constants to constants.h for gwbse

--- include/votca/tools/constants.h
+++ include/votca/tools/constants.h
@@ -34,10 +34,12 @@ namespace votca { namespace tools {
     const double hbar = 6.5821192815E-16; // eV*s
         
         //length conversions
-    
+   //votca xtp-uses for any conversions the following scheme unitA2unitB 
     const double bohr2nm =0.052917721092;
     const double nm2bohr =18.897259886;
     const double ang2bohr = 1.8897259886; 
+    
+    
     
     
     const double ryd2ev= 13.60569253;
@@ -48,6 +50,11 @@ namespace votca { namespace tools {
     
     // energy conversions
     static const double rydtoev = 13.6058;
+    
+    // floats for gwbse
+    
+    const float ryd2ev_f= 13.60569253;
+    
     }
 
 }}
