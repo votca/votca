cc861812f26a85798cbc262b5ac13f50805fc4a0
12AngryMen <jenswehner@gmail.com>
2016-04-18 17:05:27 +0200

    cout to parser

--- src/libxtp/jobcalculators/igwbse.cc
+++ src/libxtp/jobcalculators/igwbse.cc
@@ -147,7 +147,7 @@ std::map<std::string, int> IGWBSE::FillParseMaps(string Mapstring){
         boost::algorithm::split(segmentpnumber , *sit, boost::is_any_of(": "),boost::token_compress_on );
         if (segmentpnumber.size()!=2){
             for (unsigned i=0;i<segmentpnumber.size();i++){
-                cout << segmentnumber[i]<<endl;
+                cout << segmentpnumber[i]<<endl;
             }
             throw runtime_error("Parser igwbse: Segment and exciton labels are not separated properly");
         }
