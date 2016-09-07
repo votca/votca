4572ca80e252818659e5d8960e2f4b0291c0ebd2
wehner <jenswehner@gmail.com>
2016-09-07 18:06:46 +0200

    changed orbitals DMAT functions do not return reference to attribute but simply calculate result

--- include/votca/xtp/orbitals.h
+++ include/votca/xtp/orbitals.h
@@ -299,9 +299,9 @@ public:
 
     
     // functions for calculating density matrices
-    ub::matrix<double> &DensityMatrixGroundState( ub::matrix<double>& _MOs ) ;
-    std::vector<ub::matrix<double> > &DensityMatrixExcitedState( ub::matrix<double>& _MOs , ub::matrix<real_gwbse>& _BSECoefs, int state = 0 ) ;
-    ub::matrix<double > &TransitionDensityMatrix( ub::matrix<double>& _MOs , ub::matrix<real_gwbse>& _BSECoefs, int state = 0);
+    ub::matrix<double> DensityMatrixGroundState( ub::matrix<double>& _MOs ) ;
+    std::vector<ub::matrix<double> > DensityMatrixExcitedState( ub::matrix<double>& _MOs , ub::matrix<real_gwbse>& _BSECoefs, int state = 0 ) ;
+    ub::matrix<double > TransitionDensityMatrix( ub::matrix<double>& _MOs , ub::matrix<real_gwbse>& _BSECoefs, int state = 0);
     
     
     // functions for analyzing fragment charges via Mulliken populations
@@ -470,9 +470,6 @@ private:
     int                                    _couplingsA;
     int                                    _couplingsB;
     
-    ub::matrix<double>                     _dmatGS;
-    std::vector< ub::matrix<double> >      _dmatEX;
-    ub::matrix<double>                     _dmatTS;
     
     std::vector<double>                    _DqS_fragA; // fragment charge changes in exciton
     std::vector<double>                    _DqS_fragB;
