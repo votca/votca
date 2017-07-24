4f85cdcfcf6159dd6701e38837a2b3b5d24d3d6e
Christoph Junghans <junghans@votca.org>
2017-07-24 10:45:30 -0600

    fix compile error with gcc-7

--- src/libxtp/gwbse/gwbseengine.cc
+++ src/libxtp/gwbse/gwbseengine.cc
@@ -184,7 +184,7 @@ namespace votca {
         void GWBSEENGINE::SaveRedirectedLogger(ctp::Logger* pLog) {
 
             // write logger to log file
-            ofstream ofs;
+	    std::ofstream ofs;
             //string gwbse_logfile = "gwbse.log";
             ofs.open(_logger_file.c_str(), ofstream::out);
             if (!ofs.is_open()) {
