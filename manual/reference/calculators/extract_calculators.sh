fcd74c72f27287468cee425b1bd821d1b00598f3
Christoph Junghans <junghans@votca.org>
2017-08-02 09:01:41 -0600

    cmake: fix calculator generation
    For manual build we need to set VOTCASHARE to a folder inside the
    source, however, xtp_* will look for a folder xtp/xml in there,
    which doesn't exist. So moving xml to xtp/xml in share.

--- manual/reference/calculators/extract_calculators.sh
+++ manual/reference/calculators/extract_calculators.sh
@@ -3,6 +3,14 @@
 texfile="$PWD/calculators.tex"
 rm -f $texfile; touch $texfile
 
+die() {
+  echo "$*" >&2
+  exit 1
+}
+[[ -z ${VOTCASHARE} ]] && die "${0##*/}: VOTCASHARE not set"
+[[ -z ${VOTCA_PROPERTY} ]] && die "${0##*/}: VOTCA_PROPERTY not set"
+export VOTCASHARE
+
 for package in xtp_tools xtp_run xtp_parallel xtp_dump; do
 	exe=$(echo "$@" | xargs -n1 echo | grep "$package")
 	calculators="$(${exe} --list | sed -ne 's/^\s\+\([a-z,0-9]*\)\s*\(.*\)/\1/p')"
