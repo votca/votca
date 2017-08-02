51280ae76a4f17e64d2fd3f284b50d50ba437daf
Christoph Junghans <junghans@votca.org>
2017-08-02 07:14:59 -0600

    named it VOTCASHARE everywhere

--- manual/reference/calculators/extract_calculators.sh
+++ manual/reference/calculators/extract_calculators.sh
@@ -6,18 +6,18 @@ rm -f $texfile; touch $texfile
 for package in xtp_tools xtp_run xtp_parallel xtp_dump; do
 	exe=$(echo "$@" | xargs -n1 echo | grep "$package")
 	calculators="$(${exe} --list | sed -ne 's/^\s\+\([a-z,0-9]*\)\s*\(.*\)/\1/p')"
-	echo $calculators
+	echo Found calculators: $calculators
 
 	# loop over all calculators
 	for calculator in ${calculators}; do
-		xmlfile=${CSG_SHARE}/xml/${calculator}.xml
+		xmlfile=${VOTCASHARE}/xml/${calculator}.xml
 		echo $calculator
 		echo $xmlfile
 		if [ ! -f "$xmlfile" ]; then 
 			continue
 		fi
-		echo "votca_property --file $xmlfile --format TEX --level 2"
-		votca_property --file $xmlfile --format TEX --level 2 >> $texfile
+		echo "${VOTCA_PROPERTY} --file $xmlfile --format TEX --level 2"
+		${VOTCA_PROPERTY} --file $xmlfile --format TEX --level 2 >> $texfile
 
 	done
 done
