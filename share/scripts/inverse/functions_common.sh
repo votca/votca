votca/votca

commit ccc2f87fc95b0017736f094bb0f51c3d51c4df91

csg_get_property: avoid endless recursion

share/scripts/inverse/functions_common.sh
@@ -323,7 +323,9 @@ csg_get_property () { #get an property from the xml file
   if [[ -z $ret && -f $VOTCASHARE/xml/csg_defaults.xml ]]; then
     ret="$(critical -q csg_property --file "$VOTCASHARE/xml/csg_defaults.xml" --path "${1}" --short --print . | trim_all)"
     [[ $allow_empty = "yes" && -n "$res" ]] && msg "WARNING: '${FUNCNAME[0]} $1' was called with --allow-empty, but a default was found in '$VOTCASHARE/xml/csg_defaults.xml'"
-    local sim_prog="$(csg_get_property cg.inverse.program)" #no problem to call recursively as sim_prog has a default
+    #avoid endless recursion
+    [[ $1 = cg.inverse.program && -n $ret ]] || sim_prog="$ret" \
+      sim_prog="$(csg_get_property cg.inverse.program)" #no problem to call recursively as sim_prog has a default
     if [[ -z $ret ]] && [[ $1 = *${sim_prog}* ]]; then
       local path=${1/${sim_prog}/sim_prog}
       ret="$(critical -q csg_property --file "$VOTCASHARE/xml/csg_defaults.xml" --path "${path}" --short --print . | trim_all)"

commit 0b642d9d167601cfd4ebe8a8a4740354a406cc17

csg_get_property: fix default fetching for sim_prog
This issue was introduced in ccc2f87fc95b0017736f094bb0f51c3d51c4df91
but never realized.

share/scripts/inverse/functions_common.sh
@@ -344,7 +344,7 @@ csg_get_property () { #get an property from the xml file
     ret="$(critical -q csg_property --file "${VOTCA_CSG_DEFAULTS}" --path "${1}" --short --print . | trim_all)"
     [[ $allow_empty = "yes" && -n "$res" ]] && msg "WARNING: '${FUNCNAME[0]} $1' was called with --allow-empty, but a default was found in '${VOTCA_CSG_DEFAULTS}'"
     #avoid endless recursion
-    [[ $1 = cg.inverse.program && -n $ret ]] || sim_prog="$ret" \
+    [[ $1 = cg.inverse.program && -n $ret ]] && sim_prog="$ret" || \
       sim_prog="$(csg_get_property cg.inverse.program)" #no problem to call recursively as sim_prog has a default
     if [[ -z $ret ]] && [[ $1 = *${sim_prog}* ]]; then
       local path=${1/${sim_prog}/sim_prog}