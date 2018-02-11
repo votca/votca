488ec04bc58789eb7de1f019be3f587a62b15f7c
JoshuaSBrown <joshbro42867@yahoo.com>
2018-02-11 11:05:34 +0000

    1. Added .clang-format file to testing folder 2. Switched testing method to BOOST framework 3. Updated CMakeLists.txt files as necessary to utilize the boost testing framework.

--- src/tests/test_elements.cc
+++ src/tests/test_elements.cc
@@ -1,84 +1,50 @@
+/*
+ * Copyright 2009-2018 The VOTCA Development Team (http://www.votca.org)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
 
-#include <cassert>
+#define BOOST_TEST_MAIN
+
+#define BOOST_TEST_MODULE elements_test
+#include <boost/test/unit_test.hpp>
 #include <exception>
-#include <iostream>
-#include <string>
+
 #include <votca/tools/elements.h>
 
 using namespace std;
 using namespace votca::tools;
 
-int main(void) {
+BOOST_AUTO_TEST_SUITE(elements_test)
 
-  cout << "Testing: constructor" << endl;
-  { Elements ele; }
+BOOST_AUTO_TEST_CASE(constructors_test) { Elements ele; }
 
-  cout << "Testing: getVdWChelpG" << endl;
-  {
-    Elements ele;
-    assert(ele.getVdWChelpG("H") == 1.45);
-    bool test = false;
-    try {
-      ele.getVdWChelpG("Blah");
-    } catch (...) {
-      test = true;
-    }
-    assert(test);
-  }
+BOOST_AUTO_TEST_CASE(accessors_test) {
+  Elements ele;
+  BOOST_CHECK_EQUAL(ele.getVdWChelpG("H"), 1.45);
+  BOOST_CHECK_THROW(ele.getVdWChelpG("Blah"), invalid_argument);
 
-  cout << "Testing: getNucCrgECP" << endl;
-  {
-    Elements ele;
-    bool     test = false;
-    try {
-      ele.getNucCrgECP("He");
-    } catch (...) {
-      test = true;
-    }
-    assert(test);
-  }
+  BOOST_CHECK_THROW(ele.getNucCrgECP("He"), invalid_argument);
 
-  cout << "Testing: getMass" << endl;
-  {
-    Elements ele;
-    assert(ele.getMass("K") = 39.098);
-  }
+  BOOST_CHECK_EQUAL(ele.getMass("K"), 39.098);
+  BOOST_CHECK_EQUAL(ele.getEleNum("Li"), 3);
 
-  cout << "Testing: getEleNum" << endl;
-  {
-    Elements ele;
-    assert(ele.getEleNum("Li") == 3);
-  }
-
-  cout << "Testing: getEleName" << endl;
-  {
-    Elements ele;
-    assert(ele.getEleName(17) == "Cl");
-  }
-
-  cout << "Testing: getEleShort" << endl;
-  {
-    Elements ele;
-    assert(ele.getEleShort("MAGNESIUM") == "Mg");
-  }
-
-  cout << "Testing: getEleFull" << endl;
-  {
-    Elements ele;
-    assert(ele.getEleFull("Ge") == "GERMANIUM");
-  }
-
-  cout << "Testing: getVdWMK" << endl;
-  {
-    Elements ele;
-    assert(ele.getVdWMK("F") == 1.35);
-    bool test = false;
-    try {
-      ele.getVdWMK("Pb");
-    } catch (...) {
-      test = true;
-    }
-    assert(test);
-  }
-  return 0;
+  BOOST_CHECK_EQUAL(ele.getEleName(17), "Cl");
+  BOOST_CHECK_EQUAL(ele.getEleShort("MAGNESIUM"), "Mg");
+  BOOST_CHECK_EQUAL(ele.getEleFull("Ge"), "GERMANIUM");
+  BOOST_CHECK_EQUAL(ele.getVdWMK("F"), 1.35);
+  BOOST_CHECK_THROW(ele.getVdWMK("Pb"), invalid_argument);
 }
+
+BOOST_AUTO_TEST_SUITE_END()
