ae3f1db7f540e4df282947f1206c6b7797570c72
Christoph Junghans <junghans@votca.org>
2018-03-09 15:38:11 -0700

    fix EIGEN3 detection

--- CMakeModules/FindEIGEN3.cmake
+++ CMakeModules/FindEIGEN3.cmake
@@ -126,7 +126,7 @@ endif (EIGEN3_INCLUDE_DIR)
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(EIGEN3 REQUIRED_VARS EIGEN3_INCLUDE_DIR VERSION_VAR EIGEN3_VERSION)
 
-mark_as_advanced(EIGEN3_INCLUDE_DIR)
+mark_as_advanced(EIGEN3_INCLUDE_DIR EIGEN3_VERSION)
 
 
 
