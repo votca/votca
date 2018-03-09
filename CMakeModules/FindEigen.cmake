9c9889e016cd693451667cc59fc27f68ba2e2ddf
Jens Wehner <jenswehner@gmail.com>
2018-03-09 17:36:10 +0100

    fixed eigen cmake

--- CMakeModules/FindEigen.cmake
+++ CMakeModules/FindEigen.cmake
@@ -52,21 +52,81 @@
 
 find_package(PkgConfig)
 pkg_check_modules(PC_EIGEN eigen3)
-set(EIGEN_DEFINITIONS ${PC_EIGEN_CFLAGS_OTHER})
-find_path(EIGEN_INCLUDE_DIR Eigen/Core
-    HINTS ${PC_EIGEN_INCLUDEDIR} ${PC_EIGEN_INCLUDE_DIRS}
-          "${Eigen_ROOT_DIR}" "$ENV{EIGEN_ROOT_DIR}"
-          "${EIGEN_ROOT}" "$ENV{EIGEN_ROOT}"  # Backwards Compatibility
-    )
 
-set(EIGEN_INCLUDE_DIRS ${EIGEN_INCLUDE_DIR})
+
+find_package(Eigen3 QUIET NO_MODULE PATHS ${PC_EIGEN_PREFIX}/ NO_DEFAULT_PATH)
+if (NOT EIGEN3_FOUND)
+  # Search user-installed locations first, so that we prefer user installs
+  # to system installs where both exist.
+  list(APPEND EIGEN3_CHECK_INCLUDE_DIRS
+    /usr/local/include
+    /usr/local/homebrew/include # Mac OS X
+    /opt/local/var/macports/software # Mac OS X.
+    /opt/local/include
+    /usr/include)
+  # Additional suffixes to try appending to each search path.
+  list(APPEND EIGEN3_CHECK_PATH_SUFFIXES
+    eigen3 # Default root directory for Eigen.
+    Eigen/include/eigen3 # Windows (for C:/Program Files prefix) < 3.3
+    Eigen3/include/eigen3 ) # Windows (for C:/Program Files prefix) >= 3.3
+
+  # Search supplied hint directories first if supplied.
+  find_path(EIGEN3_INCLUDE_DIR
+    NAMES Eigen/Core
+    HINTS ${EIGEN3_INCLUDE_DIR_HINTS}
+    PATHS ${EIGEN3_CHECK_INCLUDE_DIRS}
+    PATH_SUFFIXES ${EIGEN3_CHECK_PATH_SUFFIXES})
+
+  if (NOT EIGEN3_INCLUDE_DIR OR
+      NOT EXISTS ${EIGEN3_INCLUDE_DIR})
+    message(ERROR 
+      "Could not find eigen3 include directory, set EIGEN_INCLUDE_DIR to "
+      "path to eigen3 include directory, e.g. /usr/local/include/eigen3.")
+  endif (NOT EIGEN3_INCLUDE_DIR OR
+    NOT EXISTS ${EIGEN3_INCLUDE_DIR})
+
+  # Mark internally as found, then verify. EIGEN_REPORT_NOT_FOUND() unsets
+  # if called.
+  set(EIGEN3_FOUND TRUE)
+endif()
+
+
+
+if (EIGEN3_INCLUDE_DIR)
+  set(EIGEN3_VERSION_FILE ${EIGEN3_INCLUDE_DIR}/Eigen/src/Core/util/Macros.h)
+  if (NOT EXISTS ${EIGEN3_VERSION_FILE})
+    message(ERROR
+      "Could not find file: ${EIGEN3_VERSION_FILE} "
+      "containing version information in Eigen install located at: "
+      "${EIGEN3_INCLUDE_DIR}.")
+  else (NOT EXISTS ${EIGEN3_VERSION_FILE})
+    file(READ ${EIGEN3_VERSION_FILE} EIGEN3_VERSION_FILE_CONTENTS)
+
+    string(REGEX MATCH "#define EIGEN_WORLD_VERSION [0-9]+"
+      EIGEN3_WORLD_VERSION "${EIGEN3_VERSION_FILE_CONTENTS}")
+    string(REGEX REPLACE "#define EIGEN_WORLD_VERSION ([0-9]+)" "\\1"
+      EIGEN3_WORLD_VERSION "${EIGEN3_WORLD_VERSION}")
+
+    string(REGEX MATCH "#define EIGEN_MAJOR_VERSION [0-9]+"
+      EIGEN3_MAJOR_VERSION "${EIGEN3_VERSION_FILE_CONTENTS}")
+    string(REGEX REPLACE "#define EIGEN_MAJOR_VERSION ([0-9]+)" "\\1"
+      EIGEN3_MAJOR_VERSION "${EIGEN3_MAJOR_VERSION}")
+
+    string(REGEX MATCH "#define EIGEN_MINOR_VERSION [0-9]+"
+      EIGEN3_MINOR_VERSION "${EIGEN3_VERSION_FILE_CONTENTS}")
+    string(REGEX REPLACE "#define EIGEN_MINOR_VERSION ([0-9]+)" "\\1"
+      EIGEN3_MINOR_VERSION "${EIGEN3_MINOR_VERSION}")
+
+    # This is on a single line s/t CMake does not interpret it as a list of
+    # elements and insert ';' separators which would result in 3.;2.;0 nonsense.
+    set(EIGEN3_VERSION "${EIGEN3_WORLD_VERSION}.${EIGEN3_MAJOR_VERSION}.${EIGEN3_MINOR_VERSION}")
+  endif (NOT EXISTS ${EIGEN3_VERSION_FILE})
+endif (EIGEN3_INCLUDE_DIR)
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(Eigen DEFAULT_MSG EIGEN_INCLUDE_DIR)
+find_package_handle_standard_args(EIGEN3 DEFAULT_MSG EIGEN3_INCLUDE_DIR)
 
-mark_as_advanced(EIGEN_INCLUDE_DIR)
+mark_as_advanced(EIGEN3_INCLUDE_DIR)
 
-set(Eigen_INCLUDE_DIRS ${EIGEN_INCLUDE_DIRS})
-set(Eigen_FOUND ${EIGEN_FOUND})
-set(Eigen_VERSION ${EIGEN_VERSION})
-set(Eigen_DEFINITIONS ${EIGEN_DEFINITIONS})
+
+
