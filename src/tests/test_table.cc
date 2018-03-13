2bfe6a605b54ad350826149f3c83329026d2535f
JoshuaSBrown <joshbro42867@yahoo.com>
2018-03-13 23:58:07 +0000

    Formatted test_table

--- src/tests/test_table.cc
+++ src/tests/test_table.cc
@@ -28,55 +28,55 @@ using namespace votca::tools;
 
 BOOST_AUTO_TEST_SUITE(table_test)
 
-BOOST_AUTO_TEST_CASE(create_test) {
-  Table tb;
-}
+BOOST_AUTO_TEST_CASE(create_test) { Table tb; }
 
 BOOST_AUTO_TEST_CASE(size_test) {
   Table tb;
-  BOOST_CHECK_EQUAL(tb.size(),0);
+  BOOST_CHECK_EQUAL(tb.size(), 0);
 }
 
-BOOST_AUTO_TEST_CASE(pushback_test){
+BOOST_AUTO_TEST_CASE(pushback_test) {
   Table tb;
-  for(double x=0;x<10;++x){
-    double y = 2*x;
-    tb.push_back(x,y);
+  for (double x = 0; x < 10; ++x) {
+    double y = 2 * x;
+    tb.push_back(x, y);
   }
-  BOOST_CHECK_EQUAL(tb.size(),10);
-} 
+  BOOST_CHECK_EQUAL(tb.size(), 10);
+}
 
-BOOST_AUTO_TEST_CASE(xy_test){
+BOOST_AUTO_TEST_CASE(xy_test) {
   Table tb;
-  for(double x=0;x<10;++x){
-    double y = 2*x;
-    tb.push_back(x,y);
+  for (double x = 0; x < 10; ++x) {
+    double y = 2 * x;
+    tb.push_back(x, y);
   }
 
   auto x_v = tb.x();
   auto y_v = tb.y();
-  for(int i=0;i<10;++i){
+  for (int i = 0; i < 10; ++i) {
     int x = i;
-    int y = 2*x;
-    BOOST_CHECK_EQUAL(boost::lexical_cast<int>(x_v(i)),boost::lexical_cast<int>(tb.x(i)));    
-    BOOST_CHECK_EQUAL(boost::lexical_cast<int>(y_v(i)),boost::lexical_cast<int>(tb.y(i)));    
-    BOOST_CHECK_EQUAL(x,boost::lexical_cast<int>(tb.x(i)));    
-    BOOST_CHECK_EQUAL(y,boost::lexical_cast<int>(tb.y(i)));    
-    BOOST_CHECK_EQUAL(x,boost::lexical_cast<int>(x_v(i)));    
-    BOOST_CHECK_EQUAL(y,boost::lexical_cast<int>(y_v(i)));    
+    int y = 2 * x;
+    BOOST_CHECK_EQUAL(boost::lexical_cast<int>(x_v(i)),
+                      boost::lexical_cast<int>(tb.x(i)));
+    BOOST_CHECK_EQUAL(boost::lexical_cast<int>(y_v(i)),
+                      boost::lexical_cast<int>(tb.y(i)));
+    BOOST_CHECK_EQUAL(x, boost::lexical_cast<int>(tb.x(i)));
+    BOOST_CHECK_EQUAL(y, boost::lexical_cast<int>(tb.y(i)));
+    BOOST_CHECK_EQUAL(x, boost::lexical_cast<int>(x_v(i)));
+    BOOST_CHECK_EQUAL(y, boost::lexical_cast<int>(y_v(i)));
   }
-} 
+}
 
-BOOST_AUTO_TEST_CASE(getMinMax_test){
+BOOST_AUTO_TEST_CASE(getMinMax_test) {
   Table tb;
-  for(double x=0;x<10;++x){
-    double y = 2*x;
-    tb.push_back(x,y);
+  for (double x = 0; x < 10; ++x) {
+    double y = 2 * x;
+    tb.push_back(x, y);
   }
-  BOOST_CHECK_EQUAL(boost::lexical_cast<int>(tb.getMinX()),0);
-  BOOST_CHECK_EQUAL(boost::lexical_cast<int>(tb.getMaxX()),9);
-  BOOST_CHECK_EQUAL(boost::lexical_cast<int>(tb.getMinY()),0);
-  BOOST_CHECK_EQUAL(boost::lexical_cast<int>(tb.getMaxY()),18);
+  BOOST_CHECK_EQUAL(boost::lexical_cast<int>(tb.getMinX()), 0);
+  BOOST_CHECK_EQUAL(boost::lexical_cast<int>(tb.getMaxX()), 9);
+  BOOST_CHECK_EQUAL(boost::lexical_cast<int>(tb.getMinY()), 0);
+  BOOST_CHECK_EQUAL(boost::lexical_cast<int>(tb.getMaxY()), 18);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
