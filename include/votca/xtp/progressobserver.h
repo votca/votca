1da4e3089c5507ef5b83f2ac76cb1067bf1dc038
JoshuaSBrown <josh@votca.org>
2018-04-09 21:09:53 -0600

    Removed using namespace std, used full scope for string,vector and map objects in progressobserver.h

--- include/votca/xtp/progressobserver.h
+++ include/votca/xtp/progressobserver.h
@@ -29,8 +29,6 @@
 #include <boost/interprocess/sync/file_lock.hpp>
 #include <boost/program_options.hpp>
 
-using namespace std;
-
 namespace votca { namespace xtp {
     
 class QMThread;
@@ -48,7 +46,7 @@ class ProgObserver
 public:
     
     typedef typename JobContainer::iterator JobItCnt;
-    typedef typename vector<pJob>::iterator JobItVec;
+    typedef typename std::vector<pJob>::iterator JobItVec;
     
     ProgObserver()
         : _lockFile("__NOFILE__"), _progFile("__NOFILE__"), _cacheSize(-1),
@@ -57,7 +55,7 @@ public:
    ~ProgObserver() { ; }
     
     void InitCmdLineOpts(const boost::program_options::variables_map &optsMap);
-    void InitFromProgFile(string progFile, QMThread *master);   
+    void InitFromProgFile(std::string progFile, QMThread *master);   
     pJob RequestNextJob(QMThread *thread);
     void ReportJobDone(pJob job, rJob *res, QMThread *thread);
     
@@ -65,27 +63,27 @@ public:
     void LockProgFile(QMThread *thread);
     void ReleaseProgFile(QMThread *thread);
     
-    string GenerateHost(QMThread *thread);
-    string GenerateTime();
+    std::string GenerateHost(QMThread *thread);
+    std::string GenerateTime();
    
    
 private:    
     
-    string _lockFile;
-    string _progFile;
+    std::string _lockFile;
+    std::string _progFile;
     int _cacheSize;
     JobContainer _jobs;
     
-    vector<pJob> _jobsToProc;
-    vector<pJob> _jobsToSync;
+    std::vector<pJob> _jobsToProc;
+    std::vector<pJob> _jobsToSync;
     
     JobItVec _nextjit;
     JobItCnt _metajit;
     Mutex _lockThread;
     boost::interprocess::file_lock *_flock;
     
-    map<string,bool> _restart_hosts;
-    map<string,bool> _restart_stats;
+    std::map<std::string,bool> _restart_hosts;
+    std::map<std::string,bool> _restart_stats;
     bool _restartMode;
     int _jobsReported;
 
@@ -99,13 +97,13 @@ private:
 
 
 template<typename JobContainer, typename pJob, typename rJob>
-JobContainer LOAD_JOBS(const string &xml_file);
+JobContainer LOAD_JOBS(const std::string &xml_file);
 
 template<typename JobContainer, typename pJob, typename rJob>
-void WRITE_JOBS(JobContainer &jobs, const string &job_file, string fileformat);
+void WRITE_JOBS(JobContainer &jobs, const std::string &job_file, std::string fileformat);
 
 template<typename JobContainer, typename pJob, typename rJob>
-void UPDATE_JOBS(JobContainer &from, JobContainer &to, string thisHost);
+void UPDATE_JOBS(JobContainer &from, JobContainer &to, std::string thisHost);
     
     
     
