4e07e20041bc6effa3c6ed0478b98e760159f7a4
JoshuaSBrown <josh@votca.org>
2018-04-09 21:07:49 -0600

    Updated namespace, includes, as well as assigned full scope to vector, string and ostream objects

--- include/votca/xtp/polartop.h
+++ include/votca/xtp/polartop.h
@@ -19,16 +19,15 @@
 /// For an earlier history see ctp repo commit 77795ea591b29e664153f9404c8655ba28dc14e9
 
 
-#ifndef __POLARTOP__H
-#define	__POLARTOP__H
+#ifndef __VOTCA_XTP_POLARTOP_H
+#define	__VOTVA_XTP_POLARTOP_H
 
-#include <votca/ctp/polarseg.h>
-#include <votca/ctp/topology.h>
+#include <votca/xtp/polarseg.h>
+#include <votca/xtp/topology.h>
 #include <boost/serialization/vector.hpp>
 #include <boost/serialization/version.hpp>
 
-namespace votca { namespace ctp {
-
+namespace votca { namespace xtp {
     
 class PolarTop
 {
@@ -40,41 +39,41 @@ public:
   ~PolarTop();
    
    // QM-MM-MM
-   vector<PolarSeg*> &QM0() { return _qm0; }
-   vector<PolarSeg*> &MM1() { return _mm1; }
-   vector<PolarSeg*> &MM2() { return _mm2; }   
+   std::vector<PolarSeg*> &QM0() { return _qm0; }
+   std::vector<PolarSeg*> &MM1() { return _mm1; }
+   std::vector<PolarSeg*> &MM2() { return _mm2; }   
    
-   void   setQM0(vector<PolarSeg*> &qm0, bool clean = true) { _qm0 = qm0; _clean_qm0 = clean; }
-   void   setMM1(vector<PolarSeg*> &mm1, bool clean = true) { _mm1 = mm1; _clean_mm1 = clean; }
-   void   setMM2(vector<PolarSeg*> &mm2, bool clean = true) { _mm2 = mm2; _clean_mm2 = clean; }
+   void   setQM0(std::vector<PolarSeg*> &qm0, bool clean = true) { _qm0 = qm0; _clean_qm0 = clean; }
+   void   setMM1(std::vector<PolarSeg*> &mm1, bool clean = true) { _mm1 = mm1; _clean_mm1 = clean; }
+   void   setMM2(std::vector<PolarSeg*> &mm2, bool clean = true) { _mm2 = mm2; _clean_mm2 = clean; }
    
-   void   setSegsQM0(vector<Segment*> &qm0) { _segs_qm0 = qm0; }
-   void   setSegsMM1(vector<Segment*> &mm1) { _segs_mm1 = mm1; }
-   void   setSegsMM2(vector<Segment*> &mm2) { _segs_mm2 = mm2; }
+   void   setSegsQM0(std::vector<Segment*> &qm0) { _segs_qm0 = qm0; }
+   void   setSegsMM1(std::vector<Segment*> &mm1) { _segs_mm1 = mm1; }
+   void   setSegsMM2(std::vector<Segment*> &mm2) { _segs_mm2 = mm2; }
    
    // EWALD FGC-FGN-BGN
-   vector<PolarSeg*> &BGN() { return _bgN; }
-   vector<PolarSeg*> &FGN() { return _fgN; }
-   vector<PolarSeg*> &FGC() { return _fgC; }
+   std::vector<PolarSeg*> &BGN() { return _bgN; }
+   std::vector<PolarSeg*> &FGN() { return _fgN; }
+   std::vector<PolarSeg*> &FGC() { return _fgC; }
    
-   void   setBGN(vector<PolarSeg*> &bgN, bool clean = true) { _bgN = bgN; _clean_bgN = clean; }
-   void   setFGN(vector<PolarSeg*> &fgN, bool clean = true) { _fgN = fgN; _clean_fgN = clean; }
-   void   setFGC(vector<PolarSeg*> &fgC, bool clean = true) { _fgC = fgC; _clean_fgC = clean; }
+   void   setBGN(std::vector<PolarSeg*> &bgN, bool clean = true) { _bgN = bgN; _clean_bgN = clean; }
+   void   setFGN(std::vector<PolarSeg*> &fgN, bool clean = true) { _fgN = fgN; _clean_fgN = clean; }
+   void   setFGC(std::vector<PolarSeg*> &fgC, bool clean = true) { _fgC = fgC; _clean_fgC = clean; }
    
-   void   setSegsBGN(vector<Segment*> &bgN) { _segs_bgN = bgN; }
-   void   setSegsFGN(vector<Segment*> &fgN) { _segs_fgN = fgN; }
-   void   setSegsFGC(vector<Segment*> &fgC) { _segs_fgC = fgC; }
+   void   setSegsBGN(std::vector<Segment*> &bgN) { _segs_bgN = bgN; }
+   void   setSegsFGN(std::vector<Segment*> &fgN) { _segs_fgN = fgN; }
+   void   setSegsFGC(std::vector<Segment*> &fgC) { _segs_fgC = fgC; }
    
    // TRANSFORMATIONS & POINT OF REFERENCE
    void   Translate(const vec &shift);
    void   CenterAround(const vec &center);
    vec    getCenter() { return _center; }
    
-   string ShellInfoStr();
-   void   PrintInfo(ostream &out);
-   void   PrintPDB(string outfile);
-   void   PrintInduState(string out, string format, bool split, double space);
-   void   PrintInduState(FILE *out,  string format, bool split, double space);
+   std::string ShellInfoStr();
+   void   PrintInfo(std::ostream &out);
+   void   PrintPDB(std::string outfile);
+   void   PrintInduState(std::string out, std::string format, bool split, double space);
+   void   PrintInduState(FILE *out,  std::string format, bool split, double space);
    
    // POLARIZATION STATE
    void   setPolarizationIter(int iter, bool converged) { 
@@ -98,8 +97,8 @@ public:
    }
    
     void RemoveAllOwnership();
-    void LoadFromDrive(string archfile);
-    void SaveToDrive(string archfile);
+    void LoadFromDrive(std::string archfile);
+    void SaveToDrive(std::string archfile);
     
 private:
     
@@ -107,26 +106,26 @@ private:
    vec _center;    // => Set in ::CenterAround(...)
    
    // QM-MM-MM
-   vector<PolarSeg*> _qm0; // QM SHELL
-   vector<PolarSeg*> _mm1; // POLARIZABLE SHELL
-   vector<PolarSeg*> _mm2; // STATIC SHELL
+   std::vector<PolarSeg*> _qm0; // QM SHELL
+   std::vector<PolarSeg*> _mm1; // POLARIZABLE SHELL
+   std::vector<PolarSeg*> _mm2; // STATIC SHELL
    
-   vector<Segment*> _segs_qm0;
-   vector<Segment*> _segs_mm1;
-   vector<Segment*> _segs_mm2;
+   std::vector<Segment*> _segs_qm0;
+   std::vector<Segment*> _segs_mm1;
+   std::vector<Segment*> _segs_mm2;
    
    bool _clean_qm0;
    bool _clean_mm1;
    bool _clean_mm2;
    
    // EWALD FGC-FGN-BGN
-   vector<PolarSeg*> _bgN; // Neutral background
-   vector<PolarSeg*> _fgN; // Neutral foreground
-   vector<PolarSeg*> _fgC; // Charged foreground
+   std::vector<PolarSeg*> _bgN; // Neutral background
+   std::vector<PolarSeg*> _fgN; // Neutral foreground
+   std::vector<PolarSeg*> _fgC; // Charged foreground
    
-   vector<Segment*> _segs_bgN;
-   vector<Segment*> _segs_fgN;
-   vector<Segment*> _segs_fgC;
+   std::vector<Segment*> _segs_bgN;
+   std::vector<Segment*> _segs_fgN;
+   std::vector<Segment*> _segs_fgC;
    
    bool _clean_bgN;
    bool _clean_fgN;
@@ -139,9 +138,4 @@ private:
 
 }}
 
-#endif
-
-
-
-
-
+#endif // __VOTCA_XTP_POLARTOP_H
