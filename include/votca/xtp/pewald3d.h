e99d9451997de3d603f63b751127a373e4ae63e5
JoshuaSBrown <josh@votca.org>
2018-04-09 19:15:37 -0600

    Updated pewald3d.h header guard, includes and namespace

--- include/votca/xtp/pewald3d.h
+++ include/votca/xtp/pewald3d.h
@@ -18,30 +18,29 @@
  */
 /// For an earlier history see ctp repo commit 77795ea591b29e664153f9404c8655ba28dc14e9
 
-
-#ifndef VOTCA_CTP_PEWALD3D_H
-#define VOTCA_CTP_PEWALD3D_H
+#ifndef __VOTCA_XTP_PEWALD3D_H
+#define __VOTCA_XTP_PEWALD3D_H
 
 #include <votca/csg/boundarycondition.h>
-#include <votca/ctp/polartop.h>
-#include <votca/ctp/xjob.h>
-#include <votca/ctp/qmthread.h>
-#include <votca/ctp/ewaldnd.h>
+#include <votca/xtp/polartop.h>
+#include <votca/xtp/xjob.h>
+#include <votca/xtp/qmthread.h>
+#include <votca/xtp/ewaldnd.h>
 
 namespace CSG = votca::csg;
 
-namespace votca { namespace ctp {
+namespace votca { namespace xtp {
     
-    // NOTE: This is not a conventional 3D Ewald summation, so use carefully
-    //       (tuned for the purpose of site-energy calculations)
-    // NOTE: PolarTop should be set-up with three containers: FGC, FGN, BGN
-    //       MGN is set-up in constructor using the real-space c/o (from input)
-    //       The topology is used to retrieve information on the sim. box (PB)
-    //       All polar segments should be positioned as nearest images of the
-    //       foreground charge density (FGC, FGN).
-    //       All polar segments should be appropriately charged (Q00).
-    // NOTE: The k-shell grouping algorithm can fail for strongly skewed boxes.
-    //        
+    /// NOTE: This is not a conventional 3D Ewald summation, so use carefully
+    ///       (tuned for the purpose of site-energy calculations)
+    /// NOTE: PolarTop should be set-up with three containers: FGC, FGN, BGN
+    ///       MGN is set-up in constructor using the real-space c/o (from input)
+    ///       The topology is used to retrieve information on the sim. box (PB)
+    ///       All polar segments should be positioned as nearest images of the
+    ///       foreground charge density (FGC, FGN).
+    ///       All polar segments should be appropriately charged (Q00).
+    /// NOTE: The k-shell grouping algorithm can fail for strongly skewed boxes.
+    ///        
     
     class PEwald3D3D : public Ewald3DnD
     {
@@ -87,4 +86,5 @@ namespace votca { namespace ctp {
 }}
 
 
-#endif
+#endif // __VOTCA_XTP_PEWALD3D_H
+
