cf6c57d69809cae520f6d00d1386edf55bc6a708
JoshuaSBrown <josh@votca.org>
2018-06-16 00:59:16 -0600

    Added formatting to graph.cc

--- src/libtools/graph.cc
+++ src/libtools/graph.cc
@@ -17,7 +17,6 @@
  *
  */
 
-#
 #include <algorithm>
 #include <string>
 #include <votca/tools/graph.h>
@@ -29,14 +28,14 @@ namespace tools {
 
 class GraphNode;
 
-void Graph::updateIds_(Graph& g){
-  if(!this->id_set_){
+void Graph::updateIds_(Graph& g) {
+  if (!this->id_set_) {
     this->calcId_();
-    this->id_set_=true;
+    this->id_set_ = true;
   }
-  if(!g.id_set_){
+  if (!g.id_set_) {
     g.calcId_();
-    g.id_set_=true;
+    g.id_set_ = true;
   }
 }
 
@@ -45,75 +44,69 @@ bool Graph::operator!=(Graph& g) {
   return id_.compare(g.id_);
 }
 
-bool Graph::operator==( Graph& g) {
-  return !(*(this)!=g);  
-}
+bool Graph::operator==(Graph& g) { return !(*(this) != g); }
 
-Graph& Graph::operator=(const Graph &g){
+Graph& Graph::operator=(const Graph& g) {
   this->adj_list_ = g.adj_list_;
-  for( auto pr : g.nodes_){
+  for (auto pr : g.nodes_) {
     this->nodes_[pr.first] = pr.second;
-  } 
+  }
   this->id_ = g.id_;
   return *this;
 }
 
-vector<pair<int, GraphNode>> Graph::getIsolatedNodes(void){
-  vector<pair<int,GraphNode>> iso_nodes;
-  for(auto node : nodes_ ){
-    if(adj_list_.count(node.first)){
-      if(adj_list_[node.first].size()==0){
-        pair<int,GraphNode> pr(node.first,node.second);
+vector<pair<int, GraphNode>> Graph::getIsolatedNodes(void) {
+  vector<pair<int, GraphNode>> iso_nodes;
+  for (auto node : nodes_) {
+    if (adj_list_.count(node.first)) {
+      if (adj_list_[node.first].size() == 0) {
+        pair<int, GraphNode> pr(node.first, node.second);
         iso_nodes.push_back(pr);
       }
-    }else{
-      pair<int,GraphNode> pr(node.first,node.second);
+    } else {
+      pair<int, GraphNode> pr(node.first, node.second);
       iso_nodes.push_back(pr);
     }
   }
   return iso_nodes;
 }
 
-vector<int> Graph::getVerticesMissingNodes(void){
+vector<int> Graph::getVerticesMissingNodes(void) {
   vector<int> missing;
-  for(auto pr_v : adj_list_){
-    if(nodes_.count(pr_v.first)==0){
+  for (auto pr_v : adj_list_) {
+    if (nodes_.count(pr_v.first) == 0) {
       missing.push_back(pr_v.first);
     }
   }
   return missing;
 }
 
-GraphNode& Graph::Node(int vert){
-  return nodes_[vert];
-}
+GraphNode& Graph::Node(int vert) { return nodes_[vert]; }
 
-GraphNode Graph::getNode(int vert){
-  return nodes_[vert];
-}
+GraphNode Graph::getNode(int vert) { return nodes_[vert]; }
 
-vector<pair<int,GraphNode>> Graph::getNodes(void){
-  vector<pair<int,GraphNode>> vec_nodes;
-  for(auto pr_node : nodes_ ){
+vector<pair<int, GraphNode>> Graph::getNodes(void) {
+  vector<pair<int, GraphNode>> vec_nodes;
+  for (auto pr_node : nodes_) {
     vec_nodes.push_back(pr_node);
-  } 
+  }
   return vec_nodes;
 }
 
-void Graph::calcId_(){
+void Graph::calcId_() {
   auto nodes = getNodes();
-  sort(nodes.begin(),nodes.end(),cmpVertNodePairStrIdLessThan);
+  sort(nodes.begin(), nodes.end(), cmpVertNode);
   string struct_Id_temp = "";
-  for( auto nd_pr : nodes ){
+  for (auto nd_pr : nodes) {
     struct_Id_temp.append(nd_pr.second.getStringId());
   }
-  id_=struct_Id_temp;
+  id_ = struct_Id_temp;
   return;
 }
 
-ostream& operator<<(ostream& os, const Graph g){
+ostream& operator<<(ostream& os, const Graph g) {
   os << "Graph" << endl;
-  for( auto p_gn : g.nodes_ ) {
+  for (auto p_gn : g.nodes_) {
     os << "Node " << p_gn.first << endl;
     os << p_gn.second << endl;
   }
@@ -121,18 +114,16 @@ ostream& operator<<(ostream& os, const Graph g){
 }
 
 string Graph::getId(void) {
-  if(!this->id_set_){
+  if (!this->id_set_) {
     this->calcId_();
-    this->id_set_=true;
+    this->id_set_ = true;
   }
   return id_;
 }
 
-bool cmpVertNodePairStrIdLessThan(pair<int,GraphNode> gn1_pr, pair<int,GraphNode> gn2_pr) {
+bool cmpVertNodePair(pair<int, GraphNode> gn1_pr, pair<int, GraphNode> gn2_pr) {
   string str1_Id = gn1_pr.second.getStringId();
-  return str1_Id.compare(gn2_pr.second.getStringId())<0;
-}
-
+  return str1_Id.compare(gn2_pr.second.getStringId()) < 0;
 }
 }
-
+}
