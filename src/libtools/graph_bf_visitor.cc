3bf167f0136981258aa762a8135bed7c32cc4434
JoshuaSBrown <josh@votca.org>
2018-06-16 01:04:00 -0600

    Formatted

--- src/libtools/graph_bf_visitor.cc
+++ src/libtools/graph_bf_visitor.cc
@@ -17,72 +17,66 @@
  *
  */
 
+#include <votca/tools/edge.h>
+#include <votca/tools/graph.h>
 #include <votca/tools/graph_bf_visitor.h>
 #include <votca/tools/graphnode.h>
-#include <votca/tools/graph.h>
-#include <votca/tools/edge.h>
 
 using namespace std;
 
 namespace votca {
 namespace tools {
 
-bool Graph_BF_Visitor::queEmpty(){
-  return edge_que_.empty(); 
-}
+bool Graph_BF_Visitor::queEmpty() { return edge_que_.empty(); }
 
-Edge Graph_BF_Visitor::getEdge_(Graph g){
+Edge Graph_BF_Visitor::getEdge_(Graph g) {
   Edge ed = edge_que_.at(0).front();
   edge_que_.at(0).pop();
-  if(edge_que_.at(0).size()==0){
+  if (edge_que_.at(0).size() == 0) {
     edge_que_.pop_front();
   }
   return ed;
 }
 
 // Add edges to be explored
-void Graph_BF_Visitor::addEdges_(Graph& g, int vertex){
+void Graph_BF_Visitor::addEdges_(Graph& g, int vertex) {
+
   auto eds = g.getNeighEdges(vertex);
 
-  // Proceed to add them to queue if the vertices
-  // they refer to are not already explored
-
   // If first edges to be added
-  if(edge_que_.empty()){
+  if (edge_que_.empty()) {
     queue<Edge> first_que;
-    for(auto ed : eds ){
+    for (auto ed : eds) {
       int neigh_vert = ed.getOtherV(vertex);
-      if(explored_.count(neigh_vert)==0){
+      if (explored_.count(neigh_vert) == 0) {
         first_que.push(ed);
       }
     }
-    if(!first_que.empty()){
+    if (!first_que.empty()) {
       edge_que_.push_back(first_que);
     }
-  }else{
+  } else {
 
-    if(edge_que_.size()==1){
+    if (edge_que_.size() == 1) {
       queue<Edge> next_que;
-      for(auto ed : eds ){
+      for (auto ed : eds) {
         int neigh_vert = ed.getOtherV(vertex);
-        if(explored_.count(neigh_vert)==0){
+        if (explored_.count(neigh_vert) == 0) {
           next_que.push(ed);
         }
       }
-      if(!next_que.empty()){
+      if (!next_que.empty()) {
         edge_que_.push_back(next_que);
       }
-    }else{
-      for(auto ed : eds ){
+    } else {
+      for (auto ed : eds) {
         int neigh_vert = ed.getOtherV(vertex);
-        if(explored_.count(neigh_vert)==0){
-          // Add the edges to the next highest distance queue    
+        if (explored_.count(neigh_vert) == 0) {
           edge_que_.at(1).push(ed);
         }
       }
     }
   }
 }
-
 }
 }
