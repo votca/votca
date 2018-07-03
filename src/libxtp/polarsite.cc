2a4e93dc26588c3601dddba120794cd6817b0cd8
Gianluca Tirimbo <g.tirimbo@tue.nl>
2018-07-03 16:18:07 +0200

    Update: Cleaner Version

--- src/libxtp/polarsite.cc
+++ src/libxtp/polarsite.cc
@@ -116,6 +116,7 @@ Eigen::MatrixXd PolarSite::Interaction(const PolarSite& otherSite){
     Eigen::Vector3d pos_b=-e_AB; //unit vector on the sites reciprocal direction; This points toward B   
     double R = r_AB.norm(); //Norm of distance vector
     double fac0=1/R;
+    
     Eigen::Matrix3d c = Eigen::Matrix3d::Identity(); //Following Stone's notation. Here we are assuming that the sites are in the same localframe (Jens dixit)
     
     
@@ -128,99 +129,100 @@ Eigen::MatrixXd PolarSite::Interaction(const PolarSite& otherSite){
     double fac2=std::pow(fac0,3);
     double fac3=std::pow(fac0,4);
     double fac4=std::pow(fac0,5);
+    double sqr3=std::sqrt(3);
+    
+    Eigen::Matrix3d AxB = pos_a*pos_b.transpose(); 
+    Eigen::Matrix3d AxA = pos_a*pos_a.transpose(); 
+    Eigen::Matrix3d BxB = pos_b*pos_b.transpose(); 
+    
+    
+    //Q_A T Q_B
     
     //Charge-Charge Interaction
-    interaction(0,0)=fac0;
-    if ( rankA>0 ){
+    interaction(0,0)=fac0; //T_00,00
+    
+    if ( rankA > 0 && rankB < 1){
         //Dipole-Charge Interaction
-        interaction.block(3,1,1,0)=fac1*pos_a;
-        if ( rankB>0 ){
-           //Charge-Dipole Interaction
-            interaction.block(1,3,0,1)=fac1*pos_b;
-            //Dipole-Dipole Interaction 
-            interaction.block(3,3,1,1)=fac2*(3*pos_a*pos_b.transpose())+c; 
-            }
-    }
-    if ( rankA > 1 ){
-        interaction(0,4)=fac2*0.5*(3*std::pow(pos_b(2),2)-1); //T00,20
-        interaction(0,5)=fac2*std::sqrt(3)*pos_b(0)*pos_b(2); //T00,21c
-        interaction(0,6)=fac2*std::sqrt(3)*pos_b(1)*pos_b(2); //T00,21s
-        interaction(0,7)=fac2*0.5*std::sqrt(3)*(std::pow(pos_b(0),2)-std::pow(pos_b(1),2)); //T00,22c
-        interaction(0,8)=fac2*std::sqrt(3)*pos_b(0)*pos_b(1); //T00,22s
-        
-        interaction.block(3,1,1,4)=0.5*fac3*(15*std::pow(pos_b(2),2)*pos_a+6*pos_b(2)*c.block(3,1,0,2)-3*pos_a); //dipole(order mu_x,mu_y,mu_z)-20
-        interaction.block(3,1,1,5)=fac3*std::sqrt(3)*(pos_b(1)*c.block(3,1,0,2)+c.block(3,1,0,0)*pos_b(2)+5*pos_b(0)*pos_b(2)*pos_a);//dipole(order mu_x,mu_y,mu_z)-21c
-        interaction.block(3,1,1,6)=fac3*std::sqrt(3)*(pos_b(1)*c.block(3,1,0,2)+c.block(3,1,0,1)*pos_b(2)+5*pos_b(1)*pos_b(2)*pos_a);//dipole(order mu_x,mu_y,mu_z)-21s
-        interaction.block(3,1,1,7)=fac3*0.5*std::sqrt(3)*(5*(std::pow(pos_b(0),2)-std::pow(pos_b(1),2))*pos_a+2*pos_b(0)*c.block(3,1,0,0)-2*pos_b(1)*c.block(3,1,0,1));//dipole(order mu_x,mu_y,mu_z)-22c
-        interaction.block(3,1,1,8)=fac3*std::sqrt(3)*(5*pos_b(0)*pos_b(1)*pos_a+pos_b(0)*c.block(1,3,1,0)+pos_b(1)*c.block(3,1,0,0));//dipole(order mu_x,mu_y,mu_z)-22s
-        if (rankB > 1 ){
-            interaction(4,0)=fac2*0.5*(3*std::pow(pos_a(2),2)-1); //T20,00
-            interaction(5,0)=fac2*std::sqrt(3)*pos_a(0)*pos_a(2); //21c,00
-            interaction(6,0)=fac2*std::sqrt(3)*pos_a(1)*pos_a(2); //T21s,000
-            interaction(7,0)=fac2*0.5*std::sqrt(3)*(std::pow(pos_a(0),2)-std::pow(pos_a(1),2)); //T22c,00
-            interaction(8,0)=fac2*std::sqrt(3)*pos_a(0)*pos_a(1); //T00,22s
-            interaction.block(3,1,1,4)=0.5*fac3*(15*std::pow(pos_b(2),2)*pos_a+6*pos_b(2)*c.block(3,1,0,2)-3*pos_a); //dipole(order mu_x,mu_y,mu_z)-20
-            interaction.block(3,1,4,1)=fac3*std::sqrt(3)*(pos_b(1)*c.block(3,1,0,2)+c.block(3,1,0,0)*pos_b(2)+5*pos_b(0)*pos_b(2)*pos_a);//dipole(order mu_x,mu_y,mu_z)-21c
-            interaction.block(3,1,1,6)=fac3*std::sqrt(3)*(pos_b(1)*c.block(3,1,0,2)+c.block(3,1,0,1)*pos_b(2)+5*pos_b(1)*pos_b(2)*pos_a);//dipole(order mu_x,mu_y,mu_z)-21s
-            interaction.block(3,1,1,7)=fac3*0.5*std::sqrt(3)*(5*(std::pow(pos_b(0),2)-std::pow(pos_b(1),2))*pos_a+2*pos_b(0)*c.block(3,1,0,0)-2*pos_b(1)*c.block(3,1,0,1));//dipole(order mu_x,mu_y,mu_z)-22c
-            interaction.block(3,1,8,1)=fac3*std::sqrt(3)*(5*pos_b(0)*pos_b(1)*pos_a+pos_b(0)*c.block(1,3,1,0)+pos_b(1)*c.block(3,1,0,0));//dipole(order mu_x,mu_y,mu_z)-22s
-            
-            interaction(4,4)=fac4*(3./4.)*(35*std::pow(pos_a(2)*pos_b(2),2)-5*std::pow(pos_a(2),2)
-                -5*std::pow(pos_b(2),2)+20*pos_a(2)*pos_b(2)*c(2,2)+2*std::pow(c(2,2),2)+1); //T20,20
-            interaction(4,5)=0.5*fac4*std::sqrt(3)*(35*std::pow(pos_a(2),2)*pos_b(0)*pos_b(2)
-                -5*pos_b(0)*pos_b(2)+10*pos_a(2)*pos_b(0)*c(2,2)+10*pos_a(2)*pos_b(2)*c(2,1)+2*c(2,0)*c(2,2)); //T20,21c
-            interaction(4,6)=0.5*fac4*std::sqrt(3)*(35*std::pow(pos_a(2),2)*pos_b(1)*pos_b(2)
-                -5*pos_b(1)*pos_b(2)+10*pos_a(2)*pos_b(1)*c(2,2)+10*pos_a(2)*pos_b(2)*c(2,1)+2*c(2,1)*c(2,2)) ; //T20,21s
-            interaction(4,7)=0.25*fac4*std::sqrt(3)*(35*std::pow(pos_a(2)*pos_b(0),2)-35*std::pow(pos_a(2)*pos_b(1),2)-5*std::pow(pos_b(0),2)
-                +5*std::pow(pos_b(1),2)+20*pos_a(2)*pos_b(0)*c(2,0)-20*pos_a(2)*pos_b(1)*c(2,1)+2*std::pow(c(2,0),2)-2*std::pow(c(2,1),2)); //T20,22c
-            interaction(4,8)=0.5*fac4*std::sqrt(3)*(35*std::pow(pos_a(2),2)*pos_b(0)*pos_b(1)
-                -5*pos_b(0)*pos_b(1)+10*pos_a(2)*pos_b(0)*c(2,1)+10*pos_a(2)*pos_b(1)*c(2,0)+2*c(2,0)*c(2,1)) ; //T20,22s
-            interaction(5,5)=fac4*(35*pos_a(0)*pos_a(2)*pos_b(0)*pos_b(2)+5*pos_a(0)*pos_b(0)*c(2,2)+5*pos_a(0)*pos_b(2)*c(2,0)+
-                5*pos_a(2)*pos_b(0)*c(0,2)+5*pos_a(2)*pos_b(2)*c(0,0)+c(0,0)*c(2,2)+c(0,2)*c(2,0)) ; //T21c,21c
-            interaction(5,6)=fac4*(35*pos_a(0)*pos_a(2)*pos_b(1)*pos_b(2)+5*pos_a(0)*pos_b(1)*c(2,2)+5*pos_a(0)*pos_b(2)*c(2,1)+
-                5*pos_a(2)*pos_b(1)*c(0,2)+5*pos_a(2)*pos_b(2)*c(0,1)+c(0,1)*c(2,2)+c(0,2)*c(2,1)) ; //T21c,21s
-            interaction(5,7)=0.5*fac4*(35*pos_a(0)*pos_a(2)*std::pow(pos_b(0),2)-35*pos_a(0)*pos_a(2)*std::pow(pos_b(1),2)
-                +10*pos_a(0)*pos_b(0)*c(2,0)-10*pos_a(0)*pos_b(1)*c(2,1)+10*pos_a(0)*pos_b(0)*c(0,0)-10*pos_a(2)*pos_b(1)*c(0,1)+2*c(0,0)*c(2,0)-2*c(0,1)*c(2,1)); //T21c,22c
-            interaction(5,8)=fac4*(35*pos_a(0)*pos_a(2)*pos_b(0)*pos_b(1)+5*pos_a(0)*pos_b(0)*c(2,1)+5*pos_a(0)*pos_b(1)*c(2,0)+
-                5*pos_a(2)*pos_b(0)*c(0,1)+5*pos_a(2)*pos_b(1)*c(0,0)+c(0,0)*c(2,1)+c(0,1)*c(2,0)) ; //T21c,22s
-            interaction(6,6)=fac4*(35*pos_a(1)*pos_a(2)*pos_b(1)*pos_b(2)+5*pos_a(1)*pos_b(1)*c(2,2)+5*pos_a(1)*pos_b(2)*c(2,1)+
-                5*pos_a(2)*pos_b(1)*c(1,2)+5*pos_a(2)*pos_b(2)*c(1,1)+c(1,1)*c(2,2)+c(1,2)*c(2,1)) ; //T21s,21s
-            interaction(6,7)=0.5*fac4*(35*pos_a(1)*pos_a(2)*std::pow(pos_b(0),2)-35*pos_a(1)*pos_a(2)*std::pow(pos_b(1),2)
-                +10*pos_a(1)*pos_b(2)*c(2,0)-10*pos_a(1)*pos_b(1)*c(2,1)+10*pos_a(2)*pos_b(0)*c(1,0)-10*pos_a(2)*pos_b(1)*c(1,1)+2*c(1,0)*c(2,0)-2*c(1,1)*c(2,1)); //T21s,22c
-            interaction(6,8)=fac4*(35*pos_a(1)*pos_a(2)*pos_b(0)*pos_b(1)+5*pos_a(1)*pos_b(0)*c(2,1)+5*pos_a(1)*pos_b(1)*c(2,1)+
-                5*pos_a(2)*pos_b(0)*c(1,1)+5*pos_a(2)*pos_b(1)*c(1,2)+c(1,0)*c(2,1)+c(1,1)*c(2,0)) ; //T21s,22s
-            interaction(7,7)=0.25*fac4*(35*std::pow(pos_a(0)*pos_b(0),2)-35*std::pow(pos_a(0)*pos_b(1),2)-35*std::pow(pos_a(1)*pos_b(0),2)
-                +35*std::pow(pos_a(1)*pos_b(1),2)+20*pos_a(0)*pos_b(0)*c(0,0)-20*pos_a(0)*pos_b(1)*c(0,1)-20*pos_a(1)*pos_b(0)*c(1,0)
-                +20*pos_a(0)*pos_b(0)*c(1,1)+2*std::pow(c(0,0),2)-2*std::pow(c(0,1),2)-2*std::pow(c(1,0),2)+2*std::pow(c(1,1),2)); //T22c,22c
-            interaction(7,8)= 0.5*fac4*(35*pos_b(0)*pos_b(1)*std::pow(pos_a(0),2)-35*pos_b(1)*pos_b(2)*std::pow(pos_a(1),2)
-                +10*pos_a(0)*pos_b(0)*c(0,1)+10*pos_a(0)*pos_b(1)*c(0,0)-10*pos_a(1)*pos_b(0)*c(1,1)-10*pos_a(1)*pos_b(1)*c(1,2)+2*c(0,0)*c(0,1)-2*c(1,0)*c(1,1)); //T22c,22s
-            interaction(8,8)=0.5*fac4*(35*pos_a(0)*pos_a(1)*pos_b(0)*pos_b(1)+5*pos_a(0)*pos_b(0)*c(1,1)+5*pos_a(0)*pos_b(1)*c(1,0)+5*pos_a(1)*pos_b(0)*c(0,1)
-                +5*pos_a(1)*pos_b(1)*c(0,0)+c(0,0)*c(1,1)+c(0,1)*c(1,0)); //T22s,22s
-            interaction(5,4)=0.5*fac4*std::sqrt(3)*(35*std::pow(pos_b(2),2)*pos_a(0)*pos_a(2)
-                -5*pos_a(0)*pos_a(2)+10*pos_b(2)*pos_a(0)*c(2,2)+10*pos_b(2)*pos_a(2)*c(1,2)+2*c(0,2)*c(2,2)); //T21c,20
-            interaction(6,4)=0.5*fac4*std::sqrt(3)*(35*std::pow(pos_b(2),2)*pos_a(1)*pos_a(2)
-                -5*pos_a(1)*pos_a(2)+10*pos_b(2)*pos_a(1)*c(2,2)+10*pos_b(2)*pos_a(2)*c(1,2)+2*c(1,2)*c(2,2)) ; //T21s,20
-            interaction(6,5)=fac4*(35*pos_b(0)*pos_b(2)*pos_a(1)*pos_a(2)+5*pos_b(0)*pos_a(1)*c(2,2)+5*pos_b(0)*pos_a(2)*c(1,2)+
-                5*pos_b(2)*pos_a(1)*c(2,0)+5*pos_b(2)*pos_a(2)*c(1,0)+c(1,0)*c(2,2)+c(2,0)*c(1,2)) ; //T21s,21c
-            interaction(7,4)=0.25*fac4*std::sqrt(3)*(35*std::pow(pos_b(2)*pos_a(0),2)-35*std::pow(pos_b(2)*pos_a(1),2)-5*std::pow(pos_a(0),2)
-                +5*std::pow(pos_a(1),2)+20*pos_b(2)*pos_a(0)*c(0,2)-20*pos_b(2)*pos_a(1)*c(1,2)+2*std::pow(c(0,2),2)-2*std::pow(c(1,2),2)); //T22c,20
-            interaction(7,5)=0.5*fac4*(35*pos_b(0)*pos_b(2)*std::pow(pos_a(0),2)-35*pos_b(0)*pos_b(2)*std::pow(pos_a(1),2)
-                +10*pos_b(0)*pos_a(0)*c(0,2)-10*pos_b(0)*pos_a(1)*c(1,2)+10*pos_b(0)*pos_a(0)*c(0,0)-10*pos_b(2)*pos_a(1)*c(1,0)+2*c(0,0)*c(0,2)-2*c(1,0)*c(1,2)); //T22c,21c
-            interaction(7,6)=0.5*fac4*(35*pos_b(1)*pos_b(2)*std::pow(pos_a(0),2)-35*pos_b(1)*pos_b(2)*std::pow(pos_a(1),2)
-                +10*pos_b(1)*pos_a(2)*c(0,2)-10*pos_b(1)*pos_a(1)*c(1,2)+10*pos_b(2)*pos_a(0)*c(0,1)-10*pos_b(2)*pos_a(1)*c(1,1)+2*c(0,1)*c(0,2)-2*c(1,1)*c(1,2)); //T22c,21s
-            interaction(8,4)=0.5*fac4*std::sqrt(3)*(35*std::pow(pos_b(2),2)*pos_a(0)*pos_a(1)
-                -5*pos_a(0)*pos_a(1)+10*pos_b(2)*pos_a(0)*c(1,2)+10*pos_b(2)*pos_a(1)*c(0,2)+2*c(0,2)*c(1,2)) ; //T22s,20
-            interaction(8,5)=fac4*(35*pos_b(0)*pos_b(2)*pos_a(0)*pos_a(1)+5*pos_b(0)*pos_a(0)*c(1,2)+5*pos_b(0)*pos_a(1)*c(0,2)+
-                5*pos_b(2)*pos_a(0)*c(1,0)+5*pos_b(2)*pos_a(1)*c(0,0)+c(0,0)*c(1,2)+c(1,0)*c(0,2)) ; //T22s,21c
-            interaction(8,6)=fac4*(35*pos_b(1)*pos_b(2)*pos_a(0)*pos_a(1)+5*pos_b(1)*pos_a(0)*c(1,2)+5*pos_b(1)*pos_a(1)*c(1,2)+
-                5*pos_b(2)*pos_a(0)*c(1,1)+5*pos_b(2)*pos_a(1)*c(2,1)+c(0,1)*c(1,2)+c(1,1)*c(0,2)) ; //T22s,21s
-            interaction(8,7)= 0.5*fac4*(35*pos_a(0)*pos_a(1)*std::pow(pos_b(0),2)-35*pos_a(1)*pos_a(2)*std::pow(pos_b(1),2)
-                +10*pos_b(0)*pos_a(0)*c(1,0)+10*pos_b(0)*pos_a(1)*c(0,0)-10*pos_b(1)*pos_a(0)*c(1,1)-10*pos_b(1)*pos_a(1)*c(2,1)+2*c(0,0)*c(1,0)-2*c(0,1)*c(1,1)); //T22s,22c
-             
-        }
+        interaction.block<3,1>(1,0)=fac1*pos_a; //T_1alpha,00 (alpha=x,y,z)
     }
     
-        return interaction; //in units of 4piepsilon0
+    if ( rankA==0 && rankB > 0 ){
+        //Charge-Dipole Interaction
+        interaction.block<1,3>(0,1)=fac1*pos_b; //T_00,1alpha (alpha=x,y,z)
+    }
+    
+    if (rankB > 0 && rankA>0){
+        //Dipole-Dipole Interaction 
+        interaction.block<3,3>(1,1)=fac2*(3*AxB)+c; //T_1alpha,1beta (alpha,beta=x,y,z)
+    }        
+    
+    if ( rankA > 1 && rankB < 1){
+        //Quadrupole-Charge interaction
+        interaction(4,0)=fac2*0.5*(3*AxA(2,2)-1); //T20,00
+        interaction(5,0)=fac2*sqr3*AxA(0,2); //21c,00
+        interaction(6,0)=fac2*sqr3*AxA(1,2); //T21s,000
+        interaction(7,0)=fac2*0.5*sqr3*(AxA(0,0)-AxA(1,1)); //T22c,00
+        interaction(8,0)=fac2*sqr3*AxA(0,1); //T22s,00
+    }
+    
+    if ( rankA > 1 && rankB > 0 ){
+        //Quadrupole-Dipole Interaction
+        interaction.block<3,1>(4,1)=0.5*fac3*(15*AxA(2,2)*pos_b+6*pos_a(2)*c.block<3,1>(2,0)-3*pos_b); //T20-1beta (beta=x,y,z)
+        interaction.block<3,1>(5,1)=fac3*sqr3*(pos_a(0)*c.block<3,1>(2,0)+c.block<3,1>(0,0)*pos_a(2)+5*AxA(0,2)*pos_b);//T21c-1beta (beta=x,y,z)
+        interaction.block<3,1>(6,1)=fac3*sqr3*(pos_a(1)*c.block<3,1>(2,0)+c.block<3,1>(1,0)*pos_a(2)+5*AxA(1,2)*pos_a);//T21s-1beta (beta=x,y,z)
+        interaction.block<3,1>(7,1)=fac3*0.5*sqr3*(5*(AxA(0,0)-AxA(1,1))*pos_b+2*pos_a(0)*c.block<3,1>(0,0)-2*pos_a(1)*c.block<3,1>(0,1));//T22c-1beta (beta=x,y,z)
+        interaction.block<3,1>(8,1)=fac3*sqr3*(5*AxA(0,1)*pos_b+pos_a(0)*c.block<3,1>(1,0)+pos_a(1)*c.block<3,1>(0,0));//T22s-1beta (beta=x,y,z)    
+    }
+    
+    if ( rankA < 1 && rankB > 1){
+        //Charge-Quadrupole Interaction
+        interaction(0,4)=fac2*0.5*(3*BxB(2,2)-1); //T00,20
+        interaction(0,5)=fac2*sqr3*BxB(0,2); //T00,21c
+        interaction(0,6)=fac2*sqr3*BxB(1,2); //T00,21s
+        interaction(0,7)=fac2*0.5*sqr3*(BxB(0,0)-BxB(1,1)); //T00,22c
+        interaction(0,8)=fac2*sqr3*BxB(0,1); //T00,22s
+    }
+    
+    if ( rankA > 0 && rankB > 1){
+        //Dipole-Quadrupole Interaction
+        interaction.block<1,3>(1,4)=0.5*fac3*(15*BxB(2,2)*pos_a.transpose()+6*pos_b(2)*c.block<1,3>(0,2)-3*pos_a.transpose()); //T1beta-20 (beta=x,y,z)
+        interaction.block<1,3>(1,5)=fac3*sqr3*(pos_b(0)*c.block<1,3>(0,2)+c.block<1,3>(0,0)*pos_b(2)+5*BxB(0,2)*pos_a.transpose());//T1beta-21c (beta=x,y,z)
+        interaction.block<1,3>(1,6)=fac3*sqr3*(pos_b(1)*c.block<1,3>(0,2)+c.block<1,3>(0,1)*pos_b(2)+5*BxB(1,2)*pos_b.transpose());//T1beta-21s (beta=x,y,z)
+        interaction.block<1,3>(1,7)=0.5*fac3*sqr3*(5*(BxB(0,0)-BxB(1,1))*pos_a.transpose()+2*pos_b(0)*c.block<1,3>(0,0)-2*pos_b(1)*c.block<1,3>(1,0));//T1beta-22c (beta=x,y,z)
+        interaction.block<1,3>(1,8)=fac3*sqr3*(5*BxB(0,1)*pos_a.transpose()+pos_b(0)*c.block<1,3>(1,0)+pos_b(1)*c.block<1,3>(0,0));//T1beta-22s (beta=x,y,z)    
+    }
+    
+    if (rankA > 1 && rankB > 1){               
+        //Quadrupole-Quadrupole Interaction
+        interaction(4,4)=fac4*(3./4.)*(35*AxA(2,2)*BxB(2,2)-5*AxA(2,2)-5*BxB(2,2)+20*AxB(2,2)*c(2,2)+2*c(2,2)*c(2,2)+1); //T20,20
+        interaction(4,5)=0.5*fac4*sqr3*(35*AxA(2,2)*BxB(0,2)-5*BxB(0,2)+10*AxB(2,0)*c(2,2)+10*AxB(2,2)*c(2,1)+2*c(2,0)*c(2,2)); //T20,21c
+        interaction(4,6)=0.5*fac4*sqr3*(35*AxA(2,2)*BxB(1,2)-5*BxB(1,2)+10*AxB(2,1)*c(2,2)+10*AxB(2,2)*c(2,1)+2*c(2,1)*c(2,2)) ; //T20,21s
+        interaction(4,7)=0.25*fac4*sqr3*(35*AxB(2,0)-35*AxB(2,1)-5*BxB(0,0)+5*BxB(1,1)+20*AxB(2,0)*c(2,0)-20*AxB(2,1)*c(2,1)+2*c(2,0)*c(2,0)-2*c(2,1)*c(2,1));//T20,22c
+        interaction(4,8)=0.5*fac4*sqr3*(35*AxA(2,2)*BxB(0,1)-5*BxB(0,1)+10*AxB(2,0)*c(2,1)+10*AxB(2,1)*c(2,0)+2*c(2,0)*c(2,1)) ; //T20,22s
+        interaction(5,5)=fac4*(35*AxA(0,2)*BxB(0,2)+5*AxB(0,0)*c(2,2)+5*AxB(0,2)*c(2,0)+5*AxB(2,0)*c(0,2)+5*AxB(2,2)*c(0,0)+c(0,0)*c(2,2)+c(0,2)*c(2,0)); //T21c,21c
+        interaction(5,6)=fac4*(35*AxA(0,2)*BxB(1,2)+5*AxB(0,1)*c(2,2)+5*AxB(0,2)*c(2,1)+5*AxB(2,1)*c(0,2)+5*AxB(2,2)*c(0,1)+c(0,1)*c(2,2)+c(0,2)*c(2,1)); //T21c,21s
+        interaction(5,7)=0.5*fac4*(35*AxA(0,2)*BxB(0,0)-35*AxA(0,2)*BxB(1,1)+10*AxB(0,0)*c(2,0)-10*AxB(0,1)*c(2,1)+10*AxB(0,0)*c(0,0)-10*AxB(2,1)*c(0,1)+2*c(0,0)*c(2,0)-2*c(0,1)*c(2,1)); //T21c,22c
+        interaction(5,8)=fac4*(35*AxA(0,2)*BxB(0,1)+5*AxB(0,0)*c(2,1)+5*AxB(0,1)*c(2,0)+5*AxB(2,0)*c(0,1)+5*AxB(2,1)*c(0,0)+c(0,0)*c(2,1)+c(0,1)*c(2,0)); //T21c,22s
+        interaction(6,6)=fac4*(35*AxA(1,2)*BxB(1,2)+5*AxB(1,1)*c(2,2)+5*AxB(1,2)*c(2,1)+5*AxB(2,1)*c(1,2)+5*AxB(2,2)*c(1,1)+c(1,1)*c(2,2)+c(1,2)*c(2,1)); //T21s,21s
+        interaction(6,7)=0.5*fac4*(35*AxA(1,2)*BxB(0,0)-35*AxA(1,2)*BxB(1,1)+10*AxB(1,2)*c(2,0)-10*AxB(1,1)*c(2,1)+10*AxB(2,0)*c(1,0)-10*AxB(2,1)*c(1,1)+2*c(1,0)*c(2,0)-2*c(1,1)*c(2,1)); //T21s,22c
+        interaction(6,8)=fac4*(35*AxA(1,2)*BxB(0,1)+5*AxB(1,0)*c(2,1)+5*AxB(1,1)*c(2,1)+5*AxB(2,0)*c(1,1)+5*AxB(2,1)*c(1,2)+c(1,0)*c(2,1)+c(1,1)*c(2,0)); //T21s,22s
+        interaction(7,7)=0.25*fac4*(35*AxA(0,0)*BxB(0,0)-35*AxA(0,0)*BxB(1,1)-35*AxA(1,1)*BxB(0,0)+35*AxA(1,1)*BxB(1,1)+20*AxB(0,0)*c(0,0)-20*AxB(0,1)*c(0,1)-20*AxB(1,0)*c(1,0)
+                +20*AxB(0,0)*c(1,1)+2*c(0,0)*c(0,0)-2*c(0,1)*c(0,1)-2*c(1,0)*c(1,0)+2*c(1,1)*c(1,1)); //T22c,22c
+        interaction(7,8)= 0.5*fac4*(35*BxB(0,1)*AxA(0,0)-35*BxB(1,2)*AxA(1,1)+10*AxB(0,0)*c(0,1)+10*AxB(0,1)*c(0,0)-10*AxB(1,0)*c(1,1)-10*AxB(1,1)*c(1,2)+2*c(0,0)*c(0,1)-2*c(1,0)*c(1,1)); //T22c,22s
+        interaction(8,8)=0.5*fac4*(35*AxA(0,1)*BxB(0,1)+5*AxB(0,0)*c(1,1)+5*AxB(0,1)*c(1,0)+5*AxB(1,0)*c(0,1)+5*AxB(1,1)*c(0,0)+c(0,0)*c(1,1)+c(0,1)*c(1,0)); //T22s,22s
+        interaction(5,4)=0.5*fac4*sqr3*(35*BxB(2,2)*AxA(0,2)-5*AxA(0,2)+10*AxB(0,2)*c(2,2)+10*AxB(2,2)*c(1,2)+2*c(0,2)*c(2,2)); //T21c,20
+        interaction(6,4)=0.5*fac4*sqr3*(35*BxB(2,2)*AxA(1,2)-5*AxA(1,2)+10*AxB(1,2)*c(2,2)+10*AxB(2,2)*c(1,2)+2*c(1,2)*c(2,2)) ; //T21s,20
+        interaction(6,5)=fac4*(35*BxB(0,2)*AxA(1,2)+5*AxB(1,0)*c(2,2)+5*AxB(2,0)*c(1,2)+5*AxB(2,1)*c(2,0)+5*AxB(2,2)*c(1,0)+c(1,0)*c(2,2)+c(2,0)*c(1,2)); //T21s,21c
+        interaction(7,4)=0.25*fac4*sqr3*(35*AxB(0,2)-35*BxB(2,2)*AxA(1,1)-5*AxA(0,0)+5*AxA(1,1)+20*AxB(0,2)*c(0,2)-20*AxB(1,2)*c(1,2)+2*c(0,2)*c(0,2)-2*c(1,2)*c(1,2)); //T22c,20
+        interaction(7,5)=0.5*fac4*(35*BxB(0,2)*AxA(0,0)-35*BxB(0,2)*AxA(1,1)+10*AxB(0,0)*c(0,2)-10*AxB(1,0)*c(1,2)+10*AxB(0,0)*c(0,0)-10*AxB(1,2)*c(1,0)+2*c(0,0)*c(0,2)-2*c(1,0)*c(1,2)); //T22c,21c
+        interaction(7,6)=0.5*fac4*(35*BxB(1,2)*AxA(0,0)-35*BxB(1,2)*AxA(1,1)+10*AxB(2,1)*c(0,2)-10*AxB(1,1)*c(1,2)+10*AxB(0,2)*c(0,1)-10*AxB(1,2)*c(1,1)+2*c(0,1)*c(0,2)-2*c(1,1)*c(1,2)); //T22c,21s
+        interaction(8,4)=0.5*fac4*sqr3*(35*BxB(2,2)*AxA(0,1)-5*AxA(0,1)+10*AxB(0,2)*c(1,2)+10*AxB(1,2)*c(0,2)+2*c(0,2)*c(1,2)) ; //T22s,20
+        interaction(8,5)=fac4*(35*BxB(0,2)*AxA(1,0)+5*AxB(0,0)*c(1,2)+5*AxB(1,0)*c(0,2)+5*AxB(0,2)*c(1,0)+5*AxB(1,2)*c(0,0)+c(0,0)*c(1,2)+c(1,0)*c(0,2)); //T22s,21c
+        interaction(8,6)=fac4*(35*BxB(1,2)*AxA(0,1)+5*AxB(0,1)*c(1,2)+5*AxB(1,1)*c(1,2)+5*AxB(0,2)*c(1,1)+5*AxB(1,2)*c(2,1)+c(0,1)*c(1,2)+c(1,1)*c(0,2)) ; //T22s,21s
+        interaction(8,7)= 0.5*fac4*(35*AxA(1,0)*BxB(0,0)-35*AxA(1,2)*BxB(1,1)+10*AxB(0,0)*c(1,0)+10*AxB(1,0)*c(0,0)-10*AxB(0,1)*c(1,1)-10*AxB(1,1)*c(2,1)+2*c(0,0)*c(1,0)-2*c(0,1)*c(1,1)); //T22s,22c          
+    }
+     
+return interaction; //in units of 4piepsilon0
         
 }
     
