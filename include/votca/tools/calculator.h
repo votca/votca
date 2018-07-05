dc361ecfb0a50bf3786f7c4c10025033e8348e10
Jens Wehner <jenswehner@gmail.com>
2018-07-05 17:07:51 +0200

    added copyrights

--- include/votca/tools/calculator.h
+++ include/votca/tools/calculator.h
@@ -1,5 +1,5 @@
 /*
- *            Copyright 2009-2016 The VOTCA Development Team
+ *            Copyright 2009-2018 The VOTCA Development Team
  *                       (http://www.votca.org)
  *
  *      Licensed under the Apache License, Version 2.0 (the "License")
