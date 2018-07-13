ac621b4dad06b8bc1b4c0d449089493879b62edd
Jens Wehner <jenswehner@gmail.com>
2018-07-13 13:29:10 +0200

    first commit on github

--- Methane/reset.sh
+++
@@ -1,4 +0,0 @@
-#!bin/bash
-
-sed -i "s/ASSIGNED/AVAILABLE/" qmmm.jobs
-sed -i "s/COMPLETE/AVAILABLE/" qmmm.jobs
--- Methane/reset_ape.sh
+++
@@ -1,4 +0,0 @@
-#!bin/bash
-
-sed -i "s/ASSIGNED/AVAILABLE/" qmape.jobs
-sed -i "s/COMPLETE/AVAILABLE/" qmape.jobs
