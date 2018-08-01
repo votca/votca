5eaa5a91bf7a3baa32945f99dc8a932ac26114dd
JoshuaSBrown <josh@votca.org>
2018-08-01 00:55:08 -0600

    Minor adjustments

--- include/votca/csg/base_bead.h
+++ include/votca/csg/base_bead.h
@@ -1,4 +1,4 @@
-/* 
+/*
  * Copyright 2009-2018 The VOTCA Development Team (http://www.votca.org)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -16,140 +16,128 @@
  */
 
 #ifndef _VOTCA_CSG_BASEBEAD_H
-#define	_VOTCA_CSG_BASEBEAD_H
+#define _VOTCA_CSG_BASEBEAD_H
 
-#include <votca/tools/name.h>
 #include <votca/tools/identity.h>
+#include <votca/tools/name.h>
 #include <votca/tools/vec.h>
 
-namespace votca { namespace csg {
+namespace votca {
+namespace csg {
 using namespace votca::tools;
 
 class Molecule;
 class BeadType;
 
 /**
-    \brief information about a base bead
- 
-    The Base Bead class describes the core functionality of an atom or a coarse grained bead. It stores information like the id, the name, the mass, the
-    charge and the residue it belongs to and the position
+ * \brief information about a base bead
+ *
+ * The Base Bead class describes the core functionality of an atom or a coarse
+ * grained bead. It stores information like the id, the name, the mass, the
+ * charge and the residue it belongs to and the position
+ *
+ **/
+class BaseBead : public TopologyItem, public Name, public Identity {
+public:
+  BaseBead()
+      : _type(nullptr), _mol(nullptr), _mass(0.0), _q(0.0), _bpos(false) {};
 
-    \todo change resnr to pointer
-    \todo make sure bead belongs to topology
-*/
-class BaseBead : 
-  public TopologyItem,
-  public Name,
-  public Identity
-{
-public:   
+  /**
+   * destructor
+   */
+  virtual ~BaseBead() {}
 
-    BaseBead() : 
-      _type(nullptr),
-      _mol(nullptr),
-      _mass(0.0),
-      _q(0.0),
-      _pos(0.0,0.0,0.0),
-      _bpos(false) {};
-    
-    /**
-     * destructor
-     */
-    virtual ~BaseBead() {}
+  /**
+   * get the bead type
+   * \return bead type object
+   */
+  const BeadType *getType() const { return _type; }
 
-    /**
-     * get the bead type
-     * \return bead type object
-     */
-    const BeadType *getType() const { return _type; }
+  /**
+   * set the bead type
+   * \param bead type object
+   */
+  void setType(BeadType *type) { _type = type; }
 
-    /**
-     * set the bead type
-     * \param bead type object
-     */
-    void setType(BeadType *type) { _type=type; }
+  /**
+   * get the mass of the base bead
+   * \return base bead mass
+   */
+  const double &getMass() const { return _m; }
 
-    /**
-     * get the mass of the base bead
-     * \return base bead mass
-     */
-    const double &getMass() const { return _m; }
+  /**
+   * get the charge of the base bead
+   * \return base bead charge
+   */
+  const double &getQ() const { return _q; }
 
-    /**
-     * get the charge of the base bead
-     * \return base bead charge
-     */
-    const double &getQ() const { return _q; }
-    
-    /**
-     * set the mass of the base bead
-     * \param m base bead mass
-     */
-    void setMass(const double &m) { _m=m; }
+  /**
+   * set the mass of the base bead
+   * \param m base bead mass
+   */
+  void setMass(const double &m) { _m = m; }
 
-    /**
-     * set the charge of the base bead
-     * \param q base bead charge
-     */
-    void setQ(const double &q) { _q=q; }
+  /**
+   * set the charge of the base bead
+   * \param q base bead charge
+   */
+  void setQ(const double &q) { _q = q; }
 
-    /**
-     * set the position of the base bead
-     * \param r base bead position
-     */
-    void setPos(const vec &r);
+  /**
+   * set the position of the base bead
+   * \param r base bead position
+   */
+  void setPos(const vec &r);
 
-    /**
-     * get the position of the base bead
-     * \return base bead position
-     */
-    const vec &getPos() const;
-       
-    /**
-     * direct access (read/write) to the position of the base bead
-     * \return reference to position 
-     */
-    vec &Pos() { assert(_bPos); return _pos; }
+  /**
+   * get the position of the base bead
+   * \return base bead position
+   */
+  const vec &getPos() const;
 
-    /** does this configuration store positions? */
-    bool HasPos() {return _bPos; }
-    
-    /**
-     * molecule the base bead belongs to
-     * \return Molecule object
-     */
-    Molecule *getMolecule() { return _mol; }
-
-    void setMolecule( Molecule * mol);
-
-protected:
-    BeadType *_type;
-    Molecule *_mol;
-    
-    double _mass;
-    double _q;
-    vec _pos;
-    
-    bool _bPos;
-};
-
-inline void Bead::setMolecule(Molecule *mol){
-	_mol = mol;
-}
-
-inline void Bead::setPos(const vec &r)
-{
-    _bPos=true;
-    _pos = r;
-}
-
-inline const vec &Bead::getPos() const
-{
+  /**
+   * direct access (read/write) to the position of the base bead
+   * \return reference to position
+   */
+  vec &Pos() {
     assert(_bPos);
     return _pos;
+  }
+
+  /** does this configuration store positions? */
+  bool HasPos() { return _bPos; }
+
+  /**
+   * molecule the base bead belongs to
+   * \return Molecule object
+   */
+  Molecule *getMolecule() { return _mol; }
+
+  void setMolecule(Molecule *mol);
+
+protected:
+  BeadType *_type;
+  Molecule *_mol;
+
+  double _mass;
+  double _q;
+  vec _pos;
+
+  bool _bPos;
+};
+
+inline void Bead::setMolecule(Molecule *mol) { _mol = mol; }
+
+inline void Bead::setPos(const vec &r) {
+  _bPos = true;
+  _pos = r;
 }
 
-}}
+inline const vec &Bead::getPos() const {
+  assert(_bPos);
+  return _pos;
+}
+}
+}
 
-#endif	// _VOTCA_CSG_BASEBEAD_H
-
+#endif // _VOTCA_CSG_BASEBEAD_H
