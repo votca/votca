6332717727e8e3e1769623eec5fe12801c4b7c98
JoshuaSBrown <josh@votca.org>
2018-08-01 16:14:24 -0600

    switched to template

--- include/votca/tools/identity.h
+++ include/votca/tools/identity.h
@@ -29,22 +29,23 @@ namespace tools {
     an object it primariy meant to be used in child classes and provides a more
     safety than other imlementations.
 */
+template<typename T>
 class Identity {
  private:
-  long int id_;
+  T id_;
   bool id_set_;
 
  public:
   /// Constructor
   Identity() : id_set_(false) {}
   /// Constructor that takes initial id
-  Identity(long int id) : id_(id), id_set_(true) {};
+  Identity(const T &id) : id_(id), id_set_(true) {};
   /// Gets the id returns error of the id has not been set
-  long int getId() {
+  const T &getId() {
     return (id_set_ ? id_ : throw std::runtime_error("ID not set"));
   }
   /// Set the id
-  void setId(long int id) { id_ = id; }
+  void setId(T id) { id_ = id; }
 };
 }
 }
