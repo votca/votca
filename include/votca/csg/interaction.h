d49ab978eea82414c204dfdc190bffe2ea9a896f
JoshuaSBrown <josh@votca.org>
2018-07-31 18:12:54 -0600

    1. Applied google style formatting

--- include/votca/csg/interaction.h
+++ include/votca/csg/interaction.h
@@ -1,4 +1,4 @@
-/* 
+/*
  * Copyright 2009-2018 The VOTCA Development Team (http://www.votca.org)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -16,14 +16,15 @@
  */
 
 #ifndef _VOTCA_CSG_INTERACTION_H
-#define	_VOTCA_CSG_INTERACTION_H
+#define _VOTCA_CSG_INTERACTION_H
 
-#include <string>
-#include <sstream>
-#include "topology.h"
 #include "bead.h"
+#include "topology.h"
+#include <sstream>
+#include <string>
 
-namespace votca { namespace csg {
+namespace votca {
+namespace csg {
 using namespace votca::tools;
 using namespace std;
 
@@ -34,80 +35,105 @@ using namespace std;
 
     \todo double names/groups right, add molecules!!
 */
-class Interaction
-{
+class Interaction {
 public:
-    Interaction() :
-      _index(-1),
-      _group(""),
-      _group_id(-1),
-      _name(""),
-      _mol(-1) {};
+  Interaction() : _index(-1), _group(""), _group_id(-1), _name(""), _mol(-1){};
 
-    virtual ~Interaction() { }
-    virtual double EvaluateVar(const Topology &top) = 0;
-        
-    string getName() const { return _name; }
-        
-    void setGroup(const string &group) { _group = group; RebuildName(); }
-    const string &getGroup() const { assert(_group.compare("")!=0); return _group; }
+  virtual ~Interaction() {}
+  virtual double EvaluateVar(const Topology &top) = 0;
 
-    // the group id is set by topology, when interaction is added to it
-    // \todo if the group name is changed later, group id should be updated by topology
-    int getGroupId() { assert(_group_id!=-1); return _group_id; }
-    void setGroupId(int id) { _group_id = id; }
+  string getName() const { return _name; }
 
-    void setIndex(const int &index) { _index = index; RebuildName(); }
-    const int &getIndex() const { assert(_index!=-1); return _index; }
-    
-    void setMolecule(const int &mol) { _mol = mol; RebuildName(); }
-    const int &getMolecule() const { assert(_mol!=-1); return _mol; }
-    
-    virtual vec Grad(const Topology &top, int bead) = 0;
-    int BeadCount() { return _beads.size(); }
-    int getBeadId(int bead) { 
-      assert(bead>-1 && bead<_beads.size());
-      return _beads[bead]; 
-    }
-    
+  void setGroup(const string &group) {
+    _group = group;
+    RebuildName();
+  }
+  const string &getGroup() const {
+    assert(_group.compare("") != 0);
+    return _group;
+  }
+
+  // the group id is set by topology, when interaction is added to it
+  // \todo if the group name is changed later, group id should be updated by
+  // topology
+  int getGroupId() {
+    assert(_group_id != -1);
+    return _group_id;
+  }
+  void setGroupId(int id) { _group_id = id; }
+
+  void setIndex(const int &index) {
+    _index = index;
+    RebuildName();
+  }
+  const int &getIndex() const {
+    assert(_index != -1);
+    return _index;
+  }
+
+  void setMolecule(const int &mol) {
+    _mol = mol;
+    RebuildName();
+  }
+  const int &getMolecule() const {
+    assert(_mol != -1);
+    return _mol;
+  }
+
+  virtual vec Grad(const Topology &top, int bead) = 0;
+  int BeadCount() { return _beads.size(); }
+  int getBeadId(int bead) {
+    assert(bead > -1 && bead < _beads.size());
+    return _beads[bead];
+  }
+
 protected:
-    int _index;
-    string _group;
-    int _group_id;
-    string _name;
-    int _mol;
-    vector<int> _beads;
-    
-    void RebuildName();
+  int _index;
+  string _group;
+  int _group_id;
+  string _name;
+  int _mol;
+  vector<int> _beads;
+
+  void RebuildName();
 };
 
-inline void Interaction::RebuildName()
-{
-    stringstream s;
-    if(_mol!=-1) s << "molecule " << _mol;
-    if(!_group.empty()) {
-      s << ":" << _group;
-      if(_group_id!=-1){
-        s << " " << _group_id;
-      }
+inline void Interaction::RebuildName() {
+  stringstream s;
+  if (_mol != -1)
+    s << "molecule " << _mol;
+  if (!_group.empty()) {
+    s << ":" << _group;
+    if (_group_id != -1) {
+      s << " " << _group_id;
     }
-    if(_index!=-1) s << ":index " << _index;
-    _name = s.str();
+  }
+  if (_index != -1)
+    s << ":index " << _index;
+  _name = s.str();
 }
 
 /**
     \brief bond interaction
 */
-class IBond : public Interaction
-{
+class IBond : public Interaction {
 public:
-    IBond(int bead1, int bead2)
-        { _beads.resize(2); _beads[0] = bead1; _beads[1] = bead2; }
+  IBond(int bead1, int bead2) {
+    _beads.resize(2);
+    _beads[0] = bead1;
+    _beads[1] = bead2;
+  }
 
-    IBond(list<int> &beads)
-        { assert(beads.size()>=2); _beads.resize(2); for(int i=0; i<2; ++i) { _beads[i] = beads.front(); beads.pop_front(); }}
-    double EvaluateVar(const Topology &top);
-    vec Grad(const Topology &top, int bead);
+  IBond(list<int> &beads) {
+    assert(beads.size() >= 2);
+    _beads.resize(2);
+    for (int i = 0; i < 2; ++i) {
+      _beads[i] = beads.front();
+      beads.pop_front();
+    }
+  }
+  double EvaluateVar(const Topology &top);
+  vec Grad(const Topology &top, int bead);
 
 private:
 };
@@ -115,16 +141,25 @@ private:
 /**
     \brief angle interaction
 */
-class IAngle : public Interaction
-{
+class IAngle : public Interaction {
 public:
-    IAngle(int bead1, int bead2, int bead3)
-        { _beads.resize(3); _beads[0] = bead1; _beads[1] = bead2; _beads[2] = bead3;}
-    IAngle(list<int> &beads)
-        { assert(beads.size()>=3); _beads.resize(3); for(int i=0; i<3; ++i) { _beads[i] = beads.front(); beads.pop_front(); }}
+  IAngle(int bead1, int bead2, int bead3) {
+    _beads.resize(3);
+    _beads[0] = bead1;
+    _beads[1] = bead2;
+    _beads[2] = bead3;
+  }
+  IAngle(list<int> &beads) {
+    assert(beads.size() >= 3);
+    _beads.resize(3);
+    for (int i = 0; i < 3; ++i) {
+      _beads[i] = beads.front();
+      beads.pop_front();
+    }
+  }
 
-    double EvaluateVar(const Topology &top);
-    vec Grad(const Topology &top, int bead);
+  double EvaluateVar(const Topology &top);
+  vec Grad(const Topology &top, int bead);
 
 private:
 };
@@ -132,132 +167,202 @@ private:
 /**
     \brief dihedral interaction
 */
-class IDihedral : public Interaction
-{
+class IDihedral : public Interaction {
 public:
-    IDihedral(int bead1, int bead2, int bead3, int bead4)
-        { _beads.resize(4); _beads[0] = bead1; _beads[1] = bead2; _beads[2] = bead3; _beads[3] = bead4;}
-    IDihedral(list<int> &beads)
-        { assert(beads.size()>=4); _beads.resize(4); for(int i=0; i<4; ++i) { _beads[i] = beads.front(); beads.pop_front(); }}
-   
-    double EvaluateVar(const Topology &top);
-    vec Grad(const Topology &top, int bead);
+  IDihedral(int bead1, int bead2, int bead3, int bead4) {
+    _beads.resize(4);
+    _beads[0] = bead1;
+    _beads[1] = bead2;
+    _beads[2] = bead3;
+    _beads[3] = bead4;
+  }
+  IDihedral(list<int> &beads) {
+    assert(beads.size() >= 4);
+    _beads.resize(4);
+    for (int i = 0; i < 4; ++i) {
+      _beads[i] = beads.front();
+      beads.pop_front();
+    }
+  }
+
+  double EvaluateVar(const Topology &top);
+  vec Grad(const Topology &top, int bead);
 
 private:
 };
 
-inline double IBond::EvaluateVar(const Topology &top)
-{
-    return abs(top.getDist(_beads[0], _beads[1]));
+inline double IBond::EvaluateVar(const Topology &top) {
+  return abs(top.getDist(_beads[0], _beads[1]));
 }
 
-inline vec IBond::Grad(const Topology &top, int bead)
-{
-    vec r = top.getDist(_beads[0], _beads[1]); 
-    r.normalize();
-    return (bead == 0) ? -r : r;
-}
-    
-inline double IAngle::EvaluateVar(const Topology &top)
-{
-    vec v1(top.getDist(_beads[1], _beads[0]));
-    vec v2(top.getDist(_beads[1], _beads[2]));
-    return  acos(v1*v2/sqrt((v1*v1) * (v2*v2)));    
+inline vec IBond::Grad(const Topology &top, int bead) {
+  vec r = top.getDist(_beads[0], _beads[1]);
+  r.normalize();
+  return (bead == 0) ? -r : r;
 }
 
-inline vec IAngle::Grad(const Topology &top, int bead)
-{
-    vec v1(top.getDist(_beads[1], _beads[0]));
-    vec v2(top.getDist(_beads[1], _beads[2]));
-    
-    double acos_prime = 1.0 / (sqrt(1 - (v1*v2) * (v1*v2)/( abs(v1) * abs(v2) * abs(v1) * abs(v2) ) ));
-    switch (bead) {
-        case (0): return acos_prime * (-v2 / ( abs(v1)*abs(v2) ) +  (v1*v2) * v1 / ( abs(v2)*abs(v1)*abs(v1)*abs(v1) ) ); break;
-        case (1): return acos_prime * ( (v1+v2)/(abs(v1) * abs(v2)) - (v1 * v2) * ((v2*v2) * v1 + (v1*v1) * v2 ) / ( abs(v1)*abs(v1)*abs(v1)*abs(v2)*abs(v2)*abs(v2) ) ); break;
-        case (2): return acos_prime * (-v1 / ( abs(v1)*abs(v2) ) +  (v1*v2) * v2 / ( abs(v1)*abs(v2)*abs(v2)*abs(v2) ) ); break;
-    }
-    // should never reach this
-    assert(false);
-    return vec(0,0,0);
+inline double IAngle::EvaluateVar(const Topology &top) {
+  vec v1(top.getDist(_beads[1], _beads[0]));
+  vec v2(top.getDist(_beads[1], _beads[2]));
+  return acos(v1 * v2 / sqrt((v1 * v1) * (v2 * v2)));
 }
 
-inline double IDihedral::EvaluateVar(const Topology &top)
-{
-    vec v1(top.getDist(_beads[0], _beads[1]));
-    vec v2(top.getDist(_beads[1], _beads[2]));
-    vec v3(top.getDist(_beads[2], _beads[3]));
-    vec n1, n2;
-    n1 = v1^v2; // calculate the normal vector
-    n2 = v2^v3; // calculate the normal vector
-    double sign = (v1*n2 < 0) ? -1 : 1;
-    return sign*acos(n1*n2/sqrt((n1*n1) * (n2*n2)));
+inline vec IAngle::Grad(const Topology &top, int bead) {
+  vec v1(top.getDist(_beads[1], _beads[0]));
+  vec v2(top.getDist(_beads[1], _beads[2]));
+
+  double acos_prime =
+      1.0 /
+      (sqrt(1 -
+            (v1 * v2) * (v1 * v2) / (abs(v1) * abs(v2) * abs(v1) * abs(v2))));
+  switch (bead) {
+  case (0):
+    return acos_prime *
+           (-v2 / (abs(v1) * abs(v2)) +
+            (v1 * v2) * v1 / (abs(v2) * abs(v1) * abs(v1) * abs(v1)));
+    break;
+  case (1):
+    return acos_prime *
+           ((v1 + v2) / (abs(v1) * abs(v2)) -
+            (v1 * v2) * ((v2 * v2) * v1 + (v1 * v1) * v2) /
+                (abs(v1) * abs(v1) * abs(v1) * abs(v2) * abs(v2) * abs(v2)));
+    break;
+  case (2):
+    return acos_prime *
+           (-v1 / (abs(v1) * abs(v2)) +
+            (v1 * v2) * v2 / (abs(v1) * abs(v2) * abs(v2) * abs(v2)));
+    break;
+  }
+  // should never reach this
+  assert(false);
+  return vec(0, 0, 0);
 }
 
-inline vec IDihedral::Grad(const Topology &top, int bead)
-{
-    vec v1(top.getDist(_beads[0], _beads[1]));
-    vec v2(top.getDist(_beads[1], _beads[2]));
-    vec v3(top.getDist(_beads[2], _beads[3]));
-    vec n1, n2;
-    n1 = v1^v2; // calculate the normal vector
-    n2 = v2^v3; // calculate the normal vector
-    double sign = (v1*n2 < 0) ? -1 : 1;
-    vec returnvec; //vector to return
-    double returnvec0,returnvec1,returnvec2; //components of the return vector    
-    vec e0(1,0,0); //unit vector pointing in x-direction
-    vec e1(0,1,0); //unit vector pointing in y-direction
-    vec e2(0,0,1); //unit vector pointing in z-direction
-    
-    
-    double acos_prime = ( - 1.0 / (sqrt(1 - (n1*n2) * (n1*n2)/( abs(n1) * abs(n2) * abs(n1) * abs(n2) ) )) ) * sign;
-    switch (bead) {
-        case (0): { //
-                    returnvec0 = acos_prime * ( ( n2*(v2^e0) )/( abs(n1)*abs(n2) ) - ( (n1*n2)*( n1*(v2^e0) ) )/( abs(n1)*abs(n1)*abs(n1)*abs(n2) ) );
-                    returnvec1 = acos_prime * ( ( n2*(v2^e1) )/( abs(n1)*abs(n2) ) - ( (n1*n2)*( n1*(v2^e1) ) )/( abs(n1)*abs(n1)*abs(n1)*abs(n2) ) );
-                    returnvec2 = acos_prime * ( ( n2*(v2^e2) )/( abs(n1)*abs(n2) ) - ( (n1*n2)*( n1*(v2^e2) ) )/( abs(n1)*abs(n1)*abs(n1)*abs(n2) ) );
-                    returnvec.setX(returnvec0);
-                    returnvec.setY(returnvec1);
-                    returnvec.setZ(returnvec2);
-                    return returnvec;
-                    break;
-        }             
-        case (1): { //
-                    returnvec0 = acos_prime * ( ( n1*(v3^e0)+n2*((e0^v1)+(e0^v2)) )/( abs(n1)*abs(n2) ) - ( (n1*n2)* ( ( n1*((e0^v1)+(e0^v2)) )/( abs(n1)*abs(n1)*abs(n1)*abs(n2) ) + ( n2*(v3^e0) )/( abs(n1)*abs(n2)*abs(n2)*abs(n2) ) ) ) );
-                    returnvec1 = acos_prime * ( ( n1*(v3^e1)+n2*((e1^v1)+(e1^v2)) )/( abs(n1)*abs(n2) ) - ( (n1*n2)* ( ( n1*((e1^v1)+(e1^v2)) )/( abs(n1)*abs(n1)*abs(n1)*abs(n2) ) + ( n2*(v3^e1) )/( abs(n1)*abs(n2)*abs(n2)*abs(n2) ) ) ) );
-                    returnvec2 = acos_prime * ( ( n1*(v3^e2)+n2*((e2^v1)+(e2^v2)) )/( abs(n1)*abs(n2) ) - ( (n1*n2)* ( ( n1*((e2^v1)+(e2^v2)) )/( abs(n1)*abs(n1)*abs(n1)*abs(n2) ) + ( n2*(v3^e2) )/( abs(n1)*abs(n2)*abs(n2)*abs(n2) ) ) ) );
-                    returnvec.setX(returnvec0);
-                    returnvec.setY(returnvec1);
-                    returnvec.setZ(returnvec2);
-                    return returnvec;
-                    break;
-        } ;
-        case (2): { //
-                    returnvec0 = acos_prime * ( ( n1*((e0^v2)+(e0^v3))+n2*(v1^e0) )/( abs(n1)*abs(n2) ) - ( (n1*n2)* ( ( n1*(v1^e0) )/( abs(n1)*abs(n1)*abs(n1)*abs(n2) ) + ( n2*((e0^v2)+(e0^v3)) )/( abs(n1)*abs(n2)*abs(n2)*abs(n2) ) ) ) );
-                    returnvec1 = acos_prime * ( ( n1*((e1^v2)+(e1^v3))+n2*(v1^e1) )/( abs(n1)*abs(n2) ) - ( (n1*n2)* ( ( n1*(v1^e1) )/( abs(n1)*abs(n1)*abs(n1)*abs(n2) ) + ( n2*((e1^v2)+(e1^v3)) )/( abs(n1)*abs(n2)*abs(n2)*abs(n2) ) ) ) );
-                    returnvec2 = acos_prime * ( ( n1*((e2^v2)+(e2^v3))+n2*(v1^e2) )/( abs(n1)*abs(n2) ) - ( (n1*n2)* ( ( n1*(v1^e2) )/( abs(n1)*abs(n1)*abs(n1)*abs(n2) ) + ( n2*((e2^v2)+(e2^v3)) )/( abs(n1)*abs(n2)*abs(n2)*abs(n2) ) ) ) );
-                    returnvec.setX(returnvec0);
-                    returnvec.setY(returnvec1);                    
-                    returnvec.setZ(returnvec2);
-                    return returnvec;
-                    break;
-        } ;
-        case (3): { //
-                    returnvec0 = acos_prime * ( ( n1*(v2^e0) )/( abs(n1)*abs(n2) ) - ( (n1*n2)*( n2*(v2^e0) ) )/( abs(n1)*abs(n2)*abs(n2)*abs(n2) ) );
-                    returnvec1 = acos_prime * ( ( n1*(v2^e1) )/( abs(n1)*abs(n2) ) - ( (n1*n2)*( n2*(v2^e1) ) )/( abs(n1)*abs(n2)*abs(n2)*abs(n2) ) );
-                    returnvec2 = acos_prime * ( ( n1*(v2^e2) )/( abs(n1)*abs(n2) ) - ( (n1*n2)*( n2*(v2^e2) ) )/( abs(n1)*abs(n2)*abs(n2)*abs(n2) ) );
-                    returnvec.setX(returnvec0);
-                    returnvec.setY(returnvec1);
-                    returnvec.setZ(returnvec2);
-                    return returnvec;
-                    break;
-        } ;
-    }
-    // should never reach this
-    assert(false);
-    return vec(0,0,0);
+inline double IDihedral::EvaluateVar(const Topology &top) {
+  vec v1(top.getDist(_beads[0], _beads[1]));
+  vec v2(top.getDist(_beads[1], _beads[2]));
+  vec v3(top.getDist(_beads[2], _beads[3]));
+  vec n1, n2;
+  n1 = v1 ^ v2; // calculate the normal vector
+  n2 = v2 ^ v3; // calculate the normal vector
+  double sign = (v1 * n2 < 0) ? -1 : 1;
+  return sign * acos(n1 * n2 / sqrt((n1 * n1) * (n2 * n2)));
 }
 
-}}
+inline vec IDihedral::Grad(const Topology &top, int bead) {
+  vec v1(top.getDist(_beads[0], _beads[1]));
+  vec v2(top.getDist(_beads[1], _beads[2]));
+  vec v3(top.getDist(_beads[2], _beads[3]));
+  vec n1, n2;
+  n1 = v1 ^ v2; // calculate the normal vector
+  n2 = v2 ^ v3; // calculate the normal vector
+  double sign = (v1 * n2 < 0) ? -1 : 1;
+  vec returnvec;                             // vector to return
+  double returnvec0, returnvec1, returnvec2; // components of the return vector
+  vec e0(1, 0, 0); // unit vector pointing in x-direction
+  vec e1(0, 1, 0); // unit vector pointing in y-direction
+  vec e2(0, 0, 1); // unit vector pointing in z-direction
 
-#endif	// _VOTCA_CSG_INTERACTION_H 
+  double acos_prime =
+      (-1.0 / (sqrt(1 -
+                    (n1 * n2) * (n1 * n2) /
+                        (abs(n1) * abs(n2) * abs(n1) * abs(n2))))) *
+      sign;
+  switch (bead) {
+  case (0): { //
+    returnvec0 = acos_prime * (
+      (n2 * (v2 ^ e0)) / (abs(n1) * abs(n2)) -
+      ((n1 * n2) * (n1 * (v2 ^ e0))) / (abs(n1) * abs(n1) * abs(n1) * abs(n2)));
+    returnvec1 = acos_prime * (
+      (n2 * (v2 ^ e1)) / (abs(n1) * abs(n2)) -
+      ((n1 * n2) * (n1 * (v2 ^ e1))) / (abs(n1) * abs(n1) * abs(n1) * abs(n2)));
+    returnvec2 = acos_prime * (
+      (n2 * (v2 ^ e2)) / (abs(n1) * abs(n2)) -
+      ((n1 * n2) * (n1 * (v2 ^ e2))) / (abs(n1) * abs(n1) * abs(n1) * abs(n2)));
+    returnvec.setX(returnvec0);
+    returnvec.setY(returnvec1);
+    returnvec.setZ(returnvec2);
+    return returnvec;
+    break;
+  }
+  case (1): { //
+    returnvec0 =
+        acos_prime *
+        ((n1 * (v3 ^ e0) + n2 * ((e0 ^ v1) + (e0 ^ v2))) / (abs(n1) * abs(n2)) -
+         ((n1 * n2) *
+          ((n1 * ((e0 ^ v1) + (e0 ^ v2))) /
+               (abs(n1) * abs(n1) * abs(n1) * abs(n2)) +
+           (n2 * (v3 ^ e0)) / (abs(n1) * abs(n2) * abs(n2) * abs(n2)))));
+    returnvec1 =
+        acos_prime *
+        ((n1 * (v3 ^ e1) + n2 * ((e1 ^ v1) + (e1 ^ v2))) / (abs(n1) * abs(n2)) -
+         ((n1 * n2) *
+          ((n1 * ((e1 ^ v1) + (e1 ^ v2))) /
+               (abs(n1) * abs(n1) * abs(n1) * abs(n2)) +
+           (n2 * (v3 ^ e1)) / (abs(n1) * abs(n2) * abs(n2) * abs(n2)))));
+    returnvec2 =
+        acos_prime *
+        ((n1 * (v3 ^ e2) + n2 * ((e2 ^ v1) + (e2 ^ v2))) / (abs(n1) * abs(n2)) -
+         ((n1 * n2) *
+          ((n1 * ((e2 ^ v1) + (e2 ^ v2))) /
+               (abs(n1) * abs(n1) * abs(n1) * abs(n2)) +
+           (n2 * (v3 ^ e2)) / (abs(n1) * abs(n2) * abs(n2) * abs(n2)))));
+    returnvec.setX(returnvec0);
+    returnvec.setY(returnvec1);
+    returnvec.setZ(returnvec2);
+    return returnvec;
+    break;
+  };
+  case (2): { //
+    returnvec0 =
+        acos_prime *
+        ((n1 * ((e0 ^ v2) + (e0 ^ v3)) + n2 * (v1 ^ e0)) / (abs(n1) * abs(n2)) -
+         ((n1 * n2) *
+          ((n1 * (v1 ^ e0)) / (abs(n1) * abs(n1) * abs(n1) * abs(n2)) +
+           (n2 * ((e0 ^ v2) + (e0 ^ v3))) /
+               (abs(n1) * abs(n2) * abs(n2) * abs(n2)))));
+    returnvec1 =
+        acos_prime *
+        ((n1 * ((e1 ^ v2) + (e1 ^ v3)) + n2 * (v1 ^ e1)) / (abs(n1) * abs(n2)) -
+         ((n1 * n2) *
+          ((n1 * (v1 ^ e1)) / (abs(n1) * abs(n1) * abs(n1) * abs(n2)) +
+           (n2 * ((e1 ^ v2) + (e1 ^ v3))) /
+               (abs(n1) * abs(n2) * abs(n2) * abs(n2)))));
+    returnvec2 =
+        acos_prime *
+        ((n1 * ((e2 ^ v2) + (e2 ^ v3)) + n2 * (v1 ^ e2)) / (abs(n1) * abs(n2)) -
+         ((n1 * n2) *
+          ((n1 * (v1 ^ e2)) / (abs(n1) * abs(n1) * abs(n1) * abs(n2)) +
+           (n2 * ((e2 ^ v2) + (e2 ^ v3))) /
+               (abs(n1) * abs(n2) * abs(n2) * abs(n2)))));
+    returnvec.setX(returnvec0);
+    returnvec.setY(returnvec1);
+    returnvec.setZ(returnvec2);
+    return returnvec;
+    break;
+  };
+  case (3): { //
+    returnvec0 = acos_prime * ((n1 * (v2 ^ e0)) / (abs(n1) * abs(n2)) -
+                               ((n1 * n2) * (n2 * (v2 ^ e0))) /
+                                   (abs(n1) * abs(n2) * abs(n2) * abs(n2)));
+    returnvec1 = acos_prime * ((n1 * (v2 ^ e1)) / (abs(n1) * abs(n2)) -
+                               ((n1 * n2) * (n2 * (v2 ^ e1))) /
+                                   (abs(n1) * abs(n2) * abs(n2) * abs(n2)));
+    returnvec2 = acos_prime * ((n1 * (v2 ^ e2)) / (abs(n1) * abs(n2)) -
+                               ((n1 * n2) * (n2 * (v2 ^ e2))) /
+                                   (abs(n1) * abs(n2) * abs(n2) * abs(n2)));
+    returnvec.setX(returnvec0);
+    returnvec.setY(returnvec1);
+    returnvec.setZ(returnvec2);
+    return returnvec;
+    break;
+  };
+  }
+  // should never reach this
+  assert(false);
+  return vec(0, 0, 0);
+}
+}
+}
 
+#endif // _VOTCA_CSG_INTERACTION_H
--- src/tests/test_interaction.cc
+++ src/tests/test_interaction.cc
@@ -22,10 +22,10 @@
 
 #include <string>
 #include <votca/csg/bead.h>
-#include <votca/csg/interaction.h>
 #include <votca/csg/beadtype.h>
-#include <votca/csg/topology.h>
+#include <votca/csg/interaction.h>
 #include <votca/csg/molecule.h>
+#include <votca/csg/topology.h>
 #include <votca/tools/vec.h>
 
 using namespace std;
@@ -34,35 +34,35 @@ using namespace votca::csg;
 BOOST_AUTO_TEST_SUITE(interaction_test)
 
 BOOST_AUTO_TEST_CASE(test_interaction_constructor) {
-  IBond bond1(1,2);
-  IAngle angle1(1,2,3);
-  IDihedral dihedral(1,2,3,4);
+  IBond bond1(1, 2);
+  IAngle angle1(1, 2, 3);
+  IDihedral dihedral(1, 2, 3, 4);
 }
 
-BOOST_AUTO_TEST_CASE(test_interaction_setters_getters){
+BOOST_AUTO_TEST_CASE(test_interaction_setters_getters) {
 
-  IBond bond1(1,2);
+  IBond bond1(1, 2);
   bond1.setGroup("large");
   bond1.setGroupId(1);
   bond1.setIndex(1);
   bond1.setMolecule(1);
-  
+
   string name = bond1.getName();
   cout << name << endl;
-  bool correctName = name.compare("molecule 1:large 1:index 1")==0;
+  bool correctName = name.compare("molecule 1:large 1:index 1") == 0;
   BOOST_CHECK(correctName);
-  BOOST_CHECK_EQUAL(bond1.getGroupId(),1);  
-  BOOST_CHECK_EQUAL(bond1.getIndex(),1);  
-  BOOST_CHECK_EQUAL(bond1.getMolecule(),1);  
-  BOOST_CHECK_EQUAL(bond1.BeadCount(),2);  
-  BOOST_CHECK_EQUAL(bond1.getBeadId(0),1);  
-  BOOST_CHECK_EQUAL(bond1.getBeadId(1),2);  
+  BOOST_CHECK_EQUAL(bond1.getGroupId(), 1);
+  BOOST_CHECK_EQUAL(bond1.getIndex(), 1);
+  BOOST_CHECK_EQUAL(bond1.getMolecule(), 1);
+  BOOST_CHECK_EQUAL(bond1.BeadCount(), 2);
+  BOOST_CHECK_EQUAL(bond1.getBeadId(0), 1);
+  BOOST_CHECK_EQUAL(bond1.getBeadId(1), 2);
   string groupName = bond1.getGroup();
-  correctName = groupName.compare("large")==0;
-  
+  correctName = groupName.compare("large") == 0;
+
   BOOST_CHECK(correctName);
 
-  IAngle angle1(1,2,3);
+  IAngle angle1(1, 2, 3);
   angle1.setGroup("medium");
   angle1.setGroupId(1);
   angle1.setIndex(1);
@@ -70,19 +70,19 @@ BOOST_AUTO_TEST_CASE(test_interaction_setters_getters){
 
   name = angle1.getName();
   cout << name << endl;
-  correctName = name.compare("molecule 1:medium 1:index 1")==0;
+  correctName = name.compare("molecule 1:medium 1:index 1") == 0;
   BOOST_CHECK(correctName);
-  BOOST_CHECK_EQUAL(angle1.getGroupId(),1);  
-  BOOST_CHECK_EQUAL(angle1.getIndex(),1);  
-  BOOST_CHECK_EQUAL(angle1.getMolecule(),1);  
-  BOOST_CHECK_EQUAL(angle1.BeadCount(),3);  
-  BOOST_CHECK_EQUAL(angle1.getBeadId(0),1);  
-  BOOST_CHECK_EQUAL(angle1.getBeadId(1),2);  
-  BOOST_CHECK_EQUAL(angle1.getBeadId(2),3);  
+  BOOST_CHECK_EQUAL(angle1.getGroupId(), 1);
+  BOOST_CHECK_EQUAL(angle1.getIndex(), 1);
+  BOOST_CHECK_EQUAL(angle1.getMolecule(), 1);
+  BOOST_CHECK_EQUAL(angle1.BeadCount(), 3);
+  BOOST_CHECK_EQUAL(angle1.getBeadId(0), 1);
+  BOOST_CHECK_EQUAL(angle1.getBeadId(1), 2);
+  BOOST_CHECK_EQUAL(angle1.getBeadId(2), 3);
   groupName = angle1.getGroup();
-  correctName = groupName.compare("medium")==0;
- 
-  IDihedral dihedral1(1,2,3,4);
+  correctName = groupName.compare("medium") == 0;
+
+  IDihedral dihedral1(1, 2, 3, 4);
   dihedral1.setGroup("small");
   dihedral1.setGroupId(1);
   dihedral1.setIndex(1);
@@ -90,19 +90,18 @@ BOOST_AUTO_TEST_CASE(test_interaction_setters_getters){
 
   name = dihedral1.getName();
   cout << name << endl;
-  correctName = name.compare("molecule 1:small 1:index 1")==0;
+  correctName = name.compare("molecule 1:small 1:index 1") == 0;
   BOOST_CHECK(correctName);
-  BOOST_CHECK_EQUAL(dihedral1.getGroupId(),1);  
-  BOOST_CHECK_EQUAL(dihedral1.getIndex(),1);  
-  BOOST_CHECK_EQUAL(dihedral1.getMolecule(),1);  
-  BOOST_CHECK_EQUAL(dihedral1.BeadCount(),4);  
-  BOOST_CHECK_EQUAL(dihedral1.getBeadId(0),1);  
-  BOOST_CHECK_EQUAL(dihedral1.getBeadId(1),2);  
-  BOOST_CHECK_EQUAL(dihedral1.getBeadId(2),3);  
-  BOOST_CHECK_EQUAL(dihedral1.getBeadId(3),4);  
+  BOOST_CHECK_EQUAL(dihedral1.getGroupId(), 1);
+  BOOST_CHECK_EQUAL(dihedral1.getIndex(), 1);
+  BOOST_CHECK_EQUAL(dihedral1.getMolecule(), 1);
+  BOOST_CHECK_EQUAL(dihedral1.BeadCount(), 4);
+  BOOST_CHECK_EQUAL(dihedral1.getBeadId(0), 1);
+  BOOST_CHECK_EQUAL(dihedral1.getBeadId(1), 2);
+  BOOST_CHECK_EQUAL(dihedral1.getBeadId(2), 3);
+  BOOST_CHECK_EQUAL(dihedral1.getBeadId(3), 4);
   groupName = dihedral1.getGroup();
-  correctName = groupName.compare("small")==0;
- 
+  correctName = groupName.compare("small") == 0;
 }
 
 BOOST_AUTO_TEST_SUITE_END()
