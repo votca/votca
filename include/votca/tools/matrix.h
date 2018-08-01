a6493cd2c7214185ad3ac24fdc1b7373d63eb657
JoshuaSBrown <josh@votca.org>
2018-08-01 16:21:02 -0600

    add type conversion

--- include/votca/tools/matrix.h
+++ include/votca/tools/matrix.h
@@ -253,7 +253,7 @@ class matrix {
 };
 
 inline void matrix::checkBounds_(const byte_t row_or_col) const {
-  assert( row_or_col > -1 && row_or_col < 3);
+  assert( lexical_cast<int>(row_or_col) > -1 && lexical_cast<int>(row_or_col) < 3);
 } 
 
 inline matrix &matrix::operator=(const double &v) {
