a4ceafbad95a201ac0a03e3daaa5d16772b027b8
JoshuaSBrown <josh@votca.org>
2018-08-08 02:14:32 -0600

    Addressed diamond depency issue here with virtualization of inheritance

--- include/votca/csg/basebead.h
+++ include/votca/csg/basebead.h
@@ -39,7 +39,7 @@ class BeadType;
  * charge and the residue it belongs to and the position
  *
  **/
-class BaseBead : public TopologyItem, public Name, public Identity<int> {
+class BaseBead : public TopologyItem, virtual Name, virtual Identity<int> {
 public:
   /**
    * destructor
