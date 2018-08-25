e2ab606b9a4bbfbafd52c069c4d317d5f0ccb7ac
JoshuaSBrown <josh@votca.org>
2018-08-25 15:15:41 -0600

    Create moleculeitem.h

---
+++ include/votca/csg/moleculeitem.h
@@ -0,0 +1,43 @@
+/* 
+ * Copyright 2009-2018 The VOTCA Development Team (http://www.votca.org)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#ifndef _VOTCA_CSG_MOLECULEITEM_H
+#define	_VOTCA_CSG_MOLECULEITEM_H
+
+namespace votca { namespace csg {
+
+class Molecule;
+
+class MoleculeItem
+{
+public:    
+    virtual ~MoleculeItem() {}
+    Molecule *getMolecule() { return _mol; }
+    void setMolecule(Molecule *mol) { _mol = mol; }
+
+protected:
+    MoleculeItem(Molecule *mol)
+        : _mol(mol) {}
+    
+    Molecule *_mol;
+    
+};
+
+}}
+
+#endif	// _VOTCA_CSG_MOLECULEITEM_H
+
