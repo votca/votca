989d1422c9df7827bd3c1d13ef5d8a009a1b7652
JoshuaSBrown <josh@votca.org>
2018-10-07 18:02:12 -0600

    Switched to relative path for includes as opposed to absolute path

--- src/libtools/thread.cc
+++ src/libtools/thread.cc
@@ -16,8 +16,8 @@
  */
 
 #include <stdexcept>
-#include <votca/tools/lexical_cast.h>
-#include <votca/tools/thread.h>
+#include "../../votca/tools/lexical_cast.h"
+#include "../../votca/tools/thread.h"
 
 using namespace std;
 
