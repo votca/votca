6c82b89c1fb93b4a41597b72c0b5ee8d3df6b63f
Majklikikik <michaelstanek97@gmail.com>
2018-10-11 16:22:05 +0200

    Made huffmanNode private

--- include/votca/xtp/huffmantree.h
+++ include/votca/xtp/huffmantree.h
@@ -12,28 +12,31 @@ namespace votca { namespace xtp {
 class huffmanTree
 {
 
-    struct huffmanNode{
-    //huffmanNode * for the inner nodes, GLink * for the nodes on the last level before the leafs (The GLinks themselves represent the "leaf" level)
-    huffmanNode * leftChild;
-    huffmanNode * rightChild;
-    GLink * rightLeaf;
-    GLink * leftLeaf;
-    double probability;
-    bool isOnLastLevel=false;
-};
-
+    
 public:
     void makeTree();
     GLink *findHoppingDestination(double p);
     void setEvents(std::vector<GLink> * v);
 
 private:
+    struct huffmanNode{
+        //huffmanNode * for the inner nodes, GLink * for the nodes on the last level before the leafs (The GLinks themselves represent the "leaf" level)
+        huffmanNode * leftChild;
+        huffmanNode * rightChild;
+        GLink * rightLeaf;
+        GLink * leftLeaf;
+        double probability;
+        bool isOnLastLevel=false;
+    };
+
+
     void addProbabilityFromRightSubtreeToLeftSubtree(huffmanNode *n, double add);
     void moveProbabilitiesFromRightSubtreesOneLevelUp(huffmanNode *n);
     vector <huffmanNode> htree;
     bool treeIsMade=false;
     double escape_rate;
     vector <GLink> * events=nullptr;
+
 
 };
 
