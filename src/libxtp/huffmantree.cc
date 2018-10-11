62d0e42908aa5ca02427f835b8756b143c6122a5
Majklikikik <michaelstanek97@gmail.com>
2018-10-11 13:29:03 +0200

    Made Improvements for Readability

--- src/libxtp/huffmantree.cc
+++ src/libxtp/huffmantree.cc
@@ -10,8 +10,9 @@ using namespace std;
 
 namespace votca {namespace xtp {
 
-huffmanTree::huffmanTree()
-{
+
+void huffmanTree::setEvents(vector<GLink> * v){
+    this->events=v;
 }
 
 //https://en.wikipedia.org/wiki/Huffman_coding
@@ -21,6 +22,8 @@ void huffmanTree::makeTree(){
     //queue of the nodes, sorted by probability
     auto compare = [](huffmanNode * n1, huffmanNode * n2)
     { return n1->probability>n2->probability;};
+
+    //priority queues, because the algorithm always needs the element with the smallest probability. Also, it keep adding nodes to it, so it would we very inefficient to sort it in every iteration.
     priority_queue<huffmanNode *,vector<huffmanNode *>, decltype(compare)> queue(compare);
 
     htree=vector<huffmanNode>(events->size()%2?events->size():events->size()-1);
@@ -30,28 +33,29 @@ void huffmanTree::makeTree(){
     };
     priority_queue<GLink *, vector<GLink *>, decltype(comp2)> eventQueue(comp2);
     escape_rate=0.0;
-    int i=0;
+
+    int firstEmptyFieldIndex=0;
     for (GLink &e:*events){
         eventQueue.push(&e);
         escape_rate+=e.rate;
      }
     while (eventQueue.size()>1){
-        htree[i].isOnLastLevel=true;
-        htree[i].leftLeaf=eventQueue.top();
+        htree[firstEmptyFieldIndex].isOnLastLevel=true;
+        htree[firstEmptyFieldIndex].leftLeaf=eventQueue.top();
         eventQueue.pop();
-        htree[i].rightLeaf=eventQueue.top();
+        htree[firstEmptyFieldIndex].rightLeaf=eventQueue.top();
         eventQueue.pop();
-        htree[i].probability=(htree[i].leftLeaf->rate+htree[i].rightLeaf->rate)/escape_rate;
-        queue.push(&(htree[i]));
-        i++;
+        htree[firstEmptyFieldIndex].probability=(htree[firstEmptyFieldIndex].leftLeaf->rate+htree[firstEmptyFieldIndex].rightLeaf->rate)/escape_rate;
+        queue.push(&(htree[firstEmptyFieldIndex]));
+        firstEmptyFieldIndex++;
     }
     if (!eventQueue.empty()){
-        htree[i].isOnLastLevel=true;
-        htree[i].rightLeaf=eventQueue.top();
-        htree[i].leftLeaf=eventQueue.top();
-        htree[i].probability=2*htree[i].leftLeaf->rate/escape_rate;
-        queue.push(&(htree[i]));
-        i++;
+        htree[firstEmptyFieldIndex].isOnLastLevel=true;
+        htree[firstEmptyFieldIndex].rightLeaf=eventQueue.top();
+        htree[firstEmptyFieldIndex].leftLeaf=eventQueue.top();
+        htree[firstEmptyFieldIndex].probability=2*htree[firstEmptyFieldIndex].leftLeaf->rate/escape_rate;
+        queue.push(&(htree[firstEmptyFieldIndex]));
+        firstEmptyFieldIndex++;
     }
 
     //now connect the hnodes, making a new one for every connection:
@@ -63,22 +67,55 @@ void huffmanTree::makeTree(){
         queue.pop();
         h2=queue.top();
         queue.pop();
-        htree[i].probability=h1->probability+h2->probability;
-        htree[i].leftChild=h1;
-        htree[i].rightChild=h2;
-        queue.push(&(htree[i]));
-        i++;
+        htree[firstEmptyFieldIndex].probability=h1->probability+h2->probability;
+        htree[firstEmptyFieldIndex].leftChild=h1;
+        htree[firstEmptyFieldIndex].rightChild=h2;
+        queue.push(&(htree[firstEmptyFieldIndex]));
+        firstEmptyFieldIndex++;
     }
     //reorganize the probabilities: in every node, add the probability of one subtree ("small")
     //to all nodes of the other subtree.
-    organizeProbabilities(&htree[htree.size()-1],0);
-    moveProbabilities(&htree[htree.size()-1]);
+    addProbabilityFromRightSubtreeToLeftSubtree(&htree[htree.size()-1],0);
+    moveProbabilitiesFromRightSubtreesOneLevelUp(&htree[htree.size()-1]);
     treeIsMade=true;
 }
 
 
 
-void huffmanTree::organizeProbabilities(huffmanNode * n,double add){
+void huffmanTree::addProbabilityFromRightSubtreeToLeftSubtree(huffmanNode * n,double add){
+    //for each node, adds the probability of the right childnode to the left childnode and every node under it. 
+    //if the Tree would look like this (with the Numbers representing the probability of every node) before calling this function
+
+    //           1.0
+    //           /\
+    //          /  \
+    //         /    \
+    //        /      \
+    //       /        \
+    //     0.4       0.6
+    //     / \       / \
+    //    /   \     /   \
+    //  0.25 0.15 0.35 0.25
+    //        /\
+    //       /  \
+    //     0.1 0.05
+    //then it would look like this after calling it
+    //           1.0
+    //           /\
+    //          /  \
+    //         /    \
+    //        /      \
+    //       /        \
+    //     1.0       0.6
+    //     / \       / \
+    //    /   \     /   \
+    //   1.0 0.75 0.6  0.25
+    //        /\
+    //       /  \
+    //     0.75 0.65
+    //now the tree could be traversed with "while (!n.isLeaf()) n=p>n.right.p?n.left:n.right"
+    //so in the function moveProbabilitiesFromRightSubtreesOneLevelUp the numbers are moved one level up to call n.p instead of n.right.p
+
 
     //adds "add" to the probability, then calls itself recursively.
     //this calculates the probabilities needed to traverse the tree quickly
@@ -88,20 +125,50 @@ void huffmanTree::organizeProbabilities(huffmanNode * n,double add){
         return;
     }
 
-    organizeProbabilities(n->leftChild,add+n->rightChild->probability);
-    organizeProbabilities(n->rightChild,add);
+    addProbabilityFromRightSubtreeToLeftSubtree(n->leftChild,add+n->rightChild->probability);
+    addProbabilityFromRightSubtreeToLeftSubtree(n->rightChild,add);
 }
 
 
 
-void huffmanTree::moveProbabilities(huffmanNode * n){
+void huffmanTree::moveProbabilitiesFromRightSubtreesOneLevelUp(huffmanNode * n){
+    //moves the Probabilities on the right subtrees one level up.
+    //if the Tree would look like this (with the Numbers representing the probability of every node) before calling this function
+    //           1.0
+    //           /\
+    //          /  \
+    //         /    \
+    //        /      \
+    //       /        \
+    //     1.0       0.6
+    //     / \       / \
+    //    /   \     /   \
+    //   1.0 0.75 0.6  0.25
+    //        /\
+    //       /  \
+    //     0.75 0.65
+    //then it would look like this after calling it
+    //           0.6
+    //           /\
+    //          /  \
+    //         /    \
+    //        /      \
+    //       /        \
+    //     0.75      0.25
+    //     / \       / \
+    //    /   \     /   \
+    //   1.0 0.65 0.6  0.25
+    //        /\
+    //       /  \
+    //     0.75 0.65
+    //note, that now the probabilities on the leaf level are not needed anymore to traverse the tree; the algorithm now is "while (!n.isLeaf()) n=p>n.p?n.left:n.right"
     if (n->isOnLastLevel){
         n->probability-=n->leftLeaf->rate/escape_rate;
     }
     else{
         n->probability=n->rightChild->probability;
-        moveProbabilities(n->rightChild);
-        moveProbabilities(n->leftChild);
+        moveProbabilitiesFromRightSubtreesOneLevelUp(n->rightChild);
+        moveProbabilitiesFromRightSubtreesOneLevelUp(n->leftChild);
     }
 }
 
@@ -109,7 +176,7 @@ void huffmanTree::moveProbabilities(huffmanNode * n){
 
 GLink * huffmanTree::findHoppingDestination(double p){
     if (!treeIsMade) throw runtime_error("Tried to find Hopping Destination without initializing the Huffmantree first!");
-    huffmanNode * node=&(htree[htree.size()-1]);
+    huffmanNode * node=&htree.back();
     while (!node->isOnLastLevel){
         if (p>node->probability) node=node->leftChild;
         else node=node->rightChild;
