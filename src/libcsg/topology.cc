b184fc06c21c59a38836009583bc80d939114262
JoshuaSBrown <joshbro42867@yahoo.com>
2019-01-03 21:56:42 -0700

    Added get function to returning bead type

--- src/libcsg/topology.cc
+++ src/libcsg/topology.cc
@@ -281,7 +281,7 @@ BeadType& Topology::GetOrCreateBeadType(string name)
         auto bt = unique_ptr<BeadType>(new BeadType(this, _beadtypes.size(), name));
         _beadtypes.push_back(move(bt));
         _beadtype_map[name] = _beadtypes.back()->getId();
-        return *(_beadtypes.back());
+        return *(_beadtypes.back().get());
     }
 
     return  *(_beadtypes[(*iter).second]).get();
