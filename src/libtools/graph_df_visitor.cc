1c718fc3e907df634ad400d145b1e81120298b9b
JoshuaSBrown <joshbro42867@yahoo.com>
2019-01-07 14:17:08 -0700

    Added graph depth first visitor

---
+++ src/libtools/graph_df_visitor.cc
@@ -0,0 +1,48 @@
+/*
+ *            Copyright 2009-2018 The VOTCA Development Team
+ *                       (http://www.votca.org)
+ *
+ *      Licensed under the Apache License, Version 2.0 (the "License")
+ *
+ * You may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *              http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#include <votca/tools/edge.h>
+#include <votca/tools/graph.h>
+#include <votca/tools/graph_df_visitor.h>
+#include <votca/tools/graphnode.h>
+
+using namespace std;
+
+namespace votca {
+namespace tools {
+
+bool Graph_DF_Visitor::queEmpty() { return edge_que_.empty(); }
+
+Edge Graph_DF_Visitor::getEdge_(Graph g) {
+
+  Edge ed = edge_stack_.top();
+  edge_stack_.pop();
+  return ed;
+}
+
+// Add edges to be explored
+void Graph_DF_Visitor::addEdges_(Graph& g, int vertex) {
+
+  auto eds = g.getNeighEdges(vertex);
+  for(auto ed : eds){
+    edge_stack_.push(ed);
+  }
+}
+}
+}
