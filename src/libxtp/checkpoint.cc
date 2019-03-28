2394b78776ebb9c36dce1369a57bc42b6bfea78b
Pranav Madhikar <pmadhikar@gmail.com>
2019-03-28 15:49:29 +0100

    Enable hdf5 space reclamation
    Requires hdf5 1.9 or higher.

--- src/libxtp/checkpoint.cc
+++ src/libxtp/checkpoint.cc
@@ -63,18 +63,21 @@ CheckpointFile::CheckpointFile(std::string fN, CheckpointAccessLevel access)
 
   try {
     H5::Exception::dontPrint();
+    hid_t fcpl_id = H5Pcreate(H5P_FILE_CREATE);
+    H5Pset_file_space(fcpl_id, H5F_FILE_SPACE_ALL_PERSIST, 1);
+    H5::FileCreatPropList fcpList(fcpl_id);
     switch (_accessLevel) {
       case CheckpointAccessLevel::READ:
         _fileHandle = H5::H5File(_fileName, H5F_ACC_RDONLY);
         break;
       case CheckpointAccessLevel::CREATE:
-        _fileHandle = H5::H5File(_fileName, H5F_ACC_TRUNC);
+          _fileHandle = H5::H5File(_fileName, H5F_ACC_TRUNC, fcpList);
         break;
       case CheckpointAccessLevel::MODIFY:
         if (!FileExists(_fileName))
-          _fileHandle = H5::H5File(_fileName, H5F_ACC_TRUNC);
+            _fileHandle = H5::H5File(_fileName, H5F_ACC_TRUNC, fcpList);
         else
-          _fileHandle = H5::H5File(_fileName, H5F_ACC_RDWR);
+            _fileHandle = H5::H5File(_fileName, H5F_ACC_RDWR, fcpList);
     }
 
   } catch (H5::Exception& error) {
