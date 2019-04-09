92381b7a6e440209e2cda9d5ba529a5966fe75c8
marvinbernhardt <marvinbernhardt@users.noreply.github.com>
2019-04-09 18:40:44 +0200

    fixed hncgn script and xml

--- share/scripts/inverse/update_hncgn_single.sh
+++ share/scripts/inverse/update_hncgn_single.sh
@@ -33,6 +33,7 @@ scheme_nr=$(( ($step_nr - 1 ) % ${#scheme[@]} ))
 name=$(csg_get_interaction_property name)
 bondtype="$(csg_get_interaction_property bondtype)"
 extrap_near_core=$(csg_get_property cg.inverse.hncgn.extrap_near_core)
+fix_near_cut_off=$(csg_get_property cg.inverse.hncgn.fix_near_cut_off)
 
 pressure_constraint=$(csg_get_property cg.inverse.hncgn.pressure_constraint)
 if is_num ${pressure_constraint}; then
@@ -52,9 +53,11 @@ if [ "${scheme[$scheme_nr]}" = 1 ]; then
     cut_off="$(csg_get_property cg.inverse.hncgn.cut_off)"
     is_num "${kBT}" || die "${0##*/}: cg.inverse.kBT should be a number, but found '$kBT'"
     if is_num ${pressure_constraint}; then
-        do_external update hncgn_pot ${name}.dist.tgt ${name}.dist.new ${name}.pot.cur ${name}.dpot.pure_hncgn "${kBT}" "${density}" "${cut_off}" "--pressure-constraint=${pressure_constraint},${p_now}" "--extrap-near-core=${extrap_near_core}"
+        do_external update hncgn_pot ${name}.dist.tgt ${name}.dist.new ${name}.pot.cur ${name}.dpot.pure_hncgn "${kBT}" "${density}" "${cut_off}" \
+        "--pressure-constraint=${pressure_constraint},${p_now}" "--extrap-near-core=${extrap_near_core}" "--fix-near-cut-off=${fix_near_cut_off}"
     else
-        do_external update hncgn_pot ${name}.dist.tgt ${name}.dist.new ${name}.pot.cur ${name}.dpot.pure_hncgn "${kBT}" "${density}" "${cut_off}" "--extrap-near-core=${extrap_near_core}"
+        do_external update hncgn_pot ${name}.dist.tgt ${name}.dist.new ${name}.pot.cur ${name}.dpot.pure_hncgn "${kBT}" "${density}" "${cut_off}" \
+        "--extrap-near-core=${extrap_near_core}" "--fix-near-cut-off=${fix_near_cut_off}"
     fi
    do_external potential shift --type "${bondtype}" ${name}.dpot.pure_hncgn ${name}.dpot.new
 else
