1c354ada08e29cd0611062fcc181015da95c84e4
Joshua Scott Brown <joshbro42867@yahoo.com>
2019-05-10 16:56:56 -0600

    Added extra mass unit

--- include/votca/tools/unitconverter.h
+++ include/votca/tools/unitconverter.h
@@ -30,6 +30,7 @@ enum MassUnit {
   picograms,
   femtograms,
   atomic_mass_units,
+  grams_per_mole,  // the same as atomic mass units
   kilograms,
   grams
 };
@@ -93,6 +94,8 @@ class UnitConverter {
       case MassUnit::attograms:
         return 602214;
       case MassUnit::atomic_mass_units:
+        return 1.0;
+      case MassUnit::grams_per_mole:
         return 1.0;
     }
     return 0.0;
