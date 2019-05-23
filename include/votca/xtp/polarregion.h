b1d501bbf562b6e58bf0e7de0fd6e76027053585
Jens Wehner <jenswehner@gmail.com>
2019-05-23 11:57:10 +0200

    working on polarregion

---
+++ include/votca/xtp/polarregion.h
@@ -0,0 +1,62 @@
+/*
+ *            Copyright 2009-2019 The VOTCA Development Team
+ *                       (http://www.votca.org)
+ *
+ *      Licensed under the Apache License, Version 2.0 (the "License")
+ *
+ * You may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *              http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#pragma once
+#ifndef VOTCA_XTP_POLARREGION_H
+#define VOTCA_XTP_POLARREGION_H
+
+#include <votca/xtp/mmregion.h>
+
+namespace votca {
+namespace xtp {
+template <class T>
+class MMRegion : public Region {
+ public:
+  MMRegion(int id) : Region(id){};
+  void WriteToCpt(CheckpointWriter& w) const;
+
+  void ReadFromCpt(CheckpointReader& r);
+
+  int size() const { return _segments.size(); }
+
+  typename std::vector<T>::iterator begin() { return _segments.begin(); }
+  typename std::vector<T>::iterator end() { return _segments.end(); }
+
+  typename std::vector<T>::const_iterator begin() const {
+    return _segments.begin();
+  }
+  typename std::vector<T>::const_iterator end() const {
+    return _segments.end();
+  }
+
+  void WritePDB(csg::PDBWriter& writer) const;
+
+  std::string identify() const;
+  void push_back(const T& seg) { _segments.push_back(seg); }
+
+ private:
+  std::vector<T> _segments;
+};
+
+typedef MMRegion<StaticSegment> StaticRegion;
+
+}  // namespace xtp
+}  // namespace votca
+
+#endif /* VOTCA_XTP_MMREGION_H */
---
+++ src/libxtp/polarregion.cc
@@ -0,0 +1,79 @@
+/*
+ *            Copyright 2009-2019 The VOTCA Development Team
+ *                       (http://www.votca.org)
+ *
+ *      Licensed under the Apache License, Version 2.0 (the "License")
+ *
+ * You may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *              http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#include <votca/xtp/mmregion.h>
+
+namespace votca {
+namespace xtp {
+template <class T>
+std::string MMRegion<T>::identify() const {
+  return "";
+}
+
+template <>
+std::string PolarRegion::identify() const {
+  return "PolarRegion";
+}
+
+template <>
+std::string StaticRegion::identify() const {
+  return "StaticRegion";
+}
+
+template <class T>
+void MMRegion<T>::WritePDB(csg::PDBWriter& writer) const {
+  for (const auto& seg : _segments) {
+    writer.WriteContainer(seg);
+  }
+}
+
+template <class T>
+void MMRegion<T>::WriteToCpt(CheckpointWriter& w) const {
+  w(_id, "id");
+  w(identify(), "type");
+  int size = _segments.size();
+  w(size, "size");
+  CheckpointWriter ww = w.openChild("segments");
+  for (const auto& seg : _segments) {
+    CheckpointWriter www =
+        ww.openChild(seg.identify() + "_" + std::to_string(seg.getId()));
+    seg.WriteToCpt(www);
+  }
+}
+template <class T>
+void MMRegion<T>::ReadFromCpt(CheckpointReader& r) {
+  r(_id, "id");
+  int size;
+  r(size, "size");
+  _segments.clear();
+  _segments.reserve(size);
+  T dummy("dummy", 0);
+  CheckpointReader rr = r.openChild("segments");
+  for (int i = 0; i < size; i++) {
+    CheckpointReader rrr =
+        rr.openChild(dummy.identify() + "_" + std::to_string(i));
+    _segments.push_back(T(rrr));
+  }
+}
+
+template class MMRegion<PolarSegment>;
+template class MMRegion<StaticSegment>;
+
+}  // namespace xtp
+}  // namespace votca
