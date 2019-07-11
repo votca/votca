c7a3037d04b8c8cc42be80da11bc7c47dc1a4cc8
Jens Wehner <jenswehner@gmail.com>
2019-07-11 16:05:50 +0200

    cleaned up application a bit

--- include/votca/tools/application.h
+++ include/votca/tools/application.h
@@ -36,7 +36,6 @@ class Application {
    * \return return code
    */
   int Exec(int argc, char **argv);
-  int ExecThreaded(int argc, char **argv);
 
   /**
    * \brief program name
@@ -50,7 +49,7 @@ class Application {
    * \brief version string of application
    * \return version string
    */
-  virtual std::string VersionString();
+  virtual std::string VersionString() { return ""; }
 
   /**
    * \brief help text of application without version information
@@ -62,18 +61,18 @@ class Application {
    * \brief Initialize application data
    *
    * Initialize is called by run before parsing the command line.
-   * All necesassary command line arguments can be added here
+   * All necessary command line arguments can be added here
    */
-  virtual void Initialize() {}
+  virtual void Initialize() = 0;
 
   /**
    * \brief Process command line options
-   * \return true to contine, false to stop
+   * \return true to continue, false to stop
    *
    * EvaluateOptions is called by Run after parsing the command line.
    * return true if everything is ok, false to stop and show help text.
    */
-  virtual bool EvaluateOptions() { return false; }
+  virtual bool EvaluateOptions() = 0;
 
   /**
    * \brief Check weather required option is set
@@ -82,7 +81,7 @@ class Application {
    *
    * CheckRequired is called from EvaluateOptions if a required options is set.
    * If not, the list of possible options is shown and an exception with
-   * the error messig given in error_msg is thrown
+   * the error message given in error_msg is thrown
    */
   void CheckRequired(const std::string &option_name,
                      const std::string &error_msg = "");
