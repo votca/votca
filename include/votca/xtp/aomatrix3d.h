012d31c02a7b7a2a47600a45611825f2c963d0b5
Jens Wehner <jenswehner@gmail.com>
2019-07-17 18:59:19 +0200

    forgot some files

---
+++ include/votca/xtp/aomatrix3d.h
@@ -0,0 +1,70 @@
+/*
+ *            Copyright 2009-2019 The VOTCA Development Team
+ *                       (http://www.votca.org)
+ *
+ *      Licensed under the Apache License, Version 2.0 (the "License")
+ *
+ * You may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *              http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#pragma once
+#ifndef VOTCA_XTP_AOMATRIX3D_H
+#define VOTCA_XTP_AOMATRIX3D_H
+
+#include <votca/xtp/aobasis.h>
+
+namespace votca {
+namespace xtp {
+
+class AOMatrix3D {
+ public:
+  const std::array<Eigen::MatrixXd, 3>& Matrix() const { return _aomatrix; }
+  void Fill(const AOBasis& aobasis);
+  int Dimension() { return _aomatrix[0].rows(); }
+
+ protected:
+  std::array<Eigen::MatrixXd, 3> _aomatrix;
+  virtual void FillBlock(std::vector<Eigen::Block<Eigen::MatrixXd>>& matrix,
+                         const AOShell& shell_row,
+                         const AOShell& shell_col) const = 0;
+};
+
+/* derived class for atomic orbital gradient matrices, required for
+ * momentum transition dipoles
+ */
+class AOMomentum : public AOMatrix3D {
+ protected:
+  void FillBlock(std::vector<Eigen::Block<Eigen::MatrixXd>>& matrix,
+                 const AOShell& shell_row, const AOShell& shell_col) const;
+};
+
+/* derived class for atomic orbital electrical dipole matrices, required for
+ * electical transition dipoles
+ */
+class AODipole : public AOMatrix3D {
+ public:
+  void setCenter(const Eigen::Vector3d& r) {
+    _r = r;
+  }  // definition of a center around which the moment should be calculated
+ protected:
+  void FillBlock(std::vector<Eigen::Block<Eigen::MatrixXd>>& matrix,
+                 const AOShell& shell_row, const AOShell& shell_col) const;
+
+ private:
+  Eigen::Vector3d _r = Eigen::Vector3d::Zero();
+};
+
+}  // namespace xtp
+}  // namespace votca
+
+#endif  // VOTCA_XTP_AOMATRIX3D_H
---
+++ include/votca/xtp/aotransform.h
@@ -0,0 +1,215 @@
+/*
+ *            Copyright 2009-2019 The VOTCA Development Team
+ *                       (http://www.votca.org)
+ *
+ *      Licensed under the Apache License, Version 2.0 (the "License")
+ *
+ * You may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *              http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#pragma once
+#ifndef VOTCA_XTP_AOTRANSFORM_H
+#define VOTCA_XTP_AOTRANSFORM_H
+
+#include <votca/xtp/aoshell.h>
+#include <votca/xtp/eigen.h>
+namespace votca {
+namespace xtp {
+
+namespace Cart {
+enum Index {
+  s,  // s
+  x,
+  y,
+  z,  // p
+  xx,
+  xy,
+  xz,
+  yy,
+  yz,
+  zz,  // d
+  xxx,
+  xxy,
+  xxz,
+  xyy,
+  xyz,
+  xzz,
+  yyy,
+  yyz,
+  yzz,
+  zzz,  // f
+  xxxx,
+  xxxy,
+  xxxz,
+  xxyy,
+  xxyz,
+  xxzz,
+  xyyy,
+  xyyz,
+  xyzz,
+  xzzz,
+  yyyy,
+  yyyz,
+  yyzz,
+  yzzz,
+  zzzz,  // g
+  xxxxx,
+  xxxxy,
+  xxxxz,
+  xxxyy,
+  xxxyz,
+  xxxzz,
+  xxyyy,
+  xxyyz,
+  xxyzz,
+  xxzzz,
+  xyyyy,
+  xyyyz,
+  xyyzz,
+  xyzzz,
+  xzzzz,
+  yyyyy,
+  yyyyz,
+  yyyzz,
+  yyzzz,
+  yzzzz,
+  zzzzz,  // h
+
+  xxxxxx,
+  xxxxxy,
+  xxxxxz,
+  xxxxyy,
+  xxxxyz,
+  xxxxzz,
+  xxxyyy,
+  xxxyyz,
+  xxxyzz,
+  xxxzzz,
+  xxyyyy,
+  xxyyyz,
+  xxyyzz,
+  xxyzzz,  // i
+  xxzzzz,
+  xyyyyy,
+  xyyyyz,
+  xyyyzz,
+  xyyzzz,
+  xyzzzz,
+  xzzzzz,
+  yyyyyy,
+  yyyyyz,
+  yyyyzz,
+  yyyzzz,
+  yyzzzz,
+  yzzzzz,
+  zzzzzz,
+
+  xxxxxxx,
+  xxxxxxy,
+  xxxxxxz,
+  xxxxxyy,
+  xxxxxyz,
+  xxxxxzz,
+  xxxxyyy,
+  xxxxyyz,
+  xxxxyzz,
+  xxxxzzz,
+  xxxyyyy,
+  xxxyyyz,  // j
+  xxxyyzz,
+  xxxyzzz,
+  xxxzzzz,
+  xxyyyyy,
+  xxyyyyz,
+  xxyyyzz,
+  xxyyzzz,
+  xxyzzzz,
+  xxzzzzz,
+  xyyyyyy,
+  xyyyyyz,
+  xyyyyzz,
+  xyyyzzz,
+  xyyzzzz,
+  xyzzzzz,
+  xzzzzzz,
+  yyyyyyy,
+  yyyyyyz,
+  yyyyyzz,
+  yyyyzzz,
+  yyyzzzz,
+  yyzzzzz,
+  yzzzzzz,
+  zzzzzzz,
+
+  xxxxxxxx,
+  xxxxxxxy,
+  xxxxxxxz,
+  xxxxxxyy,
+  xxxxxxyz,
+  xxxxxxzz,
+  xxxxxyyy,
+  xxxxxyyz,
+  xxxxxyzz,
+  xxxxxzzz,
+  xxxxyyyy,
+  xxxxyyyz,
+  xxxxyyzz,
+  xxxxyzzz,
+  xxxxzzzz,  // k
+  xxxyyyyy,
+  xxxyyyyz,
+  xxxyyyzz,
+  xxxyyzzz,
+  xxxyzzzz,
+  xxxzzzzz,
+  xxyyyyyy,
+  xxyyyyyz,
+  xxyyyyzz,
+  xxyyyzzz,
+  xxyyzzzz,
+  xxyzzzzz,
+  xxzzzzzz,
+  xyyyyyyy,
+  xyyyyyyz,
+  xyyyyyzz,
+  xyyyyzzz,
+  xyyyzzzz,
+  xyyzzzzz,
+  xyzzzzzz,
+  xzzzzzzz,
+  yyyyyyyy,
+  yyyyyyyz,
+  yyyyyyzz,
+  yyyyyzzz,
+  yyyyzzzz,
+  yyyzzzzz,
+  yyzzzzzz,
+  yzzzzzzz,
+  zzzzzzzz,
+};
+}
+
+/* "superclass" AOSuperMatrix contains all common functionality for
+ * atomic orbital matrix types
+ */
+class AOTransform {
+ public:
+  static int getBlockSize(int lmax);
+  static Eigen::MatrixXd getTrafo(const AOGaussianPrimitive& gaussian);
+  static std::vector<double> XIntegrate(int size, double U);
+};
+
+}  // namespace xtp
+}  // namespace votca
+
+#endif  // VOTCA_XTP_AOTRANSFORM_H
