ff4deaa4203c1827759c175844301dab4940cf75
Jens Wehner <jenswehner@gmail.com>
2019-07-24 19:39:19 +0200

    methane works

--- Methane/run.sh
+++ Methane/run.sh
@@ -11,19 +11,17 @@ deleteoption(){
 
 echo $VOTCASHARE
 
-#runs the mapping from MD coordinates to segments and creates .sql file
+rm state.hdf5
+#runs the mapping from MD coordinates to segments and creates .hdf5 file
 
-xtp_map -t MD_FILES/topol.tpr -c MD_FILES/conf.gro -s system.xml -f state.sql
+xtp_map -t MD_FILES/topol.tpr -c MD_FILES/conf.gro -s system.xml -f state.hdf5
 
-# you can explore the created .sql file with e.g. sqlitebrowser
+# you can explore the created .hdf5 file with e.g. hdf5itebrowser
 
 # output MD and QM mappings into extract.trajectory_md.pdb and extract.trajectory_qm.pdb files
+cp $VOTCASHARE/xtp/xml/mapchecker.xml OPTIONFILES/
+xtp_run -e mapchecker -o OPTIONFILES/mapchecker.xml -f state.hdf5
 
-xtp_dump -e trajectory2pdb -f state.sql
-
-#make OPTIONFILE folder, you can put all options into a single options.xml file but experience has shown, that this is better.
-
-mkdir OPTIONFILES
 
 #copy neighborlistfile from votca share folder to here
 
@@ -33,160 +31,117 @@ changeoption constant 0.6 OPTIONFILES/neighborlist.xml
 
 #run neighborlist calculator
 
-xtp_run -e neighborlist -o OPTIONFILES/neighborlist.xml -f state.sql
+xtp_run -e neighborlist -o OPTIONFILES/neighborlist.xml -f state.hdf5 -t 4
 
 
-# read in reorganisation energies stored in system.xml to state.sql
+# read in reorganisation energies stored in system.xml to state.hdf5
 
 cp $VOTCASHARE/xtp/xml/einternal.xml OPTIONFILES/
 
-xtp_run -e einternal -o OPTIONFILES/einternal.xml -f state.sql
+xtp_run -e einternal -o OPTIONFILES/einternal.xml -f state.hdf5
 
 
 #site energies
-
-#setup jobfile xqmultipole has no own jobfile thus wehave to use the jobwriter
-
-cp $VOTCASHARE/xtp/xml/jobwriter.xml OPTIONFILES/
-
-changeoption keys "mps.monomer mps.background" OPTIONFILES/jobwriter.xml 
-changeoption states "n e h" OPTIONFILES/jobwriter.xml 
-
-xtp_run -e jobwriter -o OPTIONFILES/jobwriter.xml -f state.sql -s 0
-mv jobwriter.mps.background.tab mps.tab
-mv jobwriter.mps.monomer.xml xqmultipole.jobs
-#Only run the first 3 jobs and set the rest to complete
-sed -i "s/AVAILABLE/COMPLETE/g" xqmultipole.jobs
-sed -i '0,/COMPLETE/s/COMPLETE/AVAILABLE/' xqmultipole.jobs
-sed -i '0,/COMPLETE/s/COMPLETE/AVAILABLE/' xqmultipole.jobs
-sed -i '0,/COMPLETE/s/COMPLETE/AVAILABLE/' xqmultipole.jobs
+#we just prepared an optionfile for the mm calculation as changing qmmm.xml for mm is too difficult with bash
 
 
-#running xqmultipole
+cp $VOTCASHARE/xtp/packages/polar.xml OPTIONFILES/
 
-cp $VOTCASHARE/ctp/xml/xqmultipole.xml OPTIONFILES/
+xtp_parallel -e qmmm -o OPTIONFILES/qmmm_mm.xml -f state.hdf5 -j "write"
+sed -i "s/AVAILABLE/COMPLETE/g" qmmm_mm_jobs.xml
+sed -i '0,/COMPLETE/s/COMPLETE/AVAILABLE/' qmmm_mm_jobs.xml
+sed -i '0,/COMPLETE/s/COMPLETE/AVAILABLE/' qmmm_mm_jobs.xml
+sed -i '0,/COMPLETE/s/COMPLETE/AVAILABLE/' qmmm_mm_jobs.xml
+sed -i '0,/COMPLETE/s/COMPLETE/AVAILABLE/' qmmm_mm_jobs.xml
 
-changeoption job_file xqmultipole.jobs OPTIONFILES/xqmultipole.xml
-changeoption emp_file mps.tab OPTIONFILES/xqmultipole.xml
-#switch polarisation off for tutorial
-changeoption induce 0 OPTIONFILES/xqmultipole.xml
+xtp_parallel -e qmmm -o OPTIONFILES/qmmm_mm.xml -f state.hdf5 -j "run"
 
-changeoption pdb_check 0 OPTIONFILES/xqmultipole.xml
-deleteoption write_chk OPTIONFILES/xqmultipole.xml
-echo "Running xqmultipole, rerouting output to xqmultipole.log"
-xtp_parallel -e xqmultipole -f state.sql -o OPTIONFILES/xqmultipole.xml -s 0 -t 1 -c 1000 -j "run" > xqmultipole.log
-
-# xqmultipole has no parser to read the siteenergies into the sql file, write a python script or look at https://github.com/JensWehner/votca-scripts/blob/master/xtp/xtp_parseewald.py
-
-
+xtp_parallel -e qmmm -o OPTIONFILES/qmmm_mm.xml -f state.hdf5 -j "read"
 #running eanalyze
 
 cp $VOTCASHARE/xtp/xml/eanalyze.xml OPTIONFILES/
 
-xtp_run -e eanalyze -o OPTIONFILES/eanalyze.xml -f state.sql
-
+xtp_run -e eanalyze -o OPTIONFILES/eanalyze.xml -f state.hdf5
 
 #running eqm
-#eqm runs qm calculations for each segment in the sql file, it consists of three stages first writing a jobfile, then running the calculations, if necessary 
-# on multiple machines and then reading them into the .sql file
+#eqm runs qm calculations for each segment in the hdf5 file, it consists of three stages first writing a jobfile, then running the calculations, if necessary 
+# on multiple machines and then reading them into the .hdf5 file
 echo "Running eQM"
 
 cp $VOTCASHARE/xtp/xml/eqm.xml OPTIONFILES/
-cp $VOTCASHARE/xtp/packages/mbgft.xml OPTIONFILES/
+cp $VOTCASHARE/xtp/packages/gwbse.xml OPTIONFILES/
 cp $VOTCASHARE/xtp/packages/xtpdft.xml OPTIONFILES/
 cp $VOTCASHARE/xtp/packages/esp2multipole.xml OPTIONFILES/
 
 changeoption dftpackage OPTIONFILES/xtpdft.xml OPTIONFILES/eqm.xml
-changeoption gwbse_options OPTIONFILES/mbgft.xml OPTIONFILES/eqm.xml
+changeoption gwbse_options OPTIONFILES/gwbse.xml OPTIONFILES/eqm.xml
 
-changeoption threads 0 OPTIONFILES/xtpdft.xml
-changeoption openmp 0 OPTIONFILES/mbgft.xml
-changeoption ranges full OPTIONFILES/mbgft.xml
-changeoption openmp 0 OPTIONFILES/esp2multipole.xml
+changeoption ranges full OPTIONFILES/gwbse.xml
 
-xtp_parallel -e eqm -o OPTIONFILES/eqm.xml -f state.sql -s 0 -j "write"
+xtp_parallel -e eqm -o OPTIONFILES/eqm.xml -f state.hdf5 -s 0 -j "write"
 sed -i "s/AVAILABLE/COMPLETE/g" eqm.jobs
 sed -i '0,/COMPLETE/s/COMPLETE/AVAILABLE/' eqm.jobs
 sed -i '0,/COMPLETE/s/COMPLETE/AVAILABLE/' eqm.jobs
 
-xtp_parallel -e eqm -o OPTIONFILES/eqm.xml -f state.sql -s 0 -j run -c 1 -t 1
+xtp_parallel -e eqm -o OPTIONFILES/eqm.xml -f state.hdf5 -s 0 -j run -c 1 -t 1
 
 #running iqm
-#iqm runs qm calculations for each pair in the sql file, it consists of three stages first writing a jobfile, then running the calculations, if necessary 
-# on multiple machines and then reading them into the .sql file
+#iqm runs qm calculations for each pair in the hdf5 file, it consists of three stages first writing a jobfile, then running the calculations, if necessary 
+# on multiple machines and then reading them into the .hdf5 file
 echo "Running iQM"
 
 cp $VOTCASHARE/xtp/xml/iqm.xml OPTIONFILES/
-cp $VOTCASHARE/xtp/packages/mbgft.xml OPTIONFILES/mbgft_pair.xml
+cp $VOTCASHARE/xtp/packages/gwbse.xml OPTIONFILES/gwbse_pair.xml
 cp $VOTCASHARE/xtp/packages/xtpdft.xml OPTIONFILES/xtpdft_pair.xml
 cp $VOTCASHARE/xtp/packages/bsecoupling.xml OPTIONFILES/
 
 changeoption bsecoupling_options OPTIONFILES/bsecoupling.xml OPTIONFILES/iqm.xml
 changeoption dftpackage OPTIONFILES/xtpdft_pair.xml OPTIONFILES/iqm.xml
-changeoption gwbse_options OPTIONFILES/mbgft_pair.xml OPTIONFILES/iqm.xml
+changeoption gwbse_options OPTIONFILES/gwbse_pair.xml OPTIONFILES/iqm.xml
 changeoption read_guess 1 OPTIONFILES/xtpdft_pair.xml
 changeoption energy 1e-2 OPTIONFILES/xtpdft_pair.xml
 
-changeoption threads 0 OPTIONFILES/xtpdft_pair.xml
-changeoption ranges full OPTIONFILES/mbgft_pair.xml
-changeoption openmp 0 OPTIONFILES/mbgft_pair.xml
-changeoption openmp 0 OPTIONFILES/bsecoupling.xml
-changeoption openmp 0 OPTIONFILES/bsecoupling.xml
+changeoption ranges full OPTIONFILES/gwbse_pair.xml
 
 changeoption singlet "Methane:s1" OPTIONFILES/iqm.xml
 changeoption triplet "Methane:t1" OPTIONFILES/iqm.xml
 changeoption electron "Methane:e1" OPTIONFILES/iqm.xml
 changeoption hole "Methane:h1" OPTIONFILES/iqm.xml
 
-changeoption tasks "singlets,triplets,iqm" OPTIONFILES/mbgft_pair.xml
+changeoption tasks "GW" OPTIONFILES/gwbse_pair.xml
 
-xtp_parallel -e iqm -o OPTIONFILES/iqm.xml -f state.sql -s 0 -j "write"
+xtp_parallel -e iqm -o OPTIONFILES/iqm.xml -f state.hdf5 -s 0 -j "write"
 sed -i "s/AVAILABLE/COMPLETE/g" iqm.jobs
 sed -i '0,/COMPLETE/s/COMPLETE/AVAILABLE/' iqm.jobs
 
-xtp_parallel -e iqm -o OPTIONFILES/iqm.xml -f state.sql -s 0 -j run -c 1 -t 1
+xtp_parallel -e iqm -o OPTIONFILES/iqm.xml -f state.hdf5 -s 0 -j run -c 1 -t 1
 
-xtp_parallel -e iqm -o OPTIONFILES/iqm.xml -f state.sql -j "read"
+xtp_parallel -e iqm -o OPTIONFILES/iqm.xml -f state.hdf5 -j "read"
 
 #running ianalyze
 
 cp $VOTCASHARE/xtp/xml/ianalyze.xml OPTIONFILES/
 
-xtp_run -e ianalyze -o OPTIONFILES/ianalyze.xml -f state.sql
-
+xtp_run -e ianalyze -o OPTIONFILES/ianalyze.xml -f state.hdf5
 
 #running qmmm 
 
+
+
 cp $VOTCASHARE/xtp/xml/qmmm.xml OPTIONFILES/
-cp $VOTCASHARE/xtp/packages/mbgft.xml OPTIONFILES/mbgft_qmmm.xml
+cp $VOTCASHARE/xtp/packages/gwbse.xml OPTIONFILES/gwbse_qmmm.xml
 cp $VOTCASHARE/xtp/packages/xtpdft.xml OPTIONFILES/xtpdft_qmmm.xml
+changeoption options_dft OPTIONFILES/xtpdft_qmmm.xml OPTIONFILES/qmmm.xml
+changeoption options_gwbse OPTIONFILES/gwbse_qmmm.xml OPTIONFILES/qmmm.xml
+changeoption options_polar OPTIONFILES/polar.xml OPTIONFILES/qmmm.xml
+xtp_parallel -e qmmm -o OPTIONFILES/qmmm.xml -f state.hdf5 -j "write"
+sed -i "s/AVAILABLE/COMPLETE/g" qmmm_jobs.xml
+sed -i '0,/COMPLETE/s/COMPLETE/AVAILABLE/' qmmm_jobs.xml
+sed -i '0,/COMPLETE/s/COMPLETE/AVAILABLE/' qmmm_jobs.xml
+xtp_parallel -e qmmm -o OPTIONFILES/qmmm.xml -f state.hdf5 -j run
 
-
-#writing jobfile
-cp $VOTCASHARE/xtp/xml/jobwriter.xml OPTIONFILES/jobwriter_qmmm.xml
-
-changeoption keys "mps.monomer mps.background" OPTIONFILES/jobwriter_qmmm.xml 
-
-xtp_run -e jobwriter -o OPTIONFILES/jobwriter_qmmm.xml -f state.sql -s 0
-mv jobwriter.mps.background.tab mps.tab
-mv jobwriter.mps.monomer.xml qmmm.jobs
-sed -i "s/AVAILABLE/COMPLETE/g" qmmm.jobs
-sed -i '0,/COMPLETE/s/COMPLETE/AVAILABLE/' qmmm.jobs
-sed -i '0,/COMPLETE/s/COMPLETE/AVAILABLE/' qmmm.jobs
-
-#config options
-changeoption archiving iterations OPTIONFILES/qmmm.xml
-changeoption dftpackage OPTIONFILES/xtpdft_qmmm.xml OPTIONFILES/qmmm.xml
-changeoption gwbse_options OPTIONFILES/mbgft_qmmm.xml OPTIONFILES/qmmm.xml
-changeoption cutoff1 1 OPTIONFILES/qmmm.xml
-changeoption cutoff2 1.2 OPTIONFILES/qmmm.xml
-
-changeoption threads 0 OPTIONFILES/xtpdft_qmmm.xml
-changeoption openmp 0 OPTIONFILES/mbgft_qmmm.xml
-changeoption ranges full OPTIONFILES/mbgft_qmmm.xml
-echo "Running qmmm, rerouting output to qmmm.log"
-xtp_parallel -e qmmm -o OPTIONFILES/qmmm.xml -f state.sql -s 0 >qmmm.log
-
+# We are not going to read it in
+#xtp_parallel -e qmmm -o OPTIONFILES/qmmm.xml -f state.hdf5 -j "read"
 
 
 
