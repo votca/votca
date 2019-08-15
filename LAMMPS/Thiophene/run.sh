813226cf1006782c0a83ac553e3f7e753f5d985c
Jens Wehner <jenswehner@gmail.com>
2019-08-15 07:27:10 +0200

    deleted iexcitoncl and reset qmmm.jobs

--- LAMMPS/Thiophene/run.sh
+++ LAMMPS/Thiophene/run.sh
@@ -118,17 +118,6 @@ xtp_parallel -e iqm -o OPTIONFILES/iqm.xml -f state.hdf5 -s 0 -j run -c 1 -t 1
 
 xtp_parallel -e iqm -o OPTIONFILES/iqm.xml -f state.hdf5 -j "read"
 
-#running iexcitoncl
-
-cp $VOTCASHARE/xtp/xml/iexcitoncl.xml OPTIONFILES
-xtp_parallel -e iexcitoncl -o OPTIONFILES/iexcitoncl.xml -f state.hdf5 -j "write"
-sed -i "s/AVAILABLE/COMPLETE/g" exciton.jobs
-sed -i '0,/COMPLETE/s/COMPLETE/AVAILABLE/' exciton.jobs
-
-xtp_parallel -e iexcitoncl -o OPTIONFILES/iexcitoncl.xml -f state.hdf5 
-
-xtp_parallel -e iexcitoncl -o OPTIONFILES/iexcitoncl.xml -f state.hdf5 -j "read"
-
 #running ianalyze
 
 cp $VOTCASHARE/xtp/xml/ianalyze.xml OPTIONFILES/
