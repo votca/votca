a7f73c889ec8f0bf20f0f581bf1054dc4bb39b7d
felipe <tifonzafel@gmail.com>
2019-08-20 12:00:50 +0200

    fixed bug calling right matrix tensor #262

--- src/libxtp/eigencuda.cc
+++ src/libxtp/eigencuda.cc
@@ -243,7 +243,7 @@ std::vector<Mat<T>> EigenCuda<T>::right_matrix_tensor(
     T *hout = rs[i].data();
     T *dout = hC[i];
     cudaMemcpyAsync(hout, dout, size_out, cudaMemcpyDeviceToHost, _stream);
-    rs[0] = Eigen::Map<Mat<T>>(hout, output.rows(), output.cols());
+    rs[i] = Eigen::Map<Mat<T>>(hout, output.rows(), output.cols());
     ;
   }
   // Deallocate all the memory from the device
