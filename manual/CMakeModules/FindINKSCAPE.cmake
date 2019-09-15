58c606392be920b3162275ccc214a4244560af69
Christoph Junghans <junghans@lanl.gov>
2019-09-15 14:56:17 -0600

    cmake: fix inkscape detection for beta versions

--- manual/CMakeModules/FindINKSCAPE.cmake
+++ manual/CMakeModules/FindINKSCAPE.cmake
@@ -9,7 +9,7 @@
 #  INKSCAPE_FOUND - system has inkscape
 #  INKSCAPE_EXECUTABLE - the inkscape executable
 #
-# Copyright 2009-2011 The VOTCA Development Team (http://www.votca.org)
+# Copyright 2009-2019 The VOTCA Development Team (http://www.votca.org)
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -55,8 +55,8 @@ if(INKSCAPE_EXECUTABLE)
   if(_inkscape_version_result)
     message(WARNING "Unable to determine inkscape version: ${_inkscape_version_result}")
   endif()
-  string(REGEX REPLACE "^Inkscape ([0-9.]+) \\(.*$" "\\1" INKSCAPE_VERSION "${_INKSCAPE_VERSION}")
-  if(NOT INKSCAPE_VERSION MATCHES "[0-9.]+")
+  string(REGEX REPLACE "^Inkscape ([0-9.]+(beta[0-9])?) \\(.*$" "\\1" INKSCAPE_VERSION "${_INKSCAPE_VERSION}")
+  if(INKSCAPE_VERSION STREQUAL _INKSCAPE_VERSION)
     message(WARNING "Unable to parse inkscape version: ${_INKSCAPE_VERSION}")
   endif()
   if(INKSCAPE_VERSION VERSION_LESS "1.0")
