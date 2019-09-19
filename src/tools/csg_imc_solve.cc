d3bef2c38e978fc8419b7d06292a7bce7640f0c1
Jens Wehner <jenswehner@gmail.com>
2019-09-19 12:18:22 +0200

    compiles and gives results

--- src/tools/csg_imc_solve.cc
+++ src/tools/csg_imc_solve.cc
@@ -19,7 +19,7 @@
 #include <fstream>
 #include <votca/tools/table.h>
 
-int main(int argc, char **argv) {
+int main(int argc, char** argv) {
   CG_IMC_solve app;
   return app.Exec(argc, argv);
 }
@@ -31,13 +31,14 @@ void CG_IMC_solve::Initialize(void) {
                       propt::value<double>()->default_value(0.0),
                       "regularization factor");
 
-  AddProgramOptions()("imcfile,i", boost::program_options::value<string>(),
+  AddProgramOptions()("imcfile,i", boost::program_options::value<std::string>(),
                       "imc statefile");
 
-  AddProgramOptions()("gmcfile,g", boost::program_options::value<string>(),
+  AddProgramOptions()("gmcfile,g", boost::program_options::value<std::string>(),
                       "gmc statefile");
 
-  AddProgramOptions()("outputfile,o", boost::program_options::value<string>(),
+  AddProgramOptions()("outputfile,o",
+                      boost::program_options::value<std::string>(),
                       "outputfile");
 }
 
@@ -45,35 +46,95 @@ bool CG_IMC_solve::EvaluateOptions() {
   CheckRequired("imcfile", "Missing imcfile");
   CheckRequired("gmcfile", "Missing gmcfile");
   CheckRequired("outputfile", "Missing outputfile");
+  return true;
 }
 
-Eigen::VectorXd LoadFile(std::string filename) {
+Eigen::MatrixXd CG_IMC_solve::LoadMatrix(const std::string& filename) const {
   std::ifstream intt;
   intt.open(filename);
-  if (!intt) throw runtime_error(string("error, cannot open file ") + filename);
+  if (!intt)
+    throw std::runtime_error(std::string("error, cannot open file ") +
+                             filename);
 
   std::string line;
   std::vector<double> result;
+  int numrows = 0;
+  int numcols = 0;
   while (getline(intt, line)) {
     if (line[0] == '#') {
       continue;
     }
-    Tokenizer tok(line, " \t");
-    vector<string> tokens = ToVector();
-    result.append(std::stod(tokens[0]));
+    votca::tools::Tokenizer tok(line, " \t");
+    std::vector<std::string> tokens = tok.ToVector();
+    if (numrows == 0) {
+      numcols = tokens.size();
+    } else if (numcols != tokens.size()) {
+      throw std::runtime_error(
+          "Matrix has not the same number of entries in each row.");
+    }
+    for (const std::string& s : tokens) {
+      result.push_back(std::stod(tokens[0]));
+    }
+    numrows++;
   }
   intt.close();
 
-  return Eigen::Map<Eigen::VectorXd>(result.data(), result.size());
+  return Eigen::Map<Eigen::MatrixXd>(result.data(), numrows, numcols);
 }
 
 void CG_IMC_solve::Run() {
-  string imcfile = _op_vm["imcfile"].as<string>();
-  string gmcfile = _op_vm["gmcfile"].as<string>();
-  string outputfile = _op_vm["outputfile"].as<string>();
+  std::string imcfile = _op_vm["imcfile"].as<std::string>();
+  std::string gmcfile = _op_vm["gmcfile"].as<std::string>();
+  std::string outputfile = _op_vm["outputfile"].as<std::string>();
 
   double reg = _op_vm["regularization"].as<double>();
 
-  Eigen::VectorXd A = LoadFile(imcfile);
-  Eigen::VectorXd B = LoadFile(gmcfile);
+  Eigen::MatrixXd A = LoadMatrix(gmcfile);
+
+  votca::tools::Table B;
+  B.Load(imcfile);
+
+  votca::tools::Table x;
+  x.resize(B.size());
+  x.x() = B.x();
+
+  // Calculating https://en.wikipedia.org/wiki/Tikhonov_regularization
+
+  Eigen::MatrixXd ATA = A.transpose() * A;
+
+  // We could add and invert but for stability reasons we will diagonalize the
+  // matrix which is symmetric by construction
+
+  Eigen::SelfAdjointEigenSolver<Eigen::MatrixXd> es(ATA);
+
+  Eigen::VectorXd inv_diag = Eigen::VectorXd::Zero(es.eigenvalues().size());
+  double etol = 1e-12;
+
+  int numerics_unstable = 0;
+  // Constructing pseudoinverse if not stable
+  // https://en.wikipedia.org/wiki/Moore%E2%80%93Penrose_inverse
+  for (int i = 0; i < es.eigenvalues().size(); i++) {
+    if (std::abs(es.eigenvalues()[i] + reg) < etol) {
+      numerics_unstable++;
+    } else {
+      inv_diag[i] = 1 / (es.eigenvalues()[i] + reg);
+    }
+  }
+
+  if (numerics_unstable > 0) {
+    std::cout
+        << "Regularisation parameter was to small, a pseudo inverse was "
+           "constructed instead.\n Use a larger regularisation parameter R."
+           " Smallest (eigenvalue+R)="
+        << (es.eigenvalues().array() + reg).abs().minCoeff() << " Found "
+        << numerics_unstable << " eigenvalues of " << es.eigenvalues().size()
+        << " below " << etol << std::endl;
+  }
+
+  Eigen::MatrixXd inverse =
+      es.eigenvectors() * inv_diag.asDiagonal() * es.eigenvectors().transpose();
+
+  x.y() = inverse * A.transpose() * B.y();
+
+  x.Save(outputfile);
 }
--- src/tools/csg_imc_solve.h
+++ src/tools/csg_imc_solve.h
@@ -18,10 +18,9 @@
 #ifndef _VOTCA_CSG_IMC_SOLVE_H
 #define _VOTCA_CSG_IMC_SOLVE_H
 
+#include <votca/csg/version.h>
 #include <votca/tools/application.h>
 #include <votca/tools/property.h>
-
-using namespace votca::csg;
 
 /**
     \brief Solves linear system for IMCS
@@ -30,12 +29,27 @@ using namespace votca::csg;
 
 class CG_IMC_solve : public votca::tools::Application {
  public:
-  string ProgramName() { return "csg_imc_solve"; }
-  void HelpText(ostream &out) { out << "Solves the linear system for IMCs"; }
+  std::string ProgramName() { return "csg_imc_solve"; }
+  void HelpText(std::ostream &out) {
+    out << "Solves the linear system for IMCs";
+  }
+
+  void ShowHelpText(std::ostream &out) {
+    std::string name = ProgramName();
+    if (VersionString() != "") name = name + ", version " + VersionString();
+
+    votca::csg::HelpTextHeader(name);
+    HelpText(out);
+
+    out << "\n\n" << VisibleOptions() << std::endl;
+  }
 
   bool EvaluateOptions();
   void Initialize();
   void Run();
+
+ private:
+  Eigen::MatrixXd LoadMatrix(const std::string &filename) const;
 };
 
 #endif /* _VOTCA_CSG_IMC_SOLVE_H */
