70e93e3e0f2be1167be1a4d150a6faa80ac7ecce
Joshua Scott Brown <joshbro42867@yahoo.com>
2019-11-21 22:56:29 -0700

    Update header define

--- include/votca/csg/boundarycondition.h
+++ include/votca/csg/boundarycondition.h
@@ -15,8 +15,8 @@
  *
  */
 #pragma once
-#ifndef _VOTCA_CSG_BOUNDARYCONDITION_H
-#define _VOTCA_CSG_BOUNDARYCONDITION_H
+#ifndef VOTCA_CSG_BOUNDARYCONDITION_H
+#define VOTCA_CSG_BOUNDARYCONDITION_H
 
 #include <votca/tools/eigen.h>
 
@@ -63,4 +63,4 @@ class BoundaryCondition {
 }  // namespace csg
 }  // namespace votca
 
-#endif /* _VOTCA_CSG_BOUNDARYCONDITION_H */
+#endif  // VOTCA_CSG_BOUNDARYCONDITION_H
