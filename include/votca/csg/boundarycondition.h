1754472cb174a47713178866196a5530a035f096
Joshua Scott Brown <joshbro42867@yahoo.com>
2019-11-21 23:01:36 -0700

    Comments, unique_ptr include, const, Clone method

--- include/votca/csg/boundarycondition.h
+++ include/votca/csg/boundarycondition.h
@@ -18,15 +18,37 @@
 #ifndef VOTCA_CSG_BOUNDARYCONDITION_H
 #define VOTCA_CSG_BOUNDARYCONDITION_H
 
+#include <memory>
 #include <votca/tools/eigen.h>
 
 namespace votca {
 namespace csg {
 
+/**
+ * @brief Class keeps track of how the boundaries of the system are handled
+ *
+ * There are a total of 3 different boundaries:
+ * open - no boundaries
+ * orthorhombic - orthorombic boundaries
+ * triclinic - triclinic boundaries
+ *
+ * This class enables the correct treatement of distances beteween topology
+ * objects, such that distances accound for the periodic boundaries.
+ */
 class BoundaryCondition {
 
  public:
   virtual ~BoundaryCondition() = default;
+
+  /**
+   * @brief Safe way to allow child classes to be copied
+   *
+   * The child classes must use the same method and override it with their type
+   * for this to work.
+   *
+   * @return standard pointer to child class
+   */
+  virtual std::unique_ptr<BoundaryCondition> Clone() const = 0;
 
   /**
    * set the simulation box
@@ -38,13 +60,13 @@ class BoundaryCondition {
    * get the simulation box
    * \return triclinic box matrix
    */
-  const Eigen::Matrix3d &getBox() { return _box; };
+  const Eigen::Matrix3d &getBox() const { return _box; };
 
   /**
    * get the volume of the box
    * \return box volume as double
    */
-  virtual double BoxVolume();
+  virtual double BoxVolume() const;
 
   /**
    * get shortest connection vector between r_i and r_j with respect to the
@@ -54,7 +76,7 @@ class BoundaryCondition {
       const Eigen::Vector3d &r_i, const Eigen::Vector3d &r_j) const = 0;
 
   enum eBoxtype { typeAuto = 0, typeTriclinic, typeOrthorhombic, typeOpen };
-  virtual eBoxtype getBoxType() = 0;
+  virtual eBoxtype getBoxType() const = 0;
 
  protected:
   Eigen::Matrix3d _box;
