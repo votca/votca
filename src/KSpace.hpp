ebadb6669b490c10735be8d6add64d7142c4e439
Rene Halver <r.halver@fz-juelich.de>
2019-11-25 08:08:42 +0100

    constuctors and initialization methods

--- src/KSpace.hpp
+++ src/KSpace.hpp
@@ -1,10 +1,72 @@
 #ifndef KSPACE_CLASS_INCLUDED
 #define KSPACE_CLASS_INCLUDED
 
-class KSpace
+#include <vector>
+
+/**
+ *
+ * Class to compute the k-space part of an Ewald sum for
+ * quadrupole, dipole and/or monopole systems
+ *
+ */
+
+template <class T> class KSpace
 {
-    KSpace(){}
+    public:
+        /**
+         *
+         * Constructor for the class (base-version)
+         *
+         */
+        KSpace(){}
+        /**
+         *
+         * Constructor for the class
+         *
+         * @param alpha     Ewald sum splitting parameter
+         * @param k_max     floating point cut-off radius for the k-space
+         *                  part
+         */
+        KSpace( T alpha, T k_max ) : init( alpha, k_max ){}
+        /*
+         *
+         * Initialization of the internal parameters
+         *
+         */
+        void init_params( T, T );
+        /**
+         *
+         * Computation of the kspace part of the Ewald sum, fills the
+         * private variables of the class which can be accessed by the
+         * corresponding getters after the computation
+         *
+         */
+        void compute( int,  );
+
+    private:
+        T alpha;                    ///< splitting parameter of the Ewald sum
+        T k_max;                    ///< floating-point version of the k-space cutoff
+        int k_max_int;              ///< integer version of the k-space cutoff
+
+        T pot_energy;               ///< potential energy computed for the k-space part
+        std::vector<T> f;           ///< forces computed for the k-space part
+        std::vector<T> tqe;         ///< torque computed for the k-space part
+
+        bool is_monopole;           ///< compute the monopole contributions
+        bool is_dipole;             ///< compute the dipole contributions
+        bool is_quadrupole;         ///< compute the quadrupole contributions
+
+        /*
+         *
+         * Compute the cosine factors for the k-space part
+         *
+         */
 };
 
-
+template <class T> void KSpace::init_params( T _alpha, T _k_max )
+{
+    alpha = _alpha;
+    k_max = _k_max;
+    k_max_int = std::ceil(k_max);
+}
 #endif
