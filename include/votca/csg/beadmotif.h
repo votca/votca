241fa8aa9091d614a4ed17b85068f9e3675c79f1
JoshuaSBrown <joshbro42867@yahoo.com>
2019-12-07 20:34:07 -0700

    Added const noexcept keywords in header

--- include/votca/csg/beadmotif.h
+++ include/votca/csg/beadmotif.h
@@ -143,7 +143,7 @@ class BeadMotif : public BeadStructure<BaseBead> {
    *
    * @param[in] basebead pointer
    **/
-  void AddBead(BaseBead & basebead);
+  void AddBead(BaseBead &basebead);
 
   /**
    * \brief Adds a new connection to the motif
@@ -166,7 +166,7 @@ class BeadMotif : public BeadStructure<BaseBead> {
   void InitializeGraph_();
   bool junctionExist_();
   void CalculateType_();
-  bool isSingle_();
+  bool isSingle_() const noexcept;
   bool isLine_();
   bool isLoop_();
   bool isFusedRing_();
