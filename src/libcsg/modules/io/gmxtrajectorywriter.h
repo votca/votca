e9ae77ff1d60c9b200f1882f03cb675b43522e13
JoshuaSBrown <joshbro42867@yahoo.com>
2019-12-17 08:59:56 -0700

    Add default units to gmx traj writer

--- src/libcsg/modules/io/gmxtrajectorywriter.h
+++ src/libcsg/modules/io/gmxtrajectorywriter.h
@@ -32,6 +32,17 @@ namespace csg {
 
 class GMXTrajectoryWriter : public TrajectoryWriter {
  public:
+  const tools::DistanceUnit distance_unit = tools::DistanceUnit::nanometers;
+  const tools::MassUnit mass_unit = tools::MassUnit::atomic_mass_units;
+  const tools::TimeUnit time_unit = tools::TimeUnit::picoseconds;
+  const tools::ChargeUnit charge_unit = tools::ChargeUnit::e;
+  const tools::MolarEnergyUnit energy_unit =
+      tools::EnergyUnit::kilojoules_per_mole;
+  const tools::VelocityUnit velocity_unit =
+      tools::VelocityUnit::nanometers_per_picosecond;
+  const tools::MolarForceUnit force_unit =
+      tools::ForceUnit::kilojoules_per_mole_nanometer;
+
   GMXTrajectoryWriter() = default;
 
   void Open(std::string file, bool = false) override;
