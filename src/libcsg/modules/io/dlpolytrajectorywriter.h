f1548fc589ad32b223fa54d9778eebefc8a25192
JoshuaSBrown <joshbro42867@yahoo.com>
2019-12-17 10:19:27 -0700

    Correct type dlpoly traj write

--- src/libcsg/modules/io/dlpolytrajectorywriter.h
+++ src/libcsg/modules/io/dlpolytrajectorywriter.h
@@ -39,11 +39,12 @@ class DLPOLYTrajectoryWriter : public TrajectoryWriter {
   const tools::MassUnit mass_unit = tools::MassUnit::atomic_mass_units;
   const tools::TimeUnit time_unit = tools::TimeUnit::picoseconds;
   const tools::ChargeUnit charge_unit = tools::ChargeUnit::e;
-  const tools::MolarEnergyUnit energy_unit = tools::EnergyUnit::joules_per_mole;
+  const tools::MolarEnergyUnit energy_unit =
+      tools::MolarEnergyUnit::joules_per_mole;
   const tools::VelocityUnit velocity_unit =
       tools::VelocityUnit::angstroms_per_picosecond;
   const tools::MolarForceUnit force_unit =
-      tools::ForceUnit::kilojoules_per_mole_angstrom;
+      tools::MolarForceUnit::kilojoules_per_mole_angstrom;
 
   // open transformed trajectory file
   void Open(std::string file, bool bAppend = false) override;
