0d2e87c690b1e839c8a92325a8b860d47d0d95d8
JoshuaSBrown <joshbro42867@yahoo.com>
2019-12-17 10:14:34 -0700

    Add default units to growriter

--- src/libcsg/modules/io/growriter.h
+++ src/libcsg/modules/io/growriter.h
@@ -28,6 +28,16 @@ namespace csg {
 
 class GROWriter : public TrajectoryWriter {
  public:
+  const tools::DistanceUnit distance_unit = tools::DistanceUnit::nanometers;
+  const tools::MassUnit mass_unit = tools::MassUnit::atomic_mass_units;
+  const tools::TimeUnit time_unit = tools::TimeUnit::picoseconds;
+  const tools::ChargeUnit charge_unit = tools::ChargeUnit::e;
+  const tools::EnergyUnit energy_unit = tools::EnergyUnit::kilojoules_per_mole;
+  const tools::VelocityUnit velocity_unit =
+      tools::VelocityUnit::nanometers_per_picosecond;
+  const tools::ForceUnit force_unit =
+      tools::ForceUnit::kilojoules_per_mole_nanometer;
+
   void Open(std::string file, bool bAppend = false) override;
   void Close() override;
 
