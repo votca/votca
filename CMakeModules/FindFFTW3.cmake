7131b5634bf1fb4768e661842bb514403c96bfa5
Christoph Junghans <junghans@lanl.gov>
2020-04-26 11:56:13 -0600

    FindFFTW3.cmake: remove non-standard function check

--- CMakeModules/FindFFTW3.cmake
+++ CMakeModules/FindFFTW3.cmake
@@ -34,15 +34,6 @@ include(FindPackageHandleStandardArgs)
 
 find_package_handle_standard_args(FFTW3 DEFAULT_MSG FFTW3_LIBRARY FFTW3_INCLUDE_DIR )
 
-if (FFTW3_FOUND)
-  include(CheckLibraryExists)
-  #adding MATH_LIBRARIES here to allow static libs, this does not harm us as we are anyway using it
-  check_library_exists("${FFTW3_LIBRARY};${MATH_LIBRARIES}" fftw_plan_r2r_1d "" FOUND_FFTW_PLAN)
-  if(NOT FOUND_FFTW_PLAN)
-    message(FATAL_ERROR "Could not find fftw_plan_r2r_1d in ${FFTW3_LIBRARY}, take a look at the error message in ${CMAKE_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/CMakeError.log to find out what was going wrong. If you are using a static lib (.a) make sure you have specified all dependencies of fftw3 in FFTW3_LIBRARY by hand (i.e. -DFFTW3_LIBRARY='/path/to/libfftw3.so;/path/to/libm.so') !")
-  endif(NOT FOUND_FFTW_PLAN)
-endif (FFTW3_FOUND)
-
 # Copy the results to the output variables and target.
 if(FFTW3_FOUND)
   set(FFTW3_LIBRARIES ${FFTW3_LIBRARY} )
