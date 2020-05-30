ee1ef1adb16c827bed8e935978a5e4001b12f279
JoshuaSBrown <josh@votca.org>
2020-05-29 21:21:19 -0600

    Address floating point comparison

--- include/votca/tools/akimaspline.h
+++ include/votca/tools/akimaspline.h
@@ -21,6 +21,7 @@
 #include "spline.h"
 #include <iostream>
 #include <votca/tools/eigen.h>
+#include <boost/math/special_functions/relative_difference.hpp>
 
 namespace votca {
 namespace tools {
@@ -95,7 +96,7 @@ inline double AkimaSpline::CalculateDerivative(double r) {
 
 inline double AkimaSpline::getSlope(double m1, double m2, double m3,
                                     double m4) {
-  if ((m1 == m2) && (m3 == m4)) {
+  if (boost::math::epsilon_difference(m1, m2) && boost::math::epsilon_difference(m3, m4)) {
     return (m2 + m3) / 2.0;
   } else {
     return (std::fabs(m4 - m3) * m2 + std::fabs(m2 - m1) * m3) /
