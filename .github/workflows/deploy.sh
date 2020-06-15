29574bffe53208a3940f581decdb708374f9161e
Christoph Junghans <junghans@votca.org>
2020-06-15 10:31:20 -0600

    CI: fix another typo in deploy

--- .github/workflows/deploy.sh
+++ .github/workflows/deploy.sh
@@ -15,7 +15,7 @@ git config --global user.name "Votca Bot"
 git config --global user.email "github@votca.org"
 git commit -m "Documentation Update for votca/votca@${GITHUB_SHA}"
 if [[ ${GITHUB_REF} = "refs/heads/master" && ${VOTCA_BOT_TOKEN} ]]; then
-  git push "https://${VOTCA_BOT_TOKEN}@github.com:votca/doxygen.git" gh-pages
+  git push "https://${VOTCA_BOT_TOKEN}@github.com/votca/doxygen.git" gh-pages
 else
   git show --no-color | head -c 1k
 fi
@@ -33,7 +33,7 @@ git config --global user.name "Votca Bot"
 git config --global user.email "github@votca.org"
 git commit -m "Documentation Update for votca/votca@${GITHUB_SHA}"
 if [[ ${GITHUB_REF} = "refs/heads/master" && ${VOTCA_BOT_TOKEN} ]]; then
-  git push "https://${VOTCA_BOT_TOKEN}@github.com:votca/votca.github.io.git" master
+  git push "https://${VOTCA_BOT_TOKEN}@github.com/votca/votca.github.io.git" master
 else
   git show --no-color | head -c 1k
 fi
