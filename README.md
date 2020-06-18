4ff659325416882f34fc41249ab6b5143a4d87ce
Christoph Junghans <junghans@lanl.gov>
2020-06-18 13:23:14 -0600

    forward: initial add

--- README.md
+++ README.md
@@ -5,3 +5,4 @@ These GitHub actions are used by multiple VOTCA modules.
 Actions:
 
 -   setup, initializes variables, like `cmake_args` to be used during the build.
+-   forward, forward commit to master/stable in a module to votca/votca
---
+++ forward/forward.sh
@@ -0,0 +1,31 @@
+#!/bin/bash -le
+
+shopt -s extglob
+
+die () {
+  [[ -n $1 ]] && echo "$*" >&2
+  exit 1
+}
+
+[[ ${INPUT_MODULE} ]] || die "No module given input"
+module=${INPUT_MODULE#votca/}
+
+[[ ${GITHUB_REF} = refs/heads/* ]] || die "GITHUB_REF isn't refs/heads/*, but ${GITHUB_REF}"
+branch=${GITHUB_REF#refs/heads/}
+push_branch="update_${branch}_submodules"
+
+echo "Working on $module, push $branch to $push_branch of votca/votca"
+
+git clone https://${INPUT_TOKEN}@github.com/votca/votca
+pushd votca
+git checkout -b "${push_branch}" "origin/${push_branch}" || git checkout -b "${push_branch}" "origin/${branch}"
+git submodule update --init --depth 1
+git -C "${module}" remote update
+git -C "${module}" checkout "origin/${branch}"
+git add "${module}"
+
+git config --global user.name "Votca Bot"
+git config --global user.email "github@votca.org"
+git commit -m "Update ${module} submodule" || true
+git push origin "${push_branch}"
+popd
