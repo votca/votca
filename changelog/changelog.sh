c8f1cdc23694379bf7842412a1011c271afbb35d
Christoph Junghans <junghans@lanl.gov>
2020-07-03 14:50:23 -0600

    changelog: add support for rst files

--- changelog/changelog.sh
+++ changelog/changelog.sh
@@ -9,23 +9,32 @@ die () {
 
 [[ ${INPUT_MESSAGE} ]] || die "No message given"
 [[ ${INPUT_PR_NUMBER} ]] || die "No PR number given"
-message="* ${INPUT_MESSAGE#*: } (#$INPUT_PR_NUMBER)"
 
 [[ -f CMakeLists.txt ]] || die "No CMakeLists.txt found"
 version="$(sed -n 's/set(PROJECT_VERSION *"\([^"]*\)").*/\1/p' CMakeLists.txt)"
 [[ ${version} ]] || die "No version found"
 
-[[ -f CHANGELOG.md ]] || die "No CHANGELOG.md found"
-version_section="$(awk "/^## Version ${version}/,/^$/{print \$0}" CHANGELOG.md)"
+if [[ -f CHANGELOG.rst ]]; then
+  CHANGELOG=CHANGELOG.rst
+  version_section="$(awk -v v="${version}" '{if (match($0,"^Version")){go=0}; if (match($0,"^Version "+v)){go=1} if(go==1){print $0}}' "${CHANGELOG}")"
+  message="-  ${INPUT_MESSAGE#*: } (#$INPUT_PR_NUMBER)"
+elif [[ -f CHANGELOG.md ]]; then
+  CHANGELOG=CHANGELOG.md
+  version_section="$(awk "/^## Version ${version}/,/^$/{print \$0}" "${CHANGELOG}")"
+  message="* ${INPUT_MESSAGE#*: } (#$INPUT_PR_NUMBER)"
+else
+  die "No supported CHANGELOG found"
+fi
+
 [[ $version_section ]] || die "Could not find section to $version"
 last_line="$(echo "$version_section" | sed -n '$p')"
 [[ $last_line ]] || die "Could not grep last line"
 
 echo "Adding message '$message' after line '${last_line}'"
-sed -i "/$last_line/a ${message}" CHANGELOG.md
+sed -i "/$last_line/a ${message}" "${CHANGELOG}"
 
-git add CHANGELOG.md
+git add "${CHANGELOG}"
 git config --global user.name "Votca Bot"
 git config --global user.email "github@votca.org"
-git commit -m "Update CHANGELOG.md"
+git commit -m "Update ${CHANGELOG}"
 git push
