a1bcee51c3350c45c80f6bacb033c824b8f6c990
Christoph Junghans <junghans@lanl.gov>
2020-07-14 21:06:20 -0600

    forward: debug

--- forward/forward.sh
+++ forward/forward.sh
@@ -16,6 +16,8 @@ push_branch="update_${branch}_submodules"
 
 echo "Working on $module, push $branch to $push_branch of votca/votca"
 
+set -x
+
 git clone https://${INPUT_TOKEN}@github.com/votca/votca
 pushd votca
 git checkout -b "${push_branch}" "origin/${push_branch}" || git checkout -b "${push_branch}" "origin/${branch}"
