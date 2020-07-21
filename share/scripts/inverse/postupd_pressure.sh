c9d207f0a56b9c2d051b3d702fc701ff2539db78
marvinbernhardt <marvinbernhardt@users.noreply.github.com>
2020-07-21 11:01:29 +0200

    max_A for pressure correction adjustable

--- share/scripts/inverse/postupd_pressure.sh
+++ share/scripts/inverse/postupd_pressure.sh
@@ -63,9 +63,10 @@ if [[ ${pscheme[$pscheme_nr]} = 1 ]]; then
      is_num "${particle_dens}" || die "${0##*/}: interaction property 'inverse.particle_dens' should be a number, but found '${particle_dens}'"
      extra_popts=( "${particle_dens}" "${name}.dist.new" )
    fi
+   max_A=$(csg_get_interaction_property inverse.post_update_options.pressure.$ptype.max_A) # use $ptype with {} for manual parsing
    scale=$(csg_get_interaction_property inverse.post_update_options.pressure.$ptype.scale) # use $ptype with {} for manual parsing
    is_num "${scale}" || die "${0##*/}: interaction property 'inverse.post_update_options.pressure.${ptype}.scale' should be a number, but found '${scale}'"
-   do_external pressure_cor $ptype $p_now ${name}.pressure_correction "${kBT}" "$min:$step:$max" "${scale}" "${p_target}" "${extra_popts[@]}" 
+   do_external pressure_cor $ptype $p_now ${name}.pressure_correction "${kBT}" "$min:$step:$max" "${scale}" "${p_target}" "${max_A}" "${extra_popts[@]}" 
    comment="$(get_table_comment ${name}.pressure_correction)"
    tmpfile=$(critical mktemp ${name}.pressure_correction_cut.XXX)
    critical csg_resample --in ${name}.pressure_correction --out ${tmpfile} --grid $min:$step:$max --comment "$comment"
