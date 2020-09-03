031789bac7bd369fb80997201aea07a4297890f8
Bjoern Baumeier <baumeier@votca.org>
2020-09-03 21:29:02 +0200

    testing parallelization

--- src/libxtp/gwbse/rpa.cc
+++ src/libxtp/gwbse/rpa.cc
@@ -65,7 +65,7 @@ Eigen::MatrixXd RPA::calculate_epsilon(double frequency) const {
   const Index n_unocc = _rpamax - lumo + 1;
   const double freq2 = frequency * frequency;
   const double eta2 = _eta * _eta;
-#pragma omp parallel for schedule(dynamic) reduction(+ : result)
+
   for (Index m_level = 0; m_level < n_occ; m_level++) {
     const double qp_energy_m = _energies(m_level);
 
@@ -94,13 +94,16 @@ template Eigen::MatrixXd RPA::calculate_epsilon<false>(double frequency) const;
 Eigen::MatrixXd RPA::calculate_epsilon_r(std::complex<double> frequency) const {
 
   const Index size = _Mmn.auxsize();
-  std::vector<Eigen::MatrixXd> thread_result = std::vector<Eigen::MatrixXd>(
-      OPENMP::getMaxThreads(), Eigen::MatrixXd::Zero(size, size));
+  //std::vector<Eigen::MatrixXd> thread_result = std::vector<Eigen::MatrixXd>(
+  //    OPENMP::getMaxThreads(), Eigen::MatrixXd::Zero(size, size));
+Eigen::MatrixXd result = Eigen::MatrixXd::Identity(size, size);
+
   const Index lumo = _homo + 1;
   const Index n_occ = lumo - _rpamin;
   const Index n_unocc = _rpamax - lumo + 1;
 
-#pragma omp parallel for schedule(guided)
+//#pragma omp parallel for schedule(guided)
+#pragma omp parallel for schedule(dynamic) reduction(+ : result)
   for (Index m_level = 0; m_level < n_occ; m_level++) {
     const double qp_energy_m = _energies(m_level);
     const Eigen::MatrixXd Mmn_RPA =
@@ -117,18 +120,20 @@ Eigen::MatrixXd RPA::calculate_epsilon_r(std::complex<double> frequency) const {
 
     chi = deltaEm * (deltaEm.cwiseAbs2() + sigma_1).cwiseInverse() -
           deltaEp * (deltaEp.cwiseAbs2() + sigma_2).cwiseInverse();
-    Eigen::MatrixXd tempresult =
-        Mmn_RPA.transpose() * chi.asDiagonal() * Mmn_RPA;
+    //Eigen::MatrixXd tempresult =
+    //    Mmn_RPA.transpose() * chi.asDiagonal() * Mmn_RPA;
+    result += Mmn_RPA.transpose() * chi.asDiagonal() * Mmn_RPA;
 
-    thread_result[OPENMP::getThreadId()] += tempresult;
+
+    //thread_result[OPENMP::getThreadId()] += tempresult;
   }
 
-  Eigen::MatrixXd result = Eigen::MatrixXd::Identity(size, size);
+  //Eigen::MatrixXd result = Eigen::MatrixXd::Identity(size, size);
 
-  for (const auto& mat : thread_result) {
-    result -= 2 * mat;
-  }
-  return result;
+  //for (const auto& mat : thread_result) {
+  //  result -= 2 * mat;
+  //}
+  return -2*result;
 }
 
 Eigen::MatrixXcd RPA::calculate_epsilon_complex(
