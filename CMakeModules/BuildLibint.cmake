10ba8748291a5b1855503a8f511d74bcbb6eedeb
Christoph Junghans <junghans@votca.org>
2020-09-28 10:17:05 -0600

    cmake: build internal libint with pic

--- CMakeModules/BuildLibint.cmake
+++ CMakeModules/BuildLibint.cmake
@@ -5,7 +5,7 @@ ExternalProject_Add(Libint_build
   URL_MD5 7241fc0005f37394302083c55a36e0e6
   BUILD_IN_SOURCE 1
   PREFIX libint INSTALL_DIR libint/install
-  CONFIGURE_COMMAND ./autogen.sh COMMAND ./configure --prefix=<INSTALL_DIR> --libdir=<INSTALL_DIR>/${CMAKE_INSTALL_LIBDIR} --disable-shared
+  CONFIGURE_COMMAND ./autogen.sh COMMAND ./configure --prefix=<INSTALL_DIR> --libdir=<INSTALL_DIR>/${CMAKE_INSTALL_LIBDIR} --disable-shared --with-pic
   BUILD_BYPRODUCTS <INSTALL_DIR>/${CMAKE_INSTALL_LIBDIR}/libint2.a
 )
 ExternalProject_get_property(Libint_build INSTALL_DIR)
