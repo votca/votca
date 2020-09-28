efbf779ef2686cafb5afd7d17f63286b9cc5729f
Jens Wehner <jenswehner@gmail.com>
2020-09-28 20:48:30 +0200

    added more configuration

--- CMakeModules/BuildLibint.cmake
+++ CMakeModules/BuildLibint.cmake
@@ -5,7 +5,7 @@ ExternalProject_Add(Libint_build
   URL_MD5 7241fc0005f37394302083c55a36e0e6
   BUILD_IN_SOURCE 1
   PREFIX libint INSTALL_DIR libint/install
-  CONFIGURE_COMMAND ./autogen.sh COMMAND ./configure --prefix=<INSTALL_DIR> --libdir=<INSTALL_DIR>/${CMAKE_INSTALL_LIBDIR} --disable-shared --with-pic
+  CONFIGURE_COMMAND ./autogen.sh COMMAND ./configure --prefix=<INSTALL_DIR> --libdir=<INSTALL_DIR>/${CMAKE_INSTALL_LIBDIR} --disable-shared --with-pic --enable-eri2 --enable-eri3 --with-max-am=6
   BUILD_BYPRODUCTS <INSTALL_DIR>/${CMAKE_INSTALL_LIBDIR}/libint2.a
 )
 ExternalProject_get_property(Libint_build INSTALL_DIR)
