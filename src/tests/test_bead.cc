299fd0c7d38e326671ea1f3e4a967ee7c6321eff
Jens Wehner <jenswehner@gmail.com>
2020-10-10 18:11:26 +0200

    fixed boost warning

--- src/tests/test_bead.cc
+++ src/tests/test_bead.cc
@@ -1,5 +1,5 @@
 /*
- * Copyright 2009-2019 The VOTCA Development Team (http://www.votca.org)
+ * Copyright 2009-2020 The VOTCA Development Team (http://www.votca.org)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -21,7 +21,7 @@
 #include "../../include/votca/csg/bead.h"
 #include "../../include/votca/csg/molecule.h"
 #include "../../include/votca/csg/topology.h"
-#include <boost/test/floating_point_comparison.hpp>
+#include <boost/test/tools/floating_point_comparison.hpp>
 #include <boost/test/unit_test.hpp>
 #include <string>
 
