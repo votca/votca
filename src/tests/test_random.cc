1c3791abbf75d578cfeff486f0622f198145dc22
Jens Wehner <jenswehner@gmail.com>
2020-10-10 18:10:16 +0200

    fixed boost warning

--- src/tests/test_random.cc
+++ src/tests/test_random.cc
@@ -24,7 +24,7 @@
 #include <string>
 
 // Third party includes
-#include <boost/test/floating_point_comparison.hpp>
+#include <boost/test/tools/floating_point_comparison.hpp>
 #include <boost/test/unit_test.hpp>
 
 // Local VOTCA includes
