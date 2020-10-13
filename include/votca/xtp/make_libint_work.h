1236541a513810ac04bfd71575550c3ee2ded2dd
Jens Wehner <jenswehner@gmail.com>
2020-10-13 17:41:12 +0200

    added votca header

--- include/votca/xtp/make_libint_work.h
+++ include/votca/xtp/make_libint_work.h
@@ -1,5 +1,24 @@
-#ifndef LIBINT2_MAX_AM_default
-#define LIBINT2_MAX_AM_default 4
+/*
+ *            Copyright 2009-2020 The VOTCA Development Team
+ *                       (http://www.votca.org)
+ *
+ *      Licensed under the Apache License, Version 2.0 (the "License")
+ *
+ * You may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *              http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#ifndef VOTCA_XTP_MAKE_LIBINT_WORK_H
+#define VOTCA_XTP_MAKE_LIBINT_WORK_H
 #endif
 
 #ifndef LIBINT2_MAX_AM_default1
@@ -8,4 +27,4 @@
 
 #ifndef LIBINT2_MAX_AM_default2
 #define LIBINT2_MAX_AM_default2 LIBINT2_MAX_AM_default
-#endif
+#endif  // VOTCA_XTP_MAKE_LIBINT_WORK_H
