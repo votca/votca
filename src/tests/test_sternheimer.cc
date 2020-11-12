089871c88525954b16abd45c8aad80d29f906d36
rubengerritsen <r.h.j.gerritsen@gmail.com>
2020-11-12 16:45:05 +0100

    added libint to sternheimer test

--- src/tests/test_sternheimer.cc
+++ src/tests/test_sternheimer.cc
@@ -13,6 +13,7 @@
  * limitations under the License.
  *
  */
+#include <libint2/initialize.h>
 #define BOOST_TEST_MAIN
 
 #define BOOST_TEST_MODULE sternheimer_test
@@ -20,9 +21,9 @@
 #include <fstream>
 #include <iostream>
 #include <votca/tools/eigenio_matrixmarket.h>
+#include <votca/xtp/logger.h>
 #include <votca/xtp/orbitals.h>
 #include <votca/xtp/sternheimer.h>
-#include <votca/xtp/logger.h>
 
 using namespace votca::xtp;
 using namespace std;
@@ -30,6 +31,7 @@ using namespace std;
 BOOST_AUTO_TEST_SUITE(sternheimer_test)
 
 BOOST_AUTO_TEST_CASE(sternheimer_polar) {
+  libint2::initialize();
 
   std::cout << "Started Sternheimer polar test" << std::endl;
 
@@ -51,18 +53,19 @@ BOOST_AUTO_TEST_CASE(sternheimer_polar) {
   BasisSet basis;
   basis.Load(std::string(XTP_TEST_DATA_FOLDER) + "/gw/3-21G.xml");
   orbitals.setDFTbasisName(std::string(XTP_TEST_DATA_FOLDER) + "/gw/3-21G.xml");
+  AOBasis aobasis;
+  aobasis.Fill(basis, orbitals.QMAtoms());
 
   BasisSet auxbasis;
   auxbasis.Load(std::string(XTP_TEST_DATA_FOLDER) + "/gw/3-21G.xml");
   orbitals.setAuxbasisName(std::string(XTP_TEST_DATA_FOLDER) + "/gw/3-21G.xml");
 
-  AOBasis aobasis;
-  aobasis.Fill(basis, orbitals.QMAtoms());
   orbitals.setBasisSetSize(17);
   orbitals.setNumberOfOccupiedLevels(4);
   orbitals.MOs().eigenvectors() = mo_eigenvectors;
   orbitals.MOs().eigenvalues() = mo_eigenvalues;
   orbitals.setXCFunctionalName("XC_LDA_X XC_LDA_C_VWN");
+
   orbitals.setNumberOfAlphaElectrons(10);
 
   Logger log;
@@ -77,21 +80,36 @@ BOOST_AUTO_TEST_CASE(sternheimer_polar) {
 
   sternheimer.setUpMatrices();
 
-  std::cout<<"Test Set Up Complete"<<std::endl;
+  std::cout << "Test Set Up Complete" << std::endl;
 
   std::vector<Eigen::Matrix3cd> polar = sternheimer.Polarisability();
 
   Eigen::Vector3d ref1;
-  ref1<<-2.60731,-2.61376,-2.61588;
+  ref1 << -2.60731, -2.61376, -2.61588;
   Eigen::Vector3d ref2;
-  ref2<<-3.33651,-3.33445,-3.33956;
+  ref2 << -3.33651, -3.33445, -3.33956;
 
   bool check_0 = polar[0].diagonal().real().isApprox(ref1, 1e-4);
   bool check_20 = polar[20].diagonal().real().isApprox(ref2, 1e-4);
 
+  if (!check_0) {
+    std::cout << "*** 0 ***" << std::endl;
+    std::cout << "res" << std::endl;
+    std::cout << polar[0].diagonal().real() << std::endl;
+    std::cout << "ref " << std::endl;
+    std::cout << ref1 << std::endl;
+  }
   BOOST_CHECK_EQUAL(check_0, true);
-  BOOST_CHECK_EQUAL(check_20, true);
 
+  if (!check_20) {
+    std::cout << "*** 20 ***" << std::endl;
+    std::cout << "res" << std::endl;
+    std::cout << polar[20].diagonal().real() << std::endl;
+    std::cout << "ref " << std::endl;
+    std::cout << ref2 << std::endl;
+  }
+  BOOST_CHECK_EQUAL(check_20, true);
+  libint2::finalize();
 }
 
 BOOST_AUTO_TEST_CASE(sternheimer_energy_gradient) {
@@ -143,33 +161,31 @@ BOOST_AUTO_TEST_CASE(sternheimer_energy_gradient) {
 
   sternheimer.setUpMatrices();
 
-  std::cout<<"Test Set Up Complete"<<std::endl;
+  std::cout << "Test Set Up Complete" << std::endl;
 
   std::vector<Eigen::Vector3cd> EPC = sternheimer.EnergyGradient();
   std::vector<Eigen::Vector3cd> EPCmo_0 = sternheimer.MOEnergyGradient(0, 0);
   std::vector<Eigen::Vector3cd> EPCmo_4 = sternheimer.MOEnergyGradient(4, 4);
 
-  std::cout<<"EPC[0]"<<EPC[0].real()<<std::endl;
-  std::cout<<"EPC[4]"<<EPC[4].real()<<std::endl;
-  std::cout<<"EPCmo0[0]"<<EPCmo_0[0].real()<<std::endl;
-  std::cout<<"EPCmo0[4]"<<EPCmo_0[4].real()<<std::endl;
-  std::cout<<"EPCmo4[0]"<<EPCmo_4[0].real()<<std::endl;
-  std::cout<<"EPCmo4[4]"<<EPCmo_4[4].real()<<std::endl;
-
+  std::cout << "EPC[0]" << EPC[0].real() << std::endl;
+  std::cout << "EPC[4]" << EPC[4].real() << std::endl;
+  std::cout << "EPCmo0[0]" << EPCmo_0[0].real() << std::endl;
+  std::cout << "EPCmo0[4]" << EPCmo_0[4].real() << std::endl;
+  std::cout << "EPCmo4[0]" << EPCmo_4[0].real() << std::endl;
+  std::cout << "EPCmo4[4]" << EPCmo_4[4].real() << std::endl;
 
   Eigen::Vector3d ref1;
-  ref1<<-4.02724e+54,0.838474,0.233327;
+  ref1 << -4.02724e+54, 0.838474, 0.233327;
   Eigen::Vector3d ref2;
-  ref2<<-0.196696,0.535469,0.43545;
+  ref2 << -0.196696, 0.535469, 0.43545;
   Eigen::Vector3d ref3;
-  ref3<<-1.29072e-14,23.3186,-1.3997;
+  ref3 << -1.29072e-14, 23.3186, -1.3997;
   Eigen::Vector3d ref4;
-  ref4<<0.136145,-4.68696e+36,0.136145;
+  ref4 << 0.136145, -4.68696e+36, 0.136145;
   Eigen::Vector3d ref5;
-  ref5<<7.06401,1.50099,0.307372;
+  ref5 << 7.06401, 1.50099, 0.307372;
   Eigen::Vector3d ref6;
-  ref6<<-2.68194e+123,-5.08698e+114,3.06678e+120;
-
+  ref6 << -2.68194e+123, -5.08698e+114, 3.06678e+120;
 
   bool check_1 = EPC[0].real().isApprox(ref1, 1e-4);
   bool check_2 = EPC[4].real().isApprox(ref2, 1e-4);
@@ -184,8 +200,6 @@ BOOST_AUTO_TEST_CASE(sternheimer_energy_gradient) {
   BOOST_CHECK_EQUAL(check_4, true);
   BOOST_CHECK_EQUAL(check_5, true);
   BOOST_CHECK_EQUAL(check_6, true);
-  
-
 }
 
 BOOST_AUTO_TEST_SUITE_END()
