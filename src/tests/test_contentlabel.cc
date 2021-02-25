d177398680ccd7133efa6bdc7c334d1e38fecebd
JoshuaSBrown <joshbro42867@yahoo.com>
2021-02-25 06:32:56 -0700

    Add tests for content label

--- src/tests/test_contentlabel.cc
+++ src/tests/test_contentlabel.cc
@@ -235,4 +235,168 @@ BOOST_AUTO_TEST_CASE(contentlabel_make_branch) {
 
 }
 
+BOOST_AUTO_TEST_CASE(contentlabel_make_furcation_stem) {
+
+  /*
+   * This test demonstrates what happens when a content label is turned into
+   * a branch 
+   *
+   * Age=32,Height=1.54,Name=Joe;Age=21,Height=1.64,Name=Randy;
+   * 
+   * Should become
+   *
+   * {{Age=32,Height=1.54,Name=Joe}{Age=21,Height=1.64,Name=Randy}}
+   */ 
+  unordered_map<string, boost::any> contents;
+  contents["Name"] = string("Joe");
+  contents["Age"] = int(32);
+  contents["Height"] = double(1.54);
+  ContentLabel label(contents);
+
+  unordered_map<string, boost::any> contents2;
+  contents2["Name"] = string("Randy");
+  contents2["Age"] = int(21);
+  contents2["Height"] = double(1.64);
+
+  ContentLabel label2(contents2);
+
+  label.append(label2);
+
+  // Before making it a branch lets check the different labels
+  string starting_node_str_label = "Age=32,Height=1.54,Name=Joe;";
+  BOOST_CHECK(starting_node_str_label.compare(label.get(LabelType::verbose_start_node)) == 0);
+  string terminating_node_str_label = "Age=21,Height=1.64,Name=Randy;";
+  BOOST_CHECK(terminating_node_str_label.compare(label.get(LabelType::verbose_terminating_node)) == 0);
+  string middle_nodes = "";
+  BOOST_CHECK(middle_nodes.compare(label.get(LabelType::verbose_without_end_nodes)) == 0);
+
+  label.makeBranchLabel();
+  label.makeFurcationStem();
+
+  string full_str_label =
+      "(Age=32,Height=1.54,Name=Joe)(Age=21,Height=1.64,Name=Randy)<";
+
+  size_t num_equals = 6; 
+  BOOST_CHECK(label.getCharLen() == full_str_label.length()-num_equals);
+
+  std::cout << "Length of label " << label.getCharLen() << std::endl;
+  std::cout << "Expected size " << (full_str_label.length()-num_equals) << std::endl;
+  string brief_str_label = "(32,1.54,Joe)(21,1.64,Randy)<";
+  std::cout << "Label after making into a branch" << std::endl;
+  std::cout << label.get() << std::endl;
+  std::cout << full_str_label << std::endl;
+  BOOST_CHECK(full_str_label.compare(label.get()) == 0);
+  BOOST_CHECK(brief_str_label.compare(label.get(LabelType::concise)) == 0);
+  BOOST_CHECK(label.isBranch());
+
+  unordered_map<string, boost::any> contents3;
+  contents3["Name"] = string("Sarah");
+  contents3["Age"] = int(30);
+  contents3["Height"] = double(1.34);
+
+  ContentLabel label3(contents3);
+
+  ContentLabel labelAll(contents);
+  labelAll.append(label2);
+  labelAll.append(label3);
+
+  string full_str_label2 =
+      "(Age=32,Height=1.54,Name=Joe){Age=21,Height=1.64,Name=Randy}(Age=30,Height=1.34,Name=Sarah)<";
+  
+  size_t num_equals2 = 9; 
+  BOOST_CHECK(labelAll.getCharLen() == full_str_label2.length()-num_equals2);
+
+  std::cout << "Length of label " << labelAll.getCharLen() << std::endl;
+  std::cout << "Expected size " << (full_str_label2.length()-num_equals) << std::endl;
+  string brief_str_label2 = "(32,1.54,Joe){21,1.64,Randy}(30,1.34,Sarah)<";
+  std::cout << "Label after making into a branch" << std::endl;
+  std::cout << labelAll.get() << std::endl;
+  std::cout << full_str_label2 << std::endl;
+  BOOST_CHECK(full_str_label2.compare(labelAll.get()) == 0);
+  BOOST_CHECK(brief_str_label2.compare(labelAll.get(LabelType::concise)) == 0);
+  BOOST_CHECK(labelAll.isBranch());
+}
+
+BOOST_AUTO_TEST_CASE(contentlabel_make_furcation_branch) {
+
+  /*
+   * This test demonstrates what happens when a content label is turned into
+   * a branch 
+   *
+   * Age=32,Height=1.54,Name=Joe;Age=21,Height=1.64,Name=Randy;
+   * 
+   * Should become
+   *
+   * {{Age=32,Height=1.54,Name=Joe}{Age=21,Height=1.64,Name=Randy}}
+   */ 
+  unordered_map<string, boost::any> contents;
+  contents["Name"] = string("Joe");
+  contents["Age"] = int(32);
+  contents["Height"] = double(1.54);
+  ContentLabel label(contents);
+
+  unordered_map<string, boost::any> contents2;
+  contents2["Name"] = string("Randy");
+  contents2["Age"] = int(21);
+  contents2["Height"] = double(1.64);
+
+  ContentLabel label2(contents2);
+
+  label.append(label2);
+
+  // Before making it a branch lets check the different labels
+  string starting_node_str_label = "Age=32,Height=1.54,Name=Joe;";
+  BOOST_CHECK(starting_node_str_label.compare(label.get(LabelType::verbose_start_node)) == 0);
+  string terminating_node_str_label = "Age=21,Height=1.64,Name=Randy;";
+  BOOST_CHECK(terminating_node_str_label.compare(label.get(LabelType::verbose_terminating_node)) == 0);
+  string middle_nodes = "";
+  BOOST_CHECK(middle_nodes.compare(label.get(LabelType::verbose_without_end_nodes)) == 0);
+
+  label.makeBranchLabel();
+  label.makeFurcationStem();
+
+  string full_str_label =
+      "[(Age=21,Height=1.64,Name=Randy)]";
+
+  size_t num_equals = 3; 
+  BOOST_CHECK(label.getCharLen() == full_str_label.length()-num_equals);
+
+  std::cout << "Length of label " << label.getCharLen() << std::endl;
+  std::cout << "Expected size " << (full_str_label.length()-num_equals) << std::endl;
+  string brief_str_label = "[(21,1.64,Randy)]";
+  std::cout << "Label after making into a branch" << std::endl;
+  std::cout << label.get() << std::endl;
+  std::cout << full_str_label << std::endl;
+  BOOST_CHECK(full_str_label.compare(label.get()) == 0);
+  BOOST_CHECK(brief_str_label.compare(label.get(LabelType::concise)) == 0);
+  BOOST_CHECK(label.isBranch());
+
+  unordered_map<string, boost::any> contents3;
+  contents3["Name"] = string("Sarah");
+  contents3["Age"] = int(30);
+  contents3["Height"] = double(1.34);
+
+  ContentLabel label3(contents3);
+
+  ContentLabel labelAll(contents);
+  labelAll.append(label2);
+  labelAll.append(label3);
+
+  string full_str_label2 =
+      "[{Age=21,Height=1.64,Name=Randy}(Age=30,Height=1.34,Name=Sarah)]";
+  
+  size_t num_equals2 = 6; 
+  BOOST_CHECK(labelAll.getCharLen() == full_str_label2.length()-num_equals2);
+
+  std::cout << "Length of label " << labelAll.getCharLen() << std::endl;
+  std::cout << "Expected size " << (full_str_label2.length()-num_equals) << std::endl;
+  string brief_str_label2 = "[{21,1.64,Randy}(30,1.34,Sarah)]";
+  std::cout << "Label after making into a branch" << std::endl;
+  std::cout << labelAll.get() << std::endl;
+  std::cout << full_str_label2 << std::endl;
+  BOOST_CHECK(full_str_label2.compare(labelAll.get()) == 0);
+  BOOST_CHECK(brief_str_label2.compare(labelAll.get(LabelType::concise)) == 0);
+  BOOST_CHECK(labelAll.isBranch());
+}
+
 BOOST_AUTO_TEST_SUITE_END()
