b1de33aeb5c8b7971ca342614e7fef317cfb5257
jenswehner <jenswehner@gmail.com>
2021-04-23 10:49:00 +0200

    added a NDim vector class

---
+++ include/votca/tools/NDimVector.h
@@ -0,0 +1,104 @@
+/*
+ *            Copyright 2009-2020 The VOTCA Development Team
+ *                       (http://www.votca.org)
+ *
+ *      Licensed under the Apache License, Version 2.0 (the "License")
+ *
+ * You may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *              http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#ifndef VOTCA_TOOLS_NDIMVECTOR_H
+#define VOTCA_TOOLS_NDIMVECTOR_H
+
+// Standard includes
+#include "types.h"
+#include <array>
+#include <cassert>
+#include <numeric>
+#include <vector>
+
+namespace votca {
+namespace tools {
+
+/**
+ * \brief N-Dim Vector
+ *
+ * This is basically a normal std::vector with an additional
+ * operator(i,j,k,l...), which allows you to treat it as a higher dim object
+ * It is column major (the first index is the fast one)
+ *
+ */
+
+template <class T, int dim>
+class NDimVector {
+
+  static constexpr int dim_ = dim;
+
+ public:
+  constexpr Index rank() { return dimensions_.size(); }
+
+  template <typename... IndexTypes>
+  NDimVector(Index firstDimension, IndexTypes... otherDimensions) {
+    // The number of dimensions used to construct a tensor must be equal to the
+    // rank of the tensor.
+    static_assert((sizeof...(otherDimensions) + 1 == dim_),
+                  "Number of dimensions given does not match rank");
+    dimensions_ = {firstDimension, otherDimensions...};
+
+    Index size = std::accumulate(dimensions_.begin(), dimensions_.end(), 1,
+                                 std::multiplies<Index>());
+    storage_.resize(size);
+    offsets_[0] = 1;
+    std::partial_sum(dimensions_.begin(), dimensions_.end() - 1,
+                     offsets_.begin() + 1, std::multiplies<Index>());
+   
+  }
+
+  template <typename... IndexTypes>
+  T& operator()(Index firstDimension, IndexTypes... otherDimensions) {
+    static_assert((sizeof...(otherDimensions) + 1 == dim_),
+                  "Number of dimensions given does not match rank");
+    std::array<Index, dim_> indices = {firstDimension, otherDimensions...};
+    Index linear_index=std::inner_product(indices.begin(),indices.end(),offsets_.begin(),0);
+    return storage_[linear_index];
+  }
+
+  template <typename... IndexTypes>
+  const T& operator()(Index firstDimension,
+                      IndexTypes... otherDimensions) const {
+    static_assert((sizeof...(otherDimensions) + 1 == dim_),
+                  "Number of dimensions given does not match rank");
+    std::array<Index, dim_> indices = {firstDimension, otherDimensions...};
+    Index linear_index=std::inner_product(indices.begin(),indices.end(),offsets_.begin(),0);
+    return storage_[linear_index];
+  }
+
+  Index dimension(Index i) const { return dimensions_[i]; }
+  Index size() const { return storage_.size(); }
+
+  typename std::vector<T>::iterator begin() { return storage_.begin(); }
+  typename std::vector<T>::iterator end() { return storage_.end(); }
+  typename std::vector<T>::const_iterator begin() const {
+    return storage_.begin();
+  }
+  typename std::vector<T>::const_iterator end() const { return storage_.end(); }
+
+ private:
+  std::vector<T> storage_;
+  std::array<Index, dim> dimensions_;
+  std::array<Index, dim> offsets_;
+};
+}  // namespace tools
+}  // namespace votca
+
+#endif  // VOTCA_TOOLS_NDIMVECTOR_H
---
+++ src/tests/test_NDimVector.cc
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2009-2020 The VOTCA Development Team (http://www.votca.org)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#include <numeric>
+#define BOOST_TEST_MAIN
+
+#define BOOST_TEST_MODULE ndimvector_test
+
+// Standard includes
+#include <iostream>
+
+// Third party includes
+#include <boost/test/unit_test.hpp>
+
+// Local VOTCA includes
+#include "votca/tools/NDimVector.h"
+
+using namespace votca::tools;
+
+BOOST_AUTO_TEST_SUITE(ndimvector_test)
+
+BOOST_AUTO_TEST_CASE(constructor) {
+
+NDimVector<int, 3> a(1,3,4);
+BOOST_CHECK_EQUAL(a.dimension(0), 1);
+BOOST_CHECK_EQUAL(a.dimension(1), 3);
+BOOST_CHECK_EQUAL(a.dimension(2), 4);
+BOOST_CHECK_EQUAL(a.size(), 12);
+
+
+a(0,1,2)=2;
+a(0,1,3)=4;
+int sum=std::accumulate(a.begin(),a.end(),0);
+BOOST_CHECK_EQUAL(sum, 6);
+
+}
+
+BOOST_AUTO_TEST_SUITE_END()
