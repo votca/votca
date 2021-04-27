a012984e490852eeeebb7899de0fdbdf67dc3591
jenswehner <jenswehner@gmail.com>
2021-04-27 19:19:36 +0200

    fixed notebook

--- clean_notebooks.sh
+++ clean_notebooks.sh
@@ -3,8 +3,9 @@
 
 files=("GROMACS/Methane/QMMM_GROMACS.ipynb" "LAMMPS/KMC_Thiophene/LAMMPS_KMC.ipynb" "LAMMPS/Thiophene/QMMM_LAMMPS.ipynb" "tools/dftgwbse_CH4/DFTGWBSE_ENERGY.ipynb" "tools/dftgwbse_CO_geoopt/DFTGWBSE_OPTIMIZATION.ipynb")
 
-for x in ${files}
+for x in ${files[@]}
 do
+    echo ${x}
     jupyter nbconvert --ClearOutputPreprocessor.enabled=True --inplace "${x}"
 done
 
