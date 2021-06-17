ecaaa9b22facc1ea37ca02dca3648662b1b1200e
jenswehner <jenswehner@gmail.com>
2021-06-18 00:42:35 +0200

    reworked commandline access

--- src/libxtp/xtpapplication.cc
+++ src/libxtp/xtpapplication.cc
@@ -21,10 +21,13 @@
 #include <boost/format.hpp>
 
 // VOTCA includes
+#include <stdexcept>
 #include <votca/tools/globals.h>
 #include <votca/tools/propertyiomanipulator.h>
 
 // Local VOTCA includes
+#include "votca/tools/property.h"
+#include "votca/tools/tokenizer.h"
 #include "votca/xtp/openmp_cuda.h"
 #include "votca/xtp/version.h"
 #include "votca/xtp/xtpapplication.h"
@@ -61,6 +64,15 @@ void XtpApplication::Initialize(void) {
       "description,d", propt::value<std::string>(),
       std::string("Short description of a " + CalculatorType() + "s").c_str());
 
+  AddProgramOptions()(
+      "cmdoptions,c", propt::value<std::vector<std::string>>()->multitoken(),
+      "Modify options via command line by e.g. '-c xmltag.subtag=value'. "
+      "Use whitespace to separate multiple options");
+
+  AddProgramOptions()(
+      "printoptions,p", propt::value<std::string>(),
+      std::string("Prints xml options of a " + CalculatorType()).c_str());
+
 #ifdef USE_CUDA
   AddProgramOptions()("gpus,g", propt::value<Index>()->default_value(-1),
                       "  Number of gpus to use");
@@ -84,37 +96,65 @@ bool XtpApplication::EvaluateOptions() {
 #endif
 
   if (OptionsMap().count("description")) {
-    CheckRequired("description", "no " + CalculatorType() + " is given");
-    tools::Tokenizer tok(OptionsMap()["description"].as<std::string>(),
-                         " ,\n\t");
-    for (const std::string& n : tok) {
-      if (CalcExists(n)) {
-        PrintDescription(std::cout, n, "xtp/xml", Application::HelpLong);
-      } else {
-        std::cout << CalculatorType() << " " << n << " does not exist\n";
-      }
+    std::string calcname = OptionsMap()["description"].as<std::string>();
+    if (CalcExists(calcname)) {
+      PrintDescription(std::cout, calcname, "xtp/xml", Application::HelpLong);
+    } else {
+      std::cout << CalculatorType() << " " << calcname << " does not exist\n";
     }
+
     StopExecution();
     return true;
   }
-  CheckRequired("execute", "Nothing to do here: Abort.");
+  if (OptionsMap().count("printoptions")) {
+    std::string calcname = OptionsMap()["printoptions"].as<std::string>();
+    if (CalcExists(calcname)) {
+      tools::Property opt;
+      std::cout << "XML options for " << calcname << std::endl;
+      opt.LoadFromXML(tools::GetVotcaShare() + "/xtp/xml/" + calcname + ".xml");
+      std::cout << opt.get("options") << std::endl;
+    } else {
+      std::cout << CalculatorType() << " " << calcname << " does not exist\n";
+    }
 
-  tools::Tokenizer calcs(OptionsMap()["execute"].as<std::string>(), " ,\n\t");
-  std::vector<std::string> calc_string = calcs.ToVector();
-  if (calc_string.size() != 1) {
-    throw std::runtime_error("You can only run one " + CalculatorType() +
-                             " at the same time.");
+    StopExecution();
+    return true;
   }
 
-  if (CalcExists(calc_string[0])) {
-    CreateCalculator(calc_string[0]);
+  CheckRequired("execute", "Nothing to do here: Abort.");
+  std::string calcname = OptionsMap()["execute"].as<std::string>();
+
+  if (CalcExists(calcname)) {
+    CreateCalculator(calcname);
   } else {
-    std::cout << CalculatorType() << " " << calc_string[0]
-              << " does not exist\n";
+    std::cout << CalculatorType() << " " << calcname << " does not exist\n";
     StopExecution();
   }
 
   EvaluateSpecificOptions();
+
+  if (OptionsMap().count("options")) {
+    std::string optionsFile = OptionsMap()["options"].as<std::string>();
+    options_.LoadFromXML(optionsFile);
+  } else {
+    // Empty user options
+    tools::Property& opts = options_.add("options", "");
+    opts.add(calcname, "");
+  }
+
+  if (OptionsMap().count("cmdoptions")) {
+    for (const std::string& opt :
+         OptionsMap()["cmdoptions"].as<std::vector<std::string>>()) {
+      std::vector<std::string> entries = tools::Tokenizer(opt, "=").ToVector();
+      if (entries.size() != 2) {
+        throw std::runtime_error(opt + " is not well formated!");
+      } else {
+        options_.getOradd("options." + calcname + "." + entries[0]).value() =
+            entries[1];
+      }
+    }
+    std::cout << options_ << std::endl;
+  }
 
   return true;
 }
