2620aaaad5fff5a3e2400a1dd5df0c5cd407ae60
rubengerritsen <r.h.j.gerritsen@gmail.com>
2021-07-13 11:21:25 +0200

    possible fix for functional

--- src/libxtp/qmpackages/orca.cc
+++ src/libxtp/qmpackages/orca.cc
@@ -506,6 +506,8 @@ bool Orca::ParseLogFile(Orbitals& orbitals) {
   bool found_success = false;
   orbitals.setQMpackage(getPackageName());
   orbitals.setDFTbasisName(basisset_name_);
+  orbitals.setXCFunctionalName(
+      options_.get("functional").as<std::vector<std::string>>()[0]);
   if (options_.exists("ecp")) {
     orbitals.setECPName(options_.get("ecp").as<std::string>());
   }
