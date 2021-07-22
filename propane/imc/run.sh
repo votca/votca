39176cd11ecd5825f3ea54b6357fba854f02d6f5
marvinbernhardt <marvinbernhardt@users.noreply.github.com>
2021-07-22 13:27:37 +0200

    redo propane/imc

--- propane/imc/run.sh
+++ propane/imc/run.sh
@@ -1,5 +1,12 @@
-#!/bin/bash -e
+#!/bin/bash
 
+set -e
+
+ln -sf grompp-ibi.mdp grompp.mdp
+echo 'running csg_inverse --options "settings_pre.xml"'
+csg_inverse --options settings-preibi.xml
+rm done
+ln -sf grompp-imc.mdp grompp.mdp
 echo 'running csg_inverse --options "settings.xml"'
 csg_inverse --options settings.xml
-
+rm -f grompp.mdp
