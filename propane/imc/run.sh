9590b438271ae5199f9bf7fa192eaf1cbd254c61
marvinbernhardt <marvinbernhardt@users.noreply.github.com>
2021-07-22 13:53:16 +0200

    fix quote

--- propane/imc/run.sh
+++ propane/imc/run.sh
@@ -3,12 +3,12 @@
 set -e
 
 # pre-iterations with IBI
-cmd='csg_inverse --options "settings-ibi.xml"'
+cmd='csg_inverse --options settings-ibi.xml'
 echo "now running: ${cmd}"
 $cmd
 rm done
 
 # IMC iterations
-cmd='csg_inverse --options "settings-imc.xml"'
+cmd='csg_inverse --options settings-imc.xml'
 echo "now running: ${cmd}"
 $cmd
