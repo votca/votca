0cc6ff0564ea7da7287afe40660ebc8ea76ae8da
marvinbernhardt <marvinbernhardt@users.noreply.github.com>
2021-07-23 15:34:23 +0200

    fix constant extrapolation in both directions for ibi update

--- share/scripts/inverse/update_ibi_pot.pl
+++ share/scripts/inverse/update_ibi_pot.pl
@@ -56,17 +56,43 @@ my @pot_flags_cur;
 (readin_table($cur_pot_file,@pot_r_cur,@pot_cur,@pot_flags_cur)) || die "$progname: error at readin_table\n";
 
 #should never happen due to resample, but better check
-die "Different grids \n" if (($r_aim[1]-$r_aim[0]-$r_cur[1]+$r_cur[0])>0.0001);
+die "Different grids \n" if (($r_aim[1]-$r_aim[0]-$r_cur[1]+$r_cur[0]) > 0.0001);
 die "Different start potential point \n" if (($r_aim[0]-$r_cur[0]) > 0.0001);
 die "Different end potential point \n" if ( $#r_aim != $#r_cur );
 
 my $outfile="$ARGV[3]";
 my @dpot;
 my @flag;
-my $value=0.0;
 
-#start from the end to make the begining have the last value
-for (my $i=$#r_aim;$i>=0;$i--){
+sub maxindex {
+  my( $aref, $idx_max ) = ( shift, 0 );
+  $aref->[$idx_max] > $aref->[$_] or $idx_max = $_ for 1 .. $#{$aref};
+  return $idx_max;
+}
+my $ndx_max_rdf_cur = maxindex(\@rdf_cur);
+
+#start from the middle and make the outside regions have the last valid value
+# go to end
+my $value=0.0;
+for (my $i=$ndx_max_rdf_cur;$i<=$#r_aim;$i++){
+  if (($rdf_aim[$i] > 1e-10) && ($rdf_cur[$i] > 1e-10)) {
+    $dpot[$i]=log($rdf_cur[$i]/$rdf_aim[$i])*$pref;
+    $flag[$i]="i";
+  } else {
+    $dpot[$i]=$value;
+    $flag[$i]="o";
+  }
+  if($pot_flags_cur[$i] =~ /[u]/) {
+    $dpot[$i]=$value;
+    $flag[$i]="o";
+  }
+  else {
+    $value=$dpot[$i];
+  }
+}
+# go to beginning
+$value=0.0;
+for (my $i=$ndx_max_rdf_cur-1;$i>=0;$i--){
   if (($rdf_aim[$i] > 1e-10) && ($rdf_cur[$i] > 1e-10)) {
     $dpot[$i]=log($rdf_cur[$i]/$rdf_aim[$i])*$pref;
     $flag[$i]="i";
