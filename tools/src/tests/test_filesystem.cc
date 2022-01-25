078c6814797749cd1b92a2b19f0302265ccf6463
Christoph Junghans <junghans@votca.org>
2022-01-24 17:28:24 -0700

    test_filesystem.cc: fix errors on FreeBSD

--- tools/src/tests/test_filesystem.cc
+++ tools/src/tests/test_filesystem.cc
@@ -34,38 +34,41 @@ BOOST_AUTO_TEST_SUITE(filesystem_test)
 BOOST_AUTO_TEST_CASE(FileExists) {
 
   std::string filename = "blablablablablabla.txt";
-  BOOST_CHECK_EQUAL(filesystem::FileExists(filename), false);
+  BOOST_CHECK_EQUAL(votca::tools::filesystem::FileExists(filename), false);
   std::string filename2 = "test_exist.txt";
   std::ofstream outfile(filename2);
 
   outfile << "my text here!" << std::endl;
 
   outfile.close();
-  BOOST_CHECK_EQUAL(filesystem::FileExists(filename2), true);
+  BOOST_CHECK_EQUAL(votca::tools::filesystem::FileExists(filename2), true);
 }
 
 BOOST_AUTO_TEST_CASE(GetFileExtension) {
 
   std::string filename = "bla.txt";
-  BOOST_CHECK_EQUAL(filesystem::GetFileExtension(filename), "txt");
+  BOOST_CHECK_EQUAL(votca::tools::filesystem::GetFileExtension(filename),
+                    "txt");
 
   std::string filename2 = "blubb";
-  BOOST_CHECK_EQUAL(filesystem::GetFileExtension(filename2), "");
+  BOOST_CHECK_EQUAL(votca::tools::filesystem::GetFileExtension(filename2), "");
 
   std::string filename3 = "a.b.c.d.gro";
-  BOOST_CHECK_EQUAL(filesystem::GetFileExtension(filename3), "gro");
+  BOOST_CHECK_EQUAL(votca::tools::filesystem::GetFileExtension(filename3),
+                    "gro");
 }
 
 BOOST_AUTO_TEST_CASE(GetFileBase) {
 
   std::string filename = "bla.txt";
-  BOOST_CHECK_EQUAL(filesystem::GetFileBase(filename), "bla");
+  BOOST_CHECK_EQUAL(votca::tools::filesystem::GetFileBase(filename), "bla");
 
   std::string filename2 = "blubb";
-  BOOST_CHECK_EQUAL(filesystem::GetFileBase(filename2), "blubb");
+  BOOST_CHECK_EQUAL(votca::tools::filesystem::GetFileBase(filename2), "blubb");
 
   std::string filename3 = "a.b.c.d.gro";
-  BOOST_CHECK_EQUAL(filesystem::GetFileBase(filename3), "a.b.c.d");
+  BOOST_CHECK_EQUAL(votca::tools::filesystem::GetFileBase(filename3),
+                    "a.b.c.d");
 }
 
 BOOST_AUTO_TEST_SUITE_END()
