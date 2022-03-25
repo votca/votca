better IMC Jacobian improvement, fix range in Gauss-Newton method

csg/share/scripts/inverse/gauss_newton.py
@@ -594,7 +594,10 @@ def gauss_newton_update(input_arrays, settings, verbose=False):
     cut_pot_p1, tail_pot_p1 = calc_slices(
         r, settings["cut_off_pot"], verbose=False, offset=0
     )
-    cut_res, tail_res = calc_slices(r, settings["cut_off_res"], verbose=False)
+    # residuum cut also -= 1 for consistency when cut-residuum = cut-off
+    cut_res, tail_res = calc_slices(
+        r, settings["cut_off_res"], verbose=False, offset=-1
+    )
     n_c_pot = len(r[cut_pot])
     n_c_res = len(r[cut_res])
 
@@ -606,7 +609,7 @@ def gauss_newton_update(input_arrays, settings, verbose=False):
         r, input_arrays["g_cur"], settings["non-bonded-dict"]
     )
     # get Jacobian
-    jac_mat = settings["jacobians"][0]
+    jac_mat = settings["jacobians"][0][cut_res, cut_res, :, :]
     # weighting
     g_tgt_vec = vectorize(g_tgt_mat)
     weights = gen_residual_weights(
@@ -700,11 +703,9 @@ def gauss_newton_update(input_arrays, settings, verbose=False):
             dpdf = -2 / 3 * np.pi * rho_i * g_tgt_avg * r3_dr * extra_factor
             C[c, :] = dpdf @ dfdu_all
             if settings["flatten_at_cut_off"]:
-                print(C[c, n_c_pot - 3 :])
                 C[
                     c, n_c_pot - 1 :: n_c_pot
                 ] = 0  # no constraint for last point of each Δu
-                print(C[c, n_c_pot - 3 :])
             d[c] = p - p_tgt
 
         elif constraint["type"] == "kirkwood-buff-integral":
@@ -783,6 +784,8 @@ def gauss_newton_update(input_arrays, settings, verbose=False):
                 "not implemented constraint type: " + constraint["type"]
             )
     # solve least squares problem with linear constraints
+    # if C.shape[0] == 0 and A.shape[0] == A.shape[1]:
+    # dx_flat = np.linalg.solve(A, b)
     dx_flat = solve_lsq_with_linear_constraints(A, C, b, d)
     # obtain potential update
     Delta_u_flat = -A0_2D @ dx_flat
@@ -805,8 +808,8 @@ def gauss_newton_update(input_arrays, settings, verbose=False):
         Delta_u_flag[tail_pot_p1] = "o"
         # shift potential to make value before cut-off zero
         if settings["flatten_at_cut_off"]:
-            # hmmmmmm
-            Delta_u[cut_pot] -= Delta_u[cut_pot][-1]  # this one invalidates constraints
+            # hmmmmmm will probably throw this out
+            Delta_u[cut_pot] -= Delta_u[cut_pot][-1]
             # Delta_u[cut_pot][-1] = 0  # this one does not really make it flat
         # add value at r=0 if it was removed
         if settings["r0-removed"]:

csg/share/scripts/inverse/imc_matrix_to_jacobian.py
@@ -140,25 +140,16 @@ def get_args(local_args=None):
         help="extension of current RDF files",
     )
     parser.add_argument(
-        "--g-cur-raw-ext",
-        type=str,
-        default=None,
-        metavar="RDF_CUR_RAW_EXT",
-        help="extension of current raw RDF files",
-    )
-    parser.add_argument(
-        "--g-tgt-raw-ext",
-        type=str,
-        default=None,
-        metavar="RDF_CUR_RAW_EXT",
-        help="extension of current raw RDF files",
-    )
-    parser.add_argument(
         "--improve-jacobian-onset",
         help="Change Jacobian slightly for better results in the RDF onset region",
         action="store_const",
         const=True,
         default=False,
+    )
+    parser.add_argument(
+        "--onset-threshold",
+        type=float,
+        help="minimum value of g_tgt or g_cur up to which the IMC jacobian is improved",
     )
     parser.add_argument(
         "--cut-res",
@@ -204,24 +195,6 @@ def process_input(args):
             "assume-zero": [],
         },
     }
-    if args.g_cur_raw_ext is not None:
-        table_infos = {
-            **table_infos,
-            "g_cur_raw": {
-                "extension": args.g_cur_raw_ext,
-                "check-grid": True,
-                "assume-zero": [],
-            },
-        }
-    if args.g_tgt_raw_ext is not None:
-        table_infos = {
-            **table_infos,
-            "g_tgt_raw": {
-                "extension": args.g_tgt_raw_ext,
-                "check-grid": True,
-                "assume-zero": [],
-            },
-        }
     state_names_temp = ["."]  # read from current dir, dict will be collapsed later
     # read in all tables based on table_infos
     input_arrays, r0_removed, r = read_all_tables(
@@ -245,6 +218,7 @@ def process_input(args):
         "verbose",
         "improve_jacobian_onset",
         "cut_res",
+        "onset_threshold",
     )
     settings = {key: vars(args)[key] for key in args_to_copy}  # all mandatory
     settings["kBT"] = float(options.find("./inverse/kBT").text)
@@ -252,6 +226,11 @@ def process_input(args):
     settings["rhos"] = rhos
     settings["n_intra"] = n_intra
     settings["r0-removed"] = r0_removed
+
+    if settings["improve_jacobian_onset"] and settings["onset_threshold"] is None:
+        raise Exception(
+            "If --improve-jacobian-onset is used, --onset-threshold has to be provided"
+        )
 
     # load IMC matrix and index
     try:
@@ -364,28 +343,29 @@ def convert_and_save_imc_matrix(
 
 
 @if_verbose_dump_io
-def improve_jacobian_onset(J, input_arrays, settings, verbose=False):
+def improve_jacobian_onset(jac_mat, input_arrays, settings, verbose=False):
     """Change Jacobian diagonal such that update becomes more stable.
     In the onset region the diagonal is approx. -k_B T g(r).
     This functions subtracts -k_B T g(r) (target or current) and adds
     -k_B T (g_cur + g_tgt)/2.
 
     Args:
-        dudg: Jacobian to be improved
+        jac_mat: Jacobian to be improved (4D)
         input_arrays: nested dict holding the distributions
         settings: dict holding relevant settings
         verbose: save parameters and return of this and contained functions as numpy
                  file
 
     Returns:
-        The improved Jacobian
+        The improved Jacobian (4D)
     """
     r = input_arrays["r"]
-    n_r = len(r)
+    # n_r = len(r)
     # number of atom types
     n_t = len(settings["rhos"])
     # number of interactions including redundand ones
     n_i = triangular_number(n_t)
+
     # generate matrices and vectorize
     g_tgt_mat = gen_interaction_matrix(
         r, input_arrays["g_tgt"], settings["non-bonded-dict"]
@@ -395,36 +375,41 @@ def improve_jacobian_onset(J, input_arrays, settings, verbose=False):
         r, input_arrays["g_cur"], settings["non-bonded-dict"]
     )
     g_cur_vec = vectorize(g_cur_mat)
-    # when dist_improve was used, subtract raw
-    if "g_cur_raw" in input_arrays.keys():
-        vec_to_subract = vectorize(
-            gen_interaction_matrix(
-                r, input_arrays["g_cur_raw"], settings["non-bonded-dict"]
-            )
-        ).T.flatten()
-    elif "g_tgt_raw" in input_arrays.keys():
-        vec_to_subract = vectorize(
-            gen_interaction_matrix(
-                r, input_arrays["g_tgt_raw"], settings["non-bonded-dict"]
-            )
-        ).T.flatten()
-    else:
-        vec_to_subract = g_cur_vec.T.flatten()
-    J_improved_2D = make_matrix_2D(J.copy())
-    J_diag = np.diag(J_improved_2D)
-    # subtract -g_cur then add average of (-g_cur, -g_tgt) for better stability
-    # TODO: This would need another version, when use_target_matrix is used
-    J_diag_improved = J_diag + 1 / settings["kBT"] * (
-        # +vec_to_subract - (g_cur_vec.T.flatten() + g_tgt_vec.T.flatten()) / 2
-        +vec_to_subract
-        - g_cur_vec.T.flatten()
-    )
-    # improve stability
-    J_diag_improved[J_diag_improved == 0] = -1e-37
+
+    # copy jacobian
+    jac_mat_improved = jac_mat.copy()
+
+    # improve jacobian
+    # loop over interactions
+    for i in range(n_i):
+        # onset region is up to where g_cur or g_tgt are larger then onset threshold
+        onset_end = max(
+            (g_cur_vec[:, i] > settings["onset_threshold"]).nonzero()[0][0],
+            (g_tgt_vec[:, i] > settings["onset_threshold"]).nonzero()[0][0],
+        )
+        onset_region = slice(0, onset_end)
+
+        # make all elements in the onset region zero
+        jac_mat_improved[:, onset_region, :, i] = 0
+        jac_mat_improved[onset_region, :, i, :] = 0
+
+        # improve diagonal in onset retgion
+        onset_diagonal = np.zeros(len(r[onset_region]))
+        onset_diagonal = (
+            -1
+            / settings["kBT"]
+            * ((g_cur_vec[onset_region, i] + g_tgt_vec[onset_region, i]) / 2)
+        )
+
+        # improve stability by converting zeros to small float
+        onset_diagonal[onset_diagonal == 0.0] = -1e-37
+
+        # change diagonal
+        np.fill_diagonal(
+            jac_mat_improved[onset_region, onset_region, i, i], onset_diagonal
+        )
     # new diagonal
-    np.fill_diagonal(J_improved_2D, J_diag_improved)
-    J_improved = make_matrix_4D(J_improved_2D, n_r, n_r, n_i, n_i)
-    return J_improved
+    return jac_mat_improved
 
 
 if __name__ == "__main__":