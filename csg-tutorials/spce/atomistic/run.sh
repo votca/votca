cea15484f4ee316d9dece601f31090d7aa791aaf
marvinbernhardt <marvinbernhardt@users.noreply.github.com>
2022-04-18 15:58:48 +0200

    unfinished changes to iie csg-tutorials

--- csg-tutorials/spce/atomistic/run.sh
+++ csg-tutorials/spce/atomistic/run.sh
@@ -17,5 +17,8 @@ nt="$(grep -c processor /proc/cpuinfo 2>/dev/null)" || nt=0
 echo Calculating distributions
 csg_stat --top topol.tpr --trj traj.trr --cg water.xml --options settings.xml --nt $nt --begin $equi
 
+# echo Calculating target IMC matrix
+# csg_stat --top topol.tpr --trj traj.trr --cg water.xml --options settings-imc.xml --nt $nt --begin $equi --do-imc
+
 echo "Mapping confout.gro to get the starting configuration for coarse-grained runs of ibi/imc/re"
-csg_map --top topol.tpr --trj confout.gro --cg water.xml --out conf_cg.gro 
+csg_map --top topol.tpr --trj confout.gro --cg water.xml --out conf_cg.gro
