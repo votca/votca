6e4f5c7bc4aeabe2997dedc750f36613bdf83406
Vivek Sundaram <sundaramvivek10@gmail.com>
2023-01-10 17:21:18 +0000

    Added external sites to embedding calculations

--- xtp/src/libxtp/qmpackages/xtpdft.cc
+++ xtp/src/libxtp/qmpackages/xtpdft.cc
@@ -71,6 +71,11 @@ bool XTPDFT::RunActiveDFT() {
   DFTEngine xtpdft;
   xtpdft.Initialize(options_);
   xtpdft.setLogger(pLog_);
+
+  if (!externalsites_.empty()) {
+    xtpdft.setExternalcharges(&externalsites_);
+  }
+  
   bool success = xtpdft.EvaluateActiveRegion(orbitals_) &&
                  xtpdft.EvaluateTruncatedActiveRegion(orbitals_);
   std::string file_name = run_dir_ + "/" + log_file_name_;
