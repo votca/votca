0dfaadb129d6f3b20a0af6b8a6e89d481f2783a1
Vivek Sundaram <v.sundaram@tue.nl>
2023-01-19 13:53:35 +0100

    Try reading static region in xtpdft

--- xtp/src/libxtp/qmregion.cc
+++ xtp/src/libxtp/qmregion.cc
@@ -142,9 +142,10 @@ void QMRegion::Evaluate(std::vector<std::unique_ptr<Region> >& regions) {
 
     // this only works with XTPDFT, so locally override global qmpackage_
     std::unique_ptr<QMPackage> xtpdft = std::unique_ptr<QMPackage>(
-      QMPackageFactory::QMPackages().Create("xtpdft"));
+      QMPackageFactory::QMPackages().Create("xtp"));
   xtpdft->setLog(&log_);
   xtpdft->Initialize(dftoptions_);
+  xtpdft->setRunDir(workdir_);
   Index charge = 0;
   if (initstate_.Type() == QMStateType::Electron) {
     charge = -1;
@@ -153,7 +154,41 @@ void QMRegion::Evaluate(std::vector<std::unique_ptr<Region> >& regions) {
   }
   xtpdft->setCharge(charge);;
 
+std::vector<double> energies = std::vector<double>(regions.size(), 0.0);
+  for (std::unique_ptr<Region>& reg : regions) {
+    Index id = reg->getId();
+    if (id == this->getId()) {
+      continue;
+    }
 
+    StaticRegion Staticdummy(0, log_);
+    PolarRegion Polardummy(0, log_);
+    XTP_LOG(Log::error, log_)
+        << TimeStamp() << " Evaluating interaction between " << this->identify()
+        << " " << this->getId() << " and " << reg->identify() << " "
+        << reg->getId() << std::flush;
+   if (reg->identify() == Staticdummy.identify()) {
+      StaticRegion* staticregion = dynamic_cast<StaticRegion*>(reg.get());
+      xtpdft->AddRegion(*staticregion);
+    } else if (reg->identify() == Polardummy.identify()) {
+      PolarRegion* polarregion = dynamic_cast<PolarRegion*>(reg.get());
+      xtpdft->AddRegion(*polarregion);
+    } else {
+      throw std::runtime_error(
+          "Interaction of regions with types:" + this->identify() + " and " +
+          reg->identify() + " not implemented");
+    }
+  }
+
+
+
+  double e_ext =
+      std::accumulate(interact_energies.begin(), interact_energies.end(), 0.0);
+  XTP_LOG(Log::info, log_)
+      << TimeStamp()
+      << " Calculated interaction potentials with other regions. E[hrt]= "
+      << e_ext << std::flush;
+  XTP_LOG(Log::info, log_) << "Writing inputs" << std::flush;
     xtpdft->WriteInputFile(orb_);
     bool active_run = xtpdft->RunActiveRegion();
     if (!active_run) {
