5f3729ee8d53186a034b692763692b68debb59e8
Christoph Junghans <junghans@votca.org>
2024-05-30 15:30:00 -0600

    fix codacy warnings

--- papers/make_paper.sh
+++ papers/make_paper.sh
@@ -1,1 +1,2 @@
-docker run --rm  --volume $PWD/v2024:/data  --user $(id -u):$(id -g)  --env JOURNAL=joss  openjournals/paperdraft
+#! /bin/bash
+docker run --rm  --volume "$PWD/v2024:/data"  --user "${UID}:${GROUPS[0]}"  --env JOURNAL=joss  openjournals/paperdraft
