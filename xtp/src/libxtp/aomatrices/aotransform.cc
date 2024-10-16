12d9d9bd1a9c08b9e900493d610bc41c4bd7baba
Christoph Junghans <junghans@votca.org>
2024-10-16 15:54:06 -0600

    aotransform.cc: fix a warning

--- xtp/src/libxtp/aomatrices/aotransform.cc
+++ xtp/src/libxtp/aomatrices/aotransform.cc
@@ -24,11 +24,14 @@
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wdeprecated-declarations"
 #elif defined(__GNUC__)
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Warray-bounds"
 #endif
 #include "libint2/solidharmonics.h"
 #if defined(__clang__)
 #pragma clang diagnostic pop
 #elif defined(__GNUC__)
+#pragma GCC diagnostic pop
 #endif
 
 namespace votca {
